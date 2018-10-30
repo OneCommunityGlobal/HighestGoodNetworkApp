adbc0b7c5157e84ecf1a1bc22cd4359635071cdc
Shubhra Mittal <shubhra.goel@gmail.com>
2018-10-30 15:13:34 -0700

    react

--- src/services/logService.js
+++ src/services/logService.js
@@ -2,7 +2,7 @@ import raven from 'raven-js'
 
 function init()
 {
-    raven.config('https://b3e356e0be00453ead1b196b9d9c392f@sentry.io/1295531').install()
+    raven.config('https://c075e03ab362427cb31e979c6c43d5c9@sentry.io/1311459').install()
 }
 
 function log(error)
