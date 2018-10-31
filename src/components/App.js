24b638902702efb96c02bac3126aa90a86c82f3a
Shubhra Mittal <shubhra.goel@gmail.com>
2018-10-30 21:41:49 -0700

    login

--- src/components/App.js
+++ src/components/App.js
@@ -24,7 +24,7 @@ class App extends Component {
       <Header user = {this.state.user} />
        <Switch>
       <PrivateRoute path ="/dashboard" component={Dashboard} />
-      <Route path="/Login" component={Login} />
+      <Route path="/login" component={Login} />
      {/* <Route path="/forcepassword" component={Forcepassword} />
       <Route path="/forgotpassword" component={Forgotpassword} />
       <PrivateRoute authed={isUserAuthenticated()} path="/Timelog" component={TimelogPage} />
