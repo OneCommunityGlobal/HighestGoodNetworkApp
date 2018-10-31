02cc87cb173e4ab7ef8d79c6bde09433ed9d4e4e
Shubhra Mittal <shubhra.goel@gmail.com>
2018-10-30 21:12:27 -0700

    testing

--- src/services/dashBoardService.js
+++ src/services/dashBoardService.js
@@ -1,12 +1,12 @@
 import httpService from './httpervice'
 
 const ApiEndpoint = `${process.env.REACT_APP_APIENDPOINT}/dashboard`;
-
+console.log(ApiEndpoint)  
+console.log(process.env) 
 
 export function getLeaderboardData(userId)
 {
-console.log(ApiEndpoint)  
-console.log(process.env)  
+ 
 return httpService.get(`${ApiEndpoint}/leaderboard/${userId}`)
 
 }
