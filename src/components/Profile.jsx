a5a6f236251da95f762857e47885770fe580fed6
Shubhra Mittal <shubhra.goel@gmail.com>
2018-10-31 10:12:18 -0700

    working on userprofile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import {getUserProfile} from '../services/profileService'
 import logger from '../services/logService'
+import {getCurrentUser} from '../services/loginService'
 
 class Profile extends Component {
     state = { 
         userId : {},
+        requestorId : {},
         userProfile : {}
      }
     async componentDidMount() {
         
        try{
+        let {userid:requestorId} = {...getCurrentUser()}        
         let userId = this.props.match.params.userId;
-        let data = await getUserProfile(userId)
-        console.log(data)
-
+        let {data:userProfile} = {...await getUserProfile(userId)}
+               
+        this.setState({userId, userProfile, requestorId})
+        console.log(this.state);
         }
         catch(error)
         {
