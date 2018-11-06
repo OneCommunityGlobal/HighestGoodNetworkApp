c597ee8878792675e52a06574abf53f33d05617d
Shubhra Mittal <shubhra.goel@gmail.com>
2018-11-06 09:18:00 -0800

    dashboard class

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Leaderboard from './Leaderboard'
+import '../App.css';
 
 const Dashboard = () => {
     return (
         <React.Fragment>
-                <div className="m-5 Dashboard">
+                <div className="m-5 dashboard">
                 <div className="col-4">
                 <Leaderboard />
                 </div>
