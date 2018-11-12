1daa3a9ac8f1843fc9fa1c4287e1804faffbc40c
AishwaryaSingh <1994aishwaryasingh@gmail.com>
2018-11-12 09:53:18 -0800

    resoleved dashboard (1)

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
-<<<<<<< HEAD
 import Leaderboard from '../components/Leaderboard'
 //import MonthyEffort from '../MonthyEffort'
 //import Badges from '../Badges'
 import { Card, Row, CardTitle, CardText, Col } from 'reactstrap';
 import MonthlyEffort from './MonthlyEffort';
 
-=======
-import Leaderboard from './Leaderboard'
+//import Leaderboard from '/Leaderboard'
 import '../App.css';
->>>>>>> 6fe1485002f03d1271d8b36e05b74bf05fbdc622
 
 const Dashboard = () => {
     return (
         <React.Fragment>
-<<<<<<< HEAD
         <div>
             <Row>
             <Col sm={{offset:1,size:7}}>
@@ -44,19 +40,6 @@ const Dashboard = () => {
               
         </React.Fragment>
     );
-=======
-                <div className="m-5 dashboard">
-                <div className="col-4">
-                <Leaderboard />
-                </div>
-                
-                {/* <Badges/>
-                <MonthlyEffort/>                 */}
-                </div>
-
-            </React.Fragment>
-      );
->>>>>>> 6fe1485002f03d1271d8b36e05b74bf05fbdc622
 }
  
 export default Dashboard;
