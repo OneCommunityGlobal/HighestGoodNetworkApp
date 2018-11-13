2b53fd4875767034ddf698881c7b5b075173e27e
shubmittal <shubhra.goel@gmail.com>
2018-11-13 11:30:25 -0800

    Update App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../components/App.js';
+import App from '../components/App';
 import {shallow} from 'enzyme'
 import Dashboard from '../components/Dashboard'
 
