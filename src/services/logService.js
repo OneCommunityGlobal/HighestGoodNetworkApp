7cde12b2e947d6b00ecee4f73bcb56160396c06a
Shubhra Mittal <shubhra.goel@gmail.com>
2018-11-15 10:35:35 -0800

    dsn

--- src/services/logService.js
+++ src/services/logService.js
@@ -4,7 +4,7 @@ function init() {
     let value = process.env.REACT_APP_SENTRY_URL;
     alert(`dsn is ${value}`);
     Sentry.init({
-        dsn: "https://c075e03ab362427cb31e979c6c43d5c9@sentry.io/1311459"
+        dsn: process.env.REACT_APP_SENTRY_URL
          });
 }
 
