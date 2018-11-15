133d764bd4c0687118a6f216f45d6306fc62f121
Shubhra Mittal <shubhra.goel@gmail.com>
2018-11-15 09:57:46 -0800

    fixed user unable to move to dashboard

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import Joi from "joi";
 import Form from "./common/form";
 import { login, getCurrentUser } from "../services/loginService";
-import {Redirect} from 'react-router-dom'
-
+import { Redirect } from "react-router-dom";
 
 class Login extends Form {
   state = {
@@ -25,42 +24,37 @@ class Login extends Form {
     const email = this.state.data.email;
     const password = this.state.data.password;
     try {
-    let result = await login({ email, password });  
-   if(result.userType && result.userType === "newUser")
-   {
-    window.location =  `/forcePasswordUpdate/${result.userId}`;
-    return;
-   }
-    const {state}   = this.props.location;
-     window.location = state? state.from.pathname : "/dashboard"
+      let result = await login({ email, password });
+      console.log(`result is ${result}`);
+      if (result && result.userType === "newUser") {
+        window.location = `/forcePasswordUpdate/${result.userId}`;
+        return;
+      }
+      const { state } = this.props.location;
+      window.location = state ? state.from.pathname : "/dashboard";
+      return;
     } catch (ex) {
-       if(ex.response && ex.response.status === 403)
-      {       
-        
+      if (ex.response && ex.response.status === 403) {
         const errors = this.state.errors;
-        
+
         errors["email"] = ex.response.data.message;
-        this.setState({errors})
+        this.setState({ errors });
       }
     }
   };
 
   render() {
-    
-if (getCurrentUser()) 
-return <Redirect to ="/"/>
+    if (getCurrentUser()) return <Redirect to="/" />;
 
     return (
+      <div className="container mt-5">
+        <h2>Please Sign in</h2>
 
-      <div className = "container mt-5">
-      <h2>Please Sign in</h2>
-     
-      <form className="col-md-6 xs-12" onSubmit={e => this.handleSubmit(e)}>
-        {this.renderInput("email", "Email:")}
-        {this.renderInput("password", "Password:", "password")}
-        {this.renderButton("Submit")}
-      </form>
-     
+        <form className="col-md-6 xs-12" onSubmit={e => this.handleSubmit(e)}>
+          {this.renderInput("email", "Email:")}
+          {this.renderInput("password", "Password:", "password")}
+          {this.renderButton("Submit")}
+        </form>
       </div>
     );
   }
