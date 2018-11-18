7e35632149de215d8825339a58ecca7090c71224
AishwaryaSingh <1994aishwaryasingh@gmail.com>
2018-11-18 15:52:08 -0800

    Header name bug fix.
    Fixed displaying user name. Profile picture still not pulled anymore.
    
    added toggle to not display header when not logged in.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Dashboard from './Dashboard'
 import {Logout} from './Logout';
-import Login from './Login'
-import Header from './Header'
+import Login from './Login';
+import Header from './Header';
 import Timelog from './Timelog';
 import Reports from './Reports';
-import Profile from './Profile'
+import Profile from './Profile';
 
 import ProtectedRoute from './common/ProtectedRoute'
 import {getCurrentUser} from "../services/loginService";
@@ -21,6 +21,7 @@ class App extends Component {
 
   componentDidMount() {
     const user = getCurrentUser();
+    //const t=user
     this.setState({user})
   }
 
@@ -29,26 +30,32 @@ class App extends Component {
   }
   
   render() {
-    
+    let navHeader = this.state.user ? <Header /> : '';
+
     return (
       <React.Fragment>
-      <Header user = {this.state.user} />
-      <ProtectedRoute path="/dashboard" exact component={Dashboard} />
-      <ProtectedRoute path="/timelog/:userId" exact component={Timelog} />
-      <ProtectedRoute path="/Reports" exact component={Reports} />
-      
+      {navHeader}
+      {/* <Header user = {this.state.user} /> */}
+     
       <Switch>
       <Route path="/login" component={Login} />
+      <ProtectedRoute path="/dashboard" exact component={Dashboard} />
+      <ProtectedRoute path="/timelog/:userId" exact component={Timelog} />
+      <ProtectedRoute path="/reports" exact component={Reports} />
       <ProtectedRoute path="/profile/:userId" component={Profile} />
-     {/* <Route path="/forcepassword" component={Forcepassword} />
-      <Route path="/forgotpassword" component={Forgotpassword} />
-      <ProtectedRoute authed={isUserAuthenticated()} path="/Timelog" component={TimelogPage} />
-      <ProtectedRoute authed={isUserAuthenticated()} path="/Usermanagement" component={UserManagement}/>
-      <ProtectedRoute authed={isUserAuthenticated()} path="/UpdatePassword" component={UpdatePassword}/> */}
-      <Route path="/Logout" component={Logout} />
+      <Route path="/logout" component={Logout} />
       <ProtectedRoute path = "/" exact component = {Dashboard}/>
-      {/* <Redirect from="/" exact to="/dashboard" /> */}
-      {/* <Route component={NoMatch} /> */}
+
+      { /* 
+        <Route path="/forcepassword" component={Forcepassword} />
+        <Route path="/forgotpassword" component={Forgotpassword} />
+        <ProtectedRoute authed={isUserAuthenticated()} path="/Timelog" component={TimelogPage} />
+        <ProtectedRoute authed={isUserAuthenticated()} path="/Usermanagement" component={UserManagement}/>
+        <ProtectedRoute authed={isUserAuthenticated()} path="/UpdatePassword" component={UpdatePassword}/> 
+        <Redirect from="/" exact to="/dashboard" />
+        <Route component={NoMatch} /> 
+      */ }
+
       </Switch>
 
     </React.Fragment>
