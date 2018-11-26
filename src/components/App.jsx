46dcbc8ba9389fd6e34f0fe4cdbcd306d533fb6b
Tyler Calvert <tyjcalvert@gmail.com>
2018-11-26 00:03:06 -0600

    restructured redux setup to pass tests

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import {Route, Switch} from 'react-router-dom';
-import { connect } from "react-redux";
-import { getCurrentUser } from "../actions";
-import { getjwt } from "../services/loginService"; 
 import Dashboard from './Dashboard'
 import {Logout} from './Logout';
 import Login from './Login'
@@ -21,44 +18,33 @@ import logger from "../services/logService"
 import '../App.css';
 
 class App extends Component {
-
-  componentDidMount() {
-    this.props.getCurrentUser(getjwt())
-  }
-
   componentDidCatch(error, errorInfo) {
    logger.logError(error)
   }
   
   render() {
-    {console.log(this.props)}
     return (
       <React.Fragment>
-      <Header/>
-      <ToastContainer/>
-      <Switch>
-      <ProtectedRoute path="/dashboard" exact component={Dashboard} />
-      <ProtectedRoute path="/timelog/:userId" exact component={Timelog} />
-      <ProtectedRoute path="/Reports" exact component={Reports} />      
-      <Route path="/login" component={Login} />
-      <ProtectedRoute path="/profile/:userId" component={Profile} />
-      <Route path="/Logout" component={Logout} />
-      <Route path = "/forcePasswordUpdate/:userId" component = {ForcePasswordUpdate}/>
-      <ProtectedRoute path = "/" exact component = {Dashboard}/>
-
-      </Switch>
-
-    </React.Fragment>
+        <Header />
+        <ToastContainer />
+        <Switch>
+          <ProtectedRoute path="/dashboard" exact component={Dashboard} />
+          <ProtectedRoute path="/timelog/:userId" exact component={Timelog} />
+          <ProtectedRoute path="/Reports" exact component={Reports} />
+          <Route path="/login" component={Login} />
+          <ProtectedRoute path="/profile/:userId" component={Profile} />
+          <Route path="/Logout" component={Logout} />
+          <Route
+            path="/forcePasswordUpdate/:userId"
+            component={ForcePasswordUpdate}
+          />
+          <ProtectedRoute path="/" exact component={Dashboard} />
+        </Switch>
+      </React.Fragment>
     );
   }
 }
 
-const mapStateToProps = state => {
-  return { state }
-}
+export default App;
 
 
-export default connect(
-  mapStateToProps, 
-  { getCurrentUser }
-)(App);
