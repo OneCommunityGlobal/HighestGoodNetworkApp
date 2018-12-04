5deb9261311413c7b6c4992e8fca9d254a38dd1e
Shubhra Mittal <shubhra.goel@gmail.com>
2018-12-04 13:36:31 -0800

    WIP for userProfile

---
+++ src/components/common/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className = "container h-100 align-middle">
+            <i className="fa fa-spinner align-middle">Loading</i>
+        </div>
+      );
+}
+ 
+export default Loading;
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Timelog from './components/Timelog';
 import Reports from './components/Reports';
-import Profile from './components/Profile'
+import UserProfile from './components/UserProfile'
 import {Route, Switch} from 'react-router-dom';
 import Dashboard from './components/Dashboard'
 import {Logout} from './components/Logout';
@@ -26,7 +26,7 @@ export default (
     <ProtectedRoute path="/dashboard" exact component={Dashboard} />
     <ProtectedRoute path="/timelog/:userId" exact component={Timelog} />
     <ProtectedRoute path="/reports" exact component={Reports} />
-    <ProtectedRoute path="/profile/:userId" component={Profile} />
+    <ProtectedRoute path="/userprofile/:userId" component={UserProfile} />
     <ProtectedRoute path="/updatepassword/:userId" component={UpdatePassword} />
     <Route path="/Logout" component={Logout} />
     <Route path = "/forcePasswordUpdate/:userId" component = {ForcePasswordUpdate}/>
