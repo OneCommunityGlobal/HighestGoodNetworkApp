8a7a52398a493d7cdd3c9f681f3ce1393eea9dbc
Shubhra Mittal <shubhra.goel@gmail.com>
2018-12-19 13:17:33 -0800

    created additional tests for profile

--- src/__tests__/Profile.test.js
+++ src/__tests__/Profile.test.js
@@ -57,69 +57,142 @@ describe("Profile Page", () => {
         profilePage.find('[name="projects"]').length
       ).toBeGreaterThanOrEqual(0);
     });
+  });
 
-    describe("Behavior", () => {
-      it("should show error if firstName is blank", () => {
-        setInputField(profilePage, "firstName", "");
-        expect(profilePage.instance().state.errors["firstName"]).toEqual(
-          errorsMessages["firstName_blank"]
-        );
-      });
-      it("should show error if firstName has only spaces", () => {
-        setInputField(profilePage, "firstName", "     ");
-        expect(profilePage.instance().state.errors["firstName"]).toEqual(
-          errorsMessages["firstName_blank"]
-        );
-      });
-      it("should show error if firstName is less than two charcaters", () => {
-        setInputField(profilePage, "firstName", "a");
-        expect(profilePage.instance().state.errors["firstName"]).toEqual(
-          errorsMessages["firstName_minlength"]
-        );
-      });
-
-      it("should show error if lastName is blank", () => {
-        setInputField(profilePage, "lastName", "");
-        expect(profilePage.instance().state.errors["lastName"]).toEqual(
-          errorsMessages["lastName_blank"]
-        );
-      });
-      it("should show error if lastName has only spaces", () => {
-        setInputField(profilePage, "lastName", "     ");
-        expect(profilePage.instance().state.errors["lastName"]).toEqual(
-          errorsMessages["lastName_blank"]
-        );
-      });
-      it("should show error if lastName is less than two charcaters", () => {
-        setInputField(profilePage, "lastName", "a");
-        expect(profilePage.instance().state.errors["lastName"]).toEqual(
-          errorsMessages["lastName_minlength"]
-        );
-      });
-      it("should show error if email is blank", () => {
-        setInputField(profilePage, "email", "");
-        expect(profilePage.instance().state.errors["email"]).toEqual(
-          errorsMessages["email_blank"]
-        );
-      });
-      it("should show error if email is invalid", () => {
-        setInputField(profilePage, "email", "abcc");
-        expect(profilePage.instance().state.errors["email"]).toEqual(
-          errorsMessages["email_invalid"]
-        );
-      });
-      it("should show error if weeklyComittedHours is not a number", () => {
-        setInputField(profilePage, "weeklyComittedHours", "abcc");
-        expect(
-          profilePage.instance().state.errors["weeklyComittedHours"]
-        ).toEqual(errorsMessages["weeklyComittedHours_Nan"]);
-      });
-      it("should show error if weeklyComittedHours is less than 0", () => {
-        setInputField(profilePage, "weeklyComittedHours", -1);
-        expect(
-          profilePage.instance().state.errors["weeklyComittedHours"]
-        ).toEqual(errorsMessages["weeklyComittedHours_LessThan0"]);
-      });
+  describe("Behavior", () => {
+    it("should show error if firstName is blank", () => {
+      setInputField(profilePage, "firstName", "");
+      expect(profilePage.instance().state.errors["firstName"]).toEqual(
+        errorsMessages["firstName_blank"]
+      );
     });
+    it("should show error if firstName has only spaces", () => {
+      setInputField(profilePage, "firstName", "     ");
+      expect(profilePage.instance().state.errors["firstName"]).toEqual(
+        errorsMessages["firstName_blank"]
+      );
+    });
+    it("should show error if firstName is less than two charcaters", () => {
+      setInputField(profilePage, "firstName", "a");
+      expect(profilePage.instance().state.errors["firstName"]).toEqual(
+        errorsMessages["firstName_minlength"]
+      );
+    });
+
+    it("should show error if lastName is blank", () => {
+      setInputField(profilePage, "lastName", "");
+      expect(profilePage.instance().state.errors["lastName"]).toEqual(
+        errorsMessages["lastName_blank"]
+      );
+    });
+    it("should show error if lastName has only spaces", () => {
+      setInputField(profilePage, "lastName", "     ");
+      expect(profilePage.instance().state.errors["lastName"]).toEqual(
+        errorsMessages["lastName_blank"]
+      );
+    });
+    it("should show error if lastName is less than two charcaters", () => {
+      setInputField(profilePage, "lastName", "a");
+      expect(profilePage.instance().state.errors["lastName"]).toEqual(
+        errorsMessages["lastName_minlength"]
+      );
+    });
+    it("should show error if email is blank", () => {
+      setInputField(profilePage, "email", "");
+      expect(profilePage.instance().state.errors["email"]).toEqual(
+        errorsMessages["email_blank"]
+      );
+    });
+    it("should show error if email is invalid", () => {
+      setInputField(profilePage, "email", "abcc");
+      expect(profilePage.instance().state.errors["email"]).toEqual(
+        errorsMessages["email_invalid"]
+      );
+    });
+    it("should show error if weeklyComittedHours is not a number", () => {
+      setInputField(profilePage, "weeklyComittedHours", "abcc");
+      expect(
+        profilePage.instance().state.errors["weeklyComittedHours"]
+      ).toEqual(errorsMessages["weeklyComittedHours_Nan"]);
+    });
+    it("should show error if weeklyComittedHours is less than 0", () => {
+      setInputField(profilePage, "weeklyComittedHours", -1);
+      expect(
+        profilePage.instance().state.errors["weeklyComittedHours"]
+      ).toEqual(errorsMessages["weeklyComittedHours_LessThan0"]);
+    });
+  });
+});
+
+describe("Behavior for props passed", () => {
+  let mountedPage;
+  let firstName = "Jane";
+  let lastName = "Doe";
+  let email = "someemail@test.com";
+  let isActive = false;
+  let role = "Volunteer";
+  let weeklyComittedHours = 100;
+  let bio = "some Bio which is realy long";
+  let links = [
+    { Name: "Link1", Link: "https://www.google.com" },
+    { Name: "This is Link2", Link: "https://www.facebook.com" }
+  ];
+  let targetUserId = 1234;
+  let userProfile = {
+    firstName,
+    lastName,
+    email,
+    isActive,
+    role,
+    weeklyComittedHours,
+    bio,
+    adminLinks: links,
+    personalLinks: links
+  };
+
+  beforeEach(() => {
+    mountedPage = shallow(
+      <Profile userProfile={userProfile} targetUserId={targetUserId} />
+    );
+  });
+  it("should correctly render the firstName", () => {
+    let field = mountedPage.find("[name='firstName']");
+    expect(field.props().value).toEqual(firstName);
+  });
+  it("should correctly render a button to user's timelog", () => {
+    let field = mountedPage.find("Link");
+    expect(field.props().to).toEqual(`/timelog/${targetUserId}`);
+  });
+  it("should correctly render the lastName", () => {
+    let field = mountedPage.find("[name='lastName']");
+    expect(field.props().value).toEqual(lastName);
+  });
+  it("should correctly render the email", () => {
+    let field = mountedPage.find("[name='email']");
+    expect(field.props().value).toEqual(email);
+  });
+  it("should correctly render isActive", () => {
+    let field = mountedPage.find("[name='isActive']");
+    expect(field.props().value).toEqual(isActive);
+  });
+  it("should correctly render role", () => {
+    let field = mountedPage.find("[name='role']");
+    expect(field.props().value).toEqual(role);
+  });
+  it("should correctly render weeklyComittedHours", () => {
+    let field = mountedPage.find("[name='weeklyComittedHours']");
+    expect(field.props().value).toEqual(weeklyComittedHours);
+  });
+  it("should correctly render user's bio", () => {
+    let field = mountedPage.find("[name='bio']");
+    expect(field.props().value).toContain(bio);
+  });
+  it("should correctly render user's admin Links", () => {
+    let field = mountedPage.find("[collection='adminLinks']");
+    expect(field.props().data).toEqual(links);
+  });
+  it("should correctly render user's personal Links", () => {
+    let field = mountedPage.find("[collection='personalLinks']");
+    expect(field.props().data).toEqual(links);
   });
 });
