df25a639a78f0d9476e8c36da85de350e3144d6e
Shubhra Mittal <shubhra.goel@gmail.com>
2018-12-19 16:22:21 -0800

    added role based test cases for profile

--- src/__tests__/Profile.test.js
+++ src/__tests__/Profile.test.js
@@ -33,7 +33,7 @@ describe("Profile Page", () => {
   });
 
   describe("Structure", () => {
-    it("should have all user profile fields field", () => {
+    it("should have all user profile fields field_Admin", () => {
       expect(profilePage.find('[name="firstName"]').length).toBe(1);
       expect(profilePage.find('[name="lastName"]').length).toBe(1);
       expect(profilePage.find('[name="email"]').length).toBe(1);
@@ -125,19 +125,28 @@ describe("Profile Page", () => {
 });
 
 describe("Behavior for props passed", () => {
-  let mountedPage;
+  let mountedPage_Admin;
+  let mountedPage_Self;
+  let mountedPage_Viewer;
   let firstName = "Jane";
   let lastName = "Doe";
   let email = "someemail@test.com";
   let isActive = false;
   let role = "Volunteer";
   let weeklyComittedHours = 100;
-  let bio = "some Bio which is realy long";
+  let bio = "some Bio which is really long";
   let links = [
     { Name: "Link1", Link: "https://www.google.com" },
     { Name: "This is Link2", Link: "https://www.facebook.com" }
   ];
+  let infringments = [];
   let targetUserId = 1234;
+  let profilePic = "";
+  let teams = [{ _id: 1, teamName: "team1" }, { _id: 2, teamName: "team2" }];
+  let projects = [
+    { _id: 1, projectName: "project1" },
+    { _id: 2, projectName: "project2" }
+  ];
   let userProfile = {
     firstName,
     lastName,
@@ -147,52 +156,116 @@ describe("Behavior for props passed", () => {
     weeklyComittedHours,
     bio,
     adminLinks: links,
-    personalLinks: links
+    personalLinks: links,
+    infringments,
+    profilePic,
+    teams,
+    projects
   };
 
   beforeEach(() => {
-    mountedPage = shallow(
-      <Profile userProfile={userProfile} targetUserId={targetUserId} />
+    mountedPage_Admin = shallow(
+      <Profile
+        userProfile={userProfile}
+        targetUserId={targetUserId}
+        requestorRole="Administrator"
+      />
+    );
+    mountedPage_Self = shallow(
+      <Profile
+        userProfile={userProfile}
+        targetUserId={targetUserId}
+        requestorId={targetUserId}
+        requestorRole="Volunteer"
+      />
+    );
+    mountedPage_Viewer = shallow(
+      <Profile
+        userProfile={userProfile}
+        targetUserId={targetUserId + 100}
+        requestorId={targetUserId}
+        requestorRole="Volunteer"
+      />
     );
   });
-  it("should correctly render the firstName", () => {
-    let field = mountedPage.find("[name='firstName']");
-    expect(field.props().value).toEqual(firstName);
+  it("should correctly render the firstName, editable by self and admin and readonly for viewer", () => {
+    //Admin
+    let field_Admin = mountedPage_Admin.find("[name='firstName']");
+    expect(field_Admin.props().value).toEqual(firstName);
+    expect(field_Admin.props().readOnly).toBeNull();
+
+    //Self
+    let field_Self = mountedPage_Self.find("[name='firstName']");
+    expect(field_Self.props().value).toEqual(firstName);
+    expect(field_Self.props().readOnly).toBeNull();
+
+    //Viewer
+    let field_Viewer = mountedPage_Viewer.find("[name='firstName']");
+    expect(field_Viewer.props().value).toEqual(firstName);
+    expect(field_Viewer.props().readOnly).toBeTruthy();
   });
+  it("should render default profile pic if no profilePic was passed", () => {
+    let field_Admin = mountedPage_Admin.find("Image");
+    expect(field_Admin.props().src).toEqual("/defaultprofilepic.jpg");
+  });
+
+  it("should render edit button only if user is admin or self but not for other viewer", () => {
+    let field_Admin = mountedPage_Admin.find("FileUpload");
+    expect(field_Admin.length).toEqual(1);
+
+    let field_Self = mountedPage_Self.find("FileUpload");
+    expect(field_Self.length).toEqual(1);
+
+    let field_Viewer = mountedPage_Viewer.find("FileUpload");
+    expect(field_Viewer.length).toEqual(0);
+  });
+
   it("should correctly render a button to user's timelog", () => {
-    let field = mountedPage.find("Link");
-    expect(field.props().to).toEqual(`/timelog/${targetUserId}`);
+    let field_Admin = mountedPage_Admin.find("Link");
+    expect(field_Admin.props().to).toEqual(`/timelog/${targetUserId}`);
   });
   it("should correctly render the lastName", () => {
-    let field = mountedPage.find("[name='lastName']");
-    expect(field.props().value).toEqual(lastName);
+    let field_Admin = mountedPage_Admin.find("[name='lastName']");
+    expect(field_Admin.props().value).toEqual(lastName);
   });
   it("should correctly render the email", () => {
-    let field = mountedPage.find("[name='email']");
-    expect(field.props().value).toEqual(email);
+    let field_Admin = mountedPage_Admin.find("[name='email']");
+    expect(field_Admin.props().value).toEqual(email);
   });
   it("should correctly render isActive", () => {
-    let field = mountedPage.find("[name='isActive']");
-    expect(field.props().value).toEqual(isActive);
+    let field_Admin = mountedPage_Admin.find("[name='isActive']");
+    expect(field_Admin.props().value).toEqual(isActive);
   });
   it("should correctly render role", () => {
-    let field = mountedPage.find("[name='role']");
-    expect(field.props().value).toEqual(role);
+    let field_Admin = mountedPage_Admin.find("[name='role']");
+    expect(field_Admin.props().value).toEqual(role);
   });
   it("should correctly render weeklyComittedHours", () => {
-    let field = mountedPage.find("[name='weeklyComittedHours']");
-    expect(field.props().value).toEqual(weeklyComittedHours);
+    let field_Admin = mountedPage_Admin.find("[name='weeklyComittedHours']");
+    expect(field_Admin.props().value).toEqual(weeklyComittedHours);
   });
   it("should correctly render user's bio", () => {
-    let field = mountedPage.find("[name='bio']");
-    expect(field.props().value).toContain(bio);
+    let field_Admin = mountedPage_Admin.find("[name='bio']");
+    expect(field_Admin.props().value).toContain(bio);
   });
   it("should correctly render user's admin Links", () => {
-    let field = mountedPage.find("[collection='adminLinks']");
-    expect(field.props().data).toEqual(links);
+    let field_Admin = mountedPage_Admin.find("[collection='adminLinks']");
+    expect(field_Admin.props().data).toEqual(links);
   });
   it("should correctly render user's personal Links", () => {
-    let field = mountedPage.find("[collection='personalLinks']");
-    expect(field.props().data).toEqual(links);
+    let field_Admin = mountedPage_Admin.find("[collection='personalLinks']");
+    expect(field_Admin.props().data).toEqual(links);
+  });
+  it("should correctly render exactly 5 infringments", () => {
+    let field_Admin = mountedPage_Admin.find("RenderInfringment");
+    expect(field_Admin.length).toEqual(5);
+  });
+  it("should correctly render teams correctly", () => {
+    let field_Admin = mountedPage_Admin.find("Memberships[label='Team']");
+    expect(field_Admin.props().data).toEqual(teams);
+  });
+  it("should correctly render projects correctly", () => {
+    let field_Admin = mountedPage_Admin.find("Memberships[label='Project']");
+    expect(field_Admin.props().data).toEqual(projects);
   });
 });
