f23e747e56f143ff57a41c086135f7bafdf92bdb
Simon X <simonxiong11@gmail.com>
2020-02-20 12:53:37 -0800

    rename Login file with .jsx

--- src/components/Login/Login.test.jsx
+++ src/components/Login/Login.test.jsx
@@ -1,19 +1,14 @@
 import React from "react";
-import Enzyme, {
-  shallow
-} from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import { shallow } from "enzyme";
 import Login from "./Login";
 
-Enzyme.configure({
-  adapter: new Adapter()
-});
+
 //test suite
 describe("Login", () => {
-  //first test case
-  it("email text field should show email adress", () => {
-    const wrapper = shallow( < Login / > );
-    const email = wrapper.find("div form");
-    expect(email.text()).toBe("simonxiong11@gmail.com");
+  // first test case
+  it("should show text", () => {
+    const wrapper = shallow(<Login />);
+    const heading = wrapper.find("div h2");
+    expect(heading.text()).toBe("Please Sign in");
   });
 });
