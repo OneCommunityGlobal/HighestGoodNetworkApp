64c8cd68d7c24e40e28fd30ed5c6b41dc2fbde1b
Simon X <simonxiong11@gmail.com>
2020-03-05 20:36:00 -0800

    pass auth default state to Login component

--- src/components/Login/Login.test.jsx
+++ src/components/Login/Login.test.jsx
@@ -2,30 +2,24 @@ import React from "react";
 import { shallow } from "enzyme";
 import { Login } from "./Login";
 
-
-
 //test suite
 describe("Login", () => {
-  let wrapper;
-  const mockLoginfn = jest.fn();
-  beforeEach(() => {
-    wrapper = shallow(<Login login={mockLoginfn} />)
-  })
-  //first test case
+
+  const props = {
+    auth: [{ isAuthenticated: false }]
+  }
+  const wrapper = shallow(<Login {...props} />)
+
   it('should show Please Sign in', () => {
-    wrapper = shallow(<Login login={mockLoginfn} />);
     expect(wrapper.find('h2')).toHaveLength(1);
   });
-  //second test case
   it("should show two input field", () => {
-    wrapper = shallow(<Login login={mockLoginfn} />);
     const inputs = wrapper.find('Input');
     expect(inputs.length).toBe(2);
   });
-  //third test case
   it('should be rendered with one button', () => {
-    wrapper = shallow(<Login login={mockLoginfn} />);
     const button = wrapper.find('button');
     expect(button.length).toBe(1);
   });
+  //it should 
 });
