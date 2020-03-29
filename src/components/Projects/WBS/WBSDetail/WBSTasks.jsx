d743c082739d0951361163097138e830eee37134
Henry Nguyen <vnseattleapp@gmail.com>
2020-03-28 19:46:18 -0600

    Added tooltip

--- src/components/Projects/WBS/WBSDetail/WBSTasks.jsx
+++ src/components/Projects/WBS/WBSDetail/WBSTasks.jsx
@@ -7,6 +7,7 @@ import { connect } from 'react-redux'
 import { fetchAllTasks } from './../../../../actions/task'
 import Task from './Task/'
 import './wbs.css';
+import ReactTooltip from 'react-tooltip'
 
 const WBSTasks = (props) => {
 
@@ -18,25 +19,26 @@ const WBSTasks = (props) => {
 
   return (
     <React.Fragment>
+      <ReactTooltip />
 
       <div className='container' >
 
         <table className="table table-bordered">
           <thead>
             <tr>
-              <th scope="col">#</th>
-              <th scope="col">Task</th>
-              <th scope="col">Priority</th>
-              <th scope="col">Resources</th>
-              <th scope="col" data-toggle="tooltip" data-placement="top" title="Tooltip on top" ><i className="fa fa-user-circle-o" aria-hidden="true"></i></th>
-              <th scope="col"><i className="fa fa-tasks" aria-hidden="true"></i></th>
-              <th scope="col"><i className="fa fa-hourglass-start" aria-hidden="true"></i></th>
-              <th scope="col"><i className="fa fa-hourglass" aria-hidden="true"></i></th>
-              <th scope="col"><i className="fa fa-hourglass-half" aria-hidden="true"></i></th>
-              <th scope="col"><i className="fa fa-clock-o" aria-hidden="true"></i></th>
-              <th scope="col"><i className="fa fa-calendar-check-o" aria-hidden="true"></i> Start</th>
-              <th scope="col"><i className="fa fa-calendar-times-o" aria-hidden="true"></i> End</th>
-              <th scope="col"><i className="fa fa-link" aria-hidden="true"></i></th>
+              <th scope="col" data-tip="WBS ID">#</th>
+              <th scope="col" data-tip="Task Name">Task</th>
+              <th scope="col" data-tip="Priority">Priority</th>
+              <th scope="col" data-tip="Resources">Resources</th>
+              <th scope="col" data-tip="Assigned" ><i className="fa fa-user-circle-o" aria-hidden="true"></i></th>
+              <th scope="col" data-tip="Status" ><i className="fa fa-tasks" aria-hidden="true"></i></th>
+              <th scope="col" data-tip="Hours-Best"><i className="fa fa-hourglass-start" aria-hidden="true"></i></th>
+              <th scope="col" data-tip="Hours-Worst"><i className="fa fa-hourglass" aria-hidden="true"></i></th>
+              <th scope="col" data-tip="Hours-Most"><i className="fa fa-hourglass-half" aria-hidden="true"></i></th>
+              <th scope="col" data-tip="Estimated Hours"><i className="fa fa-clock-o" aria-hidden="true"></i></th>
+              <th scope="col" data-tip="Start Date" ><i className="fa fa-calendar-check-o" aria-hidden="true"></i> Start</th>
+              <th scope="col" data-tip="Due Date"><i className="fa fa-calendar-times-o" aria-hidden="true"></i> End</th>
+              <th scope="col" data-tip="Links" ><i className="fa fa-link" aria-hidden="true"></i></th>
 
 
             </tr>
