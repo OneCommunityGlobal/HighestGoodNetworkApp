e89c1216cf1d468076147e8e4463976c222dcee5
Nithesh AN <getnithesh@gmail.com>
2020-06-22 22:10:34 +0530

    corrections

--- src/components/UserManagement/DeleteUserPopup.jsx
+++ src/components/UserManagement/DeleteUserPopup.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input } from 'reactstrap';
 import { useState } from 'react';
 import { UserDeleteType } from '../../utils/enums';
+import {
+  USER_DELETE_CONFIRMATION_FIRST_LINE,
+  USER_DELETE_CONFIRMATION_SECOND_LINE,
+  USER_DELETE_DATA_FOREVER,
+  USER_DELETE_DATA_INACTIVE,
+  USER_DELETE_DATA_ARCHIVE,
+  USER_DELETE_OPTION_HEADING
+} from '../../languages/en/messages';
+import { CLOSE } from '../../languages/en/ui';
 
 /**
  * Modal popup to delete the user profile 
@@ -11,28 +20,26 @@ const DeleteUserPopup = React.memo((props) => {
   const closePopup = (e) => { props.onClose() };
 
   return <Modal isOpen={props.open} toggle={closePopup}>
-    <ModalHeader toggle={closePopup}>Choose A Delete Action</ModalHeader>
+    <ModalHeader toggle={closePopup}>{USER_DELETE_OPTION_HEADING}</ModalHeader>
     <ModalBody>
-      <p>Are you sure you want to delete this user? This action cannot be undone.</p>
-      <p>Switch them to 'Inactive' if you'd like to keep them in the system or choose “Keep Data”
-      if you’d like to still delete them but add their associated data to the “Data Archive”
-      instead of completely deleting them and all their data.</p>
+      <p>{USER_DELETE_CONFIRMATION_FIRST_LINE}</p>
+      <p>{USER_DELETE_CONFIRMATION_SECOND_LINE}</p>
       <div style={{ textAlign: "center", paddingTop: "10px" }}>
         <Button color="primary btn-danger" onClick={() => { props.onDelete(UserDeleteType.HardDelete) }}>
-          Delete the data forever
+          {USER_DELETE_DATA_FOREVER}
         </Button>
         <DivSpacer />
         <Button color="primary btn-warning" onClick={() => { props.onDelete(UserDeleteType.Inactive) }}>
-          Make them Inactive
+          {USER_DELETE_DATA_INACTIVE}
         </Button>
         <DivSpacer />
         <Button color="primary btn-success " onClick={() => { props.onDelete(UserDeleteType.SoftDelete) }}>
-          Keep the data and Delete
+          {USER_DELETE_DATA_ARCHIVE}
         </Button>
       </div>
     </ModalBody>
     <ModalFooter>
-      <Button color="secondary" onClick={closePopup}>Close</Button>
+      <Button color="secondary" onClick={closePopup}>{CLOSE}</Button>
     </ModalFooter>
   </Modal>
 });
