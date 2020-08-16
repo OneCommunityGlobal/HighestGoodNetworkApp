9d6f7fa8dc5fa0d16b7daf12d7720fd968e1d53f
Jerry Zhang <jerryzhang.dev@gmail.com>
2020-08-15 21:04:35 -0400

    display team data in basic tables

--- src/components/Teams/Teams.jsx
+++ src/components/Teams/Teams.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Table } from 'reactstrap';
+import moment from 'moment';
+import _ from 'lodash';
+import httpService from '../../services/httpService';
+import { ENDPOINTS } from '../../utils/URL';
 import { fetchAllManagingTeams } from '../../actions/team';
 import { getUserProfile } from '../../actions/userProfile';
 import Loading from '../common/Loading';
@@ -8,7 +12,10 @@ import Loading from '../common/Loading';
 class Teams extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      fetched: false,
+      teams: [],
+    };
   }
 
   // async componentWillMount() {
@@ -16,19 +23,109 @@ class Teams extends Component {
   // }
 
   componentDidMount() {
-    fetchAllManagingTeams(this.props.userId, this.props.managingTeams);
+    // fetchAllManagingTeams(this.props.userId, this.props.managingTeams);
+    const leaderBoardData = this.props.leaderboardData;
+    const allManagingTeams = [];
+    let allMembers = [];
+    const teamMembersPromises = [];
+    const memberTimeEntriesPromises = [];
+    const { managingTeams } = this.props;
+    managingTeams.forEach((team) => {
+      // req = await httpService.get(ENDPOINTS.TEAM_MEMBERS(team._id));
+      teamMembersPromises.push(httpService.get(ENDPOINTS.TEAM_MEMBERS(team._id)));
+    });
+
+    Promise.all(teamMembersPromises).then((data) => {
+      for (let i = 0; i < managingTeams.length; i++) {
+        allManagingTeams[i] = {
+          ...managingTeams[i],
+          members: data[i].data,
+        };
+        allMembers = allMembers.concat(data[i].data);
+      }
+      console.log('allManagingTeams:', allManagingTeams);
+      const uniqueMembers = _.uniqBy(allMembers, '_id');
+      uniqueMembers.forEach(async (member) => {
+        const fromDate = moment()
+          .startOf('week')
+          .subtract(0, 'weeks');
+        const toDate = moment()
+          .endOf('week')
+          .subtract(0, 'weeks');
+        memberTimeEntriesPromises.push(
+          httpService.get(ENDPOINTS.TIME_ENTRIES_PERIOD(member._id, fromDate, toDate))
+        );
+      });
+
+      Promise.all(memberTimeEntriesPromises).then((data) => {
+        // console.log('After time entries: ', data);
+        // console.log('uniqueMemberTimeEntries: ', uniqueMemberTimeEntries);
+        for (let i = 0; i < uniqueMembers.length; i++) {
+          uniqueMembers[i] = {
+            ...uniqueMembers[i],
+            timeEntries: data[i].data,
+          };
+        }
+
+        for (let i = 0; i < allManagingTeams.length; i++) {
+          for (let j = 0; j < allManagingTeams[i].members.length; j++) {
+            let memberDataWithTimeEntries = uniqueMembers.find(
+              (member) => member._id === allManagingTeams[i].members[j]._id
+            );
+            const memberLeaderboardData = leaderBoardData.find(
+              (member) => member.personId === allManagingTeams[i].members[j]._id
+            );
+            // console.log('memberDataWithTimeENtries: ', memberDataWithTimeEntries);
+            // console.log('memberLeaderboardData', memberLeaderboardData);
+            if (memberLeaderboardData) {
+              memberDataWithTimeEntries = {
+                ...memberDataWithTimeEntries,
+                totaltangibletime_hrs: memberLeaderboardData.totaltangibletime_hrs,
+              };
+            } else {
+              memberDataWithTimeEntries = {
+                ...memberDataWithTimeEntries,
+                totaltangibletime_hrs: 0,
+              };
+            }
+
+            allManagingTeams[i].members[j] = memberDataWithTimeEntries;
+          }
+        }
+
+        console.log('after processing: ', allManagingTeams);
+
+        this.setState({ fetched: true, teams: allManagingTeams });
+      });
+    });
   }
 
   render() {
-    const { teams, status, fetching, fetched } = this.props.teamsInfo;
+    const { teams, fetching, fetched } = this.state;
 
-    // console.log('teams: ', teams);
+    console.log('teams: ', teams);
+    console.log('leaderboardData: ', this.props.leaderboardData);
+
     let teamsList = [];
     if (teams && teams.length > 0) {
       teamsList = teams.map((team, index) => (
-        <tr key={index}>
-          <td>{team.teamName}</td>
-        </tr>
+        <Table key={index}>
+          <thead>
+            <tr>
+              <th>{team.teamName}</th>
+            </tr>
+            <tr>
+              <th>Name</th>
+              <th>Total Hours</th>
+            </tr>
+          </thead>
+          {team.members.map((member, index) => (
+            <tr key={index}>
+              <td>{`${member.firstName} ${member.lastName}`}</td>
+              <td>{member.totaltangibletime_hrs}</td>
+            </tr>
+          ))}
+        </Table>
       ));
     }
 
@@ -39,14 +136,7 @@ class Teams extends Component {
         <div className='container'>
           {fetching || !fetched ? <Loading /> : null}
           <h1>Teams</h1>
-          <Table>
-            <thead>
-              <tr>
-                <th>Name</th>
-              </tr>
-            </thead>
-            <tbody>{teamsList}</tbody>
-          </Table>
+          {teamsList}
         </div>
       </React.Fragment>
     );
@@ -57,6 +147,7 @@ const mapStateToProps = (state) => ({
   userId: state.userProfile._id,
   managingTeams: state.userProfile.teams,
   teamsInfo: state.managingTeams,
+  leaderboardData: state.leaderBoardData,
 });
 
 export default connect(mapStateToProps, { getUserProfile, fetchAllManagingTeams })(Teams);
