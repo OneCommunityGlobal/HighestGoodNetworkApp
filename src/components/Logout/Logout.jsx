ecd156e0b82080c641b46a8c5298344d646faa0c
Chris Weilacker <chrisweilacker@gmail.com>
2020-08-19 13:22:58 -0700

    Fixed Logout Warning

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 import { logoutUser } from '../../actions/authActions'
 import { useDispatch } from 'react-redux'
 
 export const Logout = ()=>{
   const dispatch = useDispatch()
-  dispatch(logoutUser());
+  
+  useEffect(() => {
+    dispatch(logoutUser());
+  });
+  
   return (<Redirect to='/login' auth={false}/>);
 }
