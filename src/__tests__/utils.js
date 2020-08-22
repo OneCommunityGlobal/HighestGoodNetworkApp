a99aa6987627afc428007566a8df0aa61c74e37d
JahJah <junhao6503@gmail.com>
2020-08-22 08:40:31 -0700

    some changes to test util

--- src/__tests__/utils.js
+++ src/__tests__/utils.js
@@ -4,27 +4,17 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import { createMemoryHistory } from 'history';
 import { Router, Route } from 'react-router-dom';
-<<<<<<< HEAD
 import thunk from 'redux-thunk';
 import reducer from '../reducers';
 
-=======
-import reducer from '../reducers';
-import thunk from 'redux-thunk';
->>>>>>> 91c5efdb675c3e9781ac1c1e68a0a94c417c7a66
 const middleware = [thunk];
 
 
 function renderWithProvider(
   ui,
   {
-<<<<<<< HEAD
     initialState,
     store = createStore(reducer),
-=======
-    initialState = {},
-    store = createStore(reducer, initialState, compose(applyMiddleware(...middleware))),
->>>>>>> 91c5efdb675c3e9781ac1c1e68a0a94c417c7a66
     ...renderOptions
   } = {},
 ) {
@@ -35,7 +25,6 @@ function renderWithProvider(
 }
 
 // Helper function
-<<<<<<< HEAD
 export function renderWithRouterMatch(
   ui,
   {
@@ -54,29 +43,9 @@ export function renderWithRouterMatch(
     store,
     history,
   };
-=======
-function renderWithRouterMatch(
-  ui,
-  {
-    initialState = {},
-    store = createStore(reducer, initialState, compose(applyMiddleware(...middleware))),
-    route = '/',
-    history = createMemoryHistory({ initialEntries: [route] }),
-    ...renderOptions
-  } = {},
-) {
-  function Wrapper({ children }) {
-    return <Provider store={store}><Router history={history}>{children}</Router></Provider>;
-  }
-  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
->>>>>>> 91c5efdb675c3e9781ac1c1e68a0a94c417c7a66
 }
 
 // re-export everything
 export * from '@testing-library/react';
 // override render method
-<<<<<<< HEAD
 export { renderWithProvider };
-=======
-export { renderWithProvider, renderWithRouterMatch };
->>>>>>> 91c5efdb675c3e9781ac1c1e68a0a94c417c7a66
