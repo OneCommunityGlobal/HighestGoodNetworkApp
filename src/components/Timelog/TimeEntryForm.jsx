9b782d5f6a8b550bd5b28a151792432eca52e85e
yiqifeng <fengyiqi1717@126.com>
2020-08-23 00:35:09 -0700

    edit time add

--- src/components/Timelog/TimeEntryForm.jsx
+++ src/components/Timelog/TimeEntryForm.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router";
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router'
 import {
   Form,
   FormGroup,
@@ -13,258 +13,266 @@ import {
   ModalHeader,
   ModalBody,
   ModalFooter,
-} from "reactstrap";
-import moment from "moment";
-import _ from "lodash";
-import { Editor } from "@tinymce/tinymce-react";
-import ReactHtmlParser from "react-html-parser";
-import { postTimeEntry, editTimeEntry } from "../../actions/timeEntries";
-import { getUserProjects } from "../../actions/userProjects";
-import { stopTimer } from "../../actions/timer";
+} from 'reactstrap'
+import moment from 'moment'
+import _ from 'lodash'
+import { Editor } from '@tinymce/tinymce-react'
+import ReactHtmlParser from 'react-html-parser'
+import { postTimeEntry, editTimeEntry } from '../../actions/timeEntries'
+import { getUserProjects } from '../../actions/userProjects'
+import { stopTimer } from '../../actions/timer'
 
 const TimeEntryForm = ({ userId, edit, data, isOpen, toggle, timer }) => {
-  const fromTimer = !_.isEmpty(timer);
+  const fromTimer = !_.isEmpty(timer)
 
   const initialState = {
-    dateOfWork: moment().format("YYYY-MM-DD"),
+    dateOfWork: moment().format('YYYY-MM-DD'),
     hours: 0,
     minutes: 0,
-    projectId: "",
-    notes: "",
+    projectId: '',
+    notes: '',
     isTangible: data ? data.isTangible : true,
-  };
+  }
   const initialReminder = {
     notification: false,
     has_link: false,
-    remind: "",
+    remind: '',
     num_words: 0,
     edit_count: data ? data.editCount : 0,
     edit_notice: true,
-  };
+    // edittime: false,
+  }
 
-  const [inputs, setInputs] = useState(edit ? data : initialState);
-  const [errors, setErrors] = useState({});
-  const dispatch = useDispatch();
-  const history = useHistory();
-  const [reminder, setReminder] = useState(initialReminder);
+  const [inputs, setInputs] = useState(edit ? data : initialState)
+  const [errors, setErrors] = useState({})
+  const dispatch = useDispatch()
+  const history = useHistory()
+  const [reminder, setReminder] = useState(initialReminder)
 
   const openModal = () =>
-    setReminder((reminder) => ({
+    setReminder(reminder => ({
       ...reminder,
       notification: !reminder.notification,
-    }));
+    }))
 
   useEffect(() => {
-    const fetchProjects = async (userId) => {
-      await dispatch(getUserProjects(userId));
-    };
-    fetchProjects(userId);
-  }, [userId]);
+    const fetchProjects = async userId => {
+      await dispatch(getUserProjects(userId))
+    }
+    fetchProjects(userId)
+  }, [userId])
 
   useEffect(() => {
-    setInputs({ ...inputs, ...timer });
-  }, [timer]);
+    setInputs({ ...inputs, ...timer })
+  }, [timer])
 
-  const userProjects = useSelector((state) => state.userProjects);
-  let projects = [];
+  const userProjects = useSelector(state => state.userProjects)
+  let projects = []
   if (!_.isEmpty(userProjects)) {
-    projects = userProjects.projects;
+    projects = userProjects.projects
   }
-  const projectOptions = projects.map((project) => (
+  const projectOptions = projects.map(project => (
     <option value={project.projectId} key={project.projectId}>
-      {" "}
-      {project.projectName}{" "}
+      {' '}
+      {project.projectName}{' '}
     </option>
-  ));
+  ))
   projectOptions.unshift(
     <option value="" key="" disabled>
       Select Project
-    </option>
-  );
+    </option>,
+  )
 
-  const validateForm = () => {
-    const result = {};
+  const validateForm = edittime => {
+    const result = {}
 
-    if (inputs.dateOfWork === "") {
-      result.dateOfWork = "Date is required";
+    if (inputs.dateOfWork === '') {
+      result.dateOfWork = 'Date is required'
     } else {
-      const date = moment(inputs.dateOfWork);
+      const date = moment(inputs.dateOfWork)
       if (!date.isValid()) {
-        result.dateOfWork = "Invalid date";
+        result.dateOfWork = 'Invalid date'
       }
     }
 
-    if (inputs.hours === "" && inputs.minutes === "") {
-      result.time = "Time is required";
+    if (inputs.hours === '' && inputs.minutes === '') {
+      result.time = 'Time is required'
     } else {
-      const hours = inputs.hours === "" ? 0 : inputs.hours * 1;
-      const minutes = inputs.minutes === "" ? 0 : inputs.minutes * 1;
+      const hours = inputs.hours === '' ? 0 : inputs.hours * 1
+      const minutes = inputs.minutes === '' ? 0 : inputs.minutes * 1
       if (!Number.isInteger(hours) || !Number.isInteger(minutes)) {
-        result.time = "Hours and minutes should be integers";
+        result.time = 'Hours and minutes should be integers'
       }
       if (hours < 0 || minutes < 0 || (hours === 0 && minutes === 0)) {
-        result.time = "Time should be greater than 0";
+        result.time = 'Time should be greater than 0'
       }
     }
 
-    if (inputs.projectId === "") {
-      result.projectId = "Project is required";
+    if (inputs.projectId === '') {
+      result.projectId = 'Project is required'
     }
 
     if (reminder.num_words < 10) {
-      openModal();
-      setReminder((reminder) => ({
+      openModal()
+      setReminder(reminder => ({
         ...reminder,
         remind:
-          "Please write a more detailed description of your work completed, write at least 1-2 sentences.",
-      }));
-      result.notes = "Description and reference link are required";
+          'Please write a more detailed description of your work completed, write at least 1-2 sentences.',
+      }))
+      result.notes = 'Description and reference link are required'
     }
 
     if (reminder.num_words >= 10 && !reminder.has_link) {
-      openModal();
-      setReminder((reminder) => ({
+      openModal()
+      setReminder(reminder => ({
         ...reminder,
         remind:
-          "Do you have a link to your Google Doc or other place to review this work? You should add it if you do.",
-      }));
-      result.notes = "Description and reference link are required";
+          'Do you have a link to your Google Doc or other place to review this work? You should add it if you do.',
+      }))
+      result.notes = 'Description and reference link are required'
     }
 
-    if (edit && reminder.edit_notice && reminder.edit_count < 4) {
-      openModal();
-      setReminder((reminder) => ({
+    if (edit && reminder.edit_notice && reminder.edit_count < 4 && edittime) {
+      openModal()
+      setReminder(reminder => ({
         ...reminder,
         remind:
-          "You are about to edit your time, if you do this your manager will be notified you’ve edited it. The system automatically tracks how many times you’ve edited your time and will issue blue squares if you edit it repeatedly. Please use the timer properly so your time is logged accurately.",
+          'You are about to edit your time, if you do this your manager will be notified you’ve edited it. The system automatically tracks how many times you’ve edited your time and will issue blue squares if you edit it repeatedly. Please use the timer properly so your time is logged accurately.',
         edit_notice: !reminder.edit_notice,
-      }));
+      }))
+      return false
     }
 
-    if (edit && reminder.edit_notice && reminder.edit_count == 4) {
-      openModal();
-      setReminder((reminder) => ({
+    if (edit && reminder.edit_notice && reminder.edit_count == 4 && edittime) {
+      openModal()
+      setReminder(reminder => ({
         ...reminder,
         remind:
-          "You’ve edited your time 3 times already as a member of the team, are you sure you want to edit it again? Editing your time more than 5 times in a calendar year will result in you receiving a blue square.",
+          'You’ve edited your time 3 times already as a member of the team, are you sure you want to edit it again? Editing your time more than 5 times in a calendar year will result in you receiving a blue square.',
         edit_notice: !reminder.edit_notice,
-      }));
+      }))
+      return false
     }
 
-    if (edit && reminder.edit_notice && reminder.edit_count == 5) {
-      openModal();
-      setReminder((reminder) => ({
+    if (edit && reminder.edit_notice && reminder.edit_count == 5 && edittime) {
+      openModal()
+      setReminder(reminder => ({
         ...reminder,
         remind:
-          "Heads up this is your fifth and final time being allowed to edit your time without receiving a blue square. Please use the timer properly from this point forward if you’d like to avoid receiving one.",
+          'Heads up this is your fifth and final time being allowed to edit your time without receiving a blue square. Please use the timer properly from this point forward if you’d like to avoid receiving one.',
         edit_notice: !reminder.edit_notice,
-      }));
+      }))
+      return false
     }
 
-    setErrors(result);
-    return _.isEmpty(result);
-  };
+    setErrors(result)
+    return _.isEmpty(result)
+  }
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = async event => {
     if (event) {
-      event.preventDefault();
+      event.preventDefault()
     }
 
-    if (!validateForm()) {
-      return;
+    const hours = inputs.hours === '' ? '0' : inputs.hours
+    const minutes = inputs.minutes === '' ? '0' : inputs.minutes
+
+    const edittime = edit && (data.hours != hours || data.minutes != minutes)
+
+    if (!validateForm(edittime)) {
+      return
     }
+    const timeEntry = {}
+    timeEntry.personId = userId
+    timeEntry.dateOfWork = inputs.dateOfWork
 
-    const timeEntry = {};
+    timeEntry.projectId = inputs.projectId
+    timeEntry.notes = inputs.notes
+    timeEntry.isTangible = inputs.isTangible.toString()
 
-    timeEntry.personId = userId;
-    timeEntry.dateOfWork = inputs.dateOfWork;
-
-    timeEntry.projectId = inputs.projectId;
-    timeEntry.notes = inputs.notes;
-    timeEntry.isTangible = inputs.isTangible.toString();
-    timeEntry.editCount = reminder.edit_count + 1;
-
-    const hours = inputs.hours === "" ? "0" : inputs.hours;
-    const minutes = inputs.minutes === "" ? "0" : inputs.minutes;
-
-    let status;
+    let status
     if (edit) {
-      timeEntry.hours = hours;
-      timeEntry.minutes = minutes;
-      if (!reminder.edit_notice) {
-        status = await dispatch(editTimeEntry(data._id, timeEntry));
+      if (edittime) {
+        timeEntry.editCount = reminder.edit_count + 1
+      }
+      timeEntry.hours = hours
+      timeEntry.minutes = minutes
+      if (!edittime || !reminder.notice) {
+        status = await dispatch(editTimeEntry(data._id, timeEntry))
       }
     } else {
-      timeEntry.timeSpent = `${hours}:${minutes}:00`;
-      status = await dispatch(postTimeEntry(timeEntry));
+      timeEntry.timeSpent = `${hours}:${minutes}:00`
+      status = await dispatch(postTimeEntry(timeEntry))
     }
-
+    // console.log('kkk')
     if (fromTimer) {
       if (status === 200) {
-        const timerStatus = await dispatch(stopTimer(userId));
+        const timerStatus = await dispatch(stopTimer(userId))
         if (timerStatus === 200 || timerStatus === 201) {
-          setInputs((inputs) => initialState);
-          setReminder((reminder) => initialReminder);
-          toggle();
+          setInputs(inputs => initialState)
+          setReminder(reminder => initialReminder)
+          toggle()
         }
-        history.push(`/timelog/${userId}`);
+        history.push(`/timelog/${userId}`)
       }
     } else if (!edit) {
-      setInputs((inputs) => initialState);
-      setReminder((reminder) => initialReminder);
-      toggle();
-    } else if (!reminder.edit_notice) {
-      setReminder((reminder) => ({
+      setInputs(inputs => initialState)
+      setReminder(reminder => initialReminder)
+      toggle()
+    } else if (!reminder.notice && edittime) {
+      setReminder(reminder => ({
         ...reminder,
         edit_count: reminder.edit_count + 1,
         edit_notice: !reminder.edit_notice,
-      }));
-      toggle();
+        // edittime: !reminder.edittime,
+      }))
+      toggle()
+    } else if (!edittime) {
+      // setReminder(reminder => initialReminder)
+      // console.log('kkkkkkkkk')
+      toggle()
     }
-  };
+  }
 
-  const handleInputChange = (event) => {
-    event.persist();
-    setInputs((inputs) => ({
+  const handleInputChange = event => {
+    event.persist()
+    setInputs(inputs => ({
       ...inputs,
       [event.target.name]: event.target.value,
-    }));
-  };
+    }))
+  }
 
   const handleEditorChange = (content, editor) => {
-    const { wordcount } = editor.plugins;
+    const { wordcount } = editor.plugins
 
-    setInputs((inputs) => ({ ...inputs, [editor.id]: content }));
-    setReminder((reminder) => ({
+    setInputs(inputs => ({ ...inputs, [editor.id]: content }))
+    setReminder(reminder => ({
       ...reminder,
       num_words: wordcount.body.getWordCount(),
-      has_link:
-        inputs.notes.indexOf("http://") > -1 ||
-        inputs.notes.indexOf("https://") > -1,
-    }));
-  };
+      has_link: inputs.notes.indexOf('http://') > -1 || inputs.notes.indexOf('https://') > -1,
+    }))
+  }
 
-  const handleCheckboxChange = (event) => {
-    event.persist();
-    setInputs((inputs) => ({
+  const handleCheckboxChange = event => {
+    event.persist()
+    setInputs(inputs => ({
       ...inputs,
       [event.target.name]: event.target.checked,
-    }));
-  };
+    }))
+  }
 
-  const clearForm = (event) => {
-    setInputs((inputs) => initialState);
-    setReminder((reminder) => initialReminder);
-    setErrors((errors) => ({}));
-  };
+  const clearForm = event => {
+    setInputs(inputs => initialState)
+    setReminder(reminder => initialReminder)
+    setErrors(errors => ({}))
+  }
 
-  const isAdmin =
-    useSelector((state) => state.auth.user.role) === "Administrator";
+  const isAdmin = useSelector(state => state.auth.user.role) === 'Administrator'
 
   return (
     <Modal isOpen={isOpen} toggle={toggle}>
       <ModalHeader toggle={toggle}>
-        {edit ? "Edit " : "Add "}
+        {edit ? 'Edit ' : 'Add '}
         Time Entry
       </ModalHeader>
       <ModalBody>
@@ -288,7 +296,7 @@ const TimeEntryForm = ({ userId, edit, data, isOpen, toggle, timer }) => {
                 disabled
               />
             )}
-            {"dateOfWork" in errors && (
+            {'dateOfWork' in errors && (
               <div className="text-danger">
                 <small>{errors.dateOfWork}</small>
               </div>
@@ -320,7 +328,7 @@ const TimeEntryForm = ({ userId, edit, data, isOpen, toggle, timer }) => {
                 />
               </Col>
             </Row>
-            {"time" in errors && (
+            {'time' in errors && (
               <div className="text-danger">
                 <small>{errors.time}</small>
               </div>
@@ -337,7 +345,7 @@ const TimeEntryForm = ({ userId, edit, data, isOpen, toggle, timer }) => {
             >
               {projectOptions}
             </Input>
-            {"projectId" in errors && (
+            {'projectId' in errors && (
               <div className="text-danger">
                 <small>{errors.projectId}</small>
               </div>
@@ -348,13 +356,13 @@ const TimeEntryForm = ({ userId, edit, data, isOpen, toggle, timer }) => {
             <Editor
               init={{
                 menubar: false,
-                placeholder: "Description and reference link",
+                placeholder: 'Description and reference link',
                 plugins:
-                  "advlist autolink autoresize lists link charmap table paste help wordcount",
+                  'advlist autolink autoresize lists link charmap table paste help wordcount',
                 toolbar:
-                  "bold italic underline link removeformat | bullist numlist outdent indent |\
+                  'bold italic underline link removeformat | bullist numlist outdent indent |\
                                     styleselect fontsizeselect | table| strikethrough forecolor backcolor |\
-                                    subscript superscript charmap  | help",
+                                    subscript superscript charmap  | help',
                 branding: false,
                 min_height: 180,
                 max_height: 300,
@@ -367,7 +375,7 @@ const TimeEntryForm = ({ userId, edit, data, isOpen, toggle, timer }) => {
               onEditorChange={handleEditorChange}
             />
 
-            {"notes" in errors && (
+            {'notes' in errors && (
               <div className="text-danger">
                 <small>{errors.notes}</small>
               </div>
@@ -383,13 +391,8 @@ const TimeEntryForm = ({ userId, edit, data, isOpen, toggle, timer }) => {
                   onChange={handleCheckboxChange}
                 />
               ) : (
-                <Input
-                  type="checkbox"
-                  name="isTangible"
-                  checked={inputs.isTangible}
-                  disabled
-                />
-              )}{" "}
+                <Input type="checkbox" name="isTangible" checked={inputs.isTangible} disabled />
+              )}{' '}
               Tangible
             </Label>
           </FormGroup>
@@ -406,22 +409,20 @@ const TimeEntryForm = ({ userId, edit, data, isOpen, toggle, timer }) => {
         </Modal>
       </ModalBody>
       <ModalFooter>
-        <small className="mr-auto text-secondary">
-          * All the fields are required
-        </small>
+        <small className="mr-auto text-secondary">* All the fields are required</small>
         <Button onClick={clearForm} color="danger">
-          {" "}
-          Clear Form{" "}
+          {' '}
+          Clear Form{' '}
         </Button>
         <Button onClick={handleSubmit} color="primary">
-          {edit ? "Save" : "Submit"}
+          {edit ? 'Save' : 'Submit'}
         </Button>
         <Button onClick={toggle} color="danger">
           Cancel
         </Button>
       </ModalFooter>
     </Modal>
-  );
-};
+  )
+}
 
-export default TimeEntryForm;
+export default TimeEntryForm
