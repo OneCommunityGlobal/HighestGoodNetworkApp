4710b31cfa12a28a1ced8d5e14f77409d366476e
Nithesh AN <getnithesh@gmail.com>
2020-09-08 23:39:21 +0530

    fix: azure migration

--- src/components/Teams/CreateNewTeamPopup.jsx
+++ src/components/Teams/CreateNewTeamPopup.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input } from 'reactstrap'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input, Alert } from 'reactstrap'
 import { useState } from 'react'
 
 const CreateNewTeamPopup = React.memo((props) => {
   const [newTeam, onNewName] = useState('')
   const closePopup = (e) => { props.onClose() };
+  const [isValidTeam, onValidation] = useState(true);
 
   return <Modal isOpen={props.open} toggle={closePopup}>
     <ModalHeader toggle={closePopup}>Create New Team</ModalHeader>
@@ -12,14 +13,27 @@ const CreateNewTeamPopup = React.memo((props) => {
       <label>Name of the Team</label>
 
       <Input placeholder='Please enter a new team name'
-
         value={newTeam}
-        onChange={(e) => { onNewName(e.target.value) }} required />
+        onChange={(e) => {
+          onValidation(true);
+          onNewName(e.target.value);
+        }} required />
+      {(isValidTeam === false) ? (
+        <Alert color="danger">
+          Please enter a team name.
+        </Alert>
+      ) : <></>}
 
     </ModalBody>
     <ModalFooter>
       <Button color="secondary" onClick={closePopup}>Close</Button>
-      <Button color="primary" onClick={(e) => props.onOkClick(newTeam)}>OK</Button>
+      <Button color="primary" onClick={(e) => {
+        if (newTeam !== "") {
+          props.onOkClick(newTeam)
+        } else {
+          onValidation(false);
+        }
+      }}>OK</Button>
     </ModalFooter>
   </Modal>
 });
--- src/utils/URL.js
+++ src/utils/URL.js
@@ -1,9 +1,13 @@
 /* eslint-disable import/prefer-default-export */
 
+import axios from 'axios';
+
 let APIEndpoint = process.env.REACT_APP_APIENDPOINT;
 if (!APIEndpoint) {
   // This is to resolve the issue in azure env variable
-  APIEndpoint = window.BASE_URL;
+  APIEndpoint = axios.get('../../config.json').then((data) => {
+    APIEndpoint = data.data.restapi;
+  });
 }
 
 export const ENDPOINTS = {
