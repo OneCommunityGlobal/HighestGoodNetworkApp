0f120c20d4f5cc1bc5af082ab8348c23ed9bd18c
Jun <junhao6503@gmail.com>
2020-09-17 20:15:52 -0700

    updated teamsview

--- src/components/UserProfile/Teamsview.jsx
+++ src/components/UserProfile/Teamsview.jsx
@@ -1,73 +1,63 @@
 import React, { useState } from 'react'
-import { Card, CardTitle, CardText, CardBody, Button,
-  Dropdown, DropdownMenu, DropdownItem, DropdownToggle} from 'reactstrap'
+import {
+  Card,
+  CardTitle,
+  CardText,
+  CardBody,
+  Button,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  DropdownToggle,
+} from 'reactstrap'
 import { EditorPropTypes } from '@tinymce/tinymce-react/lib/cjs/main/ts/components/EditorPropTypes'
 
-
-const Teamdataheader = () => {
-  return (<tr>
+const Teamdataheader = () => (
+  <tr>
     <th>Name</th>
-  </tr>)
-}
-const Teamtabledata = (props) => {
-  return (
-    <tr>
-      <td>
-        {props.teammembers.teamName} 
-        { props.edit && <button>delete</button>}
-      </td>
-    </tr>
-  )
-}
+  </tr>
+)
+const Teamtabledata = props => (
+  <tr>
+    <td>
+      {props.teammembers.teamName}
+      {props.edit && <button>delete</button>}
+    </td>
+  </tr>
+)
 
-const Teams = React.memo((props) => {
-  
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const toggle = () => setDropdownOpen(prevState => !prevState);
-
+const Teams = React.memo(props => {
+  const [dropdownOpen, setDropdownOpen] = useState(false)
+  const toggle = () => setDropdownOpen(prevState => !prevState)
 
   return (
     <Card>
-      <CardTitle>
-        Teams
-			</CardTitle>
-      
+      <CardTitle>Teams</CardTitle>
       <CardBody>
         <CardText>
-          <table border='1'>
-            {props.teamsdata.map(
-              team => {
-                return <Teamtabledata teammembers={team} edit={props.edit}/>
-              }
-            )}
+          <table border="1">
+            {props.teamsdata.map(team => (
+              <Teamtabledata teammembers={team} edit={props.edit} />
+            ))}
           </table>
         </CardText>
-        
-        {props.edit && 
-          <Button color='primary'>
-            Add a Team
-          </Button>
-        }
+        {props.edit && <Button color="primary">Add a Team</Button>}
       </CardBody>
-      {props.edit && 
+      {props.edit && (
         <CardBody>
           <Dropdown isOpen={dropdownOpen} toggle={toggle}>
-            <DropdownToggle caret>
-              TEAMS
-            </DropdownToggle>
+            <DropdownToggle caret>TEAMS</DropdownToggle>
             <DropdownMenu>
               <DropdownItem header>Header</DropdownItem>
-              {props.allTeams.map( team => 
+              {props.allTeams.map(team => (
                 <DropdownItem>{team.teamName}</DropdownItem>
-              )}
+              ))}
             </DropdownMenu>
           </Dropdown>
         </CardBody>
-      }
-
+      )}
     </Card>
   )
-}
-)
+})
 
-export default Teams;
+export default Teams
