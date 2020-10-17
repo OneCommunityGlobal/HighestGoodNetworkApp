9a4e4172d70e24ce0f55c24c150bd0beaa045fee
Chris Weilacker <chrisweilacker@gmail.com>
2020-10-17 10:15:14 -0700

    Updated Progress Bar for lowest number

--- src/utils/effortColors.js
+++ src/utils/effortColors.js
@@ -12,7 +12,7 @@ export const getcolor = effort => {
 
 export const getprogress = effort => {
 	let progress = 92
-	if (_.inRange(effort, 0, 5)) progress = 0
+	if (_.inRange(effort, 0, 5)) progress = 5
 	if (_.inRange(effort, 5, 10)) progress = 10
 	if (_.inRange(effort, 10, 20)) progress = 20
 	if (_.inRange(effort, 20, 30)) progress = 45
