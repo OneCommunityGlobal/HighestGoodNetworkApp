69752a8d4ad2bc5e8b7699f41e2350c41d5d80c9
yiqifeng <fengyiqi1717@126.com>
2020-12-12 18:20:00 -0800

    debug conflicts

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react'
 
 import {
@@ -319,55 +318,3 @@ const Dashboard = () => {
 }
 
 export default Dashboard
-=======
-import React from 'react';
-import { Alert, Row, Col, Container } from 'reactstrap';
-import Leaderboard from '../LeaderBoard';
-import WeeklySummaryModal from '../WeeklySummary/WeeklySummaryModal';
-import Badge from '../Badge';
-import TeamMemberTasks from '../TeamMemberTasks/TeamMemberTasks'
-import Timelog from '../Timelog/Timelog';
-import '../../App.css';
-import { connect } from 'react-redux';
-
-const Dashboard = props => (
-  <Container fluid>
-    <Row>
-      <Col sm={{ size: 12 }}>
-        <Alert color="info">
-          <b>Reminder</b>: Make sure to purge the cache or "hard" refresh the page in your browser
-          if you don's see the changes you had merged with the "development" branch. This message
-          will be removed before the site goes "live".
-        </Alert>
-      </Col>
-    </Row>
-    <Row>
-      <Col lg={{ size: 7 }}>&nbsp;</Col>
-      <Col lg={{ size: 5 }}>
-        <WeeklySummaryModal />
-      </Col>
-    </Row>
-    <Row>
-      <Col lg={{ size: 5 }} className="order-sm-12">
-        <Leaderboard />
-      </Col>
-      <Col lg={{ size: 7 }} className="left-col-dashboard order-sm-1">
-        <div className="my-2">
-          <Timelog/>
-        </div>
-        <div className="my-2">
-          <TeamMemberTasks />
-        </div>
-        <Badge userId={props.auth.user.userid} />
-      </Col>
-
-    </Row>
-  </Container >
-);
-
-const mapStateToProps = state => ({
-  auth: state.auth,
-})
-
-export default connect(mapStateToProps)(Dashboard);
->>>>>>> 523d5be89a02b9458b8830a9b8141f573373b74b
