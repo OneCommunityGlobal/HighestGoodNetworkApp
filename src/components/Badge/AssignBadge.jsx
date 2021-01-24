de3036387ec635b042b7218315556e0216624304
Wen Zhang <zhangwenfd@gmail.com>
2021-01-23 22:56:01 -0500

    auto suggestion for active user names

--- src/components/Badge/AssignBadge.jsx
+++ src/components/Badge/AssignBadge.jsx
@@ -1,16 +1,78 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Button, Form, FormGroup, Label, Input, FormText, Row, Col, FormFeedback, Modal, ModalHeader, ModalBody,
 } from 'reactstrap';
 import { connect } from 'react-redux';
 import AssignBadgePopup from './AssignBadgePopup';
 import { getUserToBeAssigned, assignBadges } from '../../actions/badgeManagement';
+import { getAllUserProfile } from '../../actions/userManagement';
+import Autosuggest from 'react-autosuggest';
+
 
 const AssignBadge = (props) => {
 
   const [isOpen, setOpen] = useState(false);
   const [first, setFirst] = useState('');
   const [last, setLast] = useState('');
+  const [firstSuggestions, setFirstSuggestions] = useState([]);
+  const [lastSuggestions, setLastSuggestions] = useState([]);
+
+  useEffect(() => {
+    props.getAllUserProfile();
+  }, [])
+
+  const activeUsers = props.allUserProfiles.filter(profile => profile.isActive === true);
+
+  const escapeRegexCharacters = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
+  const getSuggestions = (value) => {
+    const escapedValue = escapeRegexCharacters(value.trim());
+    const regex = new RegExp('^' + escapedValue, 'i');
+    return activeUsers.filter(user => regex.test(user.firstName) || regex.test(user.lastName));
+  }
+
+  const getSuggestionFirst = suggestion => suggestion.firstName;
+
+  const getSuggestionLast = suggestion => suggestion.lastName;
+
+  const renderSuggestion = (suggestion) => {
+    return (
+      <div>{suggestion.firstName} {suggestion.lastName}</div>
+    );
+  }
+
+  const onFirstChange = (event, { newValue }) => {
+    setFirst(newValue);
+  };
+
+  const onLastChange = (event, { newValue }) => {
+    setLast(newValue);
+  };
+
+  const onFirstSuggestionsFetchRequested = ({ value }) => {
+    setFirstSuggestions(getSuggestions(value));
+  };
+
+  const onFirstSuggestionsClearRequested = () => {
+    setFirstSuggestions([]);
+  };
+
+  const onFirstSuggestionSelected = (event, { suggestion }) => {
+    setLast(suggestion.lastName);
+  };
+
+  const onLastSuggestionsFetchRequested = ({ value }) => {
+    setLastSuggestions(getSuggestions(value));
+  };
+
+  const onLastSuggestionsClearRequested = () => {
+    setLastSuggestions([]);
+  };
+
+  const onLastSuggestionSelected = (event, { suggestion }) => {
+    setFirst(suggestion.firstName);
+  };
+
 
   const toggle = () => setOpen(isOpen => !isOpen);
 
@@ -29,6 +91,16 @@ const AssignBadge = (props) => {
     assignBadges(props.userAssigned, props.selectedBadges);
   }
 
+  const FirstInputProps = {
+    placeholder: "first name",
+    value: first,
+    onChange: onFirstChange
+  };
+  const LastInputProps = {
+    placeholder: "last name",
+    value: last,
+    onChange: onLastChange
+  };
 
   return (
     <Form style={{
@@ -40,7 +112,16 @@ const AssignBadge = (props) => {
           <Label>Name</Label>
         </Col>
         <Col md="4">
-          <FormGroup>
+          <Autosuggest
+            suggestions={firstSuggestions}
+            onSuggestionsFetchRequested={onFirstSuggestionsFetchRequested}
+            onSuggestionsClearRequested={onFirstSuggestionsClearRequested}
+            onSuggestionSelected={onFirstSuggestionSelected}
+            getSuggestionValue={getSuggestionFirst}
+            renderSuggestion={renderSuggestion}
+            inputProps={FirstInputProps}
+          />
+          {/* <FormGroup>
             <Input
               type="text"
               name="firstName"
@@ -48,10 +129,19 @@ const AssignBadge = (props) => {
               placeholder="First Name"
               onChange={(e) => setFirst(e.target.value.trim())}
             />
-          </FormGroup>
+          </FormGroup> */}
         </Col>
         <Col md="4">
-          <FormGroup>
+          <Autosuggest
+            suggestions={lastSuggestions}
+            onSuggestionsFetchRequested={onLastSuggestionsFetchRequested}
+            onSuggestionsClearRequested={onLastSuggestionsClearRequested}
+            onSuggestionSelected={onLastSuggestionSelected}
+            getSuggestionValue={getSuggestionLast}
+            renderSuggestion={renderSuggestion}
+            inputProps={LastInputProps}
+          />
+          {/* <FormGroup>
             <Input
               type="text"
               name="lastName"
@@ -59,7 +149,7 @@ const AssignBadge = (props) => {
               placeholder="Last Name"
               onChange={(e) => setLast(e.target.value.trim())}
             />
-          </FormGroup>
+          </FormGroup> */}
         </Col>
       </Row>
       <FormGroup>
@@ -80,11 +170,13 @@ const AssignBadge = (props) => {
 
 const mapStateToProps = state => ({
   selectedBadges: state.badge.selectedBadges,
-  userAssigned: state.badge.userAssigned
+  userAssigned: state.badge.userAssigned,
+  allUserProfiles: state.allUserProfiles.userProfiles
 });
 
 const mapDispatchToProps = dispatch => ({
   getUserToBeAssigned: (userName) => dispatch(getUserToBeAssigned(userName)),
+  getAllUserProfile: () => dispatch(getAllUserProfile())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(AssignBadge);
