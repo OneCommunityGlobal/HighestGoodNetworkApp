bb1629243afa166e41f56184c5d85750cd84ca81
msuarz <mikemps@gmail.com>
2021-05-03 17:12:02 -0400

    fixed reset password pop alert test

--- src/__tests__/UserManagement/ResetPasswordButton.test.js
+++ src/__tests__/UserManagement/ResetPasswordButton.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { screen, render, fireEvent } from '@testing-library/react';
+import {screen, render, fireEvent, waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import ResetPasswordButton from '../../components/UserManagement/ResetPasswordButton';
 import * as services from '../../services/userProfileService';
 import { userProfileMock } from '../mockStates';
-// jest.mock('../../services/userProfileService.js');
+import {toast} from "react-toastify";
+jest.mock('react-toastify');
 
 describe('reset password button ', () => {
   beforeEach(() => {
@@ -24,23 +25,26 @@ describe('reset password button ', () => {
       expect(screen.getByRole('dialog')).toBeInTheDocument();
     });
     it('should call resetPassword after the user click confirm on the modal', async () => {
-      // services.resetPassword = jest.fn(() => Promise.resolve());
       const spy = jest.spyOn(services, 'resetPassword').mockImplementation(() => Promise.resolve());
-      jest.spyOn(window, 'alert').mockImplementation(() => { });
+
       userEvent.click(screen.getByRole('button', { name: /reset password/i }));
       await userEvent.type(screen.getByLabelText(/new password/i), 'ABc@12345!', { allAtOnce: false });
       await userEvent.type(screen.getByLabelText(/confirm password/i), 'ABc@12345!', { allAtOnce: false });
       userEvent.click(screen.getAllByRole('button', { name: /reset password/i })[1]);
+
       expect(spy).toHaveBeenCalled();
     });
     it('should pop a alert after the reset is done', async () => {
       jest.spyOn(services, 'resetPassword').mockImplementation(() => Promise.resolve());
-      const spy = jest.spyOn(window, 'alert').mockImplementation(() => { });
+
       userEvent.click(screen.getByRole('button', { name: /reset password/i }));
       await userEvent.type(screen.getByLabelText(/new password/i), 'ABc@12345!', { allAtOnce: false });
       await userEvent.type(screen.getByLabelText(/confirm password/i), 'ABc@12345!', { allAtOnce: false });
       userEvent.click(screen.getAllByRole('button', { name: /reset password/i })[1]);
-      expect(spy).toHaveBeenCalled();
+
+      await waitFor(() => {
+        expect(toast.success).toHaveBeenCalledWith('Password reset action has been completed.');
+      });
     });
   });
 });
