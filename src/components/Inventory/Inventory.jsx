8f4b0571c62c5d85492849939af1816f6d9daf42
Chris Weilacker <chrisweilacker@gmail.com>
2021-06-24 14:10:13 -0700

    Added Inventory Routes

--- src/components/Inventory/Inventory.jsx
+++ src/components/Inventory/Inventory.jsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { Modal, ModalHeader, ModalBody } from 'reactstrap';
 
 const Inventory = (props) => {
+  return(
+    <React.Fragment>
+      
+    </React.Fragment>)
 }
 
 export default Inventory;
