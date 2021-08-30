d806f3fd136b40e9348349b46d0088f9c6c1e403
Cameron White <cameron.white@ufl.edu>
2021-08-30 05:31:51 -0400

    Fixed the active / inactive toggle on the user profile screen

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -31,7 +31,6 @@ import BlueSqaureLayout from './BlueSqaureLayout'
 import TabToolTips from './ToolTips/TabToolTips'
 import BasicToolTips from './ToolTips/BasicTabTips'
 import ResetPasswordButton from '../UserManagement/ResetPasswordButton'
-import PauseAndResumeButton from '../UserManagement/PauseAndResumeButton'
 import Badges from './Badges'
 import TimeEntryEditHistory from './TimeEntryEditHistory.jsx';
 
@@ -599,10 +598,12 @@ const UserProfile = props => {
               <TabPane tabId="1">
                 <BasicInformationTab
                   userProfile={userProfile}
+                  getUserProfile={props.getUserProfile}
                   isUserAdmin={isUserAdmin}
                   isUserSelf={isUserSelf}
                   handleUserProfile={handleUserProfile}
                   formValid={formValid}
+                  setShouldRefresh={setShouldRefresh}
                 />
               </TabPane>
               <TabPane tabId="2">
@@ -663,7 +664,6 @@ const UserProfile = props => {
                 </Link>
               </div>
             )}
-            <PauseAndResumeButton className="mr-1" user={userProfile} isBigBtn={true} />
             <Link
               color="primary"
               to={`/userprofile/${userProfile._id}`}
