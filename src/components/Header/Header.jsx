99f53fadd93f01355dbf0c03bc9a3ab69ba5e620
Cameron White <cameron.white@ufl.edu>
2021-08-31 12:03:40 -0400

    Changed the default profile picture

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -159,8 +159,7 @@ export class Header extends React.Component {
                 <NavItem>
                   <NavLink tag={Link} to={`/userprofile/${user.userid}`}>
                     <img
-                      src={`${profilePic ||
-                        "https://www.pngfind.com/pngs/m/676-6764065_default-profile-picture-transparent-hd-png-download.png"}`}
+                      src={`${profilePic || '/pfp-default-header.png'}`}
                       alt=""
                       height="35"
                       width="40"
