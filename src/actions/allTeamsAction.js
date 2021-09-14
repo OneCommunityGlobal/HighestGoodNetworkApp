5e367b637f2297bdeaf1a19b161f76a3c4ca66ad
Cameron White <cameron@cameronwhite.io>
2021-09-14 09:52:08 -0400

    removed console log

--- src/actions/allTeamsAction.js
+++ src/actions/allTeamsAction.js
@@ -110,7 +110,6 @@ export const getAllUserTeams = () => {
   return async (dispatch) => {
     userTeamsPromise.then((res) => {
       dispatch(teamMembersFectchACtion(res.data));
-      console.log("getAllUserTeams: res:", res.data)
     }).catch(() => {
       dispatch(teamMembersFectchACtion(undefined));
     });
