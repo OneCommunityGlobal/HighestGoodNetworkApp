b8379e4ec5c397c561072c94d8459d9fd76912b9
Miguel Fernandes <migamado01@gmail.com>
2022-03-30 12:56:43 -0400

    Added logout modal

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -2,12 +2,41 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { logoutUser } from '../../actions/authActions';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-export const Logout = () => {
+export const Logout = (props) => {
   const dispatch = useDispatch();
-  if (window.confirm("Don't forget to log your time before logout!")) {
+
+  const closePopup = () => {
+    props.setLogoutPopup(false);
+  };
+
+  const onLogout = () => {
+    closePopup();
     dispatch(logoutUser());
     return <Redirect to="/login" auth={false} />;
-  }
-  return null;
+  };
+
+  return (
+    <Modal isOpen={props.open} toggle={closePopup}>
+      <ModalHeader toggle={closePopup}>Are you sure you want to logout?</ModalHeader>
+      <ModalBody>
+        <div>
+          <p>
+          Don't forget to log your time before logout!
+          </p>
+        </div>
+      </ModalBody>
+      <ModalFooter>
+        <Button color="danger" onClick={onLogout}>
+          Logout
+        </Button>{' '}
+        <Button color="primary" onClick={closePopup}>
+          Cancel
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
 };
+
+// export default Logout;
