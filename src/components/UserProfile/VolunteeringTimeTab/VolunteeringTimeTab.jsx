abc95209930b5c0c24ff1a9f6112384fcf2d5f9b
Yiyun Tan <yiyuntan.cs@gmail.com>
2022-04-29 15:08:33 -0600

    fix total xxx hours shown

--- src/components/UserProfile/VolunteeringTimeTab/VolunteeringTimeTab.jsx
+++ src/components/UserProfile/VolunteeringTimeTab/VolunteeringTimeTab.jsx
@@ -102,45 +102,96 @@ const WeeklyCommitedHours = (props) => {
   );
 };
 
-const TotalTangibleHours = (props) => {
-  const { userProfile } = props;
-  const [totalTangibleHours, setTotalTangibleHours] = useState('loading...');
-  useEffect(() => {
-    const startOfWeek = props.userProfile.createdDate.slice(0,10);
-    const endOfWeek = moment().tz('America/Los_Angeles').endOf('week').format('YYYY-MM-DD');
-    axios
-      .get(ENDPOINTS.TIME_ENTRIES_PERIOD(userProfile._id, startOfWeek, endOfWeek))
-      .then(res => {
-        let output = 0;
-        for (let i = 0; i < res.data.length; i++) {
-          const timeEntry = res.data[i];
-          if (timeEntry.isTangible === true) {
-            output += parseFloat(timeEntry.hours) + parseFloat(timeEntry.minutes) / 60;
-          }
-        }
-        setTotalTangibleHours(output.toFixed(2));
-      })
-      .catch((err) => {});
-  }, []);
+// const TotalTangibleHours = (props) => {
+//   const { userProfile } = props;
+//   const [totalTangibleHours, setTotalTangibleHours] = useState('loading...');
 
-  if (!props.isUserAdmin) {
-    return <p>{totalTangibleHours}</p>;
-  }
-  return (
-    <Input
-      type="number"
-      name="totalTangibleHours"
-      id="totalTangibleHours"
-      value={totalTangibleHours}
-      onChange={(e) => {
-        props.setUserProfile({ ...props.userProfile, totalTangibleHrs: e.target.value });
-        props.setChanged(true);
-      }}
-      placeholder="Total Tangible Time Logged"
-      invalid={!props.isUserAdmin}
-    />
-  );
-};
+//   const [totalTangibleHousingHours, setTotalTangibleHousingHours] = useState(0);
+//   const [totalTangibleFoodHours, setTotalTangibleFoodHours] = useState(0);
+//   const [totalTangibleEducationHours, setTotalTangibleEducationHours] = useState(0);
+//   const [totalTangibleSocietyHours, setTotalTangibleSocietyHours] = useState(0);
+//   const [totalTangibleEnergyHours, setTotalTangibleEnergyHours] = useState(0);
+//   const [totalTangibleUnassignedHours, setTotalTangibleUnassignedHours] = useState(0);
+
+//   useEffect(() => {
+//     const startOfWeek = props.userProfile.createdDate.slice(0,10);
+//     const endOfWeek = moment().tz('America/Los_Angeles').endOf('week').format('YYYY-MM-DD');
+//     axios
+//       .get(ENDPOINTS.TIME_ENTRIES_PERIOD(userProfile._id, startOfWeek, endOfWeek))
+//       .then(res => {
+//         console.log("all time entries are: ", res);
+
+//         let output = 0;
+//         let housingOutput = 0;
+//         let foodOutput = 0;
+//         let educationOutput = 0;
+//         let societyOutput = 0;
+//         let energyOutput = 0;
+//         let unassignedOutput = 0;
+
+//         for (let i = 0; i < res.data.length; i++) {
+//           const timeEntry = res.data[i];
+//           if (timeEntry.isTangible === true) {
+//             output += parseFloat(timeEntry.hours) + parseFloat(timeEntry.minutes) / 60;
+
+//             // start counting the different categories time
+//             console.log("start counting different categories time")
+//             const projectId = timeEntry.projectId;
+//             console.log("this projectId is: ", projectId)
+
+//             let category = "unassigned";
+//             axios.
+//               get(ENDPOINTS.PROJECT_BY_ID(projectId))
+//               .then(project => {
+//                 // console.log("project is: ", project)
+//                 category = project.data["category"];
+//                 console.log("this project's category is: ", category)
+
+//                 if (category === "Housing") {
+//                   housingOutput += parseFloat(timeEntry.hours) + parseFloat(timeEntry.minutes) / 60;
+//                   setTotalTangibleHousingHours(housingOutput.toFixed(2));
+//                 } else if (category === "Food") {
+//                   foodOutput += parseFloat(timeEntry.hours) + parseFloat(timeEntry.minutes) / 60;
+//                   setTotalTangibleFoodHours(foodOutput.toFixed(2));
+//                 }  else if (category === "Education") {
+//                   educationOutput += parseFloat(timeEntry.hours) + parseFloat(timeEntry.minutes) / 60;
+//                   setTotalTangibleEducationHours(educationOutput.toFixed(2));
+//                 }  else if (category === "Society") {
+//                   societyOutput += parseFloat(timeEntry.hours) + parseFloat(timeEntry.minutes) / 60;
+//                   setTotalTangibleSocietyHours(societyOutput.toFixed(2));
+//                 }  else if (category === "Energy") {
+//                   energyOutput += parseFloat(timeEntry.hours) + parseFloat(timeEntry.minutes) / 60;
+//                   setTotalTangibleEnergyHours(energyOutput.toFixed(2));
+//                 } else {
+//                   unassignedOutput += parseFloat(timeEntry.hours) + parseFloat(timeEntry.minutes) / 60;
+//                   setTotalTangibleUnassignedHours(unassignedOutput.toFixed(2));
+//                 }
+//               });
+//           }
+//         }
+//         setTotalTangibleHours(output.toFixed(2));
+//       })
+//       .catch((err) => {});
+//   }, []);
+
+//   if (!props.isUserAdmin) {
+//     return <p>{totalTangibleHours}</p>;
+//   }
+//   return (
+//     <Input
+//       type="number"
+//       name="totalTangibleHours"
+//       id="totalTangibleHours"
+//       value={totalTangibleHours}
+//       onChange={(e) => {
+//         props.setUserProfile({ ...props.userProfile, totalTangibleHrs: e.target.value });
+//         props.setChanged(true);
+//       }}
+//       placeholder="Total Tangible Time Logged"
+//       invalid={!props.isUserAdmin}
+//     />
+//   );
+// };
 
 
 /**
@@ -153,6 +204,7 @@ const TotalTangibleHours = (props) => {
  * @returns
  */
 const ViewTab = (props) => {
+  // console.log("ViewTab props: ", props);
   const { userProfile, setUserProfile, setChanged, isUserAdmin, handleUserProfile } = props;
 
   const [totalTangibleHoursThisWeek, setTotalTangibleHoursThisWeek] = useState('Loading...');
@@ -173,6 +225,81 @@ const ViewTab = (props) => {
       })
       .catch((err) => {});
   }, []);
+
+  const [totalTangibleHours, setTotalTangibleHours] = useState('loading...');
+
+  const [totalTangibleHousingHours, setTotalTangibleHousingHours] = useState(0);
+  const [totalTangibleFoodHours, setTotalTangibleFoodHours] = useState(0);
+  const [totalTangibleEducationHours, setTotalTangibleEducationHours] = useState(0);
+  const [totalTangibleSocietyHours, setTotalTangibleSocietyHours] = useState(0);
+  const [totalTangibleEnergyHours, setTotalTangibleEnergyHours] = useState(0);
+  const [totalTangibleUnassignedHours, setTotalTangibleUnassignedHours] = useState(0);
+
+  useEffect(() => {
+    const startOfWeek = props.userProfile.createdDate.length > 0 ? (
+                          props.userProfile.createdDate.slice(0,10)
+                        ) : (
+                          moment().tz('America/Los_Angeles').startOf('week').format('YYYY-MM-DD')
+                        );
+    const endOfWeek = moment().tz('America/Los_Angeles').endOf('week').format('YYYY-MM-DD');
+    axios
+      .get(ENDPOINTS.TIME_ENTRIES_PERIOD(userProfile._id, startOfWeek, endOfWeek))
+      .then(res => {
+        // console.log("all time entries are: ", res);
+
+        let output = 0;
+        let housingOutput = 0;
+        let foodOutput = 0;
+        let educationOutput = 0;
+        let societyOutput = 0;
+        let energyOutput = 0;
+        let unassignedOutput = 0;
+
+        for (let i = 0; i < res.data.length; i++) {
+          const timeEntry = res.data[i];
+          if (timeEntry.isTangible === true) {
+            output += parseFloat(timeEntry.hours) + parseFloat(timeEntry.minutes) / 60;
+
+            // start counting the different categories time
+            // console.log("start counting different categories time")
+            const projectId = timeEntry.projectId;
+            // console.log("this projectId is: ", projectId)
+
+            let category = "unassigned";
+            axios.
+              get(ENDPOINTS.PROJECT_BY_ID(projectId))
+              .then(project => {
+                // console.log("project is: ", project)
+                category = project.data["category"];
+                // console.log("this project's category is: ", category)
+
+                if (category === "Housing") {
+                  housingOutput += parseFloat(timeEntry.hours) + parseFloat(timeEntry.minutes) / 60;
+                  setTotalTangibleHousingHours(housingOutput.toFixed(2));
+                } else if (category === "Food") {
+                  foodOutput += parseFloat(timeEntry.hours) + parseFloat(timeEntry.minutes) / 60;
+                  setTotalTangibleFoodHours(foodOutput.toFixed(2));
+                }  else if (category === "Education") {
+                  educationOutput += parseFloat(timeEntry.hours) + parseFloat(timeEntry.minutes) / 60;
+                  setTotalTangibleEducationHours(educationOutput.toFixed(2));
+                }  else if (category === "Society") {
+                  societyOutput += parseFloat(timeEntry.hours) + parseFloat(timeEntry.minutes) / 60;
+                  setTotalTangibleSocietyHours(societyOutput.toFixed(2));
+                }  else if (category === "Energy") {
+                  energyOutput += parseFloat(timeEntry.hours) + parseFloat(timeEntry.minutes) / 60;
+                  setTotalTangibleEnergyHours(energyOutput.toFixed(2));
+                } else {
+                  unassignedOutput += parseFloat(timeEntry.hours) + parseFloat(timeEntry.minutes) / 60;
+                  setTotalTangibleUnassignedHours(unassignedOutput.toFixed(2));
+                }
+              });
+          }
+        }
+        setTotalTangibleHours(output.toFixed(2));
+      })
+      .catch((err) => {});
+  }, []);
+
 
   return (
     <div data-testid="volunteering-time-tab">
@@ -245,16 +372,167 @@ const ViewTab = (props) => {
           <Label>Total Tangible Hours </Label>
         </Col>
         <Col md="6">
-          <TotalTangibleHours
-            isUserAdmin={isUserAdmin}
-            userProfile={userProfile}
-            setUserProfile={setUserProfile}
-            setChanged={setChanged}
-          />
+          <p>{totalTangibleHours}</p>
         </Col>
       </Row>
 
-      {props?.userProfile?.hoursByCategory
+      <Row>
+        <Col md="6">
+          <Label>Total Tangible Housing Hours</Label>
+        </Col>
+        <Col md="6">
+          {props.isUserAdmin ? (
+            <input
+              type='number'
+              id='totalTangibleHousingHours'
+              value={totalTangibleHousingHours}
+              onChange={(e) => {
+                setUserProfile({
+                  ...props.userProfile,
+                  hoursByCategory: {
+                    ...props.userProfile.hoursByCategory,
+                    ["housing"]: e.target.value,
+                  },
+                });
+              }}
+            />
+          ) : (
+          <p>{totalTangibleHousingHours}</p>
+          )}
+        </Col>
+      </Row>
+
+      <Row>
+        <Col md="6">
+          <Label>Total Tangible Food Hours</Label>
+        </Col>
+        <Col md="6">
+          {props.isUserAdmin ? (
+            <input
+              type='number'
+              id='totalTangibleFoodHours'
+              value={totalTangibleFoodHours}
+              onChange={(e) => {
+                setUserProfile({
+                  ...props.userProfile,
+                  hoursByCategory: {
+                    ...props.userProfile.hoursByCategory,
+                    ["food"]: e.target.value,
+                  },
+                });
+              }}
+            />
+          ) : (
+          <p>{totalTangibleFoodHours}</p>
+          )}
+        </Col>
+      </Row>
+
+      <Row>
+        <Col md="6">
+          <Label>Total Tangible Education Hours</Label>
+        </Col>
+        <Col md="6">
+          {props.isUserAdmin ? (
+            <input
+              type='number'
+              id='totalTangibleEducationHours'
+              value={totalTangibleEducationHours}
+              onChange={(e) => {
+                setUserProfile({
+                  ...props.userProfile,
+                  hoursByCategory: {
+                    ...props.userProfile.hoursByCategory,
+                    ["education"]: e.target.value,
+                  },
+                });
+              }}
+            />
+          ) : (
+          <p>{totalTangibleEducationHours}</p>
+          )}
+        </Col>
+      </Row>
+
+      <Row>
+        <Col md="6">
+          <Label>Total Tangible Society Hours</Label>
+        </Col>
+        <Col md="6">
+          {props.isUserAdmin ? (
+            <input
+              type='number'
+              id='totalTangibleSocietyHours'
+              value={totalTangibleSocietyHours}
+              onChange={(e) => {
+                setUserProfile({
+                  ...props.userProfile,
+                  hoursByCategory: {
+                    ...props.userProfile.hoursByCategory,
+                    ["society"]: e.target.value,
+                  },
+                });
+              }}
+            />
+          ) : (
+          <p>{totalTangibleSocietyHours}</p>
+          )}
+        </Col>
+      </Row>
+
+      <Row>
+        <Col md="6">
+          <Label>Total Tangible Energy Hours</Label>
+        </Col>
+        <Col md="6">
+          {props.isUserAdmin ? (
+            <input
+              type='number'
+              id='totalTangibleEnergyHours'
+              value={totalTangibleEnergyHours}
+              onChange={(e) => {
+                setUserProfile({
+                  ...props.userProfile,
+                  hoursByCategory: {
+                    ...props.userProfile.hoursByCategory,
+                    ["energy"]: e.target.value,
+                  },
+                });
+              }}
+            />
+          ) : (
+          <p>{totalTangibleEnergyHours}</p>
+          )}
+        </Col>
+      </Row>
+
+      <Row>
+        <Col md="6">
+          <Label>Total Unassgined Category Hours</Label>
+        </Col>
+        <Col md="6">
+          {props.isUserAdmin ? (
+            <input
+              type='number'
+              id='totalTangibleUnassignedHours'
+              value={totalTangibleUnassignedHours}
+              onChange={(e) => {
+                setUserProfile({
+                  ...props.userProfile,
+                  hoursByCategory: {
+                    ...props.userProfile.hoursByCategory,
+                    ["unassigned"]: e.target.value,
+                  },
+                });
+              }}
+            />
+          ) : (
+          <p>{totalTangibleUnassignedHours}</p>
+          )}
+        </Col>
+      </Row>
+
+      {/* {props?.userProfile?.hoursByCategory
         ? Object.keys(props.userProfile.hoursByCategory).map((key) => (
             <React.Fragment key={'hours-by-category-' + key}>
               <Row>
@@ -291,7 +569,7 @@ const ViewTab = (props) => {
               </Row>
             </React.Fragment>
           ))
-        : []}
+        : []} */}
     </div>
   );
 };
