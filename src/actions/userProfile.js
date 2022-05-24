dececd34a976a90dc97aaf6369e341246b94c641
Miguel Fernandes <migamado01@gmail.com>
2022-05-24 19:18:32 -0400

    Fixed endpoint to get tasks by userid

--- src/actions/userProfile.js
+++ src/actions/userProfile.js
@@ -27,7 +27,7 @@ export const getUserProfile = (userId) => {
 };
 
 export const getUserTask = (userId) => {
-  const url = ENDPOINTS.TASKS_BY_userID(userId);
+  const url = ENDPOINTS.TASKS_BY_USERID(userId);
   return async (dispatch) => {
     const res = await axios.get(url).catch((error) => {
       if (error.status === 401) {
