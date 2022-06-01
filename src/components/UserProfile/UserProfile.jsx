839281aa80f11a96080f343db79283e9744eb4ab
Steven Wang <sywang0213@gmail.com>
2022-05-31 18:25:21 -0700

    change three buttons 'update password', 'cancel', 'save change' display logic

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -331,15 +331,13 @@ const UserProfile = props => {
   // const isUserAdmin = requestorRole === 'Administrator';
   // const canEdit = hasPermission(requestorRole, 'editUserProfile') || isUserSelf;
   let canEdit;
-  if(userProfile.role !== 'Owner'){
+  if (userProfile.role !== 'Owner') {
     canEdit = hasPermission(requestorRole, 'editUserProfile') || isUserSelf;
   } else {
     canEdit = hasPermission(requestorRole, 'addDeleteEditOwners') || isUserSelf;
   }
 
-
   return (
-
     <div>
       {showModal && (
         <UserProfileModal
@@ -596,7 +594,7 @@ const UserProfile = props => {
             {hasPermission(requestorRole, 'resetPasswordOthers') && canEdit && !isUserSelf && (
               <ResetPasswordButton className="mr-1" user={userProfile} />
             )}
-            {isUserSelf && (
+            {isUserSelf && (activeTab == '1' || hasPermission(requestorRole, 'editUserProfile')) && (
               <div className="profileEditButtonContainer">
                 <Link to={`/updatepassword/${userProfile._id}`}>
                   <Button className="mr-1" color="primary">
@@ -606,23 +604,25 @@ const UserProfile = props => {
                 </Link>
               </div>
             )}
-            {canEdit && (
+            {isUserSelf && (activeTab == '1' || hasPermission(requestorRole, 'editUserProfile')) && (
               <>
-              <span
-                onClick={() => {
-                  setUserProfile(originalUserProfile);
-                  setChanged(false);
-                }}
-                className="btn btn-outline-danger mr-1"
-              >
-                Cancel
-              </span>      
-              <SaveButton
-                className="mr-1"
-                handleSubmit={handleSubmit}
-                disabled={!formValid.firstName || !formValid.lastName || !formValid.email || !changed}
-                userProfile={userProfile}
-              />
+                <span
+                  onClick={() => {
+                    setUserProfile(originalUserProfile);
+                    setChanged(false);
+                  }}
+                  className="btn btn-outline-danger mr-1"
+                >
+                  Cancel
+                </span>
+                <SaveButton
+                  className="mr-1"
+                  handleSubmit={handleSubmit}
+                  disabled={
+                    !formValid.firstName || !formValid.lastName || !formValid.email || !changed
+                  }
+                  userProfile={userProfile}
+                />
               </>
             )}
           </Col>
