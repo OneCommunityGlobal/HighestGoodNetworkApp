f142cc0e484a2410bf4c30f8773fcf5a119ad324
vera.timohina <vera.timohina@skbkontur.ru>
2022-06-25 23:50:02 +0500

    moved TeamReport to separate directory and used ReportHeader in there

--- src/routes.js
+++ src/routes.js
@@ -25,7 +25,7 @@ import { UserRole } from './utils/enums';
 import ForgotPassword from './components/Login/ForgotPassword';
 import { PeopleReport } from './components/Reports/PeopleReport';
 import { ProjectReport } from './components/Reports/ProjectReport';
-import TeamReport from './components/Reports/TeamReport';
+import { TeamReport } from './components/Reports/TeamReport';
 import Inventory from './components/Inventory';
 import BadgeManagement from './components/Badge/BadgeManagement';
 import AutoUpdate from 'components/AutoUpdate';
