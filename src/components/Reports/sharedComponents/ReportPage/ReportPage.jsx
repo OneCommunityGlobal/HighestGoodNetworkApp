8ef3b8a65438f411e19736310df99eddb0d0d643
vera.timohina <vera.timohina@skbkontur.ru>
2022-07-01 03:36:17 +0500

    added ReportPage component in order to use it on every report page

---
+++ src/components/Reports/sharedComponents/ReportPage/ReportPage.jsx
@@ -0,0 +1,9 @@
+import { ReportHeader } from "../ReportHeader";
+import "./ReportPage.css";
+
+export const ReportPage = ({ children, renderProfile }) => (
+  <section className='report-page-wrapper'>
+    <div className='report-page-profile'>{renderProfile()}</div>
+    <div className='report-page-content'>{children}</div>
+  </section>
+);
