f0a9d4bb4ade2bdcb2bddcc162c00a1d65ccfae4
Eiki Kan <eikikan89@gmail.com>
2022-07-22 01:05:27 -0400

    Move sorting to reducer

--- src/components/TaskEditSuggestions/reducer.js
+++ src/components/TaskEditSuggestions/reducer.js
@@ -236,6 +236,8 @@ const taskEditSuggestionsInitial = [
 const initialState = {
   isLoading: false,
   taskEditSuggestions: taskEditSuggestionsInitial, // TODO: change to []
+  userSortDirection: null,
+  dateSuggestedSortDirection: "desc"
 };
 
 export const taskEditSuggestionsReducer = (state = initialState, action) => {
@@ -243,10 +245,38 @@ export const taskEditSuggestionsReducer = (state = initialState, action) => {
     case "FETCH_TASK_EDIT_SUGGESTIONS_BEGIN":
       return { ...state, isLoading: true } ;
     case "FETCH_TASK_EDIT_SUGGESTIONS_SUCESS":
-      return { ...state, isLoading: false, taskEditSuggestions: taskEditSuggestionsInitial };
+      return { ...state, isLoading: false, taskEditSuggestions: taskEditSuggestionsInitial.sort((tes1, tes2) => tes2.dateSuggested.localeCompare(tes1.dateSuggested)) };
       // return { ...state, isLoading: false, taskEditSuggestions: [...action.payload]};
     case "FETCH_TASK_EDIT_SUGGESTIONS_ERROR":
       return { ...state, isLoading: false };
+    case "TOGGLE_DATE_SUGGESTED_SORT_DIRECTION":
+      if (state.dateSuggestedSortDirection == null || state.dateSuggestedSortDirection == "asc") {
+        return { ...state,
+          dateSuggestedSortDirection: "desc", 
+          taskEditSuggestions: [...state.taskEditSuggestions].sort((tes1, tes2) => tes2.dateSuggested.localeCompare(tes1.dateSuggested)),
+          userSortDirection: null,
+         }
+      } else if (state.dateSuggestedSortDirection == "desc") {
+        return { ...state,
+          dateSuggestedSortDirection: "asc", 
+          taskEditSuggestions: [...state.taskEditSuggestions].sort((tes1, tes2) => tes1.dateSuggested.localeCompare(tes2.dateSuggested)),
+          userSortDirection: null,
+         }
+      }
+    case "TOGGLE_USER_SORT_DIRECTION":
+      if (state.userSortDirection == null || state.userSortDirection == "asc") {
+        return { ...state,
+          userSortDirection: "desc", 
+          taskEditSuggestions: [...state.taskEditSuggestions].sort((tes1, tes2) => tes2.user.localeCompare(tes1.user)),
+          dateSuggestedSortDirection: null,
+          }
+      } else if (state.userSortDirection == "desc") {
+        return { ...state,
+          userSortDirection: "asc", 
+          taskEditSuggestions: [...state.taskEditSuggestions].sort((tes1, tes2) => tes1.user.localeCompare(tes2.user)),
+          dateSuggestedSortDirection: null,
+          }
+      }
     default:
       return { ...state };
   }
--- src/components/TaskEditSuggestions/selectors.js
+++ src/components/TaskEditSuggestions/selectors.js
@@ -1,5 +1,7 @@
 export const getTaskEditSuggestionsData = (state) => {
   return ({
   isLoading: state.taskEditSuggestions.isLoading,
-  taskEditSuggestions: state.taskEditSuggestions.taskEditSuggestions
+  taskEditSuggestions: state.taskEditSuggestions.taskEditSuggestions,
+  userSortDirection: state.taskEditSuggestions.userSortDirection,
+  dateSuggestedSortDirection: state.taskEditSuggestions.dateSuggestedSortDirection,
 })};
