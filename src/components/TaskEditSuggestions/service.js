a3b2961b382030950e1c058829ecf0c75d9a5553
Eiki Kan <eikikan89@gmail.com>
2022-07-26 18:49:32 -0400

    Populate task edit suggestions from backend

--- src/components/TaskEditSuggestions/service.js
+++ src/components/TaskEditSuggestions/service.js
@@ -2,9 +2,16 @@ import axios from 'axios';
 import httpService from 'services/httpService';
 import { ENDPOINTS } from 'utils/URL';
 
+export const getTaskEditSuggestionsHTTP = async () => {
+  try {
+    return await axios.get(ENDPOINTS.TASK_EDIT_SUGGESTION());
+  } catch (error) {
+    console.log('Error on create task edit suggestion: ' + error);
+  } 
+};
+
 export const createTaskEditSuggestionHTTP = async ( taskId, userId, oldTask, updatedTask ) => {
   try {
-    console.log('createTaskEditSuggestionHTTP');
     const payload = { taskId, userId, oldTask, newTask: updatedTask };
     await axios.post(ENDPOINTS.TASK_EDIT_SUGGESTION(), payload);
   } catch (error) {
