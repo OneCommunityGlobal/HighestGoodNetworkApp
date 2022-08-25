fa2077b243618a2958c80ed0f30d95139614e668
vera.timohina <vera.timohina@kontur>
2022-08-26 04:22:09 +0500

    created a new look for the wbs table and the project members table

--- src/components/Reports/ProjectReport/ProjectReport.jsx
+++ src/components/Reports/ProjectReport/ProjectReport.jsx
@@ -3,16 +3,15 @@ import { Dropdown, DropdownButton } from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { Container } from 'reactstrap';
 import { FiBox } from 'react-icons/fi';
 import { getProjectDetail } from '../../../actions/project';
 import { fetchAllMembers } from '../../../actions/projectMembers';
 import { fetchAllWBS } from '../../../actions/wbs';
-import ProjectMemberTable from '../ProjectMemberTable';
+import { ProjectMemberTable } from '../ProjectMemberTable';
 import { ReportPage } from '../sharedComponents/ReportPage';
 import { Paging } from '../../common/Paging';
 import { TasksTable } from '../TasksTable';
-import WbsTable from '../WbsTable';
+import { WbsTable } from '../WbsTable';
 import { projectReportViewData } from './selectors';
 import '../../Teams/Team.css';
 import './ProjectReport.css';
@@ -34,7 +33,9 @@ export const ProjectReport = ({ match }) => {
     <ReportPage renderProfile={() => <ReportPage.ReportHeader isActive={isActive} avatar={<FiBox />} name={projectName} />}>
       <div className='wbs-and-members-blocks-wrapper'>
         <ReportPage.ReportBlock className='wbs-and-members-blocks'>
-          <WbsTable wbs={wbs} />
+          <Paging totalElementsCount={wbs.WBSItems.length}>
+            <WbsTable wbs={wbs} />
+          </Paging>
         </ReportPage.ReportBlock>
         <ReportPage.ReportBlock className='wbs-and-members-blocks'>
           <Paging totalElementsCount={projectMembers.members.length}>
