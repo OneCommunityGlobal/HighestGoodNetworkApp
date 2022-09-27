60f7076799f8cfc43dd991a0ed425c38c027ef12
Andrew Paulino <andrewpaulino46@gmail.com>
2022-09-26 23:51:17 -0700

    Feature: console.logs

--- src/services/timerService.js
+++ src/services/timerService.js
@@ -84,7 +84,7 @@ function initializeWebsocket(url) {
       sessionStorage.removeItem('working-session-timer');
 
       client.shouldReconnect = true;
-      client.terminate();
+      client = null;
       if (!reconnectOnClose) {
         return;
       }
