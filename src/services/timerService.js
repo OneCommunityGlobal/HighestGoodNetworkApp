5d146792739b138a9d7f7924f3255b50f08e48d6
Andrew Paulino <andrewpaulino46@gmail.com>
2022-09-26 23:17:19 -0700

    Feature: console.logs

--- src/services/timerService.js
+++ src/services/timerService.js
@@ -14,6 +14,19 @@ export const PAUSE_TIMER = ({
   intent: 'PAUSE_TIMER', isUserPaused, isApplicationPaused, saveTimerData,
 });
 
+function heartbeat() {
+  clearTimeout(this.pingTimeout);
+
+  // Use `WebSocket#terminate()`, which immediately destroys the connection,
+  // instead of `WebSocket#close()`, which waits for the close timer.
+  // Delay should be equal to the interval at which your server
+  // sends out pings plus a conservative assumption of the latency.
+  this.pingTimeout = setTimeout(() => {
+    this.terminate();
+  }, 30000 + 1000);
+}
+
+
 function initializeWebsocket(url) {
   let client = {};
   let isConnected = false;
@@ -42,6 +55,7 @@ function initializeWebsocket(url) {
 
     client.onopen = () => {
       isConnected = true;
+      // heartbeat();
       stateChangeListeners.forEach((fn) => fn(true));
     };
 
@@ -50,6 +64,7 @@ function initializeWebsocket(url) {
     // Close without reconnecting;
     client.close = () => {
       reconnectOnClose = false;
+      client.terminate();
       client.reconnectOnClose = false;
       close.call(client);
     };
