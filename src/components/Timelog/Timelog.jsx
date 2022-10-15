4049461f1eb47191625394150ef4baa5aa55b6e4
Kaung Htet Myat <programmingrelated1999@gmail.com>
2022-10-15 17:14:40 -0400

    added default tab as tasks

--- src/components/Timelog/Timelog.jsx
+++ src/components/Timelog/Timelog.jsx
@@ -46,6 +46,17 @@ import hasPermission from '../../utils/permissions';
 
 import TeamMemberTasks from 'components/TeamMemberTasks';
 
+const doesUserHaveTaskWithWBS = tasks => {
+  let check = false;
+  for (let task of tasks) {
+    if (task.wbsId) {
+      check = true;
+      break;
+    }
+  }
+  return check;
+};
+
 class Timelog extends Component {
   constructor(props) {
     super(props);
@@ -90,6 +101,15 @@ class Timelog extends Component {
     await this.props.getTimeEntriesForPeriod(userId, this.state.fromDate, this.state.toDate);
     await this.props.getUserProjects(userId);
     this.setState({ isTimeEntriesLoading: false });
+    const role = this.props.auth.user.role;
+    //if user role is admin, manager, mentor or owner then default tab is task. If user have any tasks assigned, default tab is task.
+    if (role === 'Administrator' || role === 'Manager' || role === "'Mentor'" || role === 'Owner') {
+      this.setState({ activeTab: 0 });
+    }
+    const UserHaveTask = doesUserHaveTaskWithWBS(this.userTask);
+    if (UserHaveTask) {
+      this.setState({ activeTab: 0 });
+    }
   }
 
   async componentDidUpdate(prevProps) {
@@ -553,7 +573,6 @@ class Timelog extends Component {
                           </FormGroup>
                         </Form>
                       )}
-
                       {this.state.activeTab === 0 ? (
                         <></>
                       ) : (
