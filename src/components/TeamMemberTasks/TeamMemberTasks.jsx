ebeff844c751e608d05106e0a4494c5595342e8b
Kaung Htet Myat <programmingrelated1999@gmail.com>
2022-10-28 15:04:14 -0400

    added some styles

--- src/components/TeamMemberTasks/TeamMemberTasks.jsx
+++ src/components/TeamMemberTasks/TeamMemberTasks.jsx
@@ -20,6 +20,22 @@ import { fetchAllManagingTeams } from '../../actions/team';
 import EffortBar from 'components/Timelog/EffortBar';
 import TimeEntry from 'components/Timelog/TimeEntry';
 
+const TaskButton = task => {
+  if (task.task.status === 'Complete') {
+    return (
+      <td>
+        <button className="complete-task-button">Complete</button>
+      </td>
+    );
+  } else {
+    return (
+      <td>
+        <button className="uncomplete-task-button">Mark as Done</button>
+      </td>
+    );
+  }
+};
+
 const TeamMemberTasks = props => {
   const [isTimeLogActive, setIsTimeLogActive] = useState(0);
   const [timeLogOpen, setTimeLogOpen] = useState(false);
@@ -212,6 +228,11 @@ const TeamMemberTasks = props => {
                                 </div>
                               </td>
                             )}
+                            {userRole === 'Administrator' ? (
+                              <td>
+                                <TaskButton task={task}></TaskButton>
+                              </td>
+                            ) : null}
                           </tr>
                         ),
                     )}
@@ -339,6 +360,7 @@ const TeamMemberTasks = props => {
                   <tr>
                     <th>Tasks(s)</th>
                     <th className="team-task-progress">Progress</th>
+                    {userRole === 'Administrator' ? <th>Status</th> : null}
                   </tr>
                 </thead>
               </Table>
