a89659a7b4fe759fa4e958a36bae2759f61f866f
Yiyun Tan <yiyuntan.cs@gmail.com>
2022-11-07 17:23:30 -0700

    update the sample data

--- src/components/TaskEditSuggestions/reducer.js
+++ src/components/TaskEditSuggestions/reducer.js
@@ -1,22 +1,3 @@
-
-const initialState = {
-  isLoading: false,
-  taskEditSuggestions: taskEditSuggestionsInitial, // TODO: change to []
-};
-
-export const taskEditSuggestionsReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "FETCH_TASK_EDIT_SUGGESTIONS_BEGIN":
-      return { ...state, isLoading: true} ;
-    case "FETCH_TASK_EDIT_SUGGESTIONS_SUCESS":
-      return { ...state, isLoading: false, taskEditSuggestions: [...action.payload]};
-    case "FETCH_TASK_EDIT_SUGGESTIONS_ERROR":
-      return { ...state, isLoading: false };
-    default:
-      return state;
-  }
-};
-
 // TODO: Delete
 const taskEditSuggestionsInitial = [
   {
@@ -251,3 +232,52 @@ const taskEditSuggestionsInitial = [
     }
   },
 ];
+
+const initialState = {
+  isLoading: false,
+  taskEditSuggestions: taskEditSuggestionsInitial, // TODO: change to []
+  userSortDirection: null,
+  dateSuggestedSortDirection: "desc"
+};
+
+export const taskEditSuggestionsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "FETCH_TASK_EDIT_SUGGESTIONS_BEGIN":
+      return { ...state, isLoading: true } ;
+    case "FETCH_TASK_EDIT_SUGGESTIONS_SUCESS":
+      return { ...state, isLoading: false, taskEditSuggestions: taskEditSuggestionsInitial.sort((tes1, tes2) => tes2.dateSuggested.localeCompare(tes1.dateSuggested)) };
+      // return { ...state, isLoading: false, taskEditSuggestions: [...action.payload]};
+    case "FETCH_TASK_EDIT_SUGGESTIONS_ERROR":
+      return { ...state, isLoading: false };
+    case "TOGGLE_DATE_SUGGESTED_SORT_DIRECTION":
+      if (state.dateSuggestedSortDirection == null || state.dateSuggestedSortDirection == "asc") {
+        return { ...state,
+          dateSuggestedSortDirection: "desc", 
+          taskEditSuggestions: [...state.taskEditSuggestions].sort((tes1, tes2) => tes2.dateSuggested.localeCompare(tes1.dateSuggested)),
+          userSortDirection: null,
+         }
+      } else if (state.dateSuggestedSortDirection == "desc") {
+        return { ...state,
+          dateSuggestedSortDirection: "asc", 
+          taskEditSuggestions: [...state.taskEditSuggestions].sort((tes1, tes2) => tes1.dateSuggested.localeCompare(tes2.dateSuggested)),
+          userSortDirection: null,
+         }
+      }
+    case "TOGGLE_USER_SORT_DIRECTION":
+      if (state.userSortDirection == null || state.userSortDirection == "asc") {
+        return { ...state,
+          userSortDirection: "desc", 
+          taskEditSuggestions: [...state.taskEditSuggestions].sort((tes1, tes2) => tes2.user.localeCompare(tes1.user)),
+          dateSuggestedSortDirection: null,
+          }
+      } else if (state.userSortDirection == "desc") {
+        return { ...state,
+          userSortDirection: "asc", 
+          taskEditSuggestions: [...state.taskEditSuggestions].sort((tes1, tes2) => tes1.user.localeCompare(tes2.user)),
+          dateSuggestedSortDirection: null,
+          }
+      }
+    default:
+      return { ...state };
+  }
+};
--- src/components/TaskEditSuggestions/selectors.js
+++ src/components/TaskEditSuggestions/selectors.js
@@ -1,4 +1,7 @@
-export const getTeamMemberTasksData = (state) => ({
-  isLoading: state.teamMemberTasks.isLoading,
-  usersWithTasks: state.teamMemberTasks.usersWithTasks
-});
+export const getTaskEditSuggestionsData = (state) => {
+  return ({
+  isLoading: state.taskEditSuggestions.isLoading,
+  taskEditSuggestions: state.taskEditSuggestions.taskEditSuggestions,
+  userSortDirection: state.taskEditSuggestions.userSortDirection,
+  dateSuggestedSortDirection: state.taskEditSuggestions.dateSuggestedSortDirection,
+})};
