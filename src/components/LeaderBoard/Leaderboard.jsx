1e8b98026f546af9d858c928b31c4619a2ba8c1e
Shaun Sullivan <shaunsully88@gmail.com>
2023-01-05 09:08:33 -0500

    Accepted incoming changes on BlueSquare and Leaderboard

--- src/components/LeaderBoard/Leaderboard.jsx
+++ src/components/LeaderBoard/Leaderboard.jsx
@@ -117,13 +117,8 @@ const LeaderBoard = ({
                 tangible and total hours they have completed. 
                 {/*The color and length of that bar
                 changes based on what percentage of the total committed hours for the week have been
-<<<<<<< HEAD
-                completed: &gt; 10%: Red, 10-50%: Orange, 50-60% hrs: Green, 60-70%: Blue, 70-80%:
-                Indigo, 80-100%: Violet, and More than 100%: Purple.
-=======
                 completed: 0-20%: Red, 20-40%: Orange, 40-60% hrs: Green, 60-80%: Blue, 80-100%:Indigo, 
                 and Equal or More than 100%: Purple.*/}
->>>>>>> 5deba340a91154510269bd1dee465e56dd351ed4
               </li>
               <li>
                 The red/green dot shows whether or not a person has completed their “tangible” hours
@@ -136,11 +131,7 @@ const LeaderBoard = ({
                 completed hours for the week rank you compared to other people on your team.
                 Clicking a person’s time bar will take you to the time log section on their/your
                 dashboard. This bar also changes color based on how many tangible hours you have
-<<<<<<< HEAD
-                completed: &gt; 5 hrs: Red, 5-10 hrs: Orange, 10-20 hrs: Green, 20-30 hrs: Blue, 30-40
-=======
                 completed: 0-5 hrs: Red, 5-10 hrs: Orange, 10-20 hrs: Green, 20-30 hrs: Blue, 30-40
->>>>>>> 5deba340a91154510269bd1dee465e56dd351ed4
                 hrs: Indigo, 40-50 hrs: Violet, and 50+ hrs: Purple
               </li>
               <li>Clicking a person’s name will lead to their/your profile page.</li>
