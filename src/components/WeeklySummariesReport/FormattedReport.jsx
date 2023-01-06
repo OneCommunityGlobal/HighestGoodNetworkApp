688aa540ea048f3c165350b2f182693c6f404be8
Aishwaryak01 <aishwarya.kalkundrikar@gmail.com>
2023-01-05 19:09:15 -0500

    Updated FormattedReport.jsx
    Updated FormattedReport.jsx  to add feature : make names click-to-visit-profile on weekly summaries reports page

--- src/components/WeeklySummariesReport/FormattedReport.jsx
+++ src/components/WeeklySummariesReport/FormattedReport.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import moment from 'moment';
 import 'moment-timezone';
 import ReactHtmlParser from 'react-html-parser';
+import { Link } from 'react-router-dom';
 
 const FormattedReport = ({ summaries, weekIndex }) => {
   const emails = [];
@@ -90,7 +91,10 @@ const FormattedReport = ({ summaries, weekIndex }) => {
             key={'summary-' + index}
           >
             <p>
-              <b>Name:</b> {summary.firstName} {summary.lastName}
+            <b>Name: </b>
+              <Link to={`/userProfile/${summary._id}`} title="View Profile">
+                {summary.firstName} {summary.lastName}
+              </Link>
             </p>
             <p>
               {' '}
