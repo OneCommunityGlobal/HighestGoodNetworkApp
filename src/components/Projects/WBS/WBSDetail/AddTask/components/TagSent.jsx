7afbdcc45b54556528cea2fb4de37a12e1803598
Pedro Elton <pedroelton@msn.com>
2023-02-03 11:03:16 +0900

    added a close icon to the badges. And changed the size of those icons with initials in it.

--- src/components/Projects/WBS/WBSDetail/AddTask/components/TagSent.jsx
+++ src/components/Projects/WBS/WBSDetail/AddTask/components/TagSent.jsx
@@ -2,12 +2,14 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
+import CancelIcon from '@mui/icons-material/Cancel';
 
 function TagSent({ titleName, removeTags, index }) {
   return (
     <li key={index} className="rounded-pill badge bg-primary text-wrap" onClick={removeTags}>
       <div className="text-white">
-        <span className="fs-6">{titleName}</span>
+        <small className="fs-6 mr-1">{titleName}</small>
+        <CancelIcon fontSize="small" />
       </div>
     </li>
   );
