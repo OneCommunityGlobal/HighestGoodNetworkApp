c43ecac26f2f29de5529032f1e4cd41fa6241921
Pedro Elton <pedroelton@msn.com>
2023-02-05 15:32:12 +0900

    Changed the MUI Icon to FontAswesome Icon due to incompatibility

--- src/components/Projects/WBS/WBSDetail/AddTask/components/TagSent.jsx
+++ src/components/Projects/WBS/WBSDetail/AddTask/components/TagSent.jsx
@@ -2,14 +2,15 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
-import CancelIcon from '@mui/icons-material/Cancel';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 function TagSent({ titleName, removeTags, index }) {
   return (
     <li key={index} className="rounded-pill badge bg-primary text-wrap" onClick={removeTags}>
       <div className="text-white">
         <small className="fs-6 mr-1">{titleName}</small>
-        <CancelIcon fontSize="small" />
+        <FontAwesomeIcon icon={faTimesCircle} />
       </div>
     </li>
   );
