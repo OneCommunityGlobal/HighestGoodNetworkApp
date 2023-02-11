176e1a7849545d0512932aa753c290eec1e31ec8
Johny Anastacio <johnyanastaciods@gmail.com>
2023-02-11 14:57:46 -0300

    Update SingleTask.js

--- src/components/Projects/WBS/SingleTask/SingleTask.js
+++ src/components/Projects/WBS/SingleTask/SingleTask.js
@@ -1,7 +1,5 @@
 
-import React from 'react';
 import { connect, useSelector } from 'react-redux';
-import { useState, useEffect } from 'react';
 import React, { useState, useEffect } from 'react';
 import ReactTooltip from 'react-tooltip';
 import axios from 'axios';
