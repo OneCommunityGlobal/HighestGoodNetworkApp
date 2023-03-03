ec43a6f94a5cc9e387784dcf27d4ca312ffeb7b6
Lee Sing Chan Yau <jukerhdchan@gmail.com>
2023-03-03 10:38:13 -0800

    Added imports.

--- src/components/TeamMemberTasks/TeamMemberTasks.jsx
+++ src/components/TeamMemberTasks/TeamMemberTasks.jsx
@@ -24,6 +24,8 @@ import { getAllUserProfile } from 'actions/userManagement';
 import TaskCompletedModal from './components/TaskCompletedModal';
 import { ENDPOINTS } from 'utils/URL';
 import axios from 'axios';
+import { fetchAllTasks } from 'actions/task';
+import { deleteSelectedTask } from './reducer';
 
 const TeamMemberTasks = props => {
   const [isTimeLogActive, setIsTimeLogActive] = useState(0);
