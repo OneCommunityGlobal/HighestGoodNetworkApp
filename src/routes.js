ab914b9cfde8803fcc9184f2a4d0f894f0042d5a
Harlley <harlleybastos@hotmail.com>
2023-03-17 14:52:59 -0300

    feat: create a backup for the new header

--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,7 @@ import Login from './components/Login';
 import ForcePasswordUpdate from './components/ForcePasswordUpdate';
 import ProtectedRoute from './components/common/ProtectedRoute';
 import UpdatePassword from './components/UpdatePassword';
-import Header from './components/Header';
+import NewHeader from './components/NewHeader';
 import Projects from './components/Projects';
 import Teams from './components/Teams/Teams';
 import UserManagement from './components/UserManagement';
@@ -38,7 +38,7 @@ import UserRoleTab from 'components/PermissionsManagement/UserRoleTab';
 
 export default (
   <React.Fragment>
-    <Header />
+    <NewHeader />
     <AutoUpdate />
     <ToastContainer />
     <Switch>
