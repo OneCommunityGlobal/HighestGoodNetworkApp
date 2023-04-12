d751ae9435f69b7e85459a2c36d40cce074529de
vitoreow <vitoradriel66@gmail.com>
2023-04-12 15:25:19 -0300

    better blue square behavior and optimization

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -323,8 +323,11 @@ const UserProfile = props => {
   const modifyBlueSquares = (id, dateStamp, summary, operation) => {
     if (operation === 'add') {
       const newBlueSquare = { date: dateStamp, description: summary };
-      setShowModal(false);
       setUserProfile({
+        ...userProfile,
+        infringements: userProfile.infringements?.concat(newBlueSquare),
+      });
+      setOriginalUserProfile({
         ...userProfile,
         infringements: userProfile.infringements?.concat(newBlueSquare),
       });
@@ -338,16 +341,17 @@ const UserProfile = props => {
         currentBlueSquares.find(blueSquare => blueSquare._id === id).description = summary;
       }
 
-      setShowModal(false);
       setUserProfile({ ...userProfile, infringements: currentBlueSquares });
+      setOriginalUserProfile({ ...userProfile, infringements: currentBlueSquares });
     } else if (operation === 'delete') {
       let newInfringements = [...userProfile?.infringements] || [];
       if (newInfringements !== []) {
         newInfringements = newInfringements.filter(infringement => infringement._id !== id);
         setUserProfile({ ...userProfile, infringements: newInfringements });
-        setShowModal(false);
+        setOriginalUserProfile({ ...userProfile, infringements: newInfringements });
       }
     }
+    setShowModal(false);
     setBlueSquareChanged(true);
   };
 
