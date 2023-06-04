70d44ea91ee5726a801c0f742172ee5f140d88a9
Xiao Tan <xiaoatan@usc.edu>
2023-06-03 21:58:15 -0700

    Update TimeEntryForm.jsx

--- src/components/Timelog/TimeEntryForm/TimeEntryForm.jsx
+++ src/components/Timelog/TimeEntryForm/TimeEntryForm.jsx
@@ -32,6 +32,7 @@ import { ENDPOINTS } from '../../../utils/URL';
 import hasPermission from 'utils/permissions';
 import { getTimeEntryFormData } from './selectors';
 import checkNegativeNumber from 'utils/checkNegativeHours';
+import { boxStyle } from 'styles';
 
 /**
  * Modal used to submit and edit tangible and intangible time entries.
@@ -754,11 +755,11 @@ const TimeEntryForm = props => {
         </ModalBody>
         <ModalFooter>
           <small className="mr-auto">* All the fields are required</small>
-          <Button onClick={clearForm} color="danger">
+          <Button onClick={clearForm} color="danger" style={boxStyle}>
             Clear Form
           </Button>
           {/* <Button color="primary" disabled={isSubmitting || (data.hours === inputs.hours && data.minutes === inputs.minutes && data.notes === inputs.notes)} onClick={handleSubmit}> */}
-          <Button color="primary" onClick={handleSubmit}>
+          <Button color="primary" onClick={handleSubmit} style={boxStyle}>
             {edit ? 'Save' : 'Submit'}
           </Button>
         </ModalFooter>
