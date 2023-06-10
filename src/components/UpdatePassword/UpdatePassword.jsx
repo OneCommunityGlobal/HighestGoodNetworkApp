d9d5bdd2848bb275bb4f4cbde3fe1b48e427fed4
Yongjian Pan <46613683+ypan710@users.noreply.github.com>
2023-06-10 08:23:32 -0700

    Update src/components/UpdatePassword/UpdatePassword.jsx
    Co-authored-by: Nathan Hoffman <nathanahoffman@gmail.com>

--- src/components/UpdatePassword/UpdatePassword.jsx
+++ src/components/UpdatePassword/UpdatePassword.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import Form from '../common/Form';
