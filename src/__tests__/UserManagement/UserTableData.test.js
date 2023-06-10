a632bf30e1622a4d4808e89db373d11c3d7d2ad5
Jianjun <9314962+juneluo@users.noreply.github.com>
2023-06-10 14:06:57 -0500

    fix the inconsistency of props to ActiveCell

--- src/__tests__/UserManagement/UserTableData.test.js
+++ src/__tests__/UserManagement/UserTableData.test.js
@@ -17,7 +17,6 @@ describe('User Table Data', () => {
         <tbody>
           <UserTableData
             isActive
-            canChange={true}//add to enable the test of 'change the user status' functionality in line37 and 82.
             index={0}
             user={userProfileMock}
             onActiveInactiveClick={onActiveInactiveClick}
