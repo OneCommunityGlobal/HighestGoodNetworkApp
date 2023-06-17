27de5cd21be0b326feb58d5e60d3a595c5d14001
Lucas-E <lucasgitacc@gmail.com>
2023-06-17 00:01:32 -0300

    removed console.log

--- src/actions/task.js
+++ src/actions/task.js
@@ -68,7 +68,6 @@ export const deleteTaskNotification = (userId, taskId, taskNotificationId) => as
   try {
     //dispatch(deleteTaskNotificationBegin());
     const res = await axios.delete(ENDPOINTS.DELETE_TASK_NOTIFICATION_BY_USER_ID(taskId, userId));
-    console.log(res)
     
     //const res = await axios.delete(ENDPOINTS.DELETE_TASK_NOTIFICATION(taskNotificationId));
     dispatch(deleteTaskNotificationSuccess({ userId, taskId, taskNotificationId }));
