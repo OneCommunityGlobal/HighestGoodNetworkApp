e6ff94548fb9a89232e7db301a6b2fcb65a1dc49
leonzh2k <leonzh2k@gmail.com>
2023-07-06 18:03:22 -0400

    add logic to fetch all user profile data
    This data will eventually be displayed when clicking
    markers on the map.

--- src/components/TeamLocations/TeamLocations.jsx
+++ src/components/TeamLocations/TeamLocations.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
-
+import axios from 'axios';
+import { ENDPOINTS } from '../../utils/URL';
+import { useState, useEffect } from 'react';
 const TeamLocations = () => {
-  return <p>Hello World!</p>;
+  const [userProfiles, setUserProfiles] = useState([]);
+  useEffect(() => {
+    async function getUserProfiles() {
+      const users = await axios.get(ENDPOINTS.USER_PROFILES);
+      setUserProfiles(users.data);
+    }
+    getUserProfiles();
+  }, []);
+  return userProfiles.map(profile => {
+    return (
+      <div>
+        <div>{`Name: ${profile.firstName} ${profile.lastName}`}</div>
+        <div>{`Title: ${profile.jobTitle}`}</div>
+        <div>{`Location: ${profile.location}`}</div>
+      </div>
+    );
+  });
 };
 
 export default TeamLocations;
