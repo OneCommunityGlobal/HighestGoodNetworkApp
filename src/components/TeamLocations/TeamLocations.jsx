cfc95ae2e07d87e1f6855f478e61bfc8aa8a82b3
leonzh2k <leonzh2k@gmail.com>
2023-07-13 17:30:24 -0400

    add reasonable map options
    Add map options for better UX, such as starting
    the map at a reasonable zoom level, setting the
    initial center to be a reasonable place, setting
    panning boundaries, etc.

--- src/components/TeamLocations/TeamLocations.jsx
+++ src/components/TeamLocations/TeamLocations.jsx
@@ -26,10 +26,21 @@ const TeamLocations = () => {
   }, []);
 
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      center={[51.505, -0.09]}
+      maxBounds={[
+        [-90, -225],
+        [90, 225],
+      ]}
+      maxBoundsViscosity={1.0}
+      zoom={3}
+      scrollWheelZoom={true}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        minZoom={2}
+        maxZoom={15}
       />
       <Marker position={[51.505, -0.09]}>
         <Popup>
