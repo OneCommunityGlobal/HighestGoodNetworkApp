8a26c65de9ffcbcd9ef88a46ebc4eafc457005ea
yufeiStella <yz218@rice.edu>
2023-07-21 16:14:00 -0500

    save files

--- src/components/LeaderBoard/Leaderboard.jsx
+++ src/components/LeaderBoard/Leaderboard.jsx
@@ -4,16 +4,8 @@ import { isEqual } from 'lodash';
 import { Link } from 'react-router-dom';
 import { Table, Progress, Modal, ModalBody, ModalFooter, ModalHeader, Button } from 'reactstrap';
 import Alert from 'reactstrap/lib/Alert';
-<<<<<<< HEAD
 import { hasLeaderboardPermissions, assignStarDotColors, showStar } from 'utils/leaderboardPermissions';
 import MouseoverTextTotalTime from '../mouseoverText/mouseoverTextTotalTime';
-=======
-import {
-  hasLeaderboardPermissions,
-  assignStarDotColors,
-  showStar,
-} from 'utils/leaderboardPermissions';
->>>>>>> development
 
 function useDeepEffect(effectFunc, deps) {
   const isFirst = useRef(true);
@@ -273,7 +265,6 @@ const LeaderBoard = ({
                       </ModalFooter>
                     </Modal>
                   </div>
-<<<<<<< HEAD
                   <div style={{ display: 'flex', alignItems: 'center', justifyContent: isAdmin ? 'space-between' : 'center' }}>
 
                     {/* <Link to={`/dashboard/${item.personId}`}> */}
@@ -309,49 +300,6 @@ const LeaderBoard = ({
                     </div>
                     {
                       isAdmin && item.hasSummary &&
-=======
-                  <div
-                    style={{
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: isAdmin ? 'space-between' : 'center',
-                    }}
-                  >
-                    {/* <Link to={`/dashboard/${item.personId}`}> */}
-                    <div onClick={() => dashboardToggle(item)}>
-                      {hasLeaderboardPermissions(loggedInUser.role) &&
-                      showStar(item.tangibletime, item.weeklycommittedHours) ? (
-                        <i
-                          className="fa fa-star"
-                          title={`Weekly Committed: ${item.weeklycommittedHours} hours`}
-                          style={{
-                            color: assignStarDotColors(
-                              item.tangibletime,
-                              item.weeklycommittedHours,
-                            ),
-                            fontSize: '20px',
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                          }}
-                        />
-                      ) : (
-                        <div
-                          title={`Weekly Committed: ${item.weeklycommittedHours} hours`}
-                          style={{
-                            backgroundColor:
-                              item.tangibletime >= item.weeklycommittedHours ? '#32CD32' : 'red',
-                            width: 15,
-                            height: 15,
-                            borderRadius: 7.5,
-                            margin: 'auto',
-                            verticalAlign: 'middle',
-                          }}
-                        />
-                      )}
-                    </div>
-                    {isAdmin && item.hasSummary && (
->>>>>>> development
                       <div
                         title={`Weekly Summary Submitted`}
                         style={{
@@ -361,11 +309,7 @@ const LeaderBoard = ({
                       >
                         <strong>✓</strong>
                       </div>
-<<<<<<< HEAD
                     }
-=======
-                    )}
->>>>>>> development
                   </div>
                   {/* </Link> */}
                 </td>
@@ -391,17 +335,9 @@ const LeaderBoard = ({
                 </td>
                 <td className="align-middle">
                   <span
-<<<<<<< HEAD
                     title={mouseoverText}
                     className={item.totalintangibletime_hrs > 0 ? 'boldClass' : null}
                   >{item.totaltime}</span>
-=======
-                    title="Total time"
-                    className={item.totalintangibletime_hrs > 0 ? 'boldClass' : null}
-                  >
-                    {item.totaltime}
-                  </span>
->>>>>>> development
                 </td>
               </tr>
             ))}
