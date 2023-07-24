41620f04c80949eb42027afc6b6f0562a238ad26
Nida Zaki <32549172+NidaZaki@users.noreply.github.com>
2023-07-24 13:43:57 -0400

    text prompt formatting

--- src/components/WeeklySummary/CurrentPromptModal.jsx
+++ src/components/WeeklySummary/CurrentPromptModal.jsx
@@ -9,15 +9,7 @@ import { faEdit } from '@fortawesome/free-regular-svg-icons';
 import { updateDashboardData, getDashboardDataAI } from '../../actions/dashboard';
 
 function CurrentPromptModal(props) {
-  const current = `Please edit the following summary of my week's work. 
-  Make sure it is professionally written in 3rd person format. 
-  Write it as only one paragraph. It must be only one paragraph. 
-  Keep it less than 500 words. 
-  Start the paragraph with 'This week'.
-  Make sure the paragraph contains no links or URLs and write it in a tone that is matter-of-fact and without embellishment. 
-  Do not add flowery language, keep it simple and factual. 
-  Do not add a final summary sentence. 
-  Apply all this to the following:`;
+  const current = `Please edit the following summary of my week's work. Make sure it is professionally written in 3rd person format. Write it as only one paragraph. It must be only one paragraph. Keep it less than 500 words. Start the paragraph with 'This week'. Make sure the paragraph contains no links or URLs and write it in a tone that is matter-of-fact and without embellishment. Do not add flowery language, keep it simple and factual. Do not add a final summary sentence. Apply all this to the following:`;
   const dashboardDataAIPrompt =
     props.state.dashboardData === '' || props.state.dashboardData === null
       ? current
