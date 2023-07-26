d17662ff56492f07093d19ce7ab687807abab983
Lucas-E <lucasgitacc@gmail.com>
2023-07-26 11:59:57 -0300

    added getReasonByData

--- src/actions/reasonsActions.js
+++ src/actions/reasonsActions.js
@@ -5,9 +5,18 @@ export async function addReason(userId, reasonData) {
   try {
     const url = ENDPOINTS.CREATEREASON(userId);
     const response = await axios.post(url, { userId: userId, reasonData: reasonData });
-    console.log(response);
     return Promise.resolve(response)
   } catch (error) {
-    return {message: error.response.data.message, errorCode: error.response.data.message, httpCode: error.response.status}
+    return {message: error.response.data.message, errorCode: error.response.data.message, status: error.response.status}
   }
+}
+
+export async function getReasonByDate(userId, queryDate){
+    try {
+        const url = ENDPOINTS.GETSINGLEREASONBYID(userId);
+        const response = await axios.get(url, { userId: userId, queryDate: queryDate });
+        return Promise.resolve(response)
+      } catch (error) {
+        return {message: error.response.data.message, errorCode: error.response.data.message, status: error.response.status}
+      }
 }
