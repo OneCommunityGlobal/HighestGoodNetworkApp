01850cb16e6dfe5f8f1ea0373cc9ec175ef1db93
Lucas-E <lucasgitacc@gmail.com>
2023-07-26 12:15:15 -0300

    added params to axios get request

--- src/actions/reasonsActions.js
+++ src/actions/reasonsActions.js
@@ -14,7 +14,7 @@ export async function addReason(userId, reasonData) {
 export async function getReasonByDate(userId, queryDate){
     try {
         const url = ENDPOINTS.GETSINGLEREASONBYID(userId);
-        const response = await axios.get(url, { userId: userId, queryDate: queryDate });
+        const response = await axios.get(url, {params: {queryDate: queryDate }});
         return Promise.resolve(response)
       } catch (error) {
         return {message: error.response.data.message, errorCode: error.response.data.message, status: error.response.status}
