dcabe607fb1b6e588fca939abb5a4d699d2c5128
Aaron Persaud <aaronpersaud@hotmail.com>
2023-08-06 02:24:43 -0400

    start date on volunteering time tab uses startDate instead of createdDate if it exists

--- src/components/UserProfile/VolunteeringTimeTab/VolunteeringTimeTab.jsx
+++ src/components/UserProfile/VolunteeringTimeTab/VolunteeringTimeTab.jsx
@@ -11,14 +11,16 @@ const MINIMUM_WEEK_HOURS = 0;
 const MAXIMUM_WEEK_HOURS = 168;
 
 const startEndDateValidation = props => {
+  const startDate = props.userProfile.startDate || props.userProfile.createdDate;
   return (
-    props.userProfile.createdDate > props.userProfile.endDate && props.userProfile.endDate !== ''
+    startDate > props.userProfile.endDate && props.userProfile.endDate !== ''
   );
 };
 
 const StartDate = props => {
+  const startDate = props.userProfile.startDate || props.userProfile.createdDate;
   if (!props.canEdit) {
-    return <p>{moment(props.userProfile.createdDate).format('YYYY-MM-DD')}</p>;
+    return <p>{moment(startDate).format('YYYY-MM-DD')}</p>;
   }
   return (
     <Input
@@ -26,9 +28,9 @@ const StartDate = props => {
       name="StartDate"
       id="startDate"
       className={startEndDateValidation(props) ? 'border-error-validation' : null}
-      value={moment(props.userProfile.createdDate).format('YYYY-MM-DD')}
+      value={moment(startDate).format('YYYY-MM-DD')}
       onChange={e => {
-        props.setUserProfile({ ...props.userProfile, createdDate: e.target.value });
+        props.setUserProfile({ ...props.userProfile, startDate: e.target.value });
         props.onStartDateComponent(e.target.value);
       }}
       placeholder="Start Date"
@@ -65,8 +67,8 @@ const EndDate = props => {
       placeholder="End Date"
       invalid={!props.canEdit}
       min={
-        props.userProfile.createdDate
-          ? moment(props.userProfile.createdDate).format('YYYY-MM-DD')
+        props.userProfile.startDate
+          ? moment(props.userProfile.startDate).format('YYYY-MM-DD')
           : ''
       }
     />
@@ -284,11 +286,12 @@ const ViewTab = props => {
       });
 
     //Get total tangible & intangible hours
-    const createdDate = moment(userProfile.createdDate).format('YYYY-MM-DD');
+    const start = userProfile.startDate || userProfile.createdDate;
+    const startDate = moment(start).format('YYYY-MM-DD');
     const today = moment().format('YYYY-MM-DD');
 
     axios
-      .get(ENDPOINTS.TIME_ENTRIES_PERIOD(userProfile._id, createdDate, today))
+      .get(ENDPOINTS.TIME_ENTRIES_PERIOD(userProfile._id, startDate, today))
       .then(res => {
         const timeEntries = res.data;
         sumOfCategoryHours();
