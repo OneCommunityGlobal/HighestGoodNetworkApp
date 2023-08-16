0ff935e2079d4a523900f733d1996affb467aed3
ShihaoXiong <shihao.xiong@icloud.com>
2023-08-16 18:40:45 -0400

    Implement sort function

--- src/components/Teams/TeamMembersPopup.jsx
+++ src/components/Teams/TeamMembersPopup.jsx
@@ -1,8 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Container, Alert } from 'reactstrap';
 import MembersAutoComplete from './MembersAutoComplete';
 import hasPermission from 'utils/permissions';
 import { boxStyle } from 'styles';
+import moment from 'moment';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowAltCircleDown } from '@fortawesome/free-regular-svg-icons';
 
 const TeamMembersPopup = React.memo(props => {
   const closePopup = () => {
@@ -12,6 +15,7 @@ const TeamMembersPopup = React.memo(props => {
   const [isValidUser, onValidation] = useState(true);
   const [searchText, setSearchText] = useState('');
   const [memberList, setMemberList] = useState([]);
+  const [sortOrder, setSortOrder] = useState(0)
 
   const onAddUser = () => {
     if (selectedUser && !props.members.teamMembers.some(x => x._id === selectedUser._id)) {
@@ -26,19 +30,55 @@ const TeamMembersPopup = React.memo(props => {
     onValidation(true);
   };
 
+  /**
+   * The function to sort the memberlist
+   * @param {-1 | 0 | 1} [sort = 0]
+   * -1: ascending order by date
+   * 0: alphabetized order by name
+   * 1: descending order by date
+  */
+  const sortList = (sort = 0) => {
+    let sortedList = []
+
+    if (sort === 0) {
+      sortedList = props.members.teamMembers.toSorted(sortByAlpha)
+    } else {
+      const sortByDateList = props.members.teamMembers.toSorted((a, b) => {
+        return moment(a.addDateTime).diff(moment(b.addDateTime)) * -sort;
+      });
+
+      const dataList = Object.values(sortByDateList.reduce((pre, cur) => {
+        const date = moment(cur.addDateTime).format("MMM-DD-YY");
+        pre[date] ? pre[date].push(cur) : pre[date] = [cur]
+        return pre;
+      }, {}));
+
+      dataList.forEach(item => {
+        sortedList.push(...item.toSorted(sortByAlpha));
+      });
+    }
+
+    setMemberList(sortedList);
+  }
+
+  const sortByAlpha = useCallback((a, b) => {
+    const nameA = `${a.firstName} ${a.lastName}`.toLowerCase();
+    const nameB = `${b.firstName} ${b.lastName}`.toLowerCase();
+    return nameA.localeCompare(nameB);
+  })
+
+  const toggleOrder = useCallback(() => {
+    setSortOrder((pre) => {
+      if (pre !== -1) {
+        return pre - 1;
+      }
+      return 1;
+    })
+  })
+
   useEffect(() => {
-    setMemberList(props.members.teamMembers.toSorted((a, b) => {
-      const nameA = `${a.firstName} ${a.lastName}`.toLowerCase();
-      const nameB = `${b.firstName} ${b.lastName}`.toLowerCase();
-      if (nameA < nameB) {
-        return -1;
-      }
-      if (nameA > nameB) {
-        return 1;
-      }
-      return 0;
-    }))
-  }, [props.members.teamMembers])
+    sortList(sortOrder)
+  }, [props.members.teamMembers, sortOrder])
 
   useEffect(() => {
     onValidation(true);
@@ -46,7 +86,7 @@ const TeamMembersPopup = React.memo(props => {
 
   return (
     <Container fluid>
-      <Modal isOpen={props.open} toggle={closePopup} autoFocus={false}>
+      <Modal isOpen={props.open} toggle={closePopup} autoFocus={false} size='lg'>
         <ModalHeader toggle={closePopup}>{`Members of ${props.selectedTeamName}`}</ModalHeader>
         <ModalBody style={{ textAlign: 'center' }}>
           {hasPermission(
@@ -68,50 +108,50 @@ const TeamMembersPopup = React.memo(props => {
             </div>
           )}
           {isValidUser && <Alert color="danger">Please choose a valid user.</Alert>}
-          <div>
-            <table className="table table-bordered table-responsive-sm">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>User Name</th>
-                  {hasPermission(
-                    props.requestorRole,
-                    'assignTeamToUser',
-                    props.roles,
-                    props.userPermissions,
-                  ) && <th> </th>}
-                </tr>
-              </thead>
-              <tbody>
-                {props.members.teamMembers.length > 0 &&
-                  memberList.toSorted().map((user, index) => (
-                    <tr key={`team_member_${index}`}>
-                      <td>{index + 1}</td>
-                      <td>{`${user.firstName} ${user.lastName}`}</td>
-                      {hasPermission(
-                        props.requestorRole,
-                        'assignTeamToUser',
-                        props.roles,
-                        props.userPermissions,
-                      ) && (
-                        <td>
-                          <Button
-                            color="danger"
-                            onClick={() => {
-                              props.onDeleteClick(`${user._id}`);
-                            }}
-                            style={boxStyle}
-                          >
-                            Delete
-                          </Button>
-                        </td>
-                      )}
-                    </tr>
-                  ))
-                }
-              </tbody>
-            </table>
-          </div>
+          <table className="table table-bordered table-responsive-sm">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>User Name</th>
+                <th>Date Added <div onClick={toggleOrder}>{sortOrder}</div></th>
+                {hasPermission(
+                  props.requestorRole,
+                  'assignTeamToUser',
+                  props.roles,
+                  props.userPermissions,
+                ) && <th />}
+              </tr>
+            </thead>
+            <tbody>
+              {props.members.teamMembers.length > 0 &&
+                memberList.toSorted().map((user, index) => (
+                  <tr key={`team_member_${index}`}>
+                    <td>{index + 1}</td>
+                    <td>{`${user.firstName} ${user.lastName}`}</td>
+                    <td>{moment(user.addDateTime).format('MMM-DD-YY')}</td>
+                    {hasPermission(
+                      props.requestorRole,
+                      'assignTeamToUser',
+                      props.roles,
+                      props.userPermissions,
+                    ) && (
+                      <td>
+                        <Button
+                          color="danger"
+                          onClick={() => {
+                            props.onDeleteClick(`${user._id}`);
+                          }}
+                          style={boxStyle}
+                        >
+                          Delete
+                        </Button>
+                      </td>
+                    )}
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
         </ModalBody>
         <ModalFooter>
           <Button color="secondary" onClick={closePopup} style={boxStyle}>
