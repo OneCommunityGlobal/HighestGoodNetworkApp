81b4902a82251b3c160951e65d2899b5cb5c7dfd
ThapeloMasasa <thapelomasasa5@gmail.com>
2023-08-19 16:07:36 -0700

    applied all name and description updates

--- src/components/PermissionsManagement/RolePermissions.jsx
+++ src/components/PermissionsManagement/RolePermissions.jsx
@@ -12,7 +12,6 @@ import { ENDPOINTS } from '../../utils/URL';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 import { boxStyle } from 'styles';
-import EditableInfoModal from 'components/UserProfile/EditableModal/EditableInfoModal';
 
 function getKeyByValue(object, value) {
   return Object.keys(object).find(key => object[key] === value);
@@ -31,6 +30,94 @@ const mapPermissionToLabel = permissions => {
 };
 
 function RolePermissions(props) {
+  const modalInfo = {
+    'See Only Weekly Summary Reports Tab':
+      'Make the "Other Links" -> "Reports" button appear/accessible.',
+    'See User Management Tab (Full Functionality)':
+      'Make the "Other Links" -> "User Management" button appear/accessible and be able to create, delete, and update users.',
+    'See Badge Management Tab (Full Funtionality)':
+      'Make the "Other Links" -> "Badge Management" button appear and then have the ability to create, delete, and update badges. ',
+    'Delete Badge':
+      'Gives the user permission to delete a badge on "Other Links" -> "Badge Management"',
+    'Modify Badge Amount':
+      'Gives the user permission to decrease the count of a badge on the Badge Reports Component',
+    'Assign Badges':
+      'Gives the user permission to assign badges to others users "User Profile" -> "Assign Badges"',
+    'See Popup Management Tab (create and update popups)':
+      'Make the "Other Links" -> "Popup Management" button appear and be able to create and update popups.',
+    'See Project Management Tab (Full Functionality)':
+      'Make the "Other Links" -> "Projects" button appear and be able to create or delete new projects, edit projects names, add members to projects, upload/import/create new WBSs, etc.',
+    'Delete WBS':
+      'Gives the user permission to delete any WBS. "Other Links" -> "Projects" -> "WBS Button" -> "Minus Red Icon"',
+    'Add Task':
+      'Gives the user permission to add a task on any WBS. "Other Links" -> "Projects" -> "WBS Button" -> "Choose any WBS" -> "Add task button"',
+    'Delete Task':
+      'Gives the user permission to delete a task on any WBS. "Other Links" -> "Projects" -> "WBS Button" -> "Choose any WBS" -> "Edit" -> "Remove"',
+    'Edit Task':
+      'Gives the user permission to edit a task on any WBS. "Other Links" -> "Projects" -> "WBS Button" -> "Choose any WBS" -> "Edit" -> "Edit"',
+    'Suggest Changes on a task':
+      'Gives the user permission to suggest changes on a task. "Dashboard" -> "Tasks tab" -> "Click on any task" -> "Suggest button"',
+    'Add WBS':
+      'Gives the user permission to create a new WBS. "Other Links" -> "Projects" -> "WBS Button" -> "Add new WBS Input"',
+    'Add Project':
+      'Gives the user permission to create any Project. "Other Links" -> "Projects" -> "Add new Project Input"',
+    'Delete Project':
+      'Gives the user permission to delete any Project. "Other Links" -> "Projects" -> "Delete button" ',
+    'Edit Project':
+      'Gives the user permission to edit the category or the status of any Project. "Other Links" -> "Projects"',
+    'Find User in Project':
+      'Gives the user permission to find any user on the project members page. "Other Links" -> "Projects" -> "Members" -> "Find user input"',
+    'Assign User in Project':
+      'Gives the user permission to add any user on the project members page. "Other Links" -> "Projects" -> "Members" -> "Find user input"',
+    'Unassign User in Project':
+      'Gives the user permission to remove any user on the project members page. "Other Links" -> "Projects" -> "Members" -> "Minus button"',
+      'See Teams Management Tab (Full Functionality)':
+      'Make the "Other Links" -> "Teams" button appear and be able to add/delete teams, edit team names, and add/delete members.',
+    'Edit/Delete Team': 'Gives the user permission to Edit or delete a team.',
+    'Create Team': 'Gives the user permission to create a team.',
+    'Assign Users Team':
+    'Gives the user permission to add a user to a team from their profile page. "User Profile" -> "Teams" -> "Assign Team"',
+    'Edit Timelog Information': 'Gives the user permission to edit any time log entry.',
+    'Add Time Entry (Others)':
+      'Gives the user permission to add Intangible time entry to others users "Dashboard" -> "Leaderboard" -> "Dot By the side of user\'s name" -> "Add Time entry to (Name of the user) yellow button"',
+    'Delete Time Entry (Others)':
+      'Gives the user permission to Delete time entry from others users "Dashboard" -> "Leaderboard" -> "Dot By the side of user\'s name" -> "Current Time Log" -> "Trash button on bottom right"',
+    'Toggle Tangible Time Self':
+    'Gives the user permission to toggle the Tanglible check when editing their own time entry.',
+    'Toggle Tangible/Intangible Time Others':
+      'Gives the user permission to toggle the tanglible check when editing a time entry of another user.',
+    'Edit Time Entry':
+      'Gives the user permission to edit any time entry on their own time logs tab "Dashboard" -> "Current Time Log" -> "Pencil Icon"',
+    'Delete Time Entry':
+      'Gives the user permission to delete any time entry on their own time logs tab "Dashboard" -> "Current Time Log" -> "Trash Icon"',
+    'Change User Status':
+      'Gives the user permission to change the status of any user on the user profile page or User Management Page. "User Profile" -> "Green round button"',
+    'Manage Admin Links in User Profile':
+      'Gives the user permission to edit the links of any user on the user profile page. "User Profile" -> "Links button"',
+    'Edit User Profile':
+      'Gives the user permission to edit all the information of any user on the user profile page.',
+    'See User Profiles in Projects':
+      'Gives the user permission to access the profile of an user directly from the projects members page. "Other Links" -> "Projects" -> "Members" -> "Name of any member"',
+    'Reset Password (Others)': 'Gives the user permission to Reset the password of any user.',
+    'Timelog Data is Tangible':
+      'Gives the user permission to toggle the tanglible check when editing a Time entry of another user.',
+    'Toggle Summary Submit Form (Others)':
+      'Gives the user permission to change the requirement to the user to submit a summary.',
+    'Handle Blue Squares': 'Gives the user permission to Update/Delete/Edit any blue square.',
+    'Only Assign Blue Squares': 'Gives the user permission to add blue squares to any user.',
+    'See Permissions Management Tab':
+      'Gives the user permission to access the Permissions Management tab.',
+    'Submit Weekly Summary For Others':
+      'Gives the user permission to submit weekly summary for another user',
+    'Change the Bio Announcement Status':
+      'Gives the user permission to change the annoucement status',
+    'See Summary Indicator' : 
+      'Gives the user permission to see summary indicator on leaderboard.',
+    'See Visibility Icon' : 
+      'Gives the user permission to see visibility icon on leaderboard.',
+    
+  };
+  const mainPermissions = []
   const [permissions, setPermissions] = useState(mapPermissionToLabel(props.permissions));
   const [deleteRoleModal, setDeleteRoleModal] = useState(false);
   const [editRoleNameModal, setEditRoleNameModal] = useState(false);
@@ -40,7 +127,7 @@ function RolePermissions(props) {
   const history = useHistory();
   const [infoRoleModal, setinfoRoleModal] = useState(false);
   const [modalContent, setContent] = useState(null);
-
+  
   useEffect(() => {
     setRoleName(props.role);
   }, []);
@@ -87,6 +174,7 @@ function RolePermissions(props) {
     const permissionsBackEnd = permissionsObjectName.map(permission =>
       permissionFrontToBack(permission),
     );
+
     const id = props.roleId;
 
     const updatedRole = {
@@ -178,17 +266,55 @@ function RolePermissions(props) {
       </header>
       <ul className="user-role-tab__permissionList">
         {props.permissionsList.map((permission) => (
+          mainPermissions.includes(permission) ?
           <li className="user-role-tab__permissions" key={permission}>
-            <p style={{ color: permissions.includes(permission) ? 'green' : 'red' }}>
+            <p style={{ color: permissions.includes(permission) ? 'green' : 'red' , fontSize: '20px'}}>
               {permission}
             </p>
             <div className="icon-button-container">
-              <div style={{paddingRight: '27px'}}>
-                  <EditableInfoModal
-                    role={props?.userRole}
-                    areaName={`${permission}`+'Info'}
-                    fontSize={24} />{' '}
-               </div>
+              <i
+                data-toggle="tooltip"
+                data-placement="center"
+                title="Click for more information"
+                aria-hidden="true"
+                className="fa fa-info-circle"
+                onClick={() => {
+                  handleModalOpen(permission);
+                }}
+              />
+              &nbsp;&nbsp;
+              <Button
+                className="icon-button"
+                color={permissions.includes(permission) ? 'danger' : 'success'}
+                onClick={() => {
+                  permissions.includes(permission)
+                    ? onRemovePermission(permission)
+                    : onAddPermission(permission);
+                  setChanged(true);
+                }}
+                disabled={props?.userRole !== 'Owner'}
+                style={boxStyle}
+              >
+                {permissions.includes(permission) ? 'Delete' : 'Add'}
+              </Button>
+            </div>
+          </li>:
+           <li className="user-role-tab__permissions" key={permission}>
+            <p style={{ color: permissions.includes(permission) ? 'green' : 'red' , paddingLeft: '50px'}}>
+              {permission}
+            </p>
+            <div className="icon-button-container">
+              <i
+                data-toggle="tooltip"
+                data-placement="center"
+                title="Click for more information"
+                aria-hidden="true"
+                className="fa fa-info-circle"
+                onClick={() => {
+                  handleModalOpen(permission);
+                }}
+              />
+              &nbsp;&nbsp;
               <Button
                 className="icon-button"
                 color={permissions.includes(permission) ? 'danger' : 'success'}
