0cccedbed22c8db0e79de13e301217f2763be781
Mounica <mounica.d19@gmail.com>
2023-08-19 22:19:25 -0700

    made the username bold in delete popup of user management

--- src/components/UserManagement/UserManagement.jsx
+++ src/components/UserManagement/UserManagement.jsx
@@ -62,6 +62,7 @@ class UserManagement extends React.PureComponent {
     let { userProfiles, fetching } = this.props.state.allUserProfiles;
     const { roles: rolesPermissions } = this.props.state.role;
     let userTable = this.userTableElements(userProfiles, rolesPermissions);
+
     let roles = [...new Set(userProfiles.map(item => item.role))];
     return (
       <Container fluid>
@@ -119,6 +120,7 @@ class UserManagement extends React.PureComponent {
    * 5. Popup to show the last day selection
    */
   popupElements = () => {
+    let user_name = this.state?.selectedUser?.firstName + '_' + this.state?.selectedUser?.lastName
     return (
       <React.Fragment>
         <ActivationDatePopup
@@ -133,6 +135,7 @@ class UserManagement extends React.PureComponent {
           userCreated={this.userCreated}
         />
         <DeleteUserPopup
+          username={user_name}
           open={this.state.deletePopupOpen}
           onClose={this.deletePopupClose}
           onDelete={this.onDeleteUser}
