fb873b1c8f8d1f8ad9670454e90cd7e927f510a7
Jerry Ren <jerryren.5279@gmail.com>
2023-08-26 19:47:19 -0400

    Change Permission Title to "See Only Weekly..."

--- src/components/PermissionsManagement/UserRoleTab.jsx
+++ src/components/PermissionsManagement/UserRoleTab.jsx
@@ -7,7 +7,7 @@ import { useHistory } from 'react-router-dom';
 
 export const permissionLabel = {
   seeAllReports: 'See All the Reports Tab',
-  seeWeeklySummaryReports: 'See Weekly Summary Reports Tab',
+  seeWeeklySummaryReports: 'See Only Weekly Summaries Reports Tab',
   seeUserManagement: 'See User Management Tab (Full Functionality)',
   seeUserManagementTab: 'See User Management Tab (ONLY create Users)',
   seeBadgeManagement: 'See Badge Management Tab (Full Functionality)',
