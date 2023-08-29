e79306636168c529e6ee744f4578f310fb8be026
ShihaoXiong <shihao.xiong@icloud.com>
2023-08-29 13:39:25 -0400

    fix eslint errors

--- src/components/WeeklySummary/DueDateTime.jsx
+++ src/components/WeeklySummary/DueDateTime.jsx
@@ -1,10 +1,9 @@
 import PropTypes from 'prop-types';
-import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarCheck } from '@fortawesome/free-regular-svg-icons';
 import moment from 'moment';
-import { CountdownTimer } from './CountdownTimer';
 import { boxStyle } from 'styles';
+import CountdownTimer from './CountdownTimer';
 
 function DueDateTime({ dueDate, isShow }) {
   // The display time should add 1 sec so it displays Sunday at 00:00 and not Saturday at 23:59:59.
@@ -20,8 +19,12 @@ function DueDateTime({ dueDate, isShow }) {
       )}
       <div className="mx-auto due-section" style={boxStyle}>
         <div className="text-white due-section__date">
-          <FontAwesomeIcon icon={faCalendarCheck} className="mr-1" />{' '}
-          {displayTime.format('YYYY-MMM-DD')} at {displayTime.format('HH:mm')} PST
+          <FontAwesomeIcon icon={faCalendarCheck} className="mr-1" />
+          &nbsp;
+          {displayTime.format('YYYY-MMM-DD')}
+          &nbsp; at
+          {displayTime.format('HH:mm')}
+          PST
         </div>
         <CountdownTimer date={dueDate} />
       </div>
