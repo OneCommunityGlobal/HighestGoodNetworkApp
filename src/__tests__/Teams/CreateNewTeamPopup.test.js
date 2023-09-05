38a09746eb159b430838f82969e0f9cb795f613e
eduvarjaor <eduardovarjaor@gmail.com>
2023-09-05 19:51:01 -0300

    test(CreateNewTeamPopup): title rendered correctly based on isEdit props toggle

--- src/__tests__/Teams/CreateNewTeamPopup.test.js
+++ src/__tests__/Teams/CreateNewTeamPopup.test.js
@@ -29,4 +29,18 @@ describe('CreateNewTeamPopUp', () => {
 
     expect(mock).toHaveBeenCalledTimes(1);
   });
+
+  it('should render "Update Team Name" title when isEdit is true', () => {
+    renderWithProvider(<CreateNewTeamPopup {...defaultProps} isEdit={true} />);
+
+    const titleElement = screen.getByText('Update Team Name');
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  it('should render "Create New Team" title when isEdit is false', () => {
+    renderWithProvider(<CreateNewTeamPopup {...defaultProps} isEdit={false} />);
+
+    const titleElement = screen.getByText('Create New Team');
+    expect(titleElement).toBeInTheDocument();
+  });
 });
