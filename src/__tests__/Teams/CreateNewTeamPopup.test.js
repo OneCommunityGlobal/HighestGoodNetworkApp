b75ec29f4d8af5ac40dc390b1541b253ef68a29c
eduvarjaor <eduardovarjaor@gmail.com>
2023-09-05 18:42:15 -0300

    test(CreateNewTeamPopup): closePopup function test added

--- src/__tests__/Teams/CreateNewTeamPopup.test.js
+++ src/__tests__/Teams/CreateNewTeamPopup.test.js
@@ -1,1 +1,26 @@
-describe('CreateNewTeamPopUp', () => {});
+import React from 'react';
+import CreateNewTeamPopup from 'components/Teams/CreateNewTeamPopup';
+import { screen, fireEvent } from '@testing-library/react';
+import { renderWithProvider } from '../../__tests__/utils';
+
+const defaultProps = {
+  open: true,
+  teamName: 'Example Team',
+  isEdit: false,
+  onOkClick: jest.fn(),
+  onClose: jest.fn(),
+};
+
+describe('CreateNewTeamPopUp', () => {
+  it('should call closePopup function', () => {
+    const onCloseMock = jest.fn();
+    renderWithProvider(<CreateNewTeamPopup {...defaultProps} onClose={onCloseMock} />);
+
+    console.log(screen.debug());
+
+    const closeButton = screen.getByText('Close');
+    fireEvent.click(closeButton);
+
+    expect(onCloseMock).toHaveBeenCalledTimes(1);
+  });
+});
