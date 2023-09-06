d8fe1127ca8ad0b31085b646f68b686b2ffae999
eduvarjaor <eduardovarjaor@gmail.com>
2023-09-06 15:08:00 -0300

    test(MembersAutoComplete): component rendering test added

--- src/__tests__/Teams/MembersAutoComplete.test.js
+++ src/__tests__/Teams/MembersAutoComplete.test.js
@@ -1,1 +1,9 @@
-describe('MembersAutoComplete', () => {});
+import React from 'react';
+import MemberAutoComplete from 'components/Teams/MembersAutoComplete';
+import { renderWithProvider } from '__tests__/utils';
+
+describe('MembersAutoComplete', () => {
+  it('render MembersAutoComplete component without errors', () => {
+    renderWithProvider(<MemberAutoComplete />);
+  });
+});
