ca74b1c0f61deb46ff47ea745ffbcfdd5e23bc3f
eduvarjaor <eduardovarjaor@gmail.com>
2023-09-06 12:42:06 -0300

    test(DeleteTeamPopup): closePopup function test added

--- src/__tests__/Teams/DeleteTeamPopup.test.js
+++ src/__tests__/Teams/DeleteTeamPopup.test.js
@@ -1,1 +1,23 @@
-describe('DeleteTeamPopup', () => {});
+import React from 'react';
+import DeleteTeamPopup from 'components/Teams/DeleteTeamPopup';
+import { screen, fireEvent } from '@testing-library/react';
+import { renderWithProvider } from '../../__tests__/utils';
+
+const defaultProps = {
+  open: true,
+  teamName: 'Example Team',
+  isEdit: false,
+};
+
+const mock = jest.fn();
+
+describe('DeleteTeamPopup', () => {
+  it('should call closePopup function', () => {
+    renderWithProvider(<DeleteTeamPopup {...defaultProps} onClose={mock} />);
+
+    const closeButton = screen.getByText('Close');
+    fireEvent.click(closeButton);
+
+    expect(mock).toHaveBeenCalledTimes(1);
+  });
+});
