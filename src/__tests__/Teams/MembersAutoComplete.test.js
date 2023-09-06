1626fa1508ce9b75bf7aae6ccf61d7adb27af033
eduvarjaor <eduardovarjaor@gmail.com>
2023-09-06 19:19:41 -0300

    test(MembersAutoComplete): should dropdown appear when input is filled

--- src/__tests__/Teams/MembersAutoComplete.test.js
+++ src/__tests__/Teams/MembersAutoComplete.test.js
@@ -8,21 +8,48 @@ const mock = jest.fn();
 const defaultProps = {
   searchText: '',
   setSearchText: mock,
-  userProfileData: [],
+  userProfileData: {
+    userProfiles: [],
+  },
+  onAddUser: mock,
+};
+
+const dropdownProps = {
+  searchText: 'Eduardo',
+  setSearchText: mock,
+  userProfileData: {
+    userProfiles: [
+      {
+        isActive: true,
+        firstName: 'Eduardo',
+        lastName: 'ADM',
+      },
+    ],
+  },
   onAddUser: mock,
 };
 
 describe('MembersAutoComplete', () => {
-  it('render MembersAutoComplete component without errors', () => {
+  it('should render MembersAutoComplete component without errors', () => {
     renderWithProvider(<MemberAutoComplete />);
   });
 
-  it('interacts with input field and calls setSearchText', () => {
+  it('should interacts with input field and calls setSearchText', () => {
     renderWithProvider(<MemberAutoComplete {...defaultProps} />);
 
     const inputElement = screen.getByTestId('input-search');
     fireEvent.change(inputElement, { target: { value: 'Eduardo' } });
 
     expect(defaultProps.setSearchText).toHaveBeenCalledWith('Eduardo');
+  });
+
+  it('should dropdown appears when the input is filled', () => {
+    renderWithProvider(<MemberAutoComplete {...dropdownProps} />);
+
+    const inputElement = screen.getByTestId('input-search');
+    fireEvent.change(inputElement, { target: { value: 'Eduardo' } });
+
+    const dropdownMenu = screen.queryByRole('menu');
+    expect(dropdownMenu).toBeInTheDocument();
   });
 });
