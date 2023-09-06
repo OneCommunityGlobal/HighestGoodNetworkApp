f720148997fe74ce3ee7e71da41970a9d8e4f86d
eduvarjaor <eduardovarjaor@gmail.com>
2023-09-06 17:44:27 -0300

    test(MembersAutoComplete): input field interaction test

--- src/__tests__/Teams/MembersAutoComplete.test.js
+++ src/__tests__/Teams/MembersAutoComplete.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import MemberAutoComplete from 'components/Teams/MembersAutoComplete';
 import { renderWithProvider } from '__tests__/utils';
+import { screen, fireEvent } from '@testing-library/react';
+
+const mock = jest.fn();
+
+const defaultProps = {
+  searchText: '',
+  setSearchText: mock,
+  userProfileData: [],
+  onAddUser: mock,
+};
 
 describe('MembersAutoComplete', () => {
   it('render MembersAutoComplete component without errors', () => {
     renderWithProvider(<MemberAutoComplete />);
+  });
+
+  it('interacts with input field and calls setSearchText', () => {
+    renderWithProvider(<MemberAutoComplete {...defaultProps} />);
+
+    const inputElement = screen.getByTestId('input-search');
+    fireEvent.change(inputElement, { target: { value: 'Eduardo' } });
+
+    expect(defaultProps.setSearchText).toHaveBeenCalledWith('Eduardo');
   });
 });
