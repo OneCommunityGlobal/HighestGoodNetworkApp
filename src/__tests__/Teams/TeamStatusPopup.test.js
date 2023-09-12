dc030becda9a7cab2c3bff69d640a12a6b2d784b
eduvarjaor <eduardovarjaor@gmail.com>
2023-09-12 15:50:51 -0300

    test(TeamStatusPopup): rendering test added

--- src/__tests__/Teams/TeamStatusPopup.test.js
+++ src/__tests__/Teams/TeamStatusPopup.test.js
@@ -1,1 +1,26 @@
-describe('TeamStatusPopup', () => {});
+import React from 'react';
+import TeamStatusPopup from 'components/Teams/TeamStatusPopup';
+import { renderWithProvider } from '__tests__/utils';
+
+const defaultProps = {
+  open: true,
+  selectedTeamName: 'Team 1',
+  selectedStatus: true,
+  onClose: () => {},
+  onConfirmClick: () => {},
+};
+
+describe('TeamStatusPopup', () => {
+  it('should render correctly', () => {
+    const { getByText } = renderWithProvider(<TeamStatusPopup {...defaultProps} />);
+
+    expect(getByText('Status Popup')).toBeInTheDocument();
+
+    expect(
+      getByText('Are you sure you want to change the status of this team Team 1'),
+    ).toBeInTheDocument();
+
+    expect(getByText('Confirm')).toBeInTheDocument();
+    expect(getByText('Close')).toBeInTheDocument();
+  });
+});
