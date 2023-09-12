7a3f039323b38aece00e668b6750cca18fb2bd74
eduvarjaor <eduardovarjaor@gmail.com>
2023-09-12 18:23:18 -0300

    test(TeamTableHeader): rendering test added

--- src/__tests__/Teams/TeamTableHeader.test.js
+++ src/__tests__/Teams/TeamTableHeader.test.js
@@ -1,1 +1,20 @@
-describe('TeamTableHeader', () => {});
+import React from 'react';
+import { shallow } from 'enzyme';
+import TeamTableHeader from 'components/Teams/TeamTableHeader';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+
+describe('TeamTableHeader Component', () => {
+  it('should render correctly', () => {
+    const mockStore = configureStore([]);
+    const store = mockStore({});
+
+    const wrapper = shallow(
+      <Provider store={store}>
+        <TeamTableHeader hasPermission={() => true} />
+      </Provider>,
+    );
+
+    expect(wrapper.exists()).toBe(true);
+  });
+});
