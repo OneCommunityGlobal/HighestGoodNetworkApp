4086e26afa82c29d98d9450e37715136e8da2210
eduvarjaor <eduardovarjaor@gmail.com>
2023-09-20 17:21:09 -0300

    test(TeamTableSearchPanel): rendering without crashing

--- src/__tests__/Teams/TeamTableSearchPanel.test.js
+++ src/__tests__/Teams/TeamTableSearchPanel.test.js
@@ -1,1 +1,29 @@
-describe('TeamTableSearchPanel', () => {});
+import React from 'react';
+import TeamTableSearchPanel from 'components/Teams/TeamTableSearchPanel';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import { render } from '@testing-library/react';
+import thunk from 'redux-thunk';
+import { authMock, userProfileMock, rolesMock } from '../../__tests__/mockStates';
+
+const mockStore = configureStore([thunk]);
+
+let store;
+
+beforeEach(() => {
+  store = mockStore({
+    auth: authMock,
+    userProfile: userProfileMock,
+    role: rolesMock.role,
+  });
+});
+
+describe('TeamTableSearchPanel', () => {
+  it('should render without crashing', () => {
+    render(
+      <Provider store={store}>
+        <TeamTableSearchPanel />;
+      </Provider>,
+    );
+  });
+});
