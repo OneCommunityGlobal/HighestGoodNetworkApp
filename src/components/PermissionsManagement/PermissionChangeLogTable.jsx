035162b7aa53e78ce6c512fde03f7aa235209bc1
Jerry Ren <jerryren.5279@gmail.com>
2023-09-20 20:32:00 -0400

    Added styling to table

--- src/components/PermissionsManagement/PermissionChangeLogTable.jsx
+++ src/components/PermissionsManagement/PermissionChangeLogTable.jsx
@@ -1,33 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import "./PermissionChangeLogTable.css";
+
 
 const PermissionChangeLogTable = ({ changeLogs }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 20;
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = changeLogs.slice(indexOfFirstItem, indexOfLastItem);
+
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Log Date Time</th>
-          <th>Role ID</th>
-          <th>Role Name</th>
-          <th>Permissions</th>
-          <th>Requestor ID</th>
-          <th>Requestor Role</th>
-          <th>Requestor Email</th>
-        </tr>
-      </thead>
-      <tbody>
-        {changeLogs.map(log => (
-          <tr key={log._id}>
-            <td>{log.logDateTime}</td>
-            <td>{log.roleId}</td>
-            <td>{log.roleName}</td>
-            <td>{log.permissions.join(', ')}</td>
-            <td>{log.requestorId}</td>
-            <td>{log.requestorRole}</td>
-            <td>{log.requestorEmail}</td>
+    <>
+      <table className='permission-change-log-table' style={{ borderCollapse: 'collapse', width: '100%' }}>
+        <thead>
+          <tr>
+            <th className='permission-change-log-table--header'>Log Date Time</th>
+            <th className='permission-change-log-table--header'>Role ID</th>
+            <th className='permission-change-log-table--header'>Role Name</th>
+            <th className='permission-change-log-table--header'>Permissions</th>
+            <th className='permission-change-log-table--header'>Permissions Added</th>
+            <th className='permission-change-log-table--header'>Permissions Removed</th>
+            <th className='permission-change-log-table--header'>Requestor ID</th>
+            <th className='permission-change-log-table--header'>Requestor Role</th>
+            <th className='permission-change-log-table--header'>Requestor Email</th>
           </tr>
+        </thead>
+        <tbody>
+          {currentItems.map(log => (
+            <tr key={log._id}>
+              {/* ... (same as before) */}
+              <td className='permission-change-log-table--cell'>{log.logDateTime}</td>
+              <td className='permission-change-log-table--cell'>{log.roleId}</td>
+              <td className='permission-change-log-table--cell'>{log.roleName}</td>
+              <td className='permission-change-log-table--cell'>{log.permissions.join(', ')}</td>
+              <td className='permission-change-log-table--cell'>{log.permissionsAdded.join(', ')}</td>
+              <td className='permission-change-log-table--cell'>{log.permissionsRemoved.join(', ')}</td>
+              <td className='permission-change-log-table--cell'>{log.requestorId}</td>
+              <td className='permission-change-log-table--cell'>{log.requestorRole}</td>
+              <td className='permission-change-log-table--cell'>{log.requestorEmail}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+        <div className="pagination">
+        {Array.from({ length: Math.ceil(changeLogs.length / itemsPerPage) }).map((_, index) => (
+          <button 
+            key={index} 
+            onClick={() => paginate(index + 1)} 
+            className='permission-change-log-table--button'
+            style={{
+              backgroundColor: index + 1 === currentPage ? '#103a8d' : '#1d62f0',
+            }}>
+            {index + 1}
+          </button>
         ))}
-      </tbody>
-    </table>
+      </div>
+    </>
   );
 };
 
