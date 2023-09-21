bb9efeeba5daedaca25a446fc8b697454e32dcd3
eduvarjaor <eduardovarjaor@gmail.com>
2023-09-21 13:53:32 -0300

    test(TeamsOverview): should render correctly

--- src/__tests__/Teams/TeamsOverview.test.js
+++ src/__tests__/Teams/TeamsOverview.test.js
@@ -1,1 +1,9 @@
-describe('TeamsOverview', () => {});
+import React from 'react';
+import TeamsOverview from 'components/Teams/TeamsOverview';
+import { renderWithProvider } from '__tests__/utils';
+
+describe('TeamsOverview', () => {
+  it('should render correctly', () => {
+    renderWithProvider(<TeamsOverview />);
+  });
+});
