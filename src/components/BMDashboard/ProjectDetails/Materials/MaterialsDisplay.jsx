8c85d9e0ecf0f9e370fe5d9b4e1e892f8bfe1d26
Olga Yudkin <cvtqx@protonmail.com>
2023-09-21 10:05:50 -0400

    fixed cards responsiveness

--- src/components/BMDashboard/ProjectDetails/Materials/MaterialsDisplay.jsx
+++ src/components/BMDashboard/ProjectDetails/Materials/MaterialsDisplay.jsx
@@ -5,7 +5,7 @@ import Materials from './Materials';
 function MaterialsDisplay() {
   return (
     <Card className="cards-container">
-      <h6>Materials with quantity less than 20% left</h6>
+      <h6 className="cards-container_header">Materials with quantity less than 20% left</h6>
       <Materials />
     </Card>
   );
--- src/components/BMDashboard/ProjectDetails/RentedTools/RentedToolsDisplay.jsx
+++ src/components/BMDashboard/ProjectDetails/RentedTools/RentedToolsDisplay.jsx
@@ -5,7 +5,7 @@ import ToolCards from './ToolCards';
 function RentedToolsDisplay() {
   return (
     <Card className="cards-container">
-      <h6>Rented Tools or equipment to be returned in 3 days.</h6>
+      <h6 className="cards-container_header">Rented Tools or equipment to be returned in 3 days.</h6>
       <ToolCards />  
     </Card>
   );
