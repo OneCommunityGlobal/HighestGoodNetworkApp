62a88249101ff33ef9bf0067c2d3606712dc347a
wang9hu <xwang.9hu@gmail.com>
2023-09-22 21:50:23 -0700

    add another action case for weeklysummariesreportreducer and add redux thunk function to the bio switch handler

--- src/components/WeeklySummariesReport/WeeklySummariesReport.jsx
+++ src/components/WeeklySummariesReport/WeeklySummariesReport.jsx
@@ -326,12 +326,12 @@ const mapStateToProps = state => ({
   infoCollections: state.infoCollections.infos,
 });
 
-const mapDispatchToProps = dispatch => ({
-  fetchAllBadges: () => dispatch(fetchAllBadges()),
-  getWeeklySummariesReport: () => dispatch(getWeeklySummariesReport()),
-  hasPermission: () => hasPermission(),
-  getInfoCollections: () => getInfoCollections(),
-});
+const mapDispatchToProps = {
+  fetchAllBadges,
+  getWeeklySummariesReport,
+  hasPermission,
+  getInfoCollections,
+};
 
 function WeeklySummariesReportTab({ tabId, hidden, children }) {
   return <TabPane tabId={tabId}>{!hidden && children}</TabPane>;
