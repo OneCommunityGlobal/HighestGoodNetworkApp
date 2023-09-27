798217254109a498a5aba9093a9aa8126bbb078e
Tim Kent <timkentdev@gmail.com>
2023-09-27 10:37:24 -0700

    add very basic tb layout, dummy data

--- src/components/BMDashboard/MaterialsList/MaterialsList.jsx
+++ src/components/BMDashboard/MaterialsList/MaterialsList.jsx
@@ -1,6 +1,42 @@
 // import { connect } from 'react-redux';
 import { Table } from 'reactstrap';
 
+const dummyData = [
+  {
+    id: 'P1',
+    projectId: 'M1',
+    name: 'Sand',
+    unit: 'kg',
+    bought: 55,
+    used: 25,
+    available: 25,
+    hold: 5,
+    wasted: 0,
+  },
+  {
+    id: 'P1',
+    projectId: 'M2',
+    name: 'Rock',
+    unit: 'kg',
+    bought: 100,
+    used: 50,
+    available: 50,
+    hold: 0,
+    wasted: 0,
+  },
+  {
+    id: 'P2',
+    projectId: 'M3',
+    name: 'Brick',
+    unit: 'count',
+    bought: 1000,
+    used: 500,
+    available: 400,
+    hold: 100,
+    wasted: 0,
+  },
+];
+
 export default function MaterialsList() {
   return (
     <main>
@@ -23,6 +59,26 @@ export default function MaterialsList() {
               <th>Purchase Record</th>
             </tr>
           </thead>
+          <tbody>
+            {dummyData.map((mat, idx) => {
+              return (
+                <tr key={idx}>
+                  <td>{mat.projectId}</td>
+                  <td>{mat.id}</td>
+                  <td>{mat.name}</td>
+                  <td>{mat.unit}</td>
+                  <td>{mat.bought}</td>
+                  <td>{mat.used}</td>
+                  <td>{mat.available}</td>
+                  <td>{mat.hold}</td>
+                  <td>{mat.wasted}</td>
+                  <td>Button</td>
+                  <td>Button</td>
+                  <td>Button</td>
+                </tr>
+              );
+            })}
+          </tbody>
         </Table>
       </section>
     </main>
