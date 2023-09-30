7dfd808418227aff1af8f31bfe1e163531d788b5
Zijie Yu <itvxyu@gmail.com>
2023-09-29 22:55:41 -0700

    wbsTable.jsx

--- src/components/Reports/WbsTable/WbsTable.jsx
+++ src/components/Reports/WbsTable/WbsTable.jsx
@@ -1,23 +1,24 @@
 import { Stub } from 'components/common/Stub';
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import './WbsTable.css';
 
-export const WbsTable = ({ wbs, skip, take }) => {
+function WbsTable({ wbs, skip, take }) {
   let WbsList = [];
   if (wbs.fetched) {
     if (wbs.WBSItems.length > 0) {
       WbsList = wbs.WBSItems.slice(skip, skip + take).map((item, index) => (
-        <div className="wbs-table-row" id={'tr_' + item._id} key={item._id}>
+        <div className="wbs-table-row" id={`tr_${item._id}`} key={item._id}>
           <div>{skip + index + 1}</div>
           <div>{item.wbsName}</div>
           <div className="projects__active--input">
             {item.isActive ? (
               <tasks className="isActive">
-                <i className="fa fa-circle" aria-hidden="true"></i>
+                <i className="fa fa-circle" aria-hidden="true" />
               </tasks>
             ) : (
               <div className="isNotActive">
-                <i className="fa fa-circle-o" aria-hidden="true"></i>
+                <i className="fa fa-circle-o" aria-hidden="true" />
               </div>
             )}
           </div>
@@ -29,8 +30,10 @@ export const WbsTable = ({ wbs, skip, take }) => {
 
   return (
     <div className="wbs-table">
-      <h5 style={{marginBottom: '2.125rem'}} className="wbs-table-title">WBS</h5>
-      <div style={{marginBottom: '0px'}} className="reports-table-head wbs-table-row">
+      <h5 style={{ marginBottom: '2.125rem' }} className="wbs-table-title">
+        WBS
+      </h5>
+      <div style={{ marginBottom: '0px' }} className="reports-table-head wbs-table-row">
         <div className="wbs-table-cell">#</div>
         <div className="wbs-table-cell">Name</div>
         <div className="wbs-table-cell">Active</div>
@@ -39,4 +42,6 @@ export const WbsTable = ({ wbs, skip, take }) => {
       <div>{WbsList.length > 0 ? WbsList : <Stub />}</div>
     </div>
   );
-};
+}
+
+export default WbsTable;
