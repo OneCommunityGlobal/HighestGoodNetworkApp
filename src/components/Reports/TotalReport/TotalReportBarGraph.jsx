58913f989861170bd6a60d2f53dd56b2d4629f16
Zijie Yu <itvxyu@gmail.com>
2023-09-30 11:25:40 -0700

    fixed TotalReportBarGraph.jsx

--- src/components/Reports/TotalReport/TotalReportBarGraph.jsx
+++ src/components/Reports/TotalReport/TotalReportBarGraph.jsx
@@ -1,18 +1,21 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useEffect } from 'react';
 import * as d3 from 'd3/dist/d3.min';
 import './TotalReportBarGraph.css';
 
-const TotalReportBarGraph = props => {
-  const svg_id = 'svg-container-' + props.range;
+function TotalReportBarGraph({ barData, range }) {
+  const svgId = `svg-container-${range}`;
 
   const drawChart = data => {
     data.sort((a, b) => (a.label > b.label ? 1 : -1));
-    const maxValue = Number(data.reduce((p, c) => (p.value - c.value > 0 ? p : c)).value);
+    const maxValue = Number(
+      data.reduce((prev, curr) => (prev.value - curr.value > 0 ? prev : curr)).value,
+    );
     const margin = { top: 10, right: 8, bottom: 15, left: 20 };
     const width = 500 - margin.left - margin.right;
     const height = 300 - margin.top - margin.bottom;
 
-    const svg = d3.select('#' + svg_id);
+    const svg = d3.select(`#${svgId}`);
     svg.selectAll('*').remove();
     const chart = svg.append('g').attr('transform', `translate(${margin.left}, ${margin.top})`);
 
@@ -27,7 +30,7 @@ const TotalReportBarGraph = props => {
       .range([height, 10])
       .domain([0, maxValue]);
 
-    var colorScale = d3
+    const colorScale = d3
       .scaleLinear()
       .domain([0, maxValue])
       .range(['darksalmon', 'darkslateblue']);
@@ -46,39 +49,41 @@ const TotalReportBarGraph = props => {
     barGroups
       .append('rect')
       .attr('class', 'bar')
-      .attr('x', g => xScale(g.label))
-      .attr('y', g => yScale(g.value))
-      .attr('height', g => height - yScale(g.value))
+      .attr('x', d => xScale(d.label))
+      .attr('y', d => yScale(d.value))
+      .attr('height', d => height - yScale(d.value))
       .attr('width', xScale.bandwidth())
-      .attr('fill', g => colorScale(g.value))
-      .on('mouseenter', function(actual, i) {
+      .attr('fill', d => colorScale(d.value))
+      // eslint-disable-next-line no-unused-vars
+      .on('mouseenter', (_, i) => {
         d3.selectAll('.value').attr('opacity', 0);
-        d3.select(this)
+        d3.select(d3.event.currentTarget)
           .transition()
           .duration(300)
           .attr('opacity', 0.6);
         barGroups
           .append('text')
           .attr('class', 'value')
-          .attr('x', a => xScale(a.label) + xScale.bandwidth() / 2)
-          .attr('y', a => yScale(a.value))
+          .attr('x', d => xScale(d.label) + xScale.bandwidth() / 2)
+          .attr('y', d => yScale(d.value))
           .attr('text-anchor', 'middle')
-          .text(a => `${a.value}`)
+          .text(d => `${d.value}`)
           .style('fill', 'black');
+
         if (data[0].months) {
           barGroups
             .append('text')
             .attr('class', 'value')
-            .attr('x', a => xScale(a.label) + xScale.bandwidth() / 2)
-            .attr('y', a => yScale(0)+30)
+            .attr('x', d => xScale(d.label) + xScale.bandwidth() / 2)
+            .attr('y', yScale(0) + 30)
             .attr('text-anchor', 'middle')
-            .text(a => `${a.months} mos.`)
+            .text(d => `${d.months} mos.`)
             .style('fill', 'black');
         }
       })
-      .on('mouseleave', function() {
+      .on('mouseleave', () => {
         d3.selectAll('.value').attr('opacity', 1);
-        d3.select(this)
+        d3.select(d3.event.currentTarget)
           .transition()
           .duration(300)
           .attr('opacity', 1);
@@ -87,16 +92,16 @@ const TotalReportBarGraph = props => {
   };
 
   useEffect(() => {
-    if (props.barData.length > 0) {
-      drawChart(props.barData);
+    if (barData.length > 0) {
+      drawChart(barData);
     }
-  }, [props.barData]);
+  }, [barData]);
 
   return (
     <div className="svg-container">
-      <svg id={svg_id} className="svg-chart"></svg>
+      <svg id={svgId} className="svg-chart" />
     </div>
   );
-};
+}
 
 export default TotalReportBarGraph;
