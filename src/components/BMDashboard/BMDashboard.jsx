a817b317de0f8727b4fb302d2746e63f646c5d0c
Olga Yudkin <cvtqx@protonmail.com>
2023-10-06 12:12:51 -0400

    added select functionality and history.pushto projectselectform

--- src/components/BMDashboard/BMDashboard.jsx
+++ src/components/BMDashboard/BMDashboard.jsx
@@ -30,14 +30,7 @@ export const BMDashboard = () => {
         <h1>Building and Inventory Management Dashboard</h1>
       </header>
       <main>
-        <Form className="w-100 p-3  text-center">
-          <Row className="ml-0 gx-5 w-75" md="2" sm="1" xs="1">
-            <ProjectSelectForm projects={dummyProjects} />
-            <Col className="p-3">
-              <Button className="bm-dashboard__button w-100">Go to Project Dashboard</Button>
-            </Col>
-          </Row>
-        </Form>
+        <ProjectSelectForm projects={dummyProjects} />
         <Row className="ml-0 mt-2 text-center">
           <ProjectsList projects={dummyProjects} />
         </Row>
---
+++ src/components/BMDashboard/Projects/ProjectDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProjectDetails = ({project}) => {
+  return (
+    <div>Welcome to the {project.projectName} Details Page</div>
+  )
+}
+
+export default ProjectDetails
