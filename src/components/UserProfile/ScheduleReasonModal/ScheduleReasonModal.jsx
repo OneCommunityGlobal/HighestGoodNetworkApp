1cc268f180069c5bf6800dc685e30dae42f1a1a0
Bhagyashree Birajdar <babirajdar95@gmail.com>
2023-10-13 21:12:45 -0700

    Uncomment condition in delete reason function

--- src/components/UserProfile/ScheduleReasonModal/ScheduleReasonModal.jsx
+++ src/components/UserProfile/ScheduleReasonModal/ScheduleReasonModal.jsx
@@ -7,6 +7,7 @@ import Alert from 'react-bootstrap/Alert';
 import { useEffect } from 'react';
 import { getReasonByDate } from 'actions/reasonsActions';
 import { boxStyle } from 'styles';
+import './ScheduleReason.css';
 
 const ScheduleReasonModal = ({
   handleClose,
@@ -43,20 +44,25 @@ const ScheduleReasonModal = ({
   return (
     <>
       <Modal.Header closeButton={true}>
-        <Modal.Title>Schedule a reason for the weekly blue square</Modal.Title>
+        <Modal.Title className=" text-center">Choose to Use a Blue Square<br/>
+         (function under development)
+        </Modal.Title>
+        
       </Modal.Header>
       <Form onSubmit={handleSubmit}>
         <Modal.Body>
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
             <Form.Label>
-              Schedule a reason to be used on this weekend's blue square for {user.firstName}
+              {/* Schedule a reason to be used on this weekend's blue square for {user.firstName} */}
+              Need to take a week off for an emergency or vacation? That's no problem. The system will still issue you a blue square but scheduling here will note this reason on it so it's clear you chose to use one (vs receiving one for missing something) and let us know in advance. Blue squares are meant for situations like this and we allow 5 a year.
             </Form.Label>
             <Form.Label>
               <p>
-                <em>(If a reason has already been submitted, you can edit it)</em>
+                <em>To schedule your time off, you need to CHOOSE THE SUNDAY OF THE WEEK YOU’LL RETURN. This is the date needed so your reason ends up on the blue square that will be auto-issued AT THE END OF THE WEEK YOU'LL BE GONE.</em>
               </p>
             </Form.Label>
-            <Form.Label>Pick a date to schedule your reason!</Form.Label>
+            <Form.Label>Choose the Sunday of the week you'll return: 
+              </Form.Label>
             <Form.Control
               name="datePicker"
               type="date"
@@ -66,12 +72,13 @@ const ScheduleReasonModal = ({
                 setDate(e.target.value);
               }}
             />
-            <Form.Label className="mt-4">Write the blue square's reason</Form.Label>
+            <Form.Label className="mt-4">What is your reason for requesting this time off?</Form.Label>
             <Form.Control
               as="textarea"
               rows={3}
               name="BlueSquareReason"
               className="w-100"
+              placeholder="Please be detailed in describing your reason and, if it is different than your scheduled Sunday, include the expected date you’ll return to work."
               value={reason}
               onChange={e => {
                 setReason(e.target.value);
