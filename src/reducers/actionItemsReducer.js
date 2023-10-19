d6e42617a092605367f47352651087ee8f1b795c
Zijie Yu <itvxyu@gmail.com>
2023-10-18 22:13:34 -0700

    1

--- src/reducers/actionItemsReducer.js
+++ src/reducers/actionItemsReducer.js
@@ -1,4 +1,5 @@
-export const actionItemsReducer = (actionItems = null, action) => {
+// eslint-disable-next-line import/prefer-default-export
+export const actionItemsReducer = (action, actionItems = null) => {
   if (action.type === 'GET_ACTION_ITEMS') {
     return action.payload;
   }
