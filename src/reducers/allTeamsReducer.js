30fbc2e8425922be9e92fd2ab7aa99d0cbdd8689
Zijie Yu <itvxyu@gmail.com>
2023-10-18 21:36:20 -0700

    allTimeEntriesReducer.js

--- src/reducers/allTeamsReducer.js
+++ src/reducers/allTeamsReducer.js
@@ -12,7 +12,7 @@ export const updateObject = (oldObject, updatedProperties) => ({
   ...updatedProperties,
 });
 
-export const allUserTeamsReducer = (allTeams = userTeamsInitial, action) => {
+export const allUserTeamsReducer = (action, allTeams = userTeamsInitial) => {
   switch (action.type) {
     case types.FETCH_USER_TEAMS_START:
       return { ...allTeams, fetching: true, status: '200' };
@@ -36,7 +36,7 @@ export const allUserTeamsReducer = (allTeams = userTeamsInitial, action) => {
         status: '200',
       });
 
-    case types.USER_TEAMS_UPDATE:
+    case types.USER_TEAMS_UPDATE: {
       const index = allTeams.allTeams.findIndex(team => team._id === action.team._id);
       return updateObject(allTeams, {
         allTeams: Object.assign([
@@ -48,6 +48,7 @@ export const allUserTeamsReducer = (allTeams = userTeamsInitial, action) => {
         fetched: true,
         status: '200',
       });
+    }
 
     case types.TEAMS_DELETE:
       return updateObject(allTeams, {
@@ -57,7 +58,7 @@ export const allUserTeamsReducer = (allTeams = userTeamsInitial, action) => {
         status: '200',
       });
 
-    case types.UPDATE_TEAM:
+    case types.UPDATE_TEAM: {
       const teams = Object.assign([...allTeams.allTeams]);
       const updatedTeam = teams.find(team => team._id === action.teamId);
       updatedTeam.isActive = action.isActive;
@@ -68,6 +69,7 @@ export const allUserTeamsReducer = (allTeams = userTeamsInitial, action) => {
         fetched: true,
         status: '200',
       });
+    }
 
     default:
       return allTeams;
--- src/reducers/allTimeEntriesReducer.js
+++ src/reducers/allTimeEntriesReducer.js
@@ -1,4 +1,5 @@
-export const allTimeEntriesReducer = (allTimeEntries = null, action) => {
+// eslint-disable-next-line import/prefer-default-export
+export const allTimeEntriesReducer = (action, allTimeEntries = null) => {
   if (action.type === 'GET_ALL_TIME_ENTRIES') {
     return action.payload;
   }
