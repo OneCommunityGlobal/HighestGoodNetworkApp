4124ef540acb4804d29b31099539ac83e2b19b10
Anirudh Ghildiyal <a.ghildiyal0506@gmail.com>
2023-10-21 16:16:25 +0530

    fixing edit form bug

--- src/components/Timelog/WeeklySummaries.jsx
+++ src/components/Timelog/WeeklySummaries.jsx
@@ -1,50 +1,11 @@
-// import React from 'react';
-// import parse from 'html-react-parser';
-
-// const WeeklySummaries = ({ userProfile, onEditClick }) => {
-//   if (!userProfile.weeklySummaries || userProfile.weeklySummaries.length < 3) {
-//     return <div>No weekly summaries available</div>;
-//   }
-
-//   const renderSummary = (title, summary, index) => {
-//     if (summary) {
-//       return (
-//         <div>
-//           <h3>{title}</h3>
-//           {parse(summary)}
-//           <button onClick={() =>onEditClick(index)}>Edit</button>
-//         </div>
-//       );
-//     } else {
-//       return (
-//         <div>
-//           <h3>{title}</h3>
-//           <p>
-//             {userProfile.firstName} {userProfile.lastName} did not submit a summary.
-//           </p>
-//         </div>
-//       );
-//     }
-//   };
-
-//   return (
-//     <div>
-//       {renderSummary("This week's summary", userProfile.weeklySummaries[0]?.summary)}
-//       {renderSummary("Last week's summary", userProfile.weeklySummaries[1]?.summary)}
-//       {renderSummary("The week before last's summary", userProfile.weeklySummaries[2]?.summary)}
-//     </div>
-//   );
-// };
-
-// export default WeeklySummaries;
-
-
 import React, { useState, useEffect } from 'react';
 import parse from 'html-react-parser';
 import ReactHtmlParser from 'react-html-parser';
 import './Timelog.css'
 import updateWeeklySummaries from 'actions/weeklySummaries';
-
+import { updateUserProfile } from 'actions/userProfile';
+import hasPermission from 'utils/permissions';
+import { connect, useDispatch, useSelector } from 'react-redux';
 
 const WeeklySummaries = ({ userProfile }) => {
   // Initialize state variables for editing and original summaries
@@ -55,6 +16,16 @@ const WeeklySummaries = ({ userProfile }) => {
     userProfile.weeklySummaries[2]?.summary || '',
   ]);
   const [originalSummaries, setOriginalSummaries] = useState([...editedSummaries]);
+
+  // const hasPermission = useSelector(state => state.hasPermission); // Assuming you have the permissions in your Redux state
+  // const canEditSummary = hasPermission('editWeeklySummaryOptions');
+  // if(canEditSummary){
+  //   console.log("Yes");
+  // }
+  // else{
+  //   console.log("No");
+  // }
+  const dispatch = useDispatch();
 
   if (!userProfile.weeklySummaries || userProfile.weeklySummaries.length < 3) {
     return <div>No weekly summaries available</div>;
@@ -85,34 +56,21 @@ const WeeklySummaries = ({ userProfile }) => {
   };
 
   const handleSave = (index) => {
-    // // Save the edited summary content and toggle off editing mode
-    // // Update the userProfile with the latest edited summary
-    //   const updatedUserProfile = { ...userProfile };
-    //   updatedUserProfile.weeklySummaries[index].summary = editedSummaries[index];
-
-    //   // Update the state with the edited content
-    //   // setUserProfile(updatedUserProfile);
-    //   userProfile = updatedUserProfile;
-    // // Toggle off editing mode
-    // toggleEdit(index);
+    // Save the edited summary content and toggle off editing mode
+    
     const editedSummary = editedSummaries[index];
-
+    // const editedSummary = editedSummary1.replace(/{{NEWLINE}}/g, "\n");
     // Check if the edited summary is not blank and contains at least 50 words
     const wordCount = editedSummary.split(/\s+/).filter(Boolean).length;
     if (editedSummary.trim() !== '' && wordCount >= 50) {
-      // Valid summary, proceed to save it
-
-      // Create a copy of userProfile
-      const updatedUserProfile = { ...userProfile };
-
-      // Update the summary in the copy
-      updatedUserProfile.weeklySummaries[index].summary = editedSummary;
-
-      // Update the state with the edited content using updateUserProfile
-      // setUserProfile(updatedUserProfile);
-      userProfile = updatedUserProfile;
-      // Save the edited summary to local storage
-      // localStorage.setItem(`editedSummary_${index}`, editedSummary);
+      const updatedUserProfile = {
+        ...userProfile,
+        weeklySummaries: userProfile.weeklySummaries.map((item, i) =>
+          i === index ? { ...item, summary: editedSummary } : item
+        )
+      };
+  
+    dispatch(updateUserProfile(userProfile._id, updatedUserProfile));
 
       // Toggle off editing mode
       toggleEdit(index);
@@ -125,18 +83,21 @@ const WeeklySummaries = ({ userProfile }) => {
 
   const renderSummary = (title, summary, index) => {
     if (editing[index]) {
-      const stripHtmlTags = (html) => {
-        const tmp = document.createElement("div");
-        tmp.innerHTML = html;
-        return tmp.textContent || tmp.innerText || "";
-      };
+      // const stripHtmlTags = (html) => {
+      //   const tmp = document.createElement("div");
+      //   tmp.innerHTML = html.replace(/\n/g, "<br>");
+      //   return tmp.textContent || tmp.innerText || "";
+      // };
+      // const editedContentWithPlaceholder = editedSummaries[index].replace(/\n/g, "{{NEWLINE}}");
       // Render an edit form when editing is active
       return (
         <div>
           <h3>{title}</h3>
           <textarea
             className = "edit-textarea"
-            value={stripHtmlTags(editedSummaries[index])}
+            value={editedSummaries[index]}
+            // value={stripHtmlTags(editedContentWithPlaceholder)}
+            // value={stripHtmlTags(editedSummaries[index])}
             onChange={(event) => handleSummaryChange(event, index)}
           />
           <button className = "button save-button" onClick={() => handleSave(index)}>Save</button>
@@ -174,4 +135,6 @@ const WeeklySummaries = ({ userProfile }) => {
   );
 };
 
-export default WeeklySummaries;
+const mapStateToProps = state => state;
+export default connect(mapStateToProps, { hasPermission })(WeeklySummaries);
+// export default WeeklySummaries;
