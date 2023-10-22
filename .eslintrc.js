208aa80c88214a7501fbd53511810befe67b0767
Ramya Non-IU <ramyaram2092@outlook.com>
2023-10-21 21:49:28 -0700

    added 2 new rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -54,6 +54,7 @@ module.exports = {
     'no-console': 'error',
     'jsx-a11y/click-events-have-key-events': 0,
     'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'jsx-a11y/no-static-element-interactions': 'off',
   },
   overrides: [
     {
