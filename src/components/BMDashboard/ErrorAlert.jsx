e4c61c8a657e7230a2bde8c3fcb9c107178b2701
Olga Yudkin <cvtqx@protonmail.com>
2023-10-24 11:32:38 -0400

    fixed lint errors

--- src/components/BMDashboard/ErrorAlert.jsx
+++ src/components/BMDashboard/ErrorAlert.jsx
@@ -1,11 +1,5 @@
-
-function ErrorAlert({error, message}){
-  return (
-    error && (
-      <div className="alert alert-danger m-2">{message}</div>
-    )
-  )
+function ErrorAlert({ error, message }) {
+  return error && <div className="alert alert-danger m-2">{message}</div>;
 }
 
-
-export default ErrorAlert
+export default ErrorAlert;
