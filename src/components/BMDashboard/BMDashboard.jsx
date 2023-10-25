1be430e02a440f9badeb2193f50d9a9b82a066db
Olga Yudkin <cvtqx@protonmail.com>
2023-10-25 09:49:16 -0400

    dummy data updated

--- src/components/BMDashboard/BMDashboard.jsx
+++ src/components/BMDashboard/BMDashboard.jsx
@@ -6,6 +6,289 @@ import './BMDashboard.css';
 import axios from 'axios';
 import { ENDPOINTS } from 'utils/URL';
 
+//data for project details
+
+// const dummyProjects = [
+//   {
+//     projectId: 1,
+//     projectName: 'Big project',
+//     tools: [
+//       {
+//         inventoryItemId: 1,
+//         title: 'forklift',
+//         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
+//         rentedOnDate: '',
+//         rentDuration: '',
+//         cost: '200',
+//       },
+//       {
+//         inventoryItemId: 2,
+//         title: 'saw',
+//         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
+//         rentedOnDate: '',
+//         rentDuration: '',
+//         cost: '200',
+//       },
+//     ],
+//     materials: [
+//       {
+//         inventoryItemId: 3,
+//         title: 'soil mix',
+//         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
+//         amountTotal: '100lb',
+//         amountUsed: '80lb',
+//         cost: '200',
+//       },
+//       {
+//         inventoryItemId: 4,
+//         title: '1/2" pea gravel',
+//         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
+//         amountTotal: '100lb',
+//         amountUsed: '95lb',
+//         cost: '200',
+//       },
+//     ],
+//     people: [
+//       {
+//         personId: 1,
+//         personName: 'Dora',
+//         personLastName: 'Kimberly',
+//         role: 'Carpenter',
+//         team: 'XYZ',
+//         currentTask: 'Stud wall construction',
+//         totalHrs: 169,
+//         todayHrs: 5.5,
+//       },
+//       {
+//         personId: 2,
+//         personName: 'Cailin',
+//         personLastName: 'Colby',
+//         role: 'Volunteer',
+//         team: 'ABC',
+//         currentTask: 'Foundation concreting',
+//         totalHrs: 15,
+//         todayHrs: 5,
+//       },
+//       {
+//         personId: 3,
+//         personName: 'John',
+//         personLastName: 'Smith',
+//         role: 'Role A',
+//         team: 'XYZ',
+//         currentTask: 'Task 1',
+//         totalHrs: 169,
+//         todayHrs: 5.5,
+//       },
+//     ],
+//   },
+//   {
+//     projectId: 2,
+//     projectName: 'Bigger project',
+//     tools: [
+//       {
+//         inventoryItemId: 1,
+//         title: 'forklift',
+//         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
+//         rentedOnDate: '',
+//         rentDuration: '',
+//       },
+//       {
+//         inventoryItemId: 2,
+//         title: 'saw',
+//         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
+//         rentedOnDate: '',
+//         rentDuration: '',
+//       },
+//     ],
+//     materials: [
+//       {
+//         inventoryItemId: 3,
+//         title: 'soil mix',
+//         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
+//         amountTotal: '100lb',
+//         amountUsed: '80lb',
+//       },
+//       {
+//         inventoryItemId: 4,
+//         title: '1/2" pea gravel',
+//         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
+//         amountTotal: '100lb',
+//         amountUsed: '95lb',
+//       },
+//     ],
+//     people: [
+//       {
+//         personId: 1,
+//         personName: 'Dora',
+//         personLastName: 'Kimberly',
+//         role: 'Carpenter',
+//         team: 'XYZ',
+//         currentTask: 'Stud wall construction',
+//         totalHrs: 169,
+//         todayHrs: 5.5,
+//       },
+//       {
+//         personId: 2,
+//         personName: 'Cailin',
+//         personLastName: 'Colby',
+//         role: 'Volunteer',
+//         team: 'ABC',
+//         currentTask: 'Foundation concreting',
+//         totalHrs: 15,
+//         todayHrs: 5,
+//       },
+//       {
+//         personId: 3,
+//         personName: 'John',
+//         personLastName: 'Smith',
+//         role: 'Role A',
+//         team: 'XYZ',
+//         currentTask: 'Task 1',
+//         totalHrs: 169,
+//         todayHrs: 5.5,
+//       },
+//     ],
+//   },
+//   {
+//     projectId: 3,
+//     projectName: 'Important project',
+//     tools: [
+//       {
+//         inventoryItemId: 1,
+//         title: 'forklift',
+//         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
+//         rentedOnDate: '',
+//         rentDuration: '',
+//       },
+//       {
+//         inventoryItemId: 2,
+//         title: 'saw',
+//         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
+//         rentedOnDate: '',
+//         rentDuration: '',
+//       },
+//     ],
+//     materials: [
+//       {
+//         inventoryItemId: 3,
+//         title: 'soil mix',
+//         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
+//         amountTotal: '100lb',
+//         amountUsed: '80lb',
+//       },
+//       {
+//         inventoryItemId: 4,
+//         title: '1/2" pea gravel',
+//         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
+//         amountTotal: '100lb',
+//         amountUsed: '95lb',
+//       },
+//     ],
+//     people: [
+//       {
+//         personId: 1,
+//         personName: 'Dora',
+//         personLastName: 'Kimberly',
+//         role: 'Carpenter',
+//         team: 'XYZ',
+//         currentTask: 'Stud wall construction',
+//         totalHrs: 169,
+//         todayHrs: 5.5,
+//       },
+//       {
+//         personId: 2,
+//         personName: 'Cailin',
+//         personLastName: 'Colby',
+//         role: 'Volunteer',
+//         team: 'ABC',
+//         currentTask: 'Foundation concreting',
+//         totalHrs: 15,
+//         todayHrs: 5,
+//       },
+//       {
+//         personId: 3,
+//         personName: 'John',
+//         personLastName: 'Smith',
+//         role: 'Role A',
+//         team: 'XYZ',
+//         currentTask: 'Task 1',
+//         totalHrs: 169,
+//         todayHrs: 5.5,
+//       },
+//     ],
+//   },
+//   {
+//     projectId: 4,
+//     projectName: 'Very important project',
+//     tools: [
+//       {
+//         inventoryItemId: 1,
+//         title: 'forklift',
+//         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
+//         rentedOnDate: '',
+//         rentDuration: '',
+//       },
+//       {
+//         inventoryItemId: 2,
+//         title: 'saw',
+//         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
+//         rentedOnDate: '',
+//         rentDuration: '',
+//       },
+//     ],
+//     materials: [
+//       {
+//         inventoryItemId: 3,
+//         title: 'soil mix',
+//         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
+//         amountTotal: '100lb',
+//         amountUsed: '80lb',
+//       },
+//       {
+//         inventoryItemId: 4,
+//         title: '1/2" pea gravel',
+//         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
+//         amountTotal: '100lb',
+//         amountUsed: '95lb',
+//       },
+//     ],
+//     people: [
+//       {
+//         personId: 1,
+//         personName: 'Dora',
+//         personLastName: 'Kimberly',
+//         role: 'Carpenter',
+//         team: 'XYZ',
+//         currentTask: 'Stud wall construction',
+//         totalHrs: 169,
+//         todayHrs: 5.5,
+//       },
+//       {
+//         personId: 2,
+//         personName: 'Cailin',
+//         personLastName: 'Colby',
+//         role: 'Volunteer',
+//         team: 'ABC',
+//         currentTask: 'Foundation concreting',
+//         totalHrs: 15,
+//         todayHrs: 5,
+//       },
+//       {
+//         personId: 3,
+//         personName: 'John',
+//         personLastName: 'Smith',
+//         role: 'Role A',
+//         team: 'XYZ',
+//         currentTask: 'Task 1',
+//         totalHrs: 169,
+//         todayHrs: 5.5,
+//       },
+//     ],
+//   },
+// ];
+
+//data for projects summary
+
 const dummyProjects = [
   {
     projectId: 1,
@@ -17,6 +300,7 @@ const dummyProjects = [
         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
         rentedOnDate: '',
         rentDuration: '',
+        cost: '200',
       },
       {
         inventoryItemId: 2,
@@ -24,6 +308,7 @@ const dummyProjects = [
         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
         rentedOnDate: '',
         rentDuration: '',
+        cost: '200',
       },
     ],
     materials: [
@@ -33,6 +318,7 @@ const dummyProjects = [
         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
         amountTotal: '100lb',
         amountUsed: '80lb',
+        cost: '200',
       },
       {
         inventoryItemId: 4,
@@ -40,6 +326,17 @@ const dummyProjects = [
         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
         amountTotal: '100lb',
         amountUsed: '95lb',
+        cost: '200',
+      },
+    ],
+    teams: [
+      {
+        teamId: 1,
+        name: 'ABC',
+      },
+      {
+        teamId: 2,
+        name: 'XYZ',
       },
     ],
     people: [
@@ -85,6 +382,7 @@ const dummyProjects = [
         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
         rentedOnDate: '',
         rentDuration: '',
+        cost: '110',
       },
       {
         inventoryItemId: 2,
@@ -92,6 +390,7 @@ const dummyProjects = [
         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
         rentedOnDate: '',
         rentDuration: '',
+        cost: '100',
       },
     ],
     materials: [
@@ -101,6 +400,7 @@ const dummyProjects = [
         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
         amountTotal: '100lb',
         amountUsed: '80lb',
+        cost: '100',
       },
       {
         inventoryItemId: 4,
@@ -108,6 +408,17 @@ const dummyProjects = [
         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
         amountTotal: '100lb',
         amountUsed: '95lb',
+        cost: '200',
+      },
+    ],
+    teams: [
+      {
+        teamId: 1,
+        name: 'ABC',
+      },
+      {
+        teamId: 2,
+        name: 'XYZ',
       },
     ],
     people: [
@@ -153,6 +464,7 @@ const dummyProjects = [
         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
         rentedOnDate: '',
         rentDuration: '',
+        cost: '100',
       },
       {
         inventoryItemId: 2,
@@ -160,6 +472,7 @@ const dummyProjects = [
         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
         rentedOnDate: '',
         rentDuration: '',
+        cost: '200',
       },
     ],
     materials: [
@@ -169,6 +482,7 @@ const dummyProjects = [
         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
         amountTotal: '100lb',
         amountUsed: '80lb',
+        cost: '100',
       },
       {
         inventoryItemId: 4,
@@ -176,6 +490,17 @@ const dummyProjects = [
         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
         amountTotal: '100lb',
         amountUsed: '95lb',
+        cost: '100',
+      },
+    ],
+    teams: [
+      {
+        teamId: 1,
+        name: 'ABC',
+      },
+      {
+        teamId: 2,
+        name: 'XYZ',
       },
     ],
     people: [
@@ -221,6 +546,7 @@ const dummyProjects = [
         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
         rentedOnDate: '',
         rentDuration: '',
+        cost: '100',
       },
       {
         inventoryItemId: 2,
@@ -228,6 +554,7 @@ const dummyProjects = [
         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
         rentedOnDate: '',
         rentDuration: '',
+        cost: '200',
       },
     ],
     materials: [
@@ -237,6 +564,7 @@ const dummyProjects = [
         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
         amountTotal: '100lb',
         amountUsed: '80lb',
+        cost: '100',
       },
       {
         inventoryItemId: 4,
@@ -244,6 +572,17 @@ const dummyProjects = [
         image: 'https://www.theforkliftcenter.com/images/forklift-hero-left.png',
         amountTotal: '100lb',
         amountUsed: '95lb',
+        cost: '200',
+      },
+    ],
+    teams: [
+      {
+        teamId: 1,
+        name: 'ABC',
+      },
+      {
+        teamId: 2,
+        name: 'XYZ',
       },
     ],
     people: [
