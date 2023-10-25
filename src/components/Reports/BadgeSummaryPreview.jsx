1133ab75021f31f2ee9bb9c79c7984142a842851
Zijie Yu <itvxyu@gmail.com>
2023-10-24 20:29:46 -0700

    final change

--- src/components/Reports/BadgeSummaryPreview.jsx
+++ src/components/Reports/BadgeSummaryPreview.jsx
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import { MdPreview } from 'react-icons/md';
 import {
--- src/components/Reports/BadgeSummaryViz.jsx
+++ src/components/Reports/BadgeSummaryViz.jsx
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import {
   Table,
