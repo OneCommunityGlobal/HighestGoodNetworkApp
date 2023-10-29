69c1d6dc7f1260439622e9c6be0f0e7dbe4e5a7f
Jiangwei-shi <56906163+Jiangwei-shi@users.noreply.github.com>
2023-10-28 21:37:03 -0400

    Update TeamTable.test.js

--- src/__tests__/TeamTable.test.js
+++ src/__tests__/TeamTable.test.js
@@ -1,19 +1,23 @@
 import { shallow } from 'enzyme';
 import React from 'react';
-import TeamTable from '../components/Reports/TeamTable'; // Adjust the import path as necessary
-import configureStore from 'redux-mock-store';
+import TeamTable from '../components/Reports/TeamTable';
 
 describe('<TeamTable/>', () => {
-  const mockStore = configureStore();
 
   it('should render without errors', () => {
     const mockTeamsData = [
       { _id: '1', teamName: 'Team1', isActive: true },
       { _id: '2', teamName: 'Team2', isActive: false },
     ];
-    const store = mockStore({});
-    const wrapper = shallow(<TeamTable store={store} allTeams={mockTeamsData} />);
+    const wrapper = shallow(<TeamTable allTeams={mockTeamsData} />);
     expect(wrapper.exists()).toBe(true);
+  });
+
+  it('should throw an error if allTeams is not an array', () => {
+    const erroneousTeamsData = "This is not an array";
+    expect(() => {
+      shallow(<TeamTable allTeams={erroneousTeamsData} />);
+    }).toThrow();
   });
 
   it('should render the correct number of team rows', () => {
@@ -25,4 +29,12 @@ describe('<TeamTable/>', () => {
     expect(wrapper.find('tr').length).toBe(mockTeamsData.length + 1); // +1 for the header row
   });
 
+  it('should have correct link to the team report', () => {
+    const mockTeamsData = [
+      { _id: '1', teamName: 'Team1', isActive: true }
+    ];
+    const wrapper = shallow(<TeamTable allTeams={mockTeamsData} />);
+    const link = wrapper.find('Link');
+    expect(link.prop('to')).toBe(`/teamreport/${mockTeamsData[0]._id}`);
+  });
 });
