6da3a545a26cb1b155ba29f3b0c721be09427400
lacnoskillz <kaikane3457@hotmail.com>
2023-10-30 18:26:28 -0500

    revert spaces

--- src/components/Lesson/LessonForm.jsx
+++ src/components/Lesson/LessonForm.jsx
@@ -1,9 +1,8 @@
 function LessonForm() {
-    return (
-      <div>
-        <h2>Hi kai</h2>
-      </div>
-    );
-  }
-  export default LessonForm;
-  
+  return (
+    <div>
+      <h2>Hi kai</h2>
+    </div>
+  );
+}
+export default LessonForm;
