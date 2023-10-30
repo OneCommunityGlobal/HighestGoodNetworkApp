5c98f30bf7c8069a5f4159e8452755803853a25e
lacnoskillz <kaikane3457@hotmail.com>
2023-10-30 18:07:06 -0500

    fixed spacing attemp to pass tests

--- src/components/Lesson/LessonForm.jsx
+++ src/components/Lesson/LessonForm.jsx
@@ -1,8 +1,8 @@
-const LessonForm = () => {
-    return(
-        <div>
-            <h2>Hi kai</h2>
-        </div>
-    )
+function LessonForm() {
+  return (
+    <div>
+      <h2>Hi kai</h2>
+    </div>
+  );
 }
-export default LessonForm
+export default LessonForm;
