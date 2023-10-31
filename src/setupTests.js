ecdcbbf100c6cdb0c3d57f700770a2c6a87ac0c6
Zijie Yu <itvxyu@gmail.com>
2023-10-31 00:22:23 -0700

    change .eslintrc.js file

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,9 +1,6 @@
 /** Used in jest.config.js */
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { configure } from 'enzyme';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import Adapter from 'enzyme-adapter-react-16';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import '@testing-library/jest-dom/extend-expect';
 
 configure({ adapter: new Adapter() });
