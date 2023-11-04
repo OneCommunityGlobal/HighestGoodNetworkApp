e594a9178d05486555cbd9bdc5641436f2dc16d9
Nouman <abidinouman@gmail.com>
2023-11-04 09:50:33 -0700

    adding tinyMCE edotor

--- src/actions/sendEmails.js
+++ src/actions/sendEmails.js
@@ -1,5 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 import axios from 'axios';
+import { toast } from 'react-toastify'; // Import the toast library
+import 'react-toastify/dist/ReactToastify.css'; // Import the toast styles
 import { ENDPOINTS } from '../utils/URL';
 
 export const sendEmail = (to, subject, html) => {
@@ -8,9 +10,21 @@ export const sendEmail = (to, subject, html) => {
   return async () => {
     try {
       const response = await axios.post(url, { to, subject, html });
-      console.log('Email sent successfully:', response.data.message);
+      console.log('Email sent successfully:', response);
+
+      // Display a success toast
+      toast.success('Email successfully sent', {
+        position: 'top-center', // You can adjust the position as needed
+        autoClose: 3000, // Close the toast after 3 seconds (adjust as needed)
+      });
     } catch (error) {
       console.error('Error sending email:', error);
+
+      // Display an error toast
+      toast.error('Error sending email', {
+        position: 'top-center', // You can adjust the position as needed
+        autoClose: 3000, // Close the toast after 3 seconds (adjust as needed)
+      });
     }
   };
 };
--- src/components/Announcements/index.jsx
+++ src/components/Announcements/index.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 import './Announcements.css';
 import { useDispatch } from 'react-redux';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { Editor } from '@tinymce/tinymce-react'; // Import Editor from TinyMCE
 import { sendEmail } from '../../actions/sendEmails';
 
 function Announcements() {
   const dispatch = useDispatch();
   const [emailList, setEmailList] = useState([]);
   const [emailContent, setEmailContent] = useState('');
+  const [emailSubject, setEmailSubject] = useState('');
   const [testEmail, setTestEmail] = useState('');
 
   const handleEmailListChange = e => {
@@ -20,61 +20,18 @@ function Announcements() {
     setTestEmail(e.target.value);
   };
 
-  const handleSendTestEmail = () => {};
+  const handleSendTestEmail = () => {
+    // Implement sending a test email if needed
+  };
+
+  // const htmlContent = `<html><head><title>Weekly Update</title></head><body>${emailContent}</body></html>`;
 
   const handleSendEmails = () => {
-    dispatch(sendEmail(emailList.join(','), 'Weekly Update', emailContent));
-  };
+    const htmlContent = emailContent;
+    // console.log(htmlContent);
 
-  const handleEmailContentChange = (event, editor) => {
-    const data = editor.getData();
-    setEmailContent(data);
-  };
-
-  const editorConfiguration = {
-    height: 1200,
-    width: 5000,
-    toolbar: {
-      items: [
-        'undo',
-        'redo',
-        '|',
-        'heading',
-        '|',
-        'fontFamily',
-        'fontSize',
-        'FontColor',
-        'FontBackgroundColor',
-        '|',
-        'bold',
-        'italic',
-        'strikethrough',
-        'subscript',
-        'superscript',
-        'code',
-        '|',
-        'link',
-        'uploadImage',
-        'blockQuote',
-        'codeBlock',
-        '|',
-        'bulletedList',
-        'numberedList',
-        'todoList',
-        'outdent',
-        'indent',
-      ],
-    },
-    language: 'en',
-    image: {
-      toolbar: [
-        'imageTextAlternative',
-        '|',
-        'imageStyle:alignLeft',
-        'imageStyle:alignCenter',
-        'imageStyle:alignRight',
-      ],
-    },
+    // Send the HTML content using your sendEmail function
+    dispatch(sendEmail(emailList.join(','), 'Weekly Update', htmlContent));
   };
 
   return (
@@ -82,11 +39,26 @@ function Announcements() {
       <div className="editor">
         <h3>Weekly Progress Editor</h3>
         <br />
-        <CKEditor
-          editor={ClassicEditor}
-          config={editorConfiguration} 
-          data={emailContent}
-          onChange={(event, editor) => handleEmailContentChange(event, editor)}
+        <Editor
+          apiKey="rpfrv9z58kbaoauzv9dncv73jeqv7c5lo73gqlk9rx5p726p" // Replace with your TinyMCE API key
+          initialValue="<p>This is the initial content of the editor</p>"
+          init={{
+            height: 500,
+            menubar: false,
+            plugins: [
+              'advlist autolink lists link image',
+              'charmap print preview anchor help',
+              'searchreplace visualblocks code',
+              'insertdatetime media table paste wordcount',
+            ],
+            toolbar:
+              'undo redo | formatselect | bold italic | \
+              alignleft aligncenter alignright | \
+              bullist numlist outdent indent | removeformat | help',
+          }}
+          onEditorChange={(content, editor) => {
+            setEmailContent(content);
+          }}
         />
         <button type="button" className="send-button" onClick={handleSendEmails}>
           Send Weekly Update
