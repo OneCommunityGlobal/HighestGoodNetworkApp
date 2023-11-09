ffea01df2bf8450a47697427c5c5296ad6bcc8ed
tsunami776 <43768723+tsunami776@users.noreply.github.com>
2023-11-09 09:33:08 -0600

    Fix Copy Prompt

--- src/components/WeeklySummary/CurrentPromptModal.jsx
+++ src/components/WeeklySummary/CurrentPromptModal.jsx
@@ -9,13 +9,13 @@ function CurrentPromptModal() {
 
   const toggle = () => setModal(!modal);
 
-  const currentPrompt = `Please edit the following summary of my week's work. Make sure it is professionally written in 3rd person format.
-  Write it as only one paragraph. It must be only one paragraph. Keep it less than 500 words. Start the paragraph with 'This week'.
-  Make sure the paragraph contains no links or URLs and write it in a tone that is matter-of-fact and without embellishment.
-  Do not add flowery language, keep it simple and factual. Do not add a final summary sentence. Apply all this to the following:`;
+  // const currentPrompt = `Please edit the following summary of my week's work. Make sure it is professionally written in 3rd person format.
+  // Write it as only one paragraph. It must be only one paragraph. Keep it less than 500 words. Start the paragraph with 'This week'.
+  // Make sure the paragraph contains no links or URLs and write it in a tone that is matter-of-fact and without embellishment.
+  // Do not add flowery language, keep it simple and factual. Do not add a final summary sentence. Apply all this to the following:`;
 
   const handleCopyToClipboard = () => {
-    navigator.clipboard.writeText(currentPrompt);
+    navigator.clipboard.writeText(prompt);
     toast.success('Prompt Copied!');
   };
 
