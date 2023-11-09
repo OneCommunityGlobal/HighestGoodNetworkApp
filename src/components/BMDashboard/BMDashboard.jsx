398701e4b4dd0584bc2c8be9debba770196c38e4
Olga Yudkin <cvtqx@protonmail.com>
2023-11-09 17:17:59 -0500

    added error state to bmdashboard

--- src/components/BMDashboard/BMDashboard.jsx
+++ src/components/BMDashboard/BMDashboard.jsx
@@ -1,15 +1,15 @@
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Container } from 'reactstrap';
-import axios from 'axios';
-import { ENDPOINTS } from 'utils/URL';
-import { connect } from 'react-redux';
 import { fetchAllProjects } from '../../actions/bmdashboard/projectActions';
 import ProjectsList from './Projects/ProjectsList';
 import ProjectSelectForm from './Projects/ProjectSelectForm';
+import BMError from './shared/BMError';
 import './BMDashboard.css';
 
 export function BMDashboard() {
+  const [isError, setIsError] = useState(false);
+
   const dispatch = useDispatch();
   const projects = useSelector(state => state.bmProjects);
   const errors = useSelector(state => state.errors);
@@ -22,20 +22,36 @@ export function BMDashboard() {
     dispatch(fetchAllProjects());
   }, []);
 
+  // trigger an error state if there is an errors object
+  useEffect(() => {
+    if (Object.entries(errors).length) {
+      setIsError(true);
+    }
+  }, [errors]);
+
   return (
     <Container className="justify-content-center align-items-center mw-80 px-4">
       <header className="bm-dashboard__header">
         <h1>Building and Inventory Management Dashboard</h1>
       </header>
       <main>
-        {projects.length ? (
+        {isError ? (
+          <BMError errors={errors} />
+        ) : (
+          <>
+            <ProjectSelectForm projects={projects} />
+            <ProjectsList projects={projects} />
+          </>
+        )}
+        {/* {isError && <BMError errors={errors} />}
+        {!isError && projects.length ? (
           <>
             <ProjectSelectForm projects={projects} />
             <ProjectsList projects={projects} />
           </>
         ) : (
           <div>Loading...</div>
-        )}
+        )} */}
       </main>
     </Container>
   );
