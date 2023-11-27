a63d9c8d9af654b910b4fe7d0900f58b186cb2df
ShihaoXiong <shihao.xiong@icloud.com>
2023-11-27 16:31:13 -0500

    DueDateTime unit test

---
+++ src/__tests__/WeeklySummary/DueDateTime.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import DueDateTime from '../../components/WeeklySummary/DueDateTime';  // Adjust the import path as needed
+import moment from 'moment-timezone';
+
+describe('DueDateTime Component Tests', () => {
+  const mockDueDate = moment();
+  const displayTime = moment(mockDueDate).tz('America/Los_Angeles').add(1, 'second');
+
+  it('should render correctly', () => {
+    render(<DueDateTime dueDate={mockDueDate} isShow={true} />);
+    expect(
+      screen
+        .getByText(`${displayTime.format('MMM-DD-YY')} at ${displayTime.format('HH:mm')} PST`)
+    ).toBeInTheDocument();
+  });
+
+  it('should display the right text when isShow switch', () => {
+    const { rerender } = render(<DueDateTime dueDate={mockDueDate} isShow={false} />);
+    expect(screen.getByText('Weekly Summary Due Date (click to add)')).toBeInTheDocument();
+
+    rerender(<DueDateTime dueDate={mockDueDate} isShow={true} />);
+    expect(screen.getByText('Weekly Summary Due Date (click to close)')).toBeInTheDocument();
+  });
+});
