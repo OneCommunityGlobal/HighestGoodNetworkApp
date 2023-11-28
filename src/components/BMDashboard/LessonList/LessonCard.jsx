f1c89eff8fd82aef511dc0e468e649c203a67aa5
odanykh <odanykh@ford.com>
2023-11-28 13:09:19 -0500

    made the changes to the navbar according to the Jae request and made the body card scrollable

--- src/components/BMDashboard/LessonList/LessonCard.jsx
+++ src/components/BMDashboard/LessonList/LessonCard.jsx
@@ -1,28 +1,44 @@
-import Card from 'react-bootstrap/Card';
-import Nav from 'react-bootstrap/Nav';
+import React from 'react';
+import {Card, Nav} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import './LessonCard.css';
 
-function LessonCard({ dummyData }) {
+function LessonCard({dummyData}) {
+  const maxSummaryLength = 1500;
   const lessonCards = dummyData.map(lesson => (
     <Card key={lesson.id}>
       <Card.Header>
         <Nav className="nav">
-          <Nav.Item className="nav-item-title">{lesson.lessonTitle}</Nav.Item>
-          <Nav.Item>Date: {lesson.date}</Nav.Item>
+          <div className="nav-title-and-date">
+            <Nav.Item className="nav-item-title">{lesson.lessonTitle}</Nav.Item>
+            <Nav.Item className="nav-item-date">Date: {lesson.date}</Nav.Item>
+          </div>
+          <div>
+            <Nav.Item className="card-tag">
+              {lesson.tags.map(tag => (
+                <span key={`${lesson.id} + ${tag}`} className="text-muted tag-item">
+                  {`#${tag}`}
+                </span>
+              ))}
+            </Nav.Item>
+          </div>
         </Nav>
       </Card.Header>
-      <Card.Body>
+      <Card.Body className="scrollable-card-body">
         <Card.Text className="card-tag-and-file">
-          Tag:{' '}
+          Tags:{' '}
           {lesson.tags.map(tag => (
             <span key={`${lesson.id} + ${tag}`} className="tag-item">
-              {tag}
+              {`#${tag}`}
             </span>
           ))}
         </Card.Text>
-        <Card.Text>{lesson.lessonSummary}</Card.Text>
+        <Card.Text className="lesson-summary">
+              {lesson.lessonSummary.length > maxSummaryLength
+                ? `${lesson.lessonSummary.slice(0, maxSummaryLength)}...`
+                : lesson.lessonSummary}
+            </Card.Text>
         <Card.Text className="card-tag-and-file">
           File: <span className="lesson-file">{lesson.file}</span>
         </Card.Text>
@@ -35,22 +51,37 @@ function LessonCard({ dummyData }) {
           <span className="footer-items-author-and-from">From: {lesson.project}</span>
         </div>
         <div className="footer-items">
-          <span>Edit</span>
-          <span>Delete</span>
-          <span>
-            <FontAwesomeIcon
-              className="ml-2"
-              icon={faHeart}
-              size="sm"
-              style={{ color: '##7A7D81', cursor: 'pointer' }}
-            />
-            Like:0
-          </span>
+            <div>
+              <button
+                className="text-muted"
+                type="button"
+              >
+                Edit
+              </button>
+              <span>Delete</span>
+            </div>
+          <div>
+            <span>
+              <FontAwesomeIcon
+                className="ml-2"
+                icon={faHeart}
+                size="sm"
+                style={{ color: '##7A7D81', cursor: 'pointer' }}
+              />
+              Like:0
+            </span>
+          </div>
         </div>
       </Card.Footer>
     </Card>
   ));
-  return <ul>{lessonCards}</ul>;
-}
+  return (
+  <div>
+      <ul>{lessonCards}</ul>;
+  </div>
+  
+)}
 
 export default LessonCard;
+
+
--- src/components/BMDashboard/LessonList/Lessons.jsx
+++ src/components/BMDashboard/LessonList/Lessons.jsx
@@ -6,10 +6,28 @@ const DummyData = [
     id: '12',
     firstName: 'Olena',
     lastName: 'Danykh',
+    userid: '651483422cdd3e63f9a745d1',
     project: 'Project 1',
-    lessonTitle: 'XYZ',
-    lessonSummary: 'Stud wall construction',
-    tags: ['Weather', 'Project1'],
+    lessonTitle:
+      'Stud Wall Installation: Robust framework, easy customization, and efficient insulation for durable and versatile construction.',
+    lessonSummary: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    tags: [
+      'Weather',
+      'Safety',
+      'Construction',
+      'WallInstalattion',
+      'Customization',
+      'Sunny',
+      'StudWall',
+      'Lesson',
+      'Project-1',
+      'BuildingTips',
+      'DIYTips',
+      'Renovation',
+      'HomeImprovement',
+      'BuildingProjects',
+      'DesignIdeas',
+    ],
     file: 'xxx.xls',
     date: formatDateAndTime('11/25/2020 11:35:12'),
   },
@@ -17,6 +35,7 @@ const DummyData = [
     id: '13',
     firstName: 'Cailin',
     lastName: 'Colby',
+    userid: '651483422cdd3e63f9a745d2',
     project: 'Project 2',
     lessonTitle: 'Concrete',
     lessonSummary: 'Foundation concreting',
@@ -28,6 +47,7 @@ const DummyData = [
     id: '14',
     firstName: 'Jae',
     lastName: 'Member',
+    userid: '651483422cdd3e63f9a745d5',
     project: 'Project 3',
     lessonTitle: 'Title 1',
     lessonSummary: 'Summary.....',
@@ -38,7 +58,11 @@ const DummyData = [
 ];
 
 function Lessons() {
-  return <LessonCard dummyData={DummyData} />;
+  return (
+    <LessonCard
+      dummyData={DummyData}
+    />
+  );
 }
 
 export default Lessons;
