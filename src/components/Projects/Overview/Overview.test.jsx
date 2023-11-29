9d8b0a3401237cac711de6b258a5c7fbe1f6a7f4
DavidC0126 <74082492+DavidC0126@users.noreply.github.com>
2023-11-28 22:37:42 -0800

    Remove unnecessary import Overview.test.jsx

--- src/components/Projects/Overview/Overview.test.jsx
+++ src/components/Projects/Overview/Overview.test.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect'; 
 import Overview from './Overview';
 
 describe('Overview Component', () => {
