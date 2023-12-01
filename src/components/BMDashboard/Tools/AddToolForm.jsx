2f8a96110e6bd9ca3fd189b9a3c67ac578a2a32e
Olga Yudkin <cvtqx@protonmail.com>
2023-12-01 11:53:04 -0500

    phone number

--- src/components/BMDashboard/Tools/AddToolForm.jsx
+++ src/components/BMDashboard/Tools/AddToolForm.jsx
@@ -1,8 +1,12 @@
 import { Form, FormGroup, Label, Input, Row, Col, Button } from 'reactstrap';
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/style.css';
 import { boxStyle } from 'styles';
 import './AddToolForm.css';
 
 export default function AddToolForm() {
+
+
   return (
     <Form className="add-tool-form">
       <FormGroup>
@@ -65,6 +69,58 @@ export default function AddToolForm() {
           </FormGroup>
         </Col>
       </Row>
+      <Row>
+        <Col>
+          <FormGroup>
+            <Label for="purchase-rental">Purchase or Rental</Label>
+            <Input id="purchase-rental" type="select" name="purchase-rental">
+              <option>Purchase</option>
+              <option>Rental</option>
+            </Input>
+          </FormGroup>
+        </Col>
+        <Col>
+          <FormGroup>
+            <Label for="condition">Condition</Label>
+            <Input id="condition" type="select" name="condition">
+              <option>New</option>
+              <option>Used</option>
+            </Input>
+          </FormGroup>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <FormGroup>
+            <Label for="from-date">Purchase/Rental Date</Label>
+            <Input id="from-date" type="date" name="from-date" />
+          </FormGroup>
+        </Col>
+        <Col>
+          <FormGroup>
+            <Label for="to-date">Return date (if rented)</Label>
+            <Input id="to-date" type="date" name="to-date" />
+          </FormGroup>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <FormGroup>
+            <Label for="shipping-fee">Shipping Fee excluding taxes (enter 0 if free)</Label>
+            <Input id="shipping-fee" type="number" name="shipping-fee" placeholder="100.00" />
+          </FormGroup>
+        </Col>
+        <Col>
+          <FormGroup>
+            <Label for="taxes">Taxes</Label>
+            <Input id="taxes" type="number" name="taxes" placeholder="%" />
+          </FormGroup>
+        </Col>
+      </Row>
+      <FormGroup>
+        <Label for="phone-number">Supplier Phone Number</Label>
+        <PhoneInput  id="phone-number" name="phone-number"/>
+      </FormGroup>
     </Form>
   );
 }
