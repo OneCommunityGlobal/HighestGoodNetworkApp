b01d7f2a6a546bf5422dd28bb9052903f4e0c950
wantingxu <xu.wan@northeastern.edu>
2023-12-04 14:57:55 -0800

    add comments for unit test

--- src/components/Projects/ProjectTableHeader/ProjectTableHeader.test.jsx
+++ src/components/Projects/ProjectTableHeader/ProjectTableHeader.test.jsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 import ProjectTableHeader from './ProjectTableHeader'
-import { authMock, rolesMock, userProfileMock } from '../../../__tests__/mockStates'
+import { userProfileMock } from '../../../__tests__/mockStates'
 
 const mockStore = configureMockStore([thunk])
 
@@ -12,6 +12,8 @@ const mockStore = configureMockStore([thunk])
 jest.mock('components/UserProfile/EditableModal/EditableInfoModal', () => () => (
   <div>Mock EditableInfoModal</div>
 ));
+
+// Helper function to render ProjectTableHeader with provided props and mock Redux store
 const renderProjectTableHeader = (projectTableHeaderProps) => {
 
   const initialState = {
@@ -36,15 +38,18 @@ const renderProjectTableHeader = (projectTableHeaderProps) => {
   );
 };
 
+// Test suite for ProjectTableHeader component
 describe('ProjectTableHeader Component', () => {
   const sampleProps = {
     role: 'Owner',
   };
 
+  // Test case to check if component renders without crashing
   it('renders without crashing', () => {
     renderProjectTableHeader(sampleProps);
   });
 
+  // Test case to check if the delete column is shown for users with delete permission
   it('shows delete column for users with delete permission', () => {
     const stateWithDeletePermission = {
       ...sampleProps,
@@ -53,6 +58,7 @@ describe('ProjectTableHeader Component', () => {
     expect(getByText('Delete')).toBeInTheDocument();
   });
 
+  // Test case to check if the delete column is not shown for users without delete permission
   it('does not show delete column for users without delete permission', () => {
     const stateWithoutDeletePermission = {
       ...sampleProps,
