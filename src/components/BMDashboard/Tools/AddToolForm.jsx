d924b622146a58964fdd3c98e1ec00b2329f21a2
Olga Yudkin <cvtqx@protonmail.com>
2023-12-06 14:43:35 -0500

    form refactored and draganddrop component basic design

--- src/components/BMDashboard/Tools/AddToolForm.jsx
+++ src/components/BMDashboard/Tools/AddToolForm.jsx
@@ -1,16 +1,14 @@
-import { Form, FormGroup, Label, Input, Row, Col, ListGroup, ListGroupItem , Button } from 'reactstrap';
+import { Form, FormGroup, Label, Input, Row, Col, Button } from 'reactstrap';
+
+import DragAndDrop from 'components/common/DragAndDrop/DragAndDrop';
 import PhoneInput from 'react-phone-input-2';
 import 'react-phone-input-2/lib/style.css';
-
-import DropZone  from 'components/common/DragAndDrop/DragAndDrop';
-import { boxStyle } from 'styles';
 import './AddToolForm.css';
 
 export default function AddToolForm() {
 
-
   return (
-    <Form className="add-tool-form">
+    <Form className="add-tool-form container">
       <FormGroup>
         <Label for="select-project">Project</Label>
         <Input id="select-project" name="project" type="select">
@@ -18,26 +16,23 @@ export default function AddToolForm() {
           <option>Project 2</option>
         </Input>
       </FormGroup>
-      <Row>
-        <Col>
-          <FormGroup>
-            <Label for="select-category">Tool or Equipment</Label>
-            <Input id="select-category" name="category" type="select">
-              <option>Tool</option>
-              <option>Equipment</option>
-            </Input>
-          </FormGroup>
-        </Col>
-        <Col>
-          <FormGroup>
-            <Label for="select-name">Name</Label>
-            <Input id="select-name" name="name" type="select">
-              <option>Tool 1</option>
-              <option>Tool 2</option>
-            </Input>
-          </FormGroup>
-        </Col>
-      </Row>
+      <div className="add-tool-flex-group">
+        <FormGroup>
+          <Label for="select-category">Tool or Equipment</Label>
+          <Input id="select-category" name="category" type="select">
+            <option>Tool</option>
+            <option>Equipment</option>
+          </Input>
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="select-name">Name</Label>
+          <Input id="select-name" name="name" type="select">
+            <option>Tool 1</option>
+            <option>Tool 2</option>
+          </Input>
+        </FormGroup>
+      </div>
       <FormGroup>
         <Label for="invoice-number">Invoice Number or ID</Label>
         <Input
@@ -47,83 +42,69 @@ export default function AddToolForm() {
           placeholder="Input Invoice No or  ID for the tool or equipment"
         />
       </FormGroup>
-      <Row>
-        <Col sm={6}>
-          <FormGroup>
-            <Label for="unit-price">Unit Price (excl.taxes & shipping)</Label>
-            <Input id="unit-price" type="number" name="unit-price" />
-          </FormGroup>
-        </Col>
-        <Col sm={2}>
-          <FormGroup>
-            <Label for="currency">Currency</Label>
-            <Input id="currency" type="select" name="currency">
-              <option>USD</option>
-              <option>EUR</option>
-              <option>CAD</option>
-            </Input>
-          </FormGroup>
-        </Col>
-        <Col sm={4}>
-          <FormGroup>
-            <Label for="quantity">Total quantity</Label>
-            <Input id="quantity" type="number" name="quantity" />
-          </FormGroup>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <FormGroup>
-            <Label for="purchase-rental">Purchase or Rental</Label>
-            <Input id="purchase-rental" type="select" name="purchase-rental">
-              <option>Purchase</option>
-              <option>Rental</option>
-            </Input>
-          </FormGroup>
-        </Col>
-        <Col>
-          <FormGroup>
-            <Label for="condition">Condition</Label>
-            <Input id="condition" type="select" name="condition">
-              <option>New</option>
-              <option>Used</option>
-            </Input>
-          </FormGroup>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <FormGroup>
-            <Label for="from-date">Purchase/Rental Date</Label>
-            <Input id="from-date" type="date" name="from-date" />
-          </FormGroup>
-        </Col>
-        <Col>
-          <FormGroup>
-            <Label for="to-date">Return date (if rented)</Label>
-            <Input id="to-date" type="date" name="to-date" />
-          </FormGroup>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <FormGroup>
-            <Label for="shipping-fee">Shipping Fee excluding taxes (enter 0 if free)</Label>
-            <Input id="shipping-fee" type="number" name="shipping-fee" placeholder="100.00" />
-          </FormGroup>
-        </Col>
-        <Col>
-          <FormGroup>
-            <Label for="taxes">Taxes</Label>
-            <Input id="taxes" type="number" name="taxes" placeholder="%" />
-          </FormGroup>
-        </Col>
-      </Row>
+      <div className="add-tool-flex-group">
+        <FormGroup>
+          <Label for="unit-price">Unit Price (excl.taxes & shipping)</Label>
+          <Input id="unit-price" type="number" name="unit-price" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="currency">Currency</Label>
+          <Input id="currency" type="select" name="currency">
+            <option>USD</option>
+            <option>EUR</option>
+            <option>CAD</option>
+          </Input>
+        </FormGroup>
+        <FormGroup>
+          <Label for="quantity">Total quantity</Label>
+          <Input id="quantity" type="number" name="quantity" />
+        </FormGroup>
+      </div>
+      <div className="add-tool-flex-group">
+        <FormGroup>
+          <Label for="purchase-rental">Purchase or Rental</Label>
+          <Input id="purchase-rental" type="select" name="purchase-rental">
+            <option>Purchase</option>
+            <option>Rental</option>
+          </Input>
+        </FormGroup>
+        <FormGroup>
+          <Label for="condition">Condition</Label>
+          <Input id="condition" type="select" name="condition">
+            <option>New</option>
+            <option>Used</option>
+          </Input>
+        </FormGroup>
+      </div>
+      <div className="add-tool-flex-group">
+        <FormGroup>
+          <Label for="from-date">Purchase/Rental Date</Label>
+          <Input id="from-date" type="date" name="from-date" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="to-date">Return date (if rented)</Label>
+          <Input id="to-date" type="date" name="to-date" />
+        </FormGroup>
+      </div>
+      <div className="add-tool-flex-group">
+        <FormGroup>
+          <Label for="shipping-fee">Shipping Fee excluding taxes (enter 0 if free)</Label>
+          <Input id="shipping-fee" type="number" name="shipping-fee" placeholder="100.00" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="taxes">Taxes</Label>
+          <Input id="taxes" type="number" name="taxes" placeholder="%" />
+        </FormGroup>
+      </div>
       <FormGroup>
         <Label for="phone-number">Supplier Phone Number</Label>
         <PhoneInput id="phone-number" name="phone-number" />
       </FormGroup>
-      <DropZone />
+      <FormGroup>
+        <Label for="imageUpload">Upload Tool/Equipment Picture</Label>
+        <DragAndDrop id="imageUpload" name="imageUpload" />
+      </FormGroup>
+
       <FormGroup>
         <Label for="link">Link to Buy/Rent</Label>
         <Input id="link" type="text" name="link" placeholder="https://" />
@@ -132,21 +113,14 @@ export default function AddToolForm() {
         <Label for="description">Tool/Equipment Description</Label>
         <Input type="textarea" name="description" id="description" />
       </FormGroup>
-      {/* <FormGroup row>
-        <Label for="totalPrice" sm="2">
-          Total Price
-        </Label>
-        <Col sm={10}>
-          <Input plaintext id="totalPrice" name="totalPrice">
-            {' '}
-            30.00 USD
-          </Input>
-        </Col>
-      </FormGroup> */}
-      <div>
-        <Button outline>Cancel</Button>
-        <Button>Submit</Button>
+      <div className="add-tool-total-price">
+        <div>Total Price</div>
+        <div className="total-price-calculated">30.00USD</div>
       </div>
+      <div className="add-tool-buttons">
+          <Button outline>Cancel</Button>
+          <Button>Submit</Button>
+    </div>
     </Form>
   );
 }
--- src/components/common/DragAndDrop/DragAndDrop.jsx
+++ src/components/common/DragAndDrop/DragAndDrop.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import './DragAndDrop.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faImage } from '@fortawesome/free-solid-svg-icons';
 
-const DropZone = () => {
+const DragAndDrop = () => {
   return (
-    <div>DragAndDrop</div>
+    <div id="file-upload-form">
+        <input id="file-upload-input" type="file" name="file-upload-input" multiple={false} />
+       <label htmlFor="file-upload-input" id="file-upload-label">
+          <div>
+              <FontAwesomeIcon icon={faImage} className="file-upload-icon" />
+                <p>Drag and drop your picture here or</p>
+                <button className="upload-button">Upload a file</button>
+              </div>
+      </label>
+    </div>
   )
 }
 
-export default DropZone;
+export default DragAndDrop;
