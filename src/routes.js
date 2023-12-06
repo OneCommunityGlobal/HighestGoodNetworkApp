5aef3e23688bc4cc98be2afcc94498ab716d0807
Vishala Ramasamy <vishala.ramasamy@gmail.com>
2023-12-06 12:55:08 -0800

    circleci config - restore,save cache ; removed unused deps from pkg.json , code split - projects, reports , weeksumreps

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+import { lazy } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import SetupProfile from 'components/SetupProfile/SetupProfile';
 import { ToastContainer } from 'react-toastify';
@@ -12,7 +13,7 @@ import EditableInfoModal from 'components/UserProfile/EditableModal/EditableInfo
 import RoleInfoCollections from 'components/UserProfile/EditableModal/roleInfoModal';
 import Timelog from './components/Timelog';
 import LessonForm from './components/BMDashboard/Lesson/LessonForm';
-import Reports from './components/Reports';
+//import Reports from './components/Reports';
 import UserProfile from './components/UserProfile';
 import UserProfileEdit from './components/UserProfile/UserProfileEdit';
 import Dashboard from './components/Dashboard';
@@ -22,21 +23,21 @@ import ForcePasswordUpdate from './components/ForcePasswordUpdate';
 import ProtectedRoute from './components/common/ProtectedRoute';
 import UpdatePassword from './components/UpdatePassword';
 import Header from './components/Header';
-import Projects from './components/Projects';
+//import Projects from './components/Projects';
 import Teams from './components/Teams/Teams';
 import UserManagement from './components/UserManagement';
 import Members from './components/Projects/Members';
 import WBS from './components/Projects/WBS';
 import WBSDetail from './components/Projects/WBS/WBSDetail';
 import SingleTask from './components/Projects/WBS/SingleTask';
-import WeeklySummariesReport from './components/WeeklySummariesReport';
+//import WeeklySummariesReport from './components/WeeklySummariesReport';
 import TeamLocations from './components/TeamLocations';
 import Admin from './components/Admin';
 import 'react-toastify/dist/ReactToastify.css';
 import { UserRole } from './utils/enums';
 import ForgotPassword from './components/Login/ForgotPassword';
-import { PeopleReport } from './components/Reports/PeopleReport';
-import { ProjectReport } from './components/Reports/ProjectReport';
+//import { PeopleReport } from './components/Reports/PeopleReport';
+//import { ProjectReport } from './components/Reports/ProjectReport';
 import { TeamReport } from './components/Reports/TeamReport';
 import Inventory from './components/Inventory';
 import BadgeManagement from './components/Badge/BadgeManagement';
@@ -49,7 +50,12 @@ import MaterialsList from './components/BMDashboard/MaterialsList';
 import PurchaseMaterials from './components/BMDashboard/MaterialPurchaseRequest';
 import ProjectDetails from './components/BMDashboard/Projects/ProjectDetails/ProjectDetails';
 
-
+//Code-Splitting
+const Projects = lazy(() => import('./components/Projects'));
+const WeeklySummariesReport = lazy(() => import('./components/WeeklySummariesReport'));
+const Reports = lazy(() => import('./components/Reports'));
+const PeopleReport = lazy(() => import('./components/Reports/PeopleReport'));
+const ProjectReport = lazy(() => import('./components/Reports/ProjectReport'));
 
 export default (
   <Switch>
@@ -69,8 +75,8 @@ export default (
         <ProtectedRoute path="/popupmanagement" component={Admin} />
         <ProtectedRoute path="/timelog/" exact component={Timelog} />
         <ProtectedRoute path="/timelog/:userId" exact component={Timelog} />
-        <ProtectedRoute path="/peoplereport/:userId" component={PeopleReport} />
-        <ProtectedRoute path="/projectreport/:projectId" component={ProjectReport} />
+        <ProtectedRoute path="/peoplereport/:userId" component={PeopleReport} fallback={true} />
+        <ProtectedRoute path="/projectreport/:projectId" component={ProjectReport} fallback={true} />
         <ProtectedRoute path="/teamreport/:teamId" component={TeamReport} />
         <ProtectedRoute path="/taskeditsuggestions" component={TaskEditSuggestions} />
 
@@ -89,6 +95,7 @@ export default (
           path="/weeklysummariesreport"
           exact
           component={WeeklySummariesReport}
+          fallback={true}
           allowedRoles={[
             UserRole.Administrator,
             UserRole.Manager,
@@ -102,6 +109,7 @@ export default (
           path="/reports"
           exact
           component={Reports}
+          fallback={true}
           routePermissions={RoutePermissions.reports}
         />
         <ProtectedRoute path="/teamlocations" exact component={TeamLocations} />
@@ -109,6 +117,7 @@ export default (
           path="/projects"
           exact
           component={Projects}
+          fallback={true}
           allowedRoles={[UserRole.Administrator, UserRole.Owner]}
           routePermissions={[
             RoutePermissions.projects,
@@ -169,16 +178,16 @@ export default (
 
         {/* ----- BEGIN BM Dashboard Routing ----- */}
 
-      <BMProtectedRoute path="/bmdashboard" exact component={BMDashboard} />
-      <Route path="/bmdashboard/login" component={BMLogin} />
-      <BMProtectedRoute path="/bmdashboard/materials/purchase" component={PurchaseMaterials} />
-      <BMProtectedRoute path="/bmdashboard/projects/:projectId" component={ProjectDetails} />
-      <BMProtectedRoute path="/bmdashboard/materials-list" component={MaterialsList} />
-       <BMProtectedRoute path="/bmdashboard/lessonform/" component={LessonForm} />
-      {/* Temporary route to redirect all subdirectories to login if unauthenticated */}
-      <BMProtectedRoute path="/bmdashboard/:path" component={BMDashboard} />
-      
-      {/* ----- END BM Dashboard Routing ----- */}
+        <BMProtectedRoute path="/bmdashboard" exact component={BMDashboard} />
+        <Route path="/bmdashboard/login" component={BMLogin} />
+        <BMProtectedRoute path="/bmdashboard/materials/purchase" component={PurchaseMaterials} />
+        <BMProtectedRoute path="/bmdashboard/projects/:projectId" component={ProjectDetails} />
+        <BMProtectedRoute path="/bmdashboard/materials-list" component={MaterialsList} />
+        <BMProtectedRoute path="/bmdashboard/lessonform/" component={LessonForm} />
+        {/* Temporary route to redirect all subdirectories to login if unauthenticated */}
+        <BMProtectedRoute path="/bmdashboard/:path" component={BMDashboard} />
+
+        {/* ----- END BM Dashboard Routing ----- */}
 
         <Route path="/login" component={Login} />
         <Route path="/forgotpassword" component={ForgotPassword} />
