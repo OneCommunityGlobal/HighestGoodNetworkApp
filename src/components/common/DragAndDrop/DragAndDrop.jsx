91ba0d823d3836ca220beccab6707065f0b7500d
Olga Yudkin <cvtqx@protonmail.com>
2023-12-08 10:40:55 -0500

    updates

--- src/components/common/DragAndDrop/DragAndDrop.jsx
+++ src/components/common/DragAndDrop/DragAndDrop.jsx
@@ -1,10 +1,26 @@
+import { useState } from 'react';
 import './DragAndDrop.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faImage } from '@fortawesome/free-solid-svg-icons';
 
 const DragAndDrop = () => {
+
+  const [dragActive, setDragActive] = useState(false);
+
+
+  const handleDrag = function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (e.type === "dragenter" || e.type === "dragover") {
+      setDragActive(true);
+    } else if (e.type === "drageleave") {
+      setDragActive(false);
+    }
+  };
+
   return (
-    <div id="file-upload-form">
+    <div id="file-upload-form" onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
         <input id="file-upload-input" type="file" name="file-upload-input" multiple={false} />
        <label htmlFor="file-upload-input" id="file-upload-label">
           <div>
