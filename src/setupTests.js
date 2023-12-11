907b5a6d30d78513ae9489e256031247a602f3b3
Hsin An Anny Wang <98345990+astrjc0326@users.noreply.github.com>
2023-12-11 11:38:36 -0800

    Revert "fix: the testing error"
    This reverts commit 0af1c71b0493dd15a0439da668fad3e24ddb21cb.

---
+++ src/setupTests.js
@@ -0,0 +1,6 @@
+/** Used in jest.config.js */
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import '@testing-library/jest-dom/extend-expect';
+
+configure({ adapter: new Adapter() });
