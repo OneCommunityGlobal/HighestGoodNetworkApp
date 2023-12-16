2b59df30e7b205bf960751092912d98272977b96
Ramya Non-IU <ramyaram2092@outlook.com>
2023-12-16 02:01:26 -0800

    Added testid attribute

--- src/components/UserProfile/ToolTips/TabToolTips.jsx
+++ src/components/UserProfile/ToolTips/TabToolTips.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Tooltip } from 'reactstrap';
 
-const TabToolTips = () => {
+function TabToolTips({ tabId }) {
   const [basicOpen, setBasicOpen] = useState(false);
   const [timeOpen, setTimeOpen] = useState(false);
   const [teamsOpen, setTeamsOpen] = useState(false);
@@ -11,14 +11,32 @@ const TabToolTips = () => {
   const toggleTeams = () => setTeamsOpen(!teamsOpen);
   const toggleProjects = () => setProjectsOpen(!projectsOpen);
   return (
-    <div>
-      <Tooltip placement="top" isOpen={basicOpen} target="nabLink-basic" toggle={toggleBasic}>
+    <div data-testid="tabtooltiptest">
+      <Tooltip
+        placement="top"
+        isOpen={basicOpen}
+        target="nabLink-basic"
+        toggle={toggleBasic}
+        data-testid="tabtooltiptest-1"
+      >
         This is where your contact information and bio go
       </Tooltip>
-      <Tooltip placement="top" isOpen={timeOpen} target="nabLink-time" toggle={toggleTime}>
+      <Tooltip
+        placement="top"
+        isOpen={timeOpen}
+        target="nabLink-time"
+        toggle={toggleTime}
+        data-testid="tabtooltiptest-2"
+      >
         This shares your start and end dates, committed hours, total hours volunteered, etc.
       </Tooltip>
-      <Tooltip placement="top" isOpen={teamsOpen} target="nabLink-teams" toggle={toggleTeams}>
+      <Tooltip
+        placement="top"
+        isOpen={teamsOpen}
+        target="nabLink-teams"
+        toggle={toggleTeams}
+        data-testid="tabtooltiptest-3"
+      >
         This shows any teams you are a member of. You will see all other members of your team in the
         Leaderboard on the Time Log page and have access to their Profiles and Time Logs from there.
       </Tooltip>
@@ -27,11 +45,12 @@ const TabToolTips = () => {
         isOpen={projectsOpen}
         target="nabLink-projects"
         toggle={toggleProjects}
+        data-testid="tabtooltiptest-4"
       >
         This shows all the projects and tasks you are assigned to.
       </Tooltip>
     </div>
   );
-};
+}
 
 export default TabToolTips;
