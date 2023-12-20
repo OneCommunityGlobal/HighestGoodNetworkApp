0625ca8c6276dd01602c7b08964cd6fe87b96fea
Tim Kent <timkentdev@gmail.com>
2023-12-20 12:24:52 -0800

    add basic form inputs

--- src/components/BMDashboard/Equipment/AddType/AddEquipmentModal.jsx
+++ src/components/BMDashboard/Equipment/AddType/AddEquipmentModal.jsx
@@ -1,10 +1,31 @@
-import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+} from 'reactstrap';
 
 export default function AddEquipmentModal({ modal, toggle }) {
   return (
     <Modal isOpen={modal}>
       <ModalHeader>Add Equipment Type</ModalHeader>
-      <ModalBody>Form goes here</ModalBody>
+      <ModalBody>
+        <Form>
+          <FormGroup>
+            <Label htmlFor="item-name">Item 1</Label>
+            <Input id="item-name" type="text" />
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="item-description">Description</Label>
+            <Input id="item-description" type="text" />
+          </FormGroup>
+        </Form>
+      </ModalBody>
       <ModalFooter>
         <Button size="lg" onClick={toggle}>
           Cancel
