33a39a5afae32aff55c1b49028233e0eed1aed29
Tim Kent <timkentdev@gmail.com>
2023-12-20 13:03:33 -0800

    add basic joi validation

--- src/components/BMDashboard/Equipment/AddType/AddEquipmentModal.jsx
+++ src/components/BMDashboard/Equipment/AddType/AddEquipmentModal.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   Modal,
   ModalHeader,
@@ -9,28 +10,47 @@ import {
   Input,
   Label,
 } from 'reactstrap';
+import Joi from 'joi';
+
+const schema = Joi.object({
+  name: Joi.string().required(),
+  desc: Joi.string().required(),
+});
 
 export default function AddEquipmentModal({ modal, toggle }) {
+  const [name, setName] = useState('');
+  const [desc, setDesc] = useState('');
+  const handleSubmit = e => {
+    e.preventDefault();
+    const value = schema.validate({ name, desc });
+    console.log('joi: ', value);
+    // toggle();
+  };
   return (
     <Modal isOpen={modal}>
       <ModalHeader>Add Equipment Type</ModalHeader>
       <ModalBody>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <FormGroup>
             <Label htmlFor="item-name">Item 1</Label>
-            <Input id="item-name" type="text" />
+            <Input id="item-name" type="text" onChange={({ target }) => setName(target.value)} />
           </FormGroup>
           <FormGroup>
             <Label htmlFor="item-description">Description</Label>
-            <Input id="item-description" type="text" />
+            <Input
+              id="item-description"
+              type="text"
+              onChange={({ target }) => setDesc(target.value)}
+            />
           </FormGroup>
+          <Button color="primary" size="lg">
+            Submit
+          </Button>
+          <Button color="secondary" size="lg" onClick={toggle}>
+            Cancel
+          </Button>
         </Form>
       </ModalBody>
-      <ModalFooter>
-        <Button size="lg" onClick={toggle}>
-          Cancel
-        </Button>
-      </ModalFooter>
     </Modal>
   );
 }
