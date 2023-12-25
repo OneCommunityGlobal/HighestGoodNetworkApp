e7d9cd0ca814ee7f52f8439979fac3b8038deb27
wang9hu <xwang.9hu@gmail.com>
2023-12-24 18:25:31 -0800

    fix popupbar test file

--- src/components/PopUpBar/PopUpBar.jsx
+++ src/components/PopUpBar/PopUpBar.jsx
@@ -5,7 +5,7 @@ function PopUpBar(props) {
   const { userProfile, component } = props;
   const { firstName, lastName } = userProfile;
   return (
-    <div className="popup_container">
+    <div className="popup_container" data-testid="test-popup">
       {`You are currently viewing the ${component} for ${firstName} ${lastName}`}.
     </div>
   );
