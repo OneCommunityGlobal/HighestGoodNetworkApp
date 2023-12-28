0a13e37440703749df58bdb838fb9bda940be77c
Vishala Ramasamy <vishala.ramasamy@gmail.com>
2023-12-28 10:27:10 -0800

    lint fix

---
+++ src/components/BMDashboard/AddMaterial/SimilarityCheck.js
@@ -0,0 +1,35 @@
+export function editDistance(_s1, _s2) {
+  const s1 = _s1.toLowerCase();
+  const s2 = _s2.toLowerCase();
+
+  const costs = [];
+  for (let i = 0; i <= s1.length; i += 1) {
+    let lastValue = i;
+    for (let j = 0; j <= s2.length; j += 1) {
+      if (i === 0) costs[j] = j;
+      else if (j > 0) {
+        let newValue = costs[j - 1];
+        if (s1.charAt(i - 1) !== s2.charAt(j - 1))
+          newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
+        costs[j - 1] = lastValue;
+        lastValue = newValue;
+      }
+    }
+    if (i > 0) costs[s2.length] = lastValue;
+  }
+  return costs[s2.length];
+}
+
+export function similarity(s1, s2) {
+  let longer = s1;
+  let shorter = s2;
+  if (s1.length < s2.length) {
+    longer = s2;
+    shorter = s1;
+  }
+  const longerLength = longer.length;
+  if (longerLength === 0) {
+    return 1.0;
+  }
+  return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+}
