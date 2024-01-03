0e75452e325e318518c089415bfa0638c75cf9c1
Tim Kent <timkentdev@gmail.com>
2024-01-02 20:03:32 -0800

    add empty check types modal

--- src/components/BMDashboard/Equipment/Add/AddEquipmentType.jsx
+++ src/components/BMDashboard/Equipment/Add/AddEquipmentType.jsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { BiInfoCircle } from 'react-icons/bi';
+import { BsInfoCircle } from 'react-icons/bs';
+import { Button } from 'reactstrap';
 
 import BMError from 'components/BMDashboard/shared/BMError';
+import CheckTypesModal from 'components/BMDashboard/shared/CheckTypesModal';
 import AddTypeForm from './AddTypeForm';
 import '../../BMDashboard.css';
 
 export default function AddEquipmentType() {
   const errors = useSelector(state => state.errors);
   const [isError, setIsError] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  const toggle = () => setShowModal(prev => !prev);
 
   useEffect(() => {
     if (Object.entries(errors).length) {
@@ -27,12 +32,16 @@ export default function AddEquipmentType() {
 
   return (
     <main className="inv-form-page-container">
+      <CheckTypesModal showModal={showModal} toggle={toggle} />
       <header>
         <h2>Add Type: Equipment</h2>
-        <p className="inv-form-info">
-          <BiInfoCircle />
-          Add a new type of equipment so it can be purchased and used in projects.
-        </p>
+        <div className="inv-form-info">
+          <BsInfoCircle />
+          Add a new type of equipment so it can be purchased and used in projects
+        </div>
+        <Button size="sm" outline color="primary" onClick={toggle}>
+          Check Types
+        </Button>
       </header>
       <AddTypeForm />
     </main>
