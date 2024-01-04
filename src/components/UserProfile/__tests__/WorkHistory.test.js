e192b64991363cb86456dffb78b54c2f5679f10b
Shrey Jain <sj@shreys-mbp.myfiosgateway.com>
2024-01-04 15:53:26 -0500

    Shrey change unit test path for WorkHistory

--- src/components/UserProfile/__tests__/WorkHistory.test.js
+++ src/components/UserProfile/__tests__/WorkHistory.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import WorkHistory from '../../components/UserProfile/WorkHistory';
+import WorkHistory from '../WorkHistory';
 
 describe('WorkHistory Component', () => {
   test('renders Hours Contributed and Volunteering History sections', () => {
