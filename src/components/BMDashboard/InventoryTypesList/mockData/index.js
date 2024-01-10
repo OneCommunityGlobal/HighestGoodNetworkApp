ae4ee1e5d938076865031c510edb8136340de0c9
Shuhua-L <sl.shuhualiu@gmail.com>
2024-01-10 18:03:08 -0500

    render mock data dynamically

---
+++ src/components/BMDashboard/InventoryTypesList/mockData/index.js
@@ -0,0 +1,6 @@
+import equipmentTypes from './equipment.json';
+import consumableTypes from './consumables.json';
+import reusableTypes from './reusables.json';
+import toolTypes from './tools.json';
+
+export { equipmentTypes, consumableTypes, toolTypes, reusableTypes };
