46be777c806ef4a1d93690c46f31c3292abec5fc
One Community <one-community@users.noreply.github.com>
2024-01-13 08:42:29 -0800

    Revert "Jerry - email verification admin accounts"

--- src/components/UserProfile/AddNewUserProfile/UserProfileAdd.jsx
+++ src/components/UserProfile/AddNewUserProfile/UserProfileAdd.jsx
@@ -84,8 +84,6 @@ class AddUserProfile extends Component {
         showphone: true,
         weeklySummaryOption: 'Required',
         createdDate: nextDay,
-        actualEmail: '',
-        actualPassword: '',
       },
       formValid: {},
       formErrors: {
@@ -93,8 +91,6 @@ class AddUserProfile extends Component {
         lastName: 'Last Name is required',
         email: 'Email is required',
         phoneNumber: 'Phone Number is required',
-        actualEmail: "Actual Email is required",
-        actualPassword: "Actual Password is required",
       },
       timeZoneFilter: '',
       formSubmitted: false,
@@ -126,7 +122,7 @@ class AddUserProfile extends Component {
   
   
   render() {
-    const { firstName, email, lastName, phoneNumber, role, actualEmail, actualPassword, jobTitle } = this.state.userProfile;
+    const { firstName, email, lastName, phoneNumber, role, jobTitle } = this.state.userProfile;
     const phoneNumberEntered =
       this.state.userProfile.phoneNumber === null ||
       this.state.userProfile.phoneNumber.length === 0;
@@ -288,48 +284,6 @@ class AddUserProfile extends Component {
                     </FormGroup>
                   </Col>
                 </Row>
-                {(role === 'Administrator' || role === 'Owner') && (
-                  <>
-                    <Row className="user-add-row">
-                      <Col md={{ size: 2, offset: 2 }} className="text-md-right my-2">
-                        <Label>Actual Email</Label>
-                      </Col>
-                      <Col md="6">
-                        <FormGroup>
-                          <Input
-                              type="actualEmail"
-                              name="actualEmail"
-                              id="actualEmail"
-                              value={actualEmail}
-                              onChange={this.handleUserProfile}
-                              placeholder="Actual Email"
-                              invalid={this.state.formErrors.actualEmail}
-                            />
-                            <FormFeedback>{this.state.formErrors.actualEmail}</FormFeedback>
-                        </FormGroup>
-                      </Col>
-                    </Row>
-                    <Row className="user-add-row">
-                      <Col md={{ size: 4 }} className="text-md-right my-2">
-                        <Label>Actual Password</Label>
-                      </Col>
-                      <Col md="6">
-                        <FormGroup>
-                          <Input
-                              type="password"
-                              name="actualPassword"
-                              id="actualPassword"
-                              value={actualPassword}
-                              onChange={this.handleUserProfile}
-                              placeholder="Actual Password"
-                              invalid={this.state.formErrors.actualPassword}
-                            />
-                            <FormFeedback>{this.state.formErrors.actualPassword}</FormFeedback>
-                        </FormGroup>
-                      </Col>
-                    </Row>
-                  </>
-                )}
                 <Row className="user-add-row">
                   <Col md={{ size: 4 }} className="text-md-right my-2">
                     <Label className="weeklySummaryOptionsLabel">Weekly Summary Options</Label>
@@ -651,8 +605,6 @@ class AddUserProfile extends Component {
       location,
       weeklySummaryOption,
       createdDate,
-      actualEmail,
-      actualPassword
     } = that.state.userProfile;
 
     const userData = {
@@ -677,8 +629,6 @@ class AddUserProfile extends Component {
       allowsDuplicateName: allowsDuplicateName,
       createdDate: createdDate,
       teamCode: this.state.teamCode,
-      actualEmail: actualEmail,
-      actualPassword: actualPassword
     };
 
     this.setState({ formSubmitted: true });
@@ -784,19 +734,6 @@ class AddUserProfile extends Component {
                       popupOpen: true,
                     });
                   }
-                  break;
-                case 'credentials':
-                  this.setState({
-                    formValid: {
-                      ...that.state.formValid,
-                      email: false,
-                    },
-                    formErrors: {
-                      ...that.state.formErrors,
-                      actualEmail: 'Actual email or password may be incorrect',
-                      actualPassword: 'Actual email or password may be incorrect',
-                    },
-                  });
                   break;
               }
             }
@@ -1065,30 +1002,6 @@ class AddUserProfile extends Component {
               ...userProfile.privacySettings,
               phoneNumber: !userProfile.privacySettings?.phoneNumber,
             },
-          },
-        });
-        break;
-      case 'actualEmail':
-        this.setState({
-          userProfile: {
-            ...userProfile,
-            actualEmail: event.target.value
-          },
-          formErrors: {
-            ...formErrors,
-            actualEmail: event.target.value.match(patt) ? '' : 'Actual Email is not valid',
-          },
-        });
-        break;
-      case 'actualPassword':
-        this.setState({
-          userProfile: {
-            ...userProfile,
-            actualPassword: event.target.value
-          },
-          formErrors: {
-            ...formErrors,
-            actualPassword: event.target.value.length > 0 ? '' : 'Actual Password is required',
           },
         });
         break;
