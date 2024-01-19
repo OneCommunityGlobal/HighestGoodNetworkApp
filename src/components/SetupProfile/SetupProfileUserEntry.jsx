3c1350cdb2279ca36ea8ccf39b040559989604eb
abdelmounaim lallouache <abdelmounaim.lallouache@gmail.com>
2024-01-18 23:49:45 -0600

    added input data type validation, capitalize function, video call input suggestion

--- src/components/SetupProfile/SetupProfileUserEntry.jsx
+++ src/components/SetupProfile/SetupProfileUserEntry.jsx
@@ -10,6 +10,10 @@ import {
   Label,
   Button,
   InputGroupAddon,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
   InputGroup,
   InputGroupText,
   UncontrolledTooltip,
@@ -31,6 +35,7 @@ import { setCurrentUser } from '../../actions/authActions';
 import HomeCountryModal from './homeCountryModal';
 import ProfilePictureModal from './profilePictureModal';
 import DeleteHoumeCountryModal from './deleteHomeCountryModal';
+import  collaborationOptions  from './collaborationSuggestionData' ;
 import Image from 'react-bootstrap/Image';
 import 'react-phone-input-2/lib/style.css';
 import './SetupProfileUserEntry.css';
@@ -42,6 +47,7 @@ const SetupProfileUserEntry = ({ token, userEmail }) => {
   const containCap = RegExp(/[A-Z]/);
   const containLow = RegExp(/[a-z]/);
   const containNumb = RegExp(/\d/);
+  const containOnlyLetters = RegExp(/^[a-zA-Z\s]+$/);
   const [APIkey, setAPIkey] = useState('');
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
@@ -93,6 +99,10 @@ const SetupProfileUserEntry = ({ token, userEmail }) => {
   });
   const [totalCountryCount, setTotalCountryCount] = useState('');
   const [deleteHoumeCountryModal, setDeleteHoumeCountryModal] = useState(false);
+
+  const [collaborationSuggestionOpen, setCollaborationSuggestionOpen] = useState(false);
+  const [collaborationSuggestions, setCollaborationSuggestions] = useState([]);
+  
 
   const pictureInputRef = useRef(null);
 
@@ -168,13 +178,39 @@ const SetupProfileUserEntry = ({ token, userEmail }) => {
     });
   }, []);
 
+  const togglecollaborationSuggestionDropdown = () => {
+    setCollaborationSuggestionOpen(!collaborationSuggestionOpen);
+  };
+
+  const setCollaborationSuggestionsData = e => {
+    const value = e.target.value;
+    if (value.length < 1) {
+      setCollaborationSuggestionOpen(false);
+      return;
+    }
+    const filteredSuggestions = collaborationOptions.filter(suggestion =>
+      suggestion.name.toLowerCase().includes(value.toLowerCase()),
+    );
+
+    setCollaborationSuggestions(filteredSuggestions);
+    setCollaborationSuggestionOpen(true);
+  };
+
+  const handleCollaborationSuggestionClick = suggestion => {
+    setUserProfile(prevProfile => ({
+      ...prevProfile,
+      collaborationPreference: suggestion,
+    }));
+    setCollaborationSuggestionOpen(false);
+  };
+
   const toggleHomecountryModal = () => {
     sethomecountryModalOpen(prev => !prev);
   };
 
-  const toggleDeleteHoumeCountryModal = () =>{
-    setDeleteHoumeCountryModal(prev=>!prev)
-  }
+  const toggleDeleteHoumeCountryModal = () => {
+    setDeleteHoumeCountryModal(prev => !prev);
+  };
 
   const toggleProfilePictureModal = () => {
     setprofilePictureModalOpen(prev => !prev);
@@ -267,7 +303,7 @@ const SetupProfileUserEntry = ({ token, userEmail }) => {
       ...prevErrors,
       location: '',
     }));
-    
+
     const location = userProfile.location.userProvided;
     if (!location) {
       setFormErrors(prevErrors => ({
@@ -322,6 +358,12 @@ const SetupProfileUserEntry = ({ token, userEmail }) => {
         firstName: 'First Name is required',
       }));
       isDataValid = false;
+    } else if (!containOnlyLetters.test(userProfile.firstName.trim())) {
+      setFormErrors(prevErrors => ({
+        ...prevErrors,
+        firstName: 'Please enter a valid first name',
+      }));
+      isDataValid = false;
     } else {
       setFormErrors(prevErrors => ({
         ...prevErrors,
@@ -334,6 +376,12 @@ const SetupProfileUserEntry = ({ token, userEmail }) => {
       setFormErrors(prevErrors => ({
         ...prevErrors,
         lastName: 'Last Name is required',
+      }));
+      isDataValid = false;
+    } else if (!containOnlyLetters.test(userProfile.lastName.trim())) {
+      setFormErrors(prevErrors => ({
+        ...prevErrors,
+        lastName: 'Please enter a valid last name',
       }));
       isDataValid = false;
     } else {
@@ -426,6 +474,12 @@ const SetupProfileUserEntry = ({ token, userEmail }) => {
         jobTitle: 'Job Title is required',
       }));
       isDataValid = false;
+    } else if (!containOnlyLetters.test(userProfile.jobTitle.trim())) {
+      setFormErrors(prevErrors => ({
+        ...prevErrors,
+        jobTitle: 'Please enter a valid job title',
+      }));
+      isDataValid = false;
     } else {
       setFormErrors(prevErrors => ({
         ...prevErrors,
@@ -439,6 +493,12 @@ const SetupProfileUserEntry = ({ token, userEmail }) => {
       setFormErrors(prevErrors => ({
         ...prevErrors,
         collaborationPreference: 'Video Call Preference can not be empty',
+      }));
+      isDataValid = false;
+    }else if (!containOnlyLetters.test(userProfile.jobTitle.trim())) {
+      setFormErrors(prevErrors => ({
+        ...prevErrors,
+        jobTitle: 'Please enter a valid job title',
       }));
       isDataValid = false;
     } else {
@@ -493,27 +553,48 @@ const SetupProfileUserEntry = ({ token, userEmail }) => {
     return true;
   };
 
+  const capitalizeString = (s) => {
+    
+    const words = s.split(' ');
+  
+    const capitalizedWords = words.map(word => {
+      if (word.length > 0) {
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+      } else {
+        return '';
+      }
+    });
+  
+    const capitalizedString = capitalizedWords.join(' ');
+  
+    return capitalizedString;
+  }
+
   const handleFormSubmit = e => {
     e.preventDefault();
 
     const isDataValid = validateFormData();
     const homeCoutryDataExists = areAllHomecountryValuesFilled();
     if (isDataValid) {
+
       const data = {
-        firstName: userProfile.firstName.trim(),
-        lastName: userProfile.lastName.trim(),
+        firstName: capitalizeString(userProfile.firstName.trim()),
+        lastName: capitalizeString(userProfile.lastName.trim()),
         password: userProfile.password.trim(),
         email: userProfile.email.trim().toLowerCase(),
         phoneNumber: userProfile.phoneNumber,
         weeklycommittedHours: Number(userProfile.weeklyCommittedHours.trim()),
-        collaborationPreference: userProfile.collaborationPreference.trim(),
+        collaborationPreference: capitalizeString(userProfile.collaborationPreference.trim()),
         privacySettings: {
           email: true,
           phoneNumber: true,
         },
-        jobTitle: userProfile.jobTitle.trim(),
+        jobTitle: capitalizeString(userProfile.jobTitle.trim()),
         timeZone: userProfile.timeZone.trim(),
-        location: userProfile.location,
+        location: {
+          ...userProfile.location,
+          userProvided: capitalizeString(userProfile.location.userProvided.trim()),
+        }, 
         token,
         homeCountry: homeCoutryDataExists ? homecountryLocation : userProfile.location,
         profilePicture: userProfile.profilePicture,
@@ -771,9 +852,34 @@ const SetupProfileUserEntry = ({ token, userEmail }) => {
                   value={userProfile.collaborationPreference}
                   onChange={e => {
                     handleChange(e);
+                    setCollaborationSuggestionsData(e);
                   }}
                   invalid={formErrors.collaborationPreference !== ''}
                 />
+                <InputGroupAddon addonType="append">
+                  <Dropdown
+                    isOpen={collaborationSuggestionOpen}
+                    toggle={togglecollaborationSuggestionDropdown}
+                    style={{ width: '100%'}}
+                    
+                  >
+                    <DropdownToggle tag={'span'}></DropdownToggle>
+                    {collaborationSuggestions.length > 0 && (
+                      <DropdownMenu id="collaboration-suggestion-dd" direction="down">
+                        {collaborationSuggestions.map((suggestion, index) => (
+                          <DropdownItem
+                            key={index}
+                            onClick={() => handleCollaborationSuggestionClick(suggestion.name)}
+                            className="collaboration-suggestion-dd-item"
+                          >
+                            <img className='collaboration-suggestion-dd-item-logo' src={suggestion.logo}/>  
+                            <p>{suggestion.name}</p>
+                          </DropdownItem>
+                        ))}
+                      </DropdownMenu>
+                    )}
+                  </Dropdown>
+                </InputGroupAddon>
                 <FormFeedback>{formErrors.collaborationPreference}</FormFeedback>
               </FormGroup>
             </Col>
@@ -884,24 +990,18 @@ const SetupProfileUserEntry = ({ token, userEmail }) => {
                     </b>
                     .
                   </p>
-                  <button
-                    type="button"
-                    className="close"
-                    onClick={toggleDeleteHoumeCountryModal}
-                  >
+                  <button type="button" className="close" onClick={toggleDeleteHoumeCountryModal}>
                     <span aria-hidden="true">&times;</span>
                   </button>
-                   
                 </div>
-               
               </Col>
             </Row>
           )}
           <DeleteHoumeCountryModal
-                  isOpen={deleteHoumeCountryModal}
-                  toggle={toggleDeleteHoumeCountryModal}
-                  setLocation={setHomecountryLocation}
-                />
+            isOpen={deleteHoumeCountryModal}
+            toggle={toggleDeleteHoumeCountryModal}
+            setLocation={setHomecountryLocation}
+          />
           <Row>
             <Col md={{ size: 8, offset: 3 }}>
               <div className="alert alert-info text-center">
