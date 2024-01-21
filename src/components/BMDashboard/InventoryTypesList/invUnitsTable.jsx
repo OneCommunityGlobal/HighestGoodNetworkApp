235f0d7d15edf698db745b1a7462095221269604
Shuhua-L <sl.shuhualiu@gmail.com>
2024-01-20 22:26:56 -0500

    added table component for ivnUnits

---
+++ src/components/BMDashboard/InventoryTypesList/invUnitsTable.jsx
@@ -0,0 +1,43 @@
+import { Table, Button } from 'react-bootstrap';
+
+export default function UnitsTable(props) {
+  const { invUnits } = props;
+
+  const handleDelete = () => {
+    // TODO:
+    console.log('Deleted unit:');
+  };
+
+  const handleAdd = () => {
+    // TODO:
+    console.log('Added unit:');
+  };
+
+  return (
+    <div>
+      <Table hover borderless size="sm" responsive="lg">
+        <thead className="table-header">
+          <tr>
+            <th>Name</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {invUnits?.map(unit => (
+            <tr key={`invUnit-${unit.unit}`}>
+              <td>{unit.unit}</td>
+              <td>
+                <Button size="sm" className="btn-types" onClick={handleDelete}>
+                  Delete
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <Button className="btn-types" onClick={handleAdd}>
+        Add
+      </Button>
+    </div>
+  );
+}
