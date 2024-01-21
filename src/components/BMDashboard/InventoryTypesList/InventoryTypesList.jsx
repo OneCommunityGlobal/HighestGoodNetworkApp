557b6e72976667455b5001a6fac6d9e67e7009e5
Shuhua-L <sl.shuhualiu@gmail.com>
2024-01-20 22:26:56 -0500

    added table component for ivnUnits

--- src/components/BMDashboard/InventoryTypesList/InventoryTypesList.jsx
+++ src/components/BMDashboard/InventoryTypesList/InventoryTypesList.jsx
@@ -2,17 +2,19 @@ import { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import { fetchInvTypeByType } from 'actions/bmdashboard/invTypeActions';
+import { fetchInvUnits } from 'actions/bmdashboard/invUnitActions';
 import { Accordion, Card } from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 import BMError from '../shared/BMError';
 import TypesTable from './TypesTable';
+import UnitsTable from './invUnitsTable';
 import './TypesList.css';
 import AccordionToggle from './AccordionToggle';
 
 export function InventoryTypesList(props) {
-  const { invTypes, errors, dispatch } = props;
+  const { invTypes, invUnits, errors, dispatch } = props;
 
   // NOTE: depend on redux action implementation
   const categories = ['Materials', 'Consumables', 'Equipments', 'Reusables', 'Tools'];
@@ -29,6 +31,7 @@ export function InventoryTypesList(props) {
     dispatch(fetchInvTypeByType('Equipments'));
     dispatch(fetchInvTypeByType('Reusables'));
     dispatch(fetchInvTypeByType('Tools'));
+    dispatch(fetchInvUnits());
   }, []);
 
   // trigger error state if an error object is added to props
@@ -71,13 +74,24 @@ export function InventoryTypesList(props) {
                 {category}
               </AccordionToggle>
               <Accordion.Collapse eventKey={index + 1}>
-                <Card.Body>
+                <Card.Body className="accordion-collapse">
                   <TypesTable itemTypes={invTypes[category]} category={category} />
                 </Card.Body>
               </Accordion.Collapse>
             </Card>
           );
         })}
+
+        <Card>
+          <AccordionToggle as={Card.Header} eventKey={6}>
+            Unit of Measurement
+          </AccordionToggle>
+          <Accordion.Collapse eventKey={6}>
+            <Card.Body className="accordion-collapse">
+              <UnitsTable invUnits={invUnits} />
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
       </Accordion>
 
       <div className="button-container">
@@ -93,6 +107,7 @@ export function InventoryTypesList(props) {
 const mapStateToProps = state => ({
   invTypes: state.bmInvTypes.invTypeList,
   errors: state.errors,
+  invUnits: state.bmInvUnits.list,
 });
 
 export default connect(mapStateToProps)(InventoryTypesList);
