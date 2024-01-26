536e7c68d29c14010ddedeec9aa9f20b4332c793
One Community <one-community@users.noreply.github.com>
2024-01-26 14:52:59 -0800

    Revert "Olena Lesson Card Functionality"

--- src/reducers/bmdashboard/projectByIdReducer.js
+++
@@ -1,8 +0,0 @@
-import GET_BM_PROJECT_BY_ID from "constants/bmdashboard/projectConstants"
-
-export const bmProjectByIdReducer = (project = null, action) => {
-  if(action.type === GET_BM_PROJECT_BY_ID) {
-    return action.payload
-  }
-  return project
-}
