97535aaad16720be7f7a7575c7c201075c0e32a3
ilyaflaks <ilyaflaks@gmail.com>
2024-01-30 16:20:08 -0800

    add fixed positioning for small screens

--- src/components/UserProfile/BlueSquares/BlueSquare.jsx
+++ src/components/UserProfile/BlueSquares/BlueSquare.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './BlueSquare.css';
 import hasPermission from 'utils/permissions';
 import { connect } from 'react-redux';
