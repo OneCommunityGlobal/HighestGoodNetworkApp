2f67ea088231dccbbc9fa5f99c03f6e3db39aa66
wang9hu <xwang.9hu@gmail.com>
2024-01-30 22:24:57 -0800

    add one day timeentry filter in teammemebertask tab

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -2,6 +2,7 @@ export const orange = '#f26100';
 export const silverGray = '#e9e9e9';
 export const warningRed = '#B22222';
 export const hrsFilterBtnColorMap = {
+  1: '#eb8334', // lightbrown
   2: '#DC143C', // red
   3: '#6495ED', // blue
   4: '#228B22', // green
