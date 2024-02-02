2af46205e6b6e3925d60da078ea7388d165710c5
Changhao3220K <lichanghao.mail@gmail.com>
2024-02-01 17:23:41 -0800

    Changhao-added new unit tests for weeklysummariesreport.test.jsx

--- src/components/WeeklySummariesReport/__tests__/WeeklySummariesReport.test.jsx
+++ src/components/WeeklySummariesReport/__tests__/WeeklySummariesReport.test.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { fireEvent, waitFor, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import { WeeklySummariesReport } from './WeeklySummariesReport';
-import hasPermission from '../../utils/permissions';
-import { authMock, userProfileMock, rolesMock } from '../../__tests__/mockStates';
-import { renderWithProvider } from '../../__tests__/utils';
+import { WeeklySummariesReport } from '../WeeklySummariesReport';
+import hasPermission from '../../../utils/permissions';
+import { authMock, userProfileMock, rolesMock } from '../../../__tests__/mockStates';
+import { renderWithProvider } from '../../../__tests__/utils';
 import thunk from 'redux-thunk';
 import configureStore from 'redux-mock-store';
 
@@ -55,6 +55,46 @@ describe('WeeklySummariesReport page', () => {
     });
   });
 
+  describe('Structure', () => {
+    beforeEach(() => {
+      const curr_props = {
+        hasPermission: hasPermission,
+        getWeeklySummariesReport: jest.fn(),
+        fetchAllBadges: jest.fn(),
+        getInfoCollections: jest.fn(),
+        loading: false,
+        summaries: [],
+        authUser: { role: '' },
+        roles: [],
+        badges: [],
+      };
+      renderWithProvider(<WeeklySummariesReport {...curr_props} />, { store, });
+    });
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+    it('should render Weekly Summaries Reports page correctly', () => {
+      expect(screen.getByText('Weekly Summaries Reports page')).toBeInTheDocument();
+    });
+    it('should render Select Team Code correctly', () => {
+      expect(screen.getByText('Select Team Code')).toBeInTheDocument();
+    });
+    it('should render Select Color correctly', () => {
+      expect(screen.getByText('Select Color')).toBeInTheDocument();
+    });
+    it('should render Tab This Week correctly', () => {
+      expect(screen.getByText('This Week')).toBeInTheDocument();
+    });
+    it('should render Tab Last Week correctly', () => {
+      expect(screen.getByText('Last Week')).toBeInTheDocument();
+    });
+    it('should render Tab Week Before Last correctly', () => {
+      expect(screen.getByText('Week Before Last')).toBeInTheDocument();
+    });
+    it('should render Tab Three Weeks Ago correctly', () => {
+      expect(screen.getByText('Three Weeks Ago')).toBeInTheDocument();
+    });
+  });
   describe('Tabs display', () => {
     const props = {
       hasPermission: hasPermission,
@@ -74,7 +114,6 @@ describe('WeeklySummariesReport page', () => {
     afterEach(() => {
       jest.clearAllMocks();
     });
-
     it('should have second tab set to "active" by default', () => {
       expect(screen.getByTestId('Last Week').classList.contains('active')).toBe(true);
       expect(screen.getBy)
