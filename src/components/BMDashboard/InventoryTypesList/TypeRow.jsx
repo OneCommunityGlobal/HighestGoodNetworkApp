2e43e323d8460d2a0ade0a947a1380990d89bec4
Shuhua-L <sl.shuhualiu@gmail.com>
2024-02-03 21:07:32 -0500

    added in-place form for updating types

--- src/components/BMDashboard/InventoryTypesList/TypeRow.jsx
+++ src/components/BMDashboard/InventoryTypesList/TypeRow.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { connect } from 'react-redux';
 import { Button } from 'react-bootstrap';
 import { deleteBuildingInventoryType } from 'actions/bmdashboard/invTypeActions';
@@ -5,13 +6,67 @@ import { deleteBuildingInventoryType } from 'actions/bmdashboard/invTypeActions'
 export function TypeRow(props) {
   const { itemType, id, dispatch } = props;
 
+  const [editMode, setEditMode] = useState(false);
+  const [updatedType, setUpdatedType] = useState({
+    name: itemType.name,
+    description: itemType.description,
+  });
+
   const handleEdit = () => {
     // TODO:
+    setEditMode(false);
   };
 
   const handleDelete = () => {
     dispatch(deleteBuildingInventoryType({ category: itemType.category, id: itemType._id }));
   };
+
+  const handleChangeInput = e => {
+    const { name, value } = e.target;
+    setUpdatedType({ ...updatedType, [name]: value });
+  };
+
+  const toggleEditMode = () => setEditMode(!editMode);
+
+  if (editMode) {
+    return (
+      <tr>
+        <td>{id}</td>
+        <td>
+          <input
+            className="inventory-types-input"
+            name="name"
+            type="text"
+            required
+            maxLength="50"
+            onChange={handleChangeInput}
+            value={updatedType.name}
+          />
+        </td>
+        <td>
+          <textarea
+            className="inventory-types-input"
+            name="description"
+            required
+            maxLength="150"
+            rows="1"
+            onChange={handleChangeInput}
+            value={updatedType.description}
+          />
+        </td>
+        <td>
+          <Button size="sm" className="#btn-types" variant="success" onClick={handleEdit}>
+            Save
+          </Button>
+        </td>
+        <td>
+          <Button size="sm" variant="danger" className="#btn-types" onClick={toggleEditMode}>
+            Cancel
+          </Button>
+        </td>
+      </tr>
+    );
+  }
 
   return (
     <tr>
@@ -19,7 +74,7 @@ export function TypeRow(props) {
       <td>{itemType.name}</td>
       <td>{itemType.description}</td>
       <td>
-        <Button size="sm" className="btn-types" onClick={handleEdit}>
+        <Button size="sm" className="btn-types" onClick={toggleEditMode}>
           Edit
         </Button>
       </td>
