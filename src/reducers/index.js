f5cae0ceff0229123d499193240678b838a341b1
odanykh <odanykh@ford.com>
2024-02-04 23:03:31 -0500

    copy of reverted pr1854 with fixed issues

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -33,10 +33,13 @@ import { mouseoverTextReducer } from './mouseoverTextReducer';
 import { materialsReducer } from './bmdashboard/materialsReducer';
 import { bmProjectReducer } from './bmdashboard/projectReducer';
 import { bmInvTypeReducer } from './bmdashboard/inventoryTypeReducer';
+import { lessonsReducer } from './bmdashboard/lessonsReducer';
+import { bmProjectByIdReducer } from './bmdashboard/projectByIdReducer';
 import { bmInvUnitReducer } from './bmdashboard/inventoryUnitReducer';
 import { consumablesReducer } from './bmdashboard/consumablesReducer';
 import { toolReducer } from './bmdashboard/toolReducer';
 import { timeOffRequestsReducer } from "./timeOffRequestReducer"
+
 
 const localReducers = {
   auth: authReducer,
@@ -67,6 +70,8 @@ const localReducers = {
   bmProjects: bmProjectReducer,
   bmInvTypes: bmInvTypeReducer,
   timeOffRequests: timeOffRequestsReducer,
+  lessons: lessonsReducer,
+  project: bmProjectByIdReducer,
   bmTools: toolReducer,
   bmInvUnits: bmInvUnitReducer,
   bmConsumables: consumablesReducer
@@ -84,3 +89,4 @@ const sessionReducers = {
 };
 
 export { localReducers, sessionReducers };
+
