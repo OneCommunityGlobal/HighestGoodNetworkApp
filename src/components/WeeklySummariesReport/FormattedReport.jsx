46247a0406db8d25d5c9b7772edd8a9425831954
Shiwani Rajagopalan <rajagopalanshiwani@gmail.com>
2024-02-13 16:57:19 -0500

    rebased with development to resolve conflicts

--- src/components/WeeklySummariesReport/FormattedReport.jsx
+++ src/components/WeeklySummariesReport/FormattedReport.jsx
@@ -69,6 +69,8 @@ function FormattedReport({
   canSeeBioHighlight,
 }) {
   // if (auth?.user?.role){console.log(auth.user.role)}
+  const dispatch = useDispatch();
+  const isEditCount = dispatch(hasPermission('totalValidWeeklySummaries'));
 
   return (
     <>
