dfd67496792b257fa6efdd37eb0f135b8fb22a6d
Chengyan Wang <wang.12517@osu.edu>
2024-02-19 10:32:30 -0500

    Bug fix in handleSort() function, lint checked

--- src/components/BMDashboard/Reusables/ReusablesTable.jsx
+++ src/components/BMDashboard/Reusables/ReusablesTable.jsx
@@ -23,38 +23,33 @@ function ReusablesTable({ reusable, project }) {
   }, [reusables]);
 
   const handleSort = column => {
-    if (!column || reusables.length === 0) return;
-    switch (column) {
-      case 'project': {
-        setSortOrder({ ...sortOrder, project: sortOrder.project === 'asc' ? 'desc' : 'asc' });
-        setIconToDisplay({
-          ...iconToDisplay,
-          project: iconToDisplay.project === faSortUp ? faSortDown : faSortUp,
-        });
-        const factor = sortOrder.project === 'asc' ? 1 : -1;
-        const sortedReusables = [...reusables].sort((a, b) => {
-          return factor * a.project.name.localeCompare(b.project.name);
-        });
-        setReusablesViewData(sortedReusables);
-        break;
-      }
-      case 'itemType': {
-        setSortOrder({ ...sortOrder, itemType: sortOrder.itemType === 'asc' ? 'desc' : 'asc' });
-        setIconToDisplay({
-          ...iconToDisplay,
-          itemType: iconToDisplay.itemType === faSortUp ? faSortDown : faSortUp,
-        });
-        const factor = sortOrder.itemType === 'asc' ? 1 : -1;
-        const sortedReusables = [...reusables].sort((a, b) => {
-          return factor * a.itemType.name.localeCompare(b.itemType.name);
-        });
-        setReusablesViewData(sortedReusables);
-        break;
-      }
-      default: {
-        break;
-      }
+    if (!column || reusables.length === 0) {
+      return;
     }
+
+    if (project.value !== '0' && column === 'project') {
+      return;
+    }
+
+    const filteredReusables =
+      project.value !== '0'
+        ? reusables.filter(rec => rec.project?._id === project.value)
+        : reusables;
+
+    const newSortOrder = sortOrder[column] === 'asc' ? 'desc' : 'asc';
+    setSortOrder(prevOrder => ({ ...prevOrder, [column]: newSortOrder }));
+
+    const newIcon = iconToDisplay[column] === faSortUp ? faSortDown : faSortUp;
+    setIconToDisplay(prevIcons => ({ ...prevIcons, [column]: newIcon }));
+
+    const factor = newSortOrder === 'asc' ? 1 : -1;
+    const sortedReusables = filteredReusables.sort((a, b) => {
+      const nameA = a[column]?.name || '';
+      const nameB = b[column]?.name || '';
+      return factor * nameA.localeCompare(nameB);
+    });
+
+    setReusablesViewData(sortedReusables);
   };
 
   useEffect(() => {
@@ -68,24 +63,34 @@ function ReusablesTable({ reusable, project }) {
     setReusablesViewData([...filteredReusables]);
   }, [project, reusable, reusables]);
 
+  function conditionalProjectTableHeaderUI() {
+    const isSortingAllowed = project.value === '0';
+    const sortOrderTooltip = isSortingAllowed
+      ? `Sort project ${sortOrder.project === 'asc' ? 'desc' : 'asc'}`
+      : undefined;
+    return (
+      <th onClick={isSortingAllowed ? () => handleSort('project') : undefined}>
+        <div
+          data-tip={sortOrderTooltip}
+          className={`d-flex align-items-stretch ${isSortingAllowed ? 'cursor-pointer' : ''}`}
+        >
+          <div>Project</div>
+          {isSortingAllowed && <FontAwesomeIcon icon={iconToDisplay.project} size="lg" />}
+        </div>
+        {isSortingAllowed && <ReactTooltip />}
+      </th>
+    );
+  }
+
   return (
     <div>
       <Table responsive>
         <thead className="BuildingTableHeaderLine">
           <tr>
-            <th onClick={() => handleSort('project')}>
-              <div
-                data-tip={`Sort project ${sortOrder.project}`}
-                className="d-flex align-items-stretch cursor-pointer"
-              >
-                <div>Project</div>
-                <FontAwesomeIcon icon={iconToDisplay.project} size="lg" />
-              </div>
-              <ReactTooltip />
-            </th>
+            {conditionalProjectTableHeaderUI()}
             <th onClick={() => handleSort('itemType')}>
               <div
-                data-tip={`Sort name ${sortOrder.itemType}`}
+                data-tip={`Sort name ${sortOrder.itemType === 'asc' ? 'desc' : 'asc'}`}
                 className="d-flex align-items-stretch cursor-pointer"
               >
                 <div>Name</div>
@@ -93,6 +98,7 @@ function ReusablesTable({ reusable, project }) {
               </div>
               <ReactTooltip />
             </th>
+
             <th>Bought</th>
             <th>Available</th>
             <th>Destroyed</th>
