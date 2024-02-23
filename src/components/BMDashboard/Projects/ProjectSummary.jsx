dd011dd574df23a717a824e97f345fb6bf96e62e
saideepak <dogiparthi.sai@northeastern.edu>
2024-02-22 20:20:32 -0500

    Made the web page more aligned with responsivness

--- src/components/BMDashboard/Projects/ProjectSummary.jsx
+++ src/components/BMDashboard/Projects/ProjectSummary.jsx
@@ -17,34 +17,34 @@ function ProjectSummary({ project }) {
         <h2>{project.name} Summary</h2>
       </Row>
       <Row className="project-summary_item mx-auto">
-        <Col xs="4">
+        <Col xs={window.innerWidth < 700 ? '6' : '4'}>
           <Label className="project-summary_label">Total hours of work done:</Label>
         </Col>
-        <Col xs="7">
+        <Col xs="3">
           <span className="project-summary_span">{hoursWorked}</span>
         </Col>
       </Row>
       <Row className="project-summary_item mx-auto">
-        <Col xs="4">
+        <Col xs={window.innerWidth < 700 ? '6' : '4'}>
           <Label className="project-summary_label">Total cost of materials:</Label>
         </Col>
-        <Col xs="7">
+        <Col xs="3">
           <span className="project-summary_span">{totalMaterialsCost} USD</span>
         </Col>
       </Row>
       <Row className="project-summary_item mx-auto">
-        <Col xs="4">
+        <Col xs={window.innerWidth < 700 ? '6' : '4'}>
           <Label className="project-summary_label">Total cost of equipment:</Label>
         </Col>
-        <Col xs="7">
+        <Col xs="3">
           <span className="project-summary_span">{totalEquipmentCost} USD</span>
         </Col>
       </Row>
       <Row className="project-summary_item mx-auto">
-        <Col xs="4">
+        <Col xs={window.innerWidth < 700 ? '6' : '4'}>
           <Label className="project-summary_label">Waste:</Label>
         </Col>
-        <Col xs="7">
+        <Col xs={window.innerWidth < 700 ? '6' : '7'}>
           <span className="project-summary_span">
             {mostMaterialWaste?.stockWasted} {mostMaterialWaste?.itemType.unit} of{' '}
             {mostMaterialWaste?.itemType.name} has been wasted!
@@ -52,26 +52,26 @@ function ProjectSummary({ project }) {
         </Col>
       </Row>
       <Row className="project-summary_item mx-auto">
-        <Col xs="4">
+        <Col xs={window.innerWidth < 700 ? '6' : '4'}>
           <Label className="project-summary_label">Total members:</Label>
         </Col>
-        <Col xs="7">
+        <Col xs="3">
           <span className="project-summary_span">{members.length}</span>
         </Col>
       </Row>
       <Row className="project-summary_item mx-auto">
-        <Col xs="4">
+        <Col xs={window.innerWidth < 700 ? '6' : '4'}>
           <Label className="project-summary_label">Rentals:</Label>
         </Col>
-        <Col xs="7">
+        <Col xs={window.innerWidth < 700 ? '6' : '7'}>
           <span className="project-summary_span">Excavator 2 rental ends in 72 hours!</span>
         </Col>
       </Row>
       <Row className="project-summary_item mx-auto">
-        <Col xs="4">
+        <Col xs={window.innerWidth < 700 ? '6' : '4'}>
           <Label className="project-summary_label">Most material bought:</Label>
         </Col>
-        <Col xs="7">
+        <Col xs={window.innerWidth < 700 ? '6' : '7'}>
           <span className="project-summary_span">
             {mostMaterialBought?.stockBought} {mostMaterialBought?.itemType.unit} of{' '}
             {mostMaterialBought?.itemType.name} purchased for this project
@@ -79,10 +79,10 @@ function ProjectSummary({ project }) {
         </Col>
       </Row>
       <Row className="project-summary_item mx-auto">
-        <Col xs="4">
+        <Col xs={window.innerWidth < 700 ? '6' : '4'}>
           <Label className="project-summary_label">Stock:</Label>
         </Col>
-        <Col xs="8">
+        <Col xs={window.innerWidth < 700 ? '6' : '8'}>
           <span className="project-summary_span">
             {leastMaterialAvailable?.itemType.name} is nearly out of stock (
             {leastMaterialAvailable?.stockAvailable} {leastMaterialAvailable?.itemType.unit}{' '}
