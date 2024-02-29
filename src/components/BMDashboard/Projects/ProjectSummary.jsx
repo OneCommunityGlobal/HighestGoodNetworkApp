ae0012de318de62e0b9b42d52841e42415b8b111
saideepak <dogiparthi.sai@northeastern.edu>
2024-02-28 22:45:29 -0500

    Implemented the use of usestate and useEffect react hooks for dynamic page rendering and better responsiveness and removed duplicate values

--- src/components/BMDashboard/Projects/ProjectSummary.jsx
+++ src/components/BMDashboard/Projects/ProjectSummary.jsx
@@ -1,4 +1,5 @@
 import { Row, Col, Label } from 'reactstrap';
+import { useState, useEffect } from 'react';
 
 function ProjectSummary({ project }) {
   const {
@@ -11,13 +12,29 @@ function ProjectSummary({ project }) {
     leastMaterialAvailable,
   } = project;
 
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const summaryLabelCol = windowWidth < 700 ? '6' : '4';
+
   return (
     <div className="project-summary_content">
       <Row className="project-summary_header mx-auto">
         <h2>{project.name} Summary</h2>
       </Row>
       <Row className="project-summary_item mx-auto">
-        <Col xs={window.innerWidth < 700 ? '6' : '4'}>
+        <Col xs={summaryLabelCol}>
           <Label className="project-summary_label">Total hours of work done:</Label>
         </Col>
         <Col xs="3">
@@ -25,7 +42,7 @@ function ProjectSummary({ project }) {
         </Col>
       </Row>
       <Row className="project-summary_item mx-auto">
-        <Col xs={window.innerWidth < 700 ? '6' : '4'}>
+        <Col xs={summaryLabelCol}>
           <Label className="project-summary_label">Total cost of materials:</Label>
         </Col>
         <Col xs="3">
@@ -33,7 +50,7 @@ function ProjectSummary({ project }) {
         </Col>
       </Row>
       <Row className="project-summary_item mx-auto">
-        <Col xs={window.innerWidth < 700 ? '6' : '4'}>
+        <Col xs={summaryLabelCol}>
           <Label className="project-summary_label">Total cost of equipment:</Label>
         </Col>
         <Col xs="3">
@@ -41,10 +58,10 @@ function ProjectSummary({ project }) {
         </Col>
       </Row>
       <Row className="project-summary_item mx-auto">
-        <Col xs={window.innerWidth < 700 ? '6' : '4'}>
+        <Col xs={summaryLabelCol}>
           <Label className="project-summary_label">Waste:</Label>
         </Col>
-        <Col xs={window.innerWidth < 700 ? '6' : '7'}>
+        <Col xs={summaryLabelCol}>
           <span className="project-summary_span">
             {mostMaterialWaste?.stockWasted} {mostMaterialWaste?.itemType.unit} of{' '}
             {mostMaterialWaste?.itemType.name} has been wasted!
@@ -52,7 +69,7 @@ function ProjectSummary({ project }) {
         </Col>
       </Row>
       <Row className="project-summary_item mx-auto">
-        <Col xs={window.innerWidth < 700 ? '6' : '4'}>
+        <Col xs={summaryLabelCol}>
           <Label className="project-summary_label">Total members:</Label>
         </Col>
         <Col xs="3">
@@ -60,7 +77,7 @@ function ProjectSummary({ project }) {
         </Col>
       </Row>
       <Row className="project-summary_item mx-auto">
-        <Col xs={window.innerWidth < 700 ? '6' : '4'}>
+        <Col xs={summaryLabelCol}>
           <Label className="project-summary_label">Rentals:</Label>
         </Col>
         <Col xs={window.innerWidth < 700 ? '6' : '7'}>
@@ -68,7 +85,7 @@ function ProjectSummary({ project }) {
         </Col>
       </Row>
       <Row className="project-summary_item mx-auto">
-        <Col xs={window.innerWidth < 700 ? '6' : '4'}>
+        <Col xs={summaryLabelCol}>
           <Label className="project-summary_label">Most material bought:</Label>
         </Col>
         <Col xs={window.innerWidth < 700 ? '6' : '7'}>
@@ -79,7 +96,7 @@ function ProjectSummary({ project }) {
         </Col>
       </Row>
       <Row className="project-summary_item mx-auto">
-        <Col xs={window.innerWidth < 700 ? '6' : '4'}>
+        <Col xs={summaryLabelCol}>
           <Label className="project-summary_label">Stock:</Label>
         </Col>
         <Col xs={window.innerWidth < 700 ? '6' : '8'}>
