a4297ca4735509fe56c019396e256decadbc2576
ilyaflaks <ilyaflaks@gmail.com>
2024-03-07 16:15:59 -0800

    cleaned console logs

--- src/components/BMDashboard/MaterialsList/RecordsModal.jsx
+++ src/components/BMDashboard/MaterialsList/RecordsModal.jsx
@@ -6,7 +6,6 @@ import './RecordsModal.css';
 export default function RecordsModal({ modal, setModal, record, setRecord, recordType }) {
   if (record) {
     const toggle = () => {
-      console.log("RecordModal toggled")
       setModal(false);
       setRecord(null);
     };
