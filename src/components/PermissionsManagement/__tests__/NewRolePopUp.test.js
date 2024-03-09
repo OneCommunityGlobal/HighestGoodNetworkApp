a1be6a7aacb7adbc37f92198db8a7fbf7787dbab
Jerry Ren <jerryren.5279@gmail.com>
2024-03-09 01:26:20 -0500

    Added 'Error message appears when adding a duplicate role'

--- src/components/PermissionsManagement/__tests__/NewRolePopUp.test.js
+++ src/components/PermissionsManagement/__tests__/NewRolePopUp.test.js
@@ -1,54 +1,13 @@
 import React from 'react';
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
-import thunk from 'redux-thunk'; // I don't think I need thunk because the store update is synchronous. It is not asynchronous (i.e., it does not involve an HTTP request). 
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
-import { shallow } from 'enzyme';
 import PermissionList from '../PermissionList';
-
-// Check that input element's value changes with user interaction
-
 const { default: NewRolePopUp } = require("../NewRolePopUp")
 
-
-// Since the PermissionList child component has complex behavior and log crucial to the overall functionality of the NewRolePopUp component, I will write test for it to ensure it works correctly. Such as...
-  // Renders a list of permissions passed to it via props. (It actually just renders all the Permissions from the external file containing all the permission names and descriptions.)
-
-// Use enzyme to mock the child component, PermissionList. To verify that it renders correctly. (Nah, I can't figure out how to use enzyme correctly with Redux components)
-
-// Check that the Create button runs the handleSubmit function when clicked
-
-// Check that the reducers work correctly (maybe not needed as the Unit test file specifically tests for the Reducers)
-
-// Check that addNewRole works. 
-  // mock addNewRole() and checks that it was called.
-
-// mock togglePopUpNewRole() and roleNames data
 const mockTogglePopUpNewRole = jest.fn()
 
-// time entry 1: changed mockRoleNames from this to that. Found this out by console logging the props passed into NewRolePopUp when interacting with the component normally.
-// const mockRoleNames = [
-//   {
-//     permissions: [
-//       'postRole',
-//       'deleteRole',
-//       'putRole',
-//       'addDeleteEditOwners',
-//       'putUserProfilePermissions'
-//     ],
-//     roleName: 'Owner',
-//   },
-//   {
-//     permissions: [
-//       'seeBadges',
-//       'assignBadges',
-//       'createBadges',
-//       'deleteBadges',
-//       'updateBadges',
-//     ],
-//     roleName: 'Administrator',
-//   }
-// ]
 const mockRoleNames = [ 'Owner', 'Administrator' ]
 
 const mockAuth = {
@@ -77,8 +36,6 @@ const store = mockStore({
   role: [mockRoleNames]
 })
 
-
-
 const renderComponent = () => {
   const { container } = render(
     <Provider store={store}>
@@ -89,61 +46,36 @@ const renderComponent = () => {
   return container
 }
 
+beforeEach(() => {
+  store.clearActions(); // clear the dispatched actions stored internally by the mock store instance
+});
+
+
 describe('Render NewRolePopUp', () => {
   it('Renders without crashing', () => {
     renderComponent();
   });
   it('Render PermissionList child component', () => {
-    // mock props
     const mockPermissionsChecked = []; // permissionsChecked is initially empty and remains empty in NewRolePopUp.jsx.
     const mockSetPermissionChecked = jest.fn();
-    // The rendered component is stored in the 'container' variable.
     const { container } = render(
       <Provider store={store}>
         <PermissionList rolePermissions={mockPermissionsChecked} editable={true} setPermissions={mockSetPermissionChecked} />
       </Provider>
     );
-    
-    // Retrieve the first child element.
     const permissionList = container.firstChild // expect .user-role-tab__permissionList
-    // jest-dom's toHaveClass() function (https://github.com/testing-library/jest-dom?tab=readme-ov-file#tohaveclass)
     expect(permissionList).toHaveClass('user-role-tab__permissionList')
-    
-    // Check that it renders somethings.
-
-    /////////////////// My attempt at using Enzyme...  /////////////////// 
-    // const wrapper = shallow(
-    //   <Provider store={store}>
-    //     <PermissionList rolePermissions={mockPermissionsChecked} editable={true} setPermissions={mockSetPermissionChecked} />
-    //   </Provider>
-    // );
-
-    // expect(wrapper.find('.user-role-tab__permissionList')).to.have.lengthOf(1);
-    // expect(wrapper.find('.user-role-tab__permissionList').length).toBe(1);
-    // console.log(wrapper.debug()); // renders the PermissionList component like this: <Connect(PermissionList) rolePermissions={{...}} ... />
-    
-    /////////////////////////////////////////////////////////////////////
   });
   it('Create button on click dispatches action to store', async () => {
-    // render component
     const container = renderComponent();
-
-    // console.log(container.innerHTML);
-
-    // select button
-    // const createBtn = document.getElementById('createRole')
     const createBtn = screen.getByText('Create')
     expect(createBtn).toBeInTheDocument();
-    
     
     // Add an input to Role field
     const roleInput = screen.getByPlaceholderText('Please enter a new role name')
     expect(roleInput).toBeInTheDocument()
-    // Simulate a change event with a new value
-    fireEvent.change(roleInput, { target: { value: 'temp' } });
-    // console.log('roleInput.value: ' +  roleInput.value)
-
-    expect(roleInput.value).toBe('temp')
+    fireEvent.change(roleInput, { target: { value: 'newRole' } });
+    expect(roleInput.value).toBe('newRole')
 
     // click on button
     fireEvent.click(createBtn);
@@ -151,11 +83,23 @@ describe('Render NewRolePopUp', () => {
     // check that action is dispatched to store.
     const actions = store.getActions()
     console.log("🚀 ~ file: NewRolePopUp.test.js:139 ~ it ~ actions:", actions)
-
+    
     await waitFor(() => {
       expect(actions).toHaveLength(1)
+      expect(actions).toContainEqual({ type: 'ADD_NEW_ROLE', payload: {} })
+      console.log("🚀 ~ file: NewRolePopUp.test.js:159 ~ it ~ actions:", actions)
     })
   });
-
-
+  it('Error message appears when adding a duplicate role', async () => {
+    const container = renderComponent();
+    const roleInput = screen.getByPlaceholderText('Please enter a new role name')
+    
+    expect(roleInput).toBeInTheDocument()
+    fireEvent.change(roleInput, { target: { value: 'Administrator' } });
+    expect(roleInput.value).toBe('Administrator')
+    
+    await waitFor(() => {
+      expect(screen.getByText('Please enter a different role name')).toBeInTheDocument()
+    })
+  });
 })
