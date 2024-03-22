3ffe7cae1c36d2ffe0bee9e6830505ea0dc15a5f
abdelmounaim lallouache <abdelmounaim.lallouache@gmail.com>
2024-03-22 14:23:06 -0500

    fix linter issue

--- src/components/LeaderBoard/Leaderboard.jsx
+++ src/components/LeaderBoard/Leaderboard.jsx
@@ -339,41 +339,44 @@ function LeaderBoard({
                   )}
                 </th>
                 <td className="align-middle">
-                  {allRequests[item.personId] &&
-                    allRequests[item.personId]?.length > 0 && (
-                      <div>
-                        <button
-                          type="button"
-                          onClick={() => {
-                            const data = {requests:[...allRequests[item.personId]], name: item.name , leaderboard: true}
-                            handleTimeOffModalOpen(data);
-                          }}
-                          style={{ width: '35px', height: 'auto' }}
+                  {allRequests[item.personId] && allRequests[item.personId]?.length > 0 && (
+                    <div>
+                      <button
+                        type="button"
+                        onClick={() => {
+                          const data = {
+                            requests: [...allRequests[item.personId]],
+                            name: item.name,
+                            leaderboard: true,
+                          };
+                          handleTimeOffModalOpen(data);
+                        }}
+                        style={{ width: '35px', height: 'auto' }}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="22"
+                          height="19"
+                          viewBox="0 0 448 512"
+                          className="show-time-off-calender-svg"
                         >
+                          <path d="M128 0c17.7 0 32 14.3 32 32V64H288V32c0-17.7 14.3-32 32-32s32 14.3 32 32V64h48c26.5 0 48 21.5 48 48v48H0V112C0 85.5 21.5 64 48 64H96V32c0-17.7 14.3-32 32-32zM0 192H448V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V192zm64 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm128 0v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H336zM64 400v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H208zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H336c-8.8 0-16 7.2-16 16z" />
+                        </svg>
+
+                        <i className="show-time-off-icon">
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
-                            width="22"
-                            height="19"
-                            viewBox="0 0 448 512"
-                            className="show-time-off-calender-svg"
+                            width="18"
+                            height="18"
+                            viewBox="0 0 512 512"
+                            className="show-time-off-icon-svg"
                           >
-                            <path d="M128 0c17.7 0 32 14.3 32 32V64H288V32c0-17.7 14.3-32 32-32s32 14.3 32 32V64h48c26.5 0 48 21.5 48 48v48H0V112C0 85.5 21.5 64 48 64H96V32c0-17.7 14.3-32 32-32zM0 192H448V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V192zm64 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm128 0v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H336zM64 400v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H208zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H336c-8.8 0-16 7.2-16 16z" />
+                            <path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z" />
                           </svg>
-
-                          <i className="show-time-off-icon">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="18"
-                              height="18"
-                              viewBox="0 0 512 512"
-                              className="show-time-off-icon-svg"
-                            >
-                              <path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z" />
-                            </svg>
-                          </i>
-                        </button>
-                      </div>
-                    )}
+                        </i>
+                      </button>
+                    </div>
+                  )}
                 </td>
                 <td className="align-middle" id={`id${item.personId}`}>
                   <span title="Tangible time">{item.tangibletime}</span>
