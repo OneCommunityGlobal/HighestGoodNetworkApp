447b5a9fbceafffcdcd43583981762bcc9ff54a3
xiao <49748716+JYXiao-2021@users.noreply.github.com>
2024-03-24 00:23:13 -0400

    update

--- src/components/Header/__tests__/Header.test.js
+++ src/components/Header/__tests__/Header.test.js
@@ -3,11 +3,10 @@ import { shallow } from 'enzyme';
 import { Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store';
 import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { MemoryRouter as Router } from 'react-router-dom';
 import { Header } from '../Header'; 
 
-
+jest.mock('axios');
 const mockStore = configureMockStore();
 
 describe('Header Component', () => {
@@ -44,33 +43,52 @@ describe('Header Component', () => {
 
     expect(wrapper.exists()).toBe(true);
   });
-
-  it('displays the welcome message correctly', () => {
-    const wrapper =shallow(
-      <Provider store={store}>
-        <Router>
-          <Header />
-        </Router>
-      </Provider>
-    );
-    console.log(wrapper.text());
-
-    expect(wrapper.text()).toContain('Welcome, John');
-  });
-
-  it('renders elements based on permissions', () => {
-    // Assuming hasPermission mock is set up to return true for certain permissions
-  
-    // Check for User Management link based on permissions
-    const userManagementLink = component.findWhere(node => node.type() === 'NavLink' && node.props().to === '/usermanagement');
-    expect(userManagementLink.exists()).toBe(true); // Adjust based on actual permissions
-  
-  });
-
 });
 
+describe('Header Component with Mocked Axios', () => {
+  const initialState = {
+    auth: {
+      isAuthenticated: true,
+      user: {
+        userid: 'volunteerUserId',
+        role: 'Volunteer',
+      },
+    },
+    //userProfile: '',
+    taskEditSuggestionCount: 0,
+    role:{roles:[],},
+  }; 
+  const store = mockStore(initialState);
+  const mockHasPermission = (permission) => {
+    const permissions = {
+      getReports: false,
+      getWeeklySummaries: false,
+      postUserProfile: false,
+      seeBadges: false,
+      createPopup: false,
+      updatePopup: false,
+    };
+    return permissions[permission] || false;
+  };
+  beforeEach(() => {
+    // Mock the Axios call
+    axios.get.mockResolvedValue({
+      data: { name: 'Test User', role: 'Volunteer', profilePic: '/path/to/img' },
+    });
+    store.clearActions();
+  });
 
-describe('Header Component volunteer', () => {
+  it('loads and displays the user dashboard profile', () => {
+    // Directly shallow render the Header component
+    const wrapper = shallow(
+    <Provider store={store}>
+      <Header hasPermission={mockHasPermission} />
+    </Provider>);
+    expect(wrapper.exists()).toBe(true);
+  });
+});
+
+describe('Header Component - volunteer, Assistan Manager', () => {
   it('renders correctly for a volunteer', () => {
     const mockHasPermission = (permission) => {
       const permissions = {
@@ -103,8 +121,69 @@ describe('Header Component volunteer', () => {
         </Router>
       </Provider>
     );  
+    expect(wrapper.exists()).toBe(true);
+
   });
 });
 
+describe('Header Component - Owner, Administrator, Mentor', () => {
+  let store;
+  beforeEach(() => {
+    store = mockStore({
+      auth: {
+        isAuthenticated: true,
+        user: {
+          userid: 'test-user-id',
+          role: 'Owner'
+        },
+        firstName: 'John',
+        profilePic: '/path/to/profile/pic',
+      },
+      userProfile: {
+        email: 'test@example.com',
+      },
+      taskEditSuggestions: {
+        count: 0,
+      },
+      role: {
+        roles: [],
+      },
+    });
+  });
+
+  it('renders correctly for a Owner', () => {
+    const wrapper = shallow(
+      <Provider store={store}>
+        <Router>
+          <Header />
+        </Router>
+      </Provider>
+    );
+    expect(wrapper.exists()).toBe(true);
+  
+  });
+
+  it('renders correctly for an Administrator', () => {
+    store.getState().auth.user.role = 'Administrator';
+    shallow(
+      <Provider store={store}>
+        <Router>
+          <Header />
+        </Router>
+      </Provider>
+    );
+  });
 
 
+  it('renders correctly for a Mentor', () => {
+    store.getState().auth.user.role = 'Mentor';
+    shallow(
+      <Provider store={store}>
+        <Router>
+          <Header />
+        </Router>
+      </Provider>
+    );
+  });
+
+});
