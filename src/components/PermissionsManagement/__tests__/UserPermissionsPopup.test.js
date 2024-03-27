2e2a465d75687b91145889fb98e3290ed232e2f7
Shiwani Rajagopalan <rajagopalanshiwani@gmail.com>
2024-03-27 10:36:09 -0400

    removed unused imports

--- src/components/PermissionsManagement/__tests__/UserPermissionsPopup.test.js
+++ src/components/PermissionsManagement/__tests__/UserPermissionsPopup.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent, waitFor, act, wait } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import UserPermissionsPopUp from '../UserPermissionsPopUp';
 import thunk from 'redux-thunk';
