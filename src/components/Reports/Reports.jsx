7a86828783629f910c187d3076484cb602d068a0
yvonnep165 <pan.432@osu.edu>
2024-03-28 21:57:23 -0700

    adjust the formatting for different screen size to show the result table below the search function when in narrow screen and show it on the right when the screen is wide enough. Add conditional class to show the search function in the middle of the page when no selection made.

--- src/components/Reports/Reports.jsx
+++ src/components/Reports/Reports.jsx
@@ -395,7 +395,7 @@ class ReportsPage extends Component {
     }
     return (
       <Container fluid className="mb-5 container-component-wrapper">
-        <div className="category-data-container">
+        <div className={`category-data-container ${this.state.showPeople || this.state.showProjects || this.state.showTeams || this.state.showTotalProject || this.state.showTotalPeople || this.state.showTotalTeam || this.state.showAddTimeForm || this.state.showAddPersonHistory || this.state.showAddTeamHistory || this.state.showAddProjHistory ? '' : 'no-active-selection'}`}>
         <div className="container-component-category">
         <h2 className="mt-3 mb-5">
           <div className="d-flex align-items-center">
