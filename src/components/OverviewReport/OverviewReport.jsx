49397cc6983e500d82688dc372647c288bde5bdc
Abi Liu <abiliu018@gmail.com>
2024-04-01 19:06:18 -0700

    refactor: refactored OverviewReport page from class component to functional

--- src/components/OverviewReport/OverviewReport.jsx
+++ src/components/OverviewReport/OverviewReport.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prefer-stateless-function */
-import { Component } from 'react';
+import { useState } from 'react';
 import { Container, Row, Col, NavItem, NavLink, Nav, TabContent } from 'reactstrap';
 import EditableInfoModal from 'components/UserProfile/EditableModal/EditableInfoModal';
 import { connect } from 'react-redux';
@@ -7,67 +7,58 @@ import OverviewReportPanel from './OverviewReportTab';
 
 const navItems = ['This Week', 'Last Week', 'Week Before Last', 'Three Weeks Ago'];
 
-export class OverviewReport extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeTab: sessionStorage.getItem('tabSelection') || navItems[0],
-    };
-  }
+function OverviewReport({ role }) {
+  const [activeTab, setActiveTab] = useState(sessionStorage.getItem('tabSelection') || navItems[0]);
 
-  toggleTab = tab => {
-    const { activeTab } = this.state;
+  const toggleTab = tab => {
     if (activeTab !== tab) {
-      this.setState({ activeTab: tab });
+      setActiveTab(tab);
       sessionStorage.setItem('tabSelection', tab);
     }
   };
 
-  render() {
-    const { activeTab, role } = this.state;
-    return (
-      <Container fluid className="bg--white-smoke py-3 mb-5">
-        <Row>
-          <Col lg={{ size: 10, offset: 1 }}>
-            <h3 className="mt-3 mb-5">
-              <div className="d-flex align-items-center">
-                <span className="mr-2">Overview Report Page</span>
-                <EditableInfoModal
-                  areaName="OverviewReport"
-                  areaTitle="Overview Report"
-                  role={role}
-                  fontSize={24}
-                  isPermissionPage
-                  className="p-2"
-                />
-              </div>
-            </h3>
-          </Col>
-        </Row>
-        <Row>
-          <Col lg={{ size: 10, offset: 1 }}>
-            <Nav tabs>
-              {navItems.map(item => (
-                <NavItem key={item}>
-                  <NavLink
-                    href="#"
-                    data-testid={item}
-                    active={item === activeTab}
-                    onClick={() => this.toggleTab(item)}
-                  >
-                    {item}
-                  </NavLink>
-                </NavItem>
-              ))}
-            </Nav>
-            <TabContent activeTab={activeTab}>
-              <OverviewReportPanel activeTab={activeTab} userRole={role} />
-            </TabContent>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+  return (
+    <Container fluid className="bg--white-smoke py-3 mb-5">
+      <Row>
+        <Col lg={{ size: 10, offset: 1 }}>
+          <h3 className="mt-3 mb-5">
+            <div className="d-flex align-items-center">
+              <span className="mr-2">Overview Report Page</span>
+              <EditableInfoModal
+                areaName="OverviewReport"
+                areaTitle="Overview Report"
+                role={role}
+                fontSize={24}
+                isPermissionPage
+                className="p-2"
+              />
+            </div>
+          </h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={{ size: 10, offset: 1 }}>
+          <Nav tabs>
+            {navItems.map(item => (
+              <NavItem key={item}>
+                <NavLink
+                  href="#"
+                  data-testid={item}
+                  active={item === activeTab}
+                  onClick={() => toggleTab(item)}
+                >
+                  {item}
+                </NavLink>
+              </NavItem>
+            ))}
+          </Nav>
+          <TabContent activeTab={activeTab}>
+            <OverviewReportPanel activeTab={activeTab} userRole={role} />
+          </TabContent>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 const mapStateToProps = state => ({
