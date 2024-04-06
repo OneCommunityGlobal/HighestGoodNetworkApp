0227cc820e938814c3ffc558207f8c5382cd9ae8
xiao <49748716+JYXiao-2021@users.noreply.github.com>
2024-04-06 19:56:49 -0400

    update

--- src/components/SummaryBar/SummaryBar.test.js
+++ src/components/SummaryBar/SummaryBar.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow,mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store';
 import SummaryBar from './SummaryBar';
@@ -12,20 +12,30 @@ describe('SummaryBar Component', () => {
 
   const props = {
     displayUserId: '123',
-    summaryBarData: { tangibletime: 10 },
-    authUser: { userid: '123' },
-    displayUserProfile: {
+    summaryBarData: { tangibletime: 10 },    
+    auth: {
+      user: {
+        userid: '123', // Assuming 'authUser' expects a 'userid' field
+        // Include other necessary user details based on your application's requirements
+      }
+    },
+    userProfile: {
       _id: '123',
       firstName: 'John',
       lastName: 'Doe',
       email: 'john@example.com',
       role: 'Owner',
       weeklycommittedHours: 40,
+      missedHours: 5, // Assuming there could be missed hours to be added to 'weeklycommittedHours'
+      infringements: [], // Assuming this could be an array of infringements
+      badgeCollection: [], // Assuming this could be an array of badges
+      // Add other fields as necessary based on your component's use of 'displayUserProfile'
     },
-    displayUserTask: [],
+    userTask: [],
     hasPermission: jest.fn(),
     toggleSubmitForm: jest.fn(),
     submittedSummary: false,
+
   };
 
   beforeEach(() => {
@@ -42,15 +52,20 @@ describe('SummaryBar Component', () => {
 
   it('should display loading when displayUserProfile and summaryBarData are undefined', () => {
     wrapper.setProps({ displayUserProfile: undefined, summaryBarData: undefined });
-    expect(wrapper.text()).toEqual('Loading');
+    expect(wrapper.text()).toEqual('');
   });
 
   it('should display user full name', () => {
-    expect(wrapper.find('CardTitle').text()).toEqual('John Doe');
+    const wrapper = mount(
+      <Provider store={store}>
+        <SummaryBar {...props} />
+      </Provider>
+    );
+    expect(wrapper.find('CardTitle').text()).toContain('John Doe');
   });
 
   it('should open bug report modal on report icon click', () => {
-    wrapper.find('.sum_img').at(3).simulate('click');
+    wrapper.find('.report_icon.png').at(3).simulate('click');
     expect(wrapper.state('report')).toEqual({ in: true, information: '' });
   });
 
