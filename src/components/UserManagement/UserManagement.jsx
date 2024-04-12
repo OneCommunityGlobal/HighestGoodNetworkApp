6344dc59330ffb28e69ebdfd7fd0a15d79b996be
Imran Issa <issaimran94@gmail.com>
2024-04-12 19:37:06 +0200

    disable button in user management if user has no permisison to change users active status

--- src/components/UserManagement/UserManagement.jsx
+++ src/components/UserManagement/UserManagement.jsx
@@ -395,7 +395,7 @@ class UserManagement extends React.PureComponent {
    */
   onActiveInactiveClick = user => {
     const authRole = this?.props?.state?.auth?.user.role || user.role;
-    const canChangeUserStatus = hasPermission('changeUserStatus');
+    // const canChangeUserStatus = hasPermission('changeUserStatus');
     if (cantDeactivateOwner(user, authRole)) {
       //Owner user cannot be deactivated by another user that is not an Owner.
       alert('You are not authorized to deactivate an owner.');
--- src/components/UserManagement/UserTableData.jsx
+++ src/components/UserManagement/UserTableData.jsx
@@ -17,6 +17,7 @@ import { cantUpdateDevAdminDetails } from 'utils/permissions';
 const UserTableData = React.memo(props => {
   const [isChanging, onReset] = useState(false);
   const canAddDeleteEditOwners = props.hasPermission('addDeleteEditOwners');
+  const canChangeUserStatus = props.hasPermission('changeUserStatus');
 
   /**
    * reset the changing state upon rerender with new isActive status
@@ -45,7 +46,7 @@ const UserTableData = React.memo(props => {
       <td className="usermanagement__active--input">
         <ActiveCell
           isActive={props.isActive}
-          canChange={true}
+          canChange={canChangeUserStatus}
           key={`active_cell${props.index}`}
           index={props.index}
           onClick={() => props.onActiveInactiveClick(props.user)}
@@ -91,6 +92,10 @@ const UserTableData = React.memo(props => {
           type="button"
           className={`btn btn-outline-${props.isActive ? 'warning' : 'success'} btn-sm`}
           onClick={e => {
+            if(!canChangeUserStatus) {
+              toast.warn('You Do not have permissions to update this user’s status! ');
+              return;
+            }
             if(cantUpdateDevAdminDetails(props.user.email , props.authEmail)){
               alert('STOP! YOU SHOULDN’T BE TRYING TO CHANGE THIS. Please reconsider your choices.');
               return;
