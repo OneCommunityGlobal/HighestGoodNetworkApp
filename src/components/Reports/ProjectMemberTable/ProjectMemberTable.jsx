e2054a639373baa356aa9ee30fb2337364608434
Sarthak32 <sarth08.jai@gmail.com>
2024-04-13 11:09:10 +0530

    new commit

--- src/components/Reports/ProjectMemberTable/ProjectMemberTable.jsx
+++ src/components/Reports/ProjectMemberTable/ProjectMemberTable.jsx
@@ -110,7 +110,7 @@ export const ProjectMemberTable = ({ projectMembers, skip, take, handleMemberCou
         <div className="reports-table-head-cell">Active</div>
         <div className="reports-table-head-cell">ID</div>
       </div>
-      <div>
+      <div >
         {memberFilter == 'all-time' ? (
           allMemberTable.length > 0 ? (
             allMemberTable
