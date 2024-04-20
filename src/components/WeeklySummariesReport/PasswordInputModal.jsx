27b10818f818c0743ed03f510da00d845281fad4
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2024-04-20 21:15:19 +0530

    removes unused variable e from the function that fixes lint error

--- src/components/WeeklySummariesReport/PasswordInputModal.jsx
+++ src/components/WeeklySummariesReport/PasswordInputModal.jsx
@@ -42,7 +42,7 @@ export default function PasswordInputModal({
     setPasswordField(event.target.value);
   };
 
-  const revealPassword = e => {
+  const revealPassword = () => {
     setShowPassword(prev => !prev);
   };
 
