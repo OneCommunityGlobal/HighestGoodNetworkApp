79c2b0370f3ce1f9652f08252880181d62a50f6d
ilyaflaks <ilyaflaks@gmail.com>
2024-04-25 16:29:32 -0700

    fix: cleaned commented code

--- src/reducers/bmdashboard/toolReducer.js
+++ src/reducers/bmdashboard/toolReducer.js
@@ -16,14 +16,10 @@ const defaultState = {
 
 // eslint-disable-next-line import/prefer-default-export, default-param-last
 export const toolReducer = (tools = defaultState, action) => {
-  // console.log("toolReducer. action: ", action)
-  // console.log("action.payload: ", action)
   switch (action.type) {
     case GET_TOOLS: {
-      // console.log("GET_TOOLS in the reducer. action.payload: ", action.payload)
       // eslint-disable-next-line no-param-reassign
       tools.toolslist = action.payload;
-      // console.log("GET_TOOLS in reducer: ", tools)
       return {
         ...tools,
         updateTools: { ...defaultState.updateTools },
@@ -31,16 +27,7 @@ export const toolReducer = (tools = defaultState, action) => {
       };
     }
     case GET_TOOL_BY_ID:
-    case GET_TOOL_TYPES: {
-      // console.log("GET_TOOL_TYPES in the reducer.")
-      // eslint-disable-next-line no-param-reassign
-      // tools.toolslist = action.payload;
-      // return {
-      //   ...tools,
-      //   updateTools: { ...defaultState.updateTools },
-      //   updateToolsBulk: { ...defaultState.updateToolsBulk },
-      // };
-    }
+    case GET_TOOL_TYPES:
     case SET_TOOLS:
       return action.payload;
     default:
