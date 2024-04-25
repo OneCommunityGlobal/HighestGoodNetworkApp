39b7fb1b99939522fdf5ee9748bb919e31a07f52
Shiwani Rajagopalan <rajagopalanshiwani@gmail.com>
2024-04-25 01:25:05 -0400

    added 7 more test cases

--- src/components/BMDashboard/Login/__tests__/BMLogin.test.js
+++ src/components/BMDashboard/Login/__tests__/BMLogin.test.js
@@ -7,6 +7,7 @@ import thunk from 'redux-thunk';
 import configureStore from 'redux-mock-store';
 import { useHistory } from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import axios from 'axios';
 
 const mockStore = configureStore([thunk]);
 let store;
@@ -30,10 +31,19 @@ beforeEach(() => {
   });
 });
 
+jest.mock('axios');
+
+jest.mock('jwt-decode', () => jest.fn(token => ({ decodedPayload: 'mocked_decoded_payload' })));
+
+const history = {
+  push: jest.fn(),
+  location: { pathname: '/' },
+};
+
 const renderComponent = testStore => {
   function LoginWrapper() {
     const dispatch = useDispatch();
-    const history = useHistory();
+    //const history = useHistory();
     const location = {};
 
     return <BMLogin dispatch={dispatch} history={history} location={location} />;
@@ -96,15 +106,117 @@ describe('BMLogin component', () => {
     const buttonElement = screen.getByText('Submit');
     expect(buttonElement).toBeDisabled();
   });
-  it('check if validation works for invalid email id', async () => {
+  it('check if validation for invalid email id works as expected', () => {
     const { container } = renderComponent(store);
     const emailElement = container.querySelector('[name="email"]');
     fireEvent.change(emailElement, { target: { value: 'test' } });
 
+    const passwordElement = container.querySelector('[name="password"]');
+    fireEvent.change(passwordElement, { target: { value: '12' } });
+
     const submitElement = screen.getByText('Submit');
     fireEvent.click(submitElement);
 
-    const updatedEmailElement = container.querySelector('[name="email"]');
-    expect(updatedEmailElement).toBeInvalid();
+    expect(emailElement).toBeInvalid();
+    expect(screen.getByText('"email" must be a valid email')).toBeInTheDocument();
+  });
+  it('check if validation for password works as expected', () => {
+    const { container } = renderComponent(store);
+    const emailElement = container.querySelector('[name="email"]');
+    fireEvent.change(emailElement, { target: { value: 'test@gmail.com' } });
+
+    const passwordElement = container.querySelector('[name="password"]');
+    fireEvent.change(passwordElement, { target: { value: '12' } });
+
+    const submitElement = screen.getByText('Submit');
+    fireEvent.click(submitElement);
+
+    expect(passwordElement).toBeInvalid();
+    expect(
+      screen.getByText('"password" length must be at least 8 characters long'),
+    ).toBeInTheDocument();
+  });
+  it('check if entering the right email and password logs in as expected', async () => {
+    axios.post.mockResolvedValue({
+      statusText: 'OK',
+      data: { token: '1234' },
+    });
+    const { container } = renderComponent(store);
+
+    const emailElement = container.querySelector('[name="email"]');
+    fireEvent.change(emailElement, { target: { value: 'test@gmail.com' } });
+
+    const passwordElement = container.querySelector('[name="password"]');
+    fireEvent.change(passwordElement, { target: { value: 'Test12345' } });
+
+    const submitElement = screen.getByText('Submit');
+    fireEvent.click(submitElement);
+
+    expect(screen.queryByText('"email" must be a valid email')).not.toBeInTheDocument();
+    expect(
+      screen.queryByText('"password" length must be at least 8 characters long'),
+    ).not.toBeInTheDocument();
+    expect(emailElement).not.toBeInvalid();
+    expect(passwordElement).not.toBeInvalid();
+    await waitFor(() => {});
+    expect(history.push).toHaveBeenCalledWith('/bmdashboard');
+  });
+  it("check if statusText in response is not 'OK' and status is 422 and displays validation error", async () => {
+    axios.post.mockResolvedValue({
+      statusText: 'ERROR',
+      status: 422,
+      data: { token: '1234', label: 'email', message: 'User not found' },
+    });
+    const { container } = renderComponent(store);
+
+    const emailElement = container.querySelector('[name="email"]');
+    fireEvent.change(emailElement, { target: { value: 'test@gmail.com' } });
+
+    const passwordElement = container.querySelector('[name="password"]');
+    fireEvent.change(passwordElement, { target: { value: 'Test12345' } });
+
+    const submitElement = screen.getByText('Submit');
+    fireEvent.click(submitElement);
+
+    await waitFor(() => {});
+    expect(screen.getByText('User not found')).toBeInTheDocument();
+  });
+  it("check if statusText in response is not 'OK' and status is not 422 and does not display any validation error", async () => {
+    axios.post.mockResolvedValue({
+      statusText: 'ERROR',
+      status: 500,
+      data: { token: '1234' },
+    });
+    const { container } = renderComponent(store);
+
+    const emailElement = container.querySelector('[name="email"]');
+    fireEvent.change(emailElement, { target: { value: 'test@gmail.com' } });
+
+    const passwordElement = container.querySelector('[name="password"]');
+    fireEvent.change(passwordElement, { target: { value: 'Test12345' } });
+
+    const submitElement = screen.getByText('Submit');
+    fireEvent.click(submitElement);
+
+    await waitFor(() => {});
+    const messageElement = container.querySelector('.invalid-feedback');
+    expect(messageElement).not.toBeInTheDocument();
+  });
+  it('check failed post request does not display any validation error', async () => {
+    axios.post.mockRejectedValue({ response: 'server error' });
+    const { container } = renderComponent(store);
+
+    const emailElement = container.querySelector('[name="email"]');
+    fireEvent.change(emailElement, { target: { value: 'test@gmail.com' } });
+
+    const passwordElement = container.querySelector('[name="password"]');
+    fireEvent.change(passwordElement, { target: { value: 'Test12345' } });
+
+    const submitElement = screen.getByText('Submit');
+    fireEvent.click(submitElement);
+
+    await waitFor(() => {});
+    const messageElement = container.querySelector('.invalid-feedback');
+    expect(messageElement).not.toBeInTheDocument();
   });
 });
