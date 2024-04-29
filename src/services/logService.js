23e7d25c589dcac60cb66f5c8723fc1b9cd6d6bf
Shengwei Peng <ptrpengdev@gmail.com>
2024-04-29 19:16:30 -0400

    Update Sentry to a newer version

--- src/services/logService.js
+++ src/services/logService.js
@@ -1,10 +1,33 @@
-import * as Sentry from '@sentry/browser';
+import React from 'react';
+import * as Sentry from '@sentry/react';
 
-function init() {
-  Sentry.init({
-    dsn: process.env.REACT_APP_SENTRY_URL,
-  });
-}
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_URL,
+  integrations: [
+    // See docs for support of different versions of variation of react router
+    // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+      // useLocation,
+      // useNavigationType,
+      // createRoutesFromChildren,
+      // matchRoutes,
+    }),
+    Sentry.replayIntegration(),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  tracesSampleRate: 1.0,
+
+  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 function logError(error) {
   Sentry.captureException(error);
