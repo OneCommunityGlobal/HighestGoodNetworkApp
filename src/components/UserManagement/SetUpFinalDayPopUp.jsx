05f4fe8665a7b12fe434a315e16dd9a5c3ee1c24
ll <137429365+yaow62023@users.noreply.github.com>
2024-05-04 01:05:28 -0500

    add some code

--- src/components/UserManagement/SetUpFinalDayPopUp.jsx
+++ src/components/UserManagement/SetUpFinalDayPopUp.jsx
@@ -2,6 +2,9 @@ import moment from 'moment';
 import React, { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input, Alert } from 'reactstrap';
 import { boxStyle } from 'styles';
+import httpService from '../../services/httpService';
+import { ApiEndpoint } from 'utils/URL';
+
 /**
  * Modal popup to show the user profile in create mode
  */
@@ -13,9 +16,22 @@ const SetUpFinalDayPopUp = React.memo(props => {
     props.onClose();
   };
 
-  const deactiveUser = () => {
+  const sendDeactivationEmail = async () => {
+    const emailData = {
+      userId: props.userId,  // Assuming you have access to userId from props
+      finalDayDate: finalDayDate
+    };
+    try {
+      await httpService.post(`${ApiEndpoint}/userProfile/deactivationNotice`, emailData);
+    } catch (error) {
+      console.error("Failed to send email:", error);
+    }
+  };
+
+  const deactiveUser = async () => {
     if (moment().isBefore(moment(finalDayDate))) {
-      props.onSave(finalDayDate);
+      await props.onSave(finalDayDate);
+      sendDeactivationEmail();  // Send email after deactivating the user    
     } else {
       setDateError(true);
     }
