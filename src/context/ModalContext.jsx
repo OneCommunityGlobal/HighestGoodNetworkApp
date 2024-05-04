c6c06592be990fe7616c623c1b2cc1fbf92f68d5
ClemarNunes <clemar_nunes@hotmail.com>
2024-05-04 03:12:15 +0100

    Fixing code to match remote branch

--- src/context/ModalContext.jsx
+++ src/context/ModalContext.jsx
@@ -1,17 +1,17 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useMemo } from 'react';
 
 export const ModalContext = createContext();
 
-export const ModalProvider = ({ children }) => {
+export function ModalProvider({ children }) {
   const [modalStatus, setModalStatus] = useState(false);
 
   const updateModalStatus = () => {
     setModalStatus(true);
   };
 
-  return (
-    <ModalContext.Provider value={{ modalStatus, updateModalStatus }}>
-      {children}
-    </ModalContext.Provider>
-  );
-};
+  const contextValue = useMemo(() => {
+    return { modalStatus, updateModalStatus };
+  }, [modalStatus]);
+
+  return <ModalContext.Provider value={contextValue}>{children}</ModalContext.Provider>;
+}
