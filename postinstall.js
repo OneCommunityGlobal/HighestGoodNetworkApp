fa02299614990c22ea23138f0fb9bd4dd8be3154
Shengwei Peng <ptrpengdev@gmail.com>
2024-05-05 19:49:52 -0400

    Remove Cloud TinyMCE from the Project

---
+++ postinstall.js
@@ -0,0 +1,9 @@
+const fse = require('fs-extra');
+const path = require('path');
+
+const topDir = __dirname;
+// const topDir = import.meta.dirname;
+fse.emptyDirSync(path.join(topDir, 'public', 'tinymce'));
+fse.copySync(path.join(topDir, 'node_modules', 'tinymce'), path.join(topDir, 'public', 'tinymce'), {
+  overwrite: true,
+});
