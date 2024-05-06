3c865ca0a177d05d9ca9ed20d8e61c27d6f5115e
One Community <one-community@users.noreply.github.com>
2024-05-05 18:19:24 -0700

    Revert "Revert "Frontend Release to Main [3.49]""

--- src/components/Teams/TeamsOverview.jsx
+++ src/components/Teams/TeamsOverview.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { TOTAL_TEAMS, ACTIVE_TEAMS } from '../../languages/en/ui';
 
-export const TeamsOverview = props => {
+export const TeamsOverview = ({numberOfTeams, numberOfActiveTeams}) => {
   return (
     <div className="teams__overview--top">
       <div className="card" id="card_team" data-testid="card_team">
         <div className="card-body">
-          <h4 className="card-title">{props.numberOfTeams}</h4>
+          <h4 className="card-title">{numberOfTeams}</h4>
           <h6 className="card-subtitle">
             <i className="fa fa-users" aria-hidden="true"></i> {TOTAL_TEAMS}
           </h6>
@@ -15,7 +15,7 @@ export const TeamsOverview = props => {
 
       <div className="card" id="card_active" data-testid="card_active">
         <div className="card-body">
-          <h4 className="card-title">{props.numberOfActiveTeams}</h4>
+          <h4 className="card-title">{numberOfActiveTeams}</h4>
           <h6 className="card-subtitle">
             {/* <div className="isActive"> */}
               <i className="fa fa-circle fa-circle-isActive" aria-hidden="true"></i> {ACTIVE_TEAMS}
