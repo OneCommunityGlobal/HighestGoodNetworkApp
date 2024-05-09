476a425b261d934b087a02df462cfb48a7d1362b
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2024-05-09 16:23:39 -0400

    adds css styles to classes instead of inline styles

--- src/components/Projects/SearchProject/SearchProjectByPerson.jsx
+++ src/components/Projects/SearchProject/SearchProjectByPerson.jsx
@@ -13,35 +13,35 @@ export default function SearchProjectByPerson(props){
 
  const {onChangeInputField, firstName, lastName, handleNameSubmit, cancelSearchByName} = props;
 
-  return <Form className='d-flex align-item-center justify-content-evenly border border-grey rounded p-1 mb-3' style={{height: "3em"}} >
-    <Form.Group className='d-flex w-100'>
-    <Form.Label className='w-50 mt-2 ml-2'>First name</Form.Label>
+  return <Form id="project_search_by_user_form" >
+    <Form.Group className='project_search_by_user_form-group'>
+          <Form.Label className='project_search_by_user_form-label'>First name</Form.Label>
           <Form.Control
             required
             type="text"
             placeholder="First name"
-            className='w-60 ml-0'
+            className=''
             name='firstName'
             value={firstName}
             onChange={onChangeInputField}
             // defaultValue=""
           />
     </Form.Group>
-    <Form.Group className='d-flex w-100 ml-3'>
-          <Form.Label className='w-50 mt-2'>Last name</Form.Label>
+    <Form.Group className='project_search_by_user_form-group'>
+          <Form.Label className='project_search_by_user_form-label'>Last name</Form.Label>
           <Form.Control
             required
             type="text"
             placeholder="Last name"
             // defaultValue=""
-            className='w-60'
+            className=''
             name='lastName'
             value={lastName}
             onChange={onChangeInputField}
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
-    <Form.Group className='d-flex w-100 ml-3'>
+    <Form.Group className='project_search_by_user_form-group'>
     <Button className='p-2 ml-2 btn-md btn-grey border w-100 text-center' style={{height: "2.4em"}} onClick={(e) => handleNameSubmit(e,firstName, lastName)}>Search Project</Button>
     <Button className='p-2 ml-2 btn-md btn-grey border w-100 text-center' style={{height: "2.4em"}} onClick={cancelSearchByName}>Back</Button>
       </Form.Group>    
