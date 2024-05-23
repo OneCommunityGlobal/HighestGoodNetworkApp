efbb07105227b61dd157b041d2bfb9942f1428c8
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2024-05-23 18:00:20 -0300

    Peterson_Implement_toast_when_user_is_already_in_a_team

--- src/components/UserProfile/TeamsAndProjects/AddTeamPopup.jsx
+++ src/components/UserProfile/TeamsAndProjects/AddTeamPopup.jsx
@@ -21,6 +21,7 @@ const AddTeamPopup = React.memo(props => {
   const [newTeamName, setNewTeamName] = useState('');
   const [newTeamIsActive, setNewTeamIsActive] = useState(true);
   const [isDuplicateTeam, setDuplicateTeam] = useState(false);
+  const [isNotDisplayToast, setIsNotDisplayToast] = useState(false);
 
   const onAssignTeam = () => {
     if (!searchText) {
@@ -28,7 +29,24 @@ const AddTeamPopup = React.memo(props => {
       onValidation(false);
       return;
     }
-    if (selectedTeam && !props.userTeamsById.some(x => x._id === selectedTeam._id)) {
+
+    isNotDisplayToast && toast.warn('Please wait for the team to be created to add your user.');
+
+    if (selectedTeam && selectedTeam.members.length > 0 && !isNotDisplayToast) {
+      const userId = props.userProfile._id;
+      const usersTeam = selectedTeam.members.map(item => item.userId);
+      const userIsAlreadyInATeam = usersTeam.includes(userId);
+      userIsAlreadyInATeam &&
+        toast.error(
+          'Your user has been found in this team. Please select another team to add your user.',
+        );
+    }
+
+    if (
+      selectedTeam &&
+      !props.userTeamsById.some(x => x._id === selectedTeam._id) &&
+      !isNotDisplayToast
+    ) {
       props.onSelectAssignTeam(selectedTeam);
       toast.success('Team assigned successfully'); // toast notification
       onSelectTeam(undefined);
@@ -45,30 +63,34 @@ const AddTeamPopup = React.memo(props => {
 
   const onCreateTeam = async () => {
     if (newTeamName !== '') {
+      setIsNotDisplayToast(true);
       const response = await dispatch(postNewTeam(newTeamName, newTeamIsActive));
-        
-      if (response.status === 200) {
-        toast.success('Team created successfully');
-        setNewTeamName('');
-        setNewTeamIsActive(true);
-        setDuplicateTeam(false);
-  
-        // Get updated teams list and select the new team
-        await dispatch(getAllUserTeams());
-        const newTeam = response.data; // Assuming response contains the new team data
-        onSelectTeam(newTeam);
-        setSearchText(newTeam.teamName); // Update search text to reflect new team name
-  
-      } else if (response.status === 400) {
-        setDuplicateTeam(true);
-      } else {
-        toast.error('Error occurred while creating team');
+      try {
+        if (response.status === 200) {
+          toast.success('Team created successfully');
+          setNewTeamName('');
+          setNewTeamIsActive(true);
+          setDuplicateTeam(false);
+
+          // Get updated teams list and select the new team
+          await dispatch(getAllUserTeams());
+          const newTeam = response.data; // Assuming response contains the new team data
+          onSelectTeam(newTeam);
+          setSearchText(newTeam.teamName); // Update search text to reflect new team name
+        }
+      } catch (error) {
+        if (response.status === 400) {
+          setDuplicateTeam(true);
+        } else {
+          toast.error('Error occurred while creating team');
+        }
+      } finally {
+        setIsNotDisplayToast(false);
       }
     } else {
       onNewTeamValidation(false);
     }
   };
-  
 
   useEffect(() => {
     onValidation(true);
@@ -83,7 +105,7 @@ const AddTeamPopup = React.memo(props => {
     <Modal isOpen={props.open} toggle={closePopup} autoFocus={false}>
       <ModalHeader toggle={closePopup}>Add Team</ModalHeader>
       <ModalBody style={{ textAlign: 'center' }}>
-        <label style={{textAlign: 'left'}}>Add to Team</label>
+        <label style={{ textAlign: 'left' }}>Add to Team</label>
         <div className="input-group-prepend" style={{ marginBottom: '10px' }}>
           <AddTeamsAutoComplete
             teamsData={props.teamsData}
@@ -91,24 +113,24 @@ const AddTeamPopup = React.memo(props => {
             onCreateNewTeam={onCreateTeam}
             selectedTeam={selectedTeam}
             searchText={searchText}
-            setNewTeamName={setNewTeamName} 
+            setNewTeamName={setNewTeamName}
             newTeamName={newTeamName}
-            setSearchText={setSearchText}  // Added setSearchText prop
+            setSearchText={setSearchText} // Added setSearchText prop
           />
           <Button color="primary" style={{ marginLeft: '5px' }} onClick={onAssignTeam}>
             Confirm
           </Button>
         </div>
-          {!isValidTeam && searchText && !selectedTeam && (
+        {!isValidTeam && searchText && !selectedTeam && (
           <Alert color="danger">Oops, this team does not exist! Create it if you want it.</Alert>
-          )}
+        )}
         {!isValidTeam && !searchText && (
           <Alert color="danger">Hey, You need to pick a team first!</Alert>
         )}
-        {!isValidNewTeam && !isDuplicateTeam ? <Alert color="danger">Please enter a team name.</Alert> : null}
-        {isDuplicateTeam && (
-          <Alert color="danger">A team with this name already exists</Alert>
-        )}
+        {!isValidNewTeam && !isDuplicateTeam ? (
+          <Alert color="danger">Please enter a team name.</Alert>
+        ) : null}
+        {isDuplicateTeam && <Alert color="danger">A team with this name already exists</Alert>}
       </ModalBody>
       <ModalFooter>
         <Button color="secondary" onClick={closePopup}>
