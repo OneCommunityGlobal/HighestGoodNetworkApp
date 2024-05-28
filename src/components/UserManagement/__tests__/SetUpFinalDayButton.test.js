a57930c20a49edf1e89b8b6e3d17cc8fcbea8df7
jayyy-s <notactuallyjay@gmail.com>
2024-05-28 18:56:47 -0400

    fixed test coverage for onFinalDayClick

--- src/components/UserManagement/__tests__/SetUpFinalDayButton.test.js
+++ src/components/UserManagement/__tests__/SetUpFinalDayButton.test.js
@@ -1,30 +1,32 @@
 import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import userEvent from '@testing-library/user-event';
 import configureStore from 'redux-mock-store';
 import { themeMock } from '__tests__/mockStates';
 import { Provider } from 'react-redux';
+import axios from 'axios';
 import SetUpFinalDayButton from '../SetUpFinalDayButton';
 import { SET_FINAL_DAY, CANCEL } from '../../../languages/en/ui';
-import { updateUserFinalDayStatus } from '../../../actions/userManagement.js';
-import axios from 'axios';
+// import { updateUserFinalDayStatus } from '../../../actions/userManagement.js';
 
-jest.mock('react-toastify');
-jest.mock('axios', () => {
-  return {
-    patch: jest.fn(() => Promise.resolve({ data: { status: 200 } })),
-  };
+jest.mock('axios');
+
+const mockToastSuccess = jest.fn();
+
+beforeAll(() => {
+  jest.mock('react-toastify', () => ({
+    toast: { success: mockToastSuccess },
+  }));
 });
-const mockStore = configureStore([]);
+
+const mockStore = configureStore();
 
 // const userProfileUrl = ENDPOINTS.USER_PROFILE(mockState.auth.user.userid);
 
 describe('SetUpFinalDayButton', () => {
+  const store = mockStore({
+    theme: themeMock,
+  });
   let renderSetUpFinalDayButton = props => {
-    const store = mockStore({
-      theme: themeMock,
-    });
-
     render(
       <Provider store={store}>
         <SetUpFinalDayButton {...props} />
@@ -61,35 +63,34 @@ describe('SetUpFinalDayButton', () => {
   describe('onFinalDayClick tests', () => {
     let props;
 
-    // it('If isSet is true, then an alert will apear', async () => {
-    //   renderSetUpFinalDayButton(props);
-    //   fireEvent.click(screen.getByText(CANCEL));
+    it('If isSet is true, then an alert will appear', async () => {
+      renderSetUpFinalDayButton(props);
+      const mockedResponse = { data: { status: 200 } };
+      axios.patch.mockResolvedValue(mockedResponse);
 
-    //   // expect(await screen.findByRole('dialog')).toBeInTheDocument();
-    //   await waitFor(
-    //     async () =>
-    //       expect(
-    //         await screen.findByText("This user's final day has been deleted."),
-    //       ).toBeInTheDocument(),
-    //     { timeout: 1000 },
-    //   );
-    // });
+      const message = "This user's final day has been deleted.";
+
+      const cancelFinalDayButton = screen.getByText(CANCEL);
+      fireEvent.click(cancelFinalDayButton);
+      waitFor(() => {
+        expect(mockToastSuccess).toHaveBeenCalledWith(message);
+      });
+    });
 
     it('If isSet is false then SetUpFinalDayPopUp will be present', async () => {
-      console.log(axios.patch('', {}));
       props.userProfile.endDate = undefined;
 
       renderSetUpFinalDayButton(props);
       fireEvent.click(screen.getByText(SET_FINAL_DAY));
-      await waitFor(async () =>
-        expect(await screen.findByText('Set Your Final Day')).toBeInTheDocument(),
-      );
+      const setYourFinalDayElement = await screen.findByText('Set Your Final Day');
+      await waitFor(() => expect(setYourFinalDayElement).toBeInTheDocument());
     });
 
     beforeEach(() => {
       props = {
         userProfile: {
-          endDate: 'mockEndDate',
+          endDate: '20210311',
+          _id: '1',
         },
         loadUserProfile: jest.fn().mockReturnValueOnce(200),
         isBigBtn: true,
