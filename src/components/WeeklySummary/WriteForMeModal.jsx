d988d292a65b9f048b0032d59bdf22913f74dca0
Kaushik Malikireddy <malikireddy.k@northeastern.edu>
2024-05-29 13:08:58 +0530

    ai request update

--- src/components/WeeklySummary/WriteForMeModal.jsx
+++ src/components/WeeklySummary/WriteForMeModal.jsx
@@ -3,10 +3,60 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import { boxStyle } from 'styles';
 import httpService from 'services/httpService';
 import { connect } from 'react-redux';
+import moment from 'moment-timezone';
+import axios from 'axios';
 import { ENDPOINTS } from '../../utils/URL';
-import { SET_CURRENT_USER } from '../../constants/auth';
 // eslint-disable-next-line import/named
 import { getUserInfo } from '../../utils/permissions';
+
+// eslint-disable-next-line consistent-return
+const getTimeEntriesForWeek = async (userId, offset) => {
+  const fromDate = moment()
+    .tz('America/Los_Angeles')
+    .startOf('week')
+    .subtract(offset, 'weeks')
+    .format('YYYY-MM-DDTHH:mm:ss');
+
+  const toDate = moment()
+    .tz('America/Los_Angeles')
+    .endOf('week')
+    .subtract(offset, 'weeks')
+    .format('YYYY-MM-DDTHH:mm:ss');
+
+  const url = ENDPOINTS.TIME_ENTRIES_PERIOD(userId, fromDate, toDate);
+
+  try {
+    return axios
+      .get(url)
+      .then(response => {
+        // console.log('time entry data: ', response.data[0].notes);
+        const { data } = response;
+
+        const cleanedAndConcatenatedNotes = data.reduce((acc, entry) => {
+          // Clean the current note
+          const cleanedNotes = entry.notes
+            .replace(/<a\b[^>]*>(.*?)<\/a>|&nbsp;|https?:\/\/\S+/gi, ' ')
+            .replace(/<\/?[^>]+(>|$)/g, '');
+
+          // Concatenate the cleaned note to the accumulator with newline separator
+          return `${acc + cleanedNotes}\n`;
+        }, '');
+
+        return cleanedAndConcatenatedNotes;
+      })
+      .catch(error => {
+        throw new Error(`Error fetching time entries:: ${error}`);
+      });
+  } catch (error) {
+    if (error.response && error.response.status === 401) {
+      // Handle logout or unauthorized access
+      throw new Error(`User is not authorized. Please log in.:  ${error}`);
+      // return null;
+    } else {
+      throw new Error(`Error fetching time entries:: ${error}`);
+    }
+  }
+};
 
 function WriteItForMeModal(props) {
   // const { pasteResponse } = props;
@@ -14,23 +64,24 @@ function WriteItForMeModal(props) {
   const [summary, setSummary] = useState();
   const toggle = () => setModal(!modal);
 
-  const fetchSummary = async () => {
+  const { displayUserProfile } = props; // Access displayUserProfile and authUser from props
+  const fetchSummary = async prompt => {
     // eslint-disable-next-line react/destructuring-assignment
-    const { userid } = getUserInfo();
-    const { displayUserProfile } = props;
     toggle();
 
     try {
       const res = await httpService.post(ENDPOINTS.INTERACT_WITH_GPT, {
-        // prompt: 'Please summarize my week\'s work.',
-        userid,
+        prompt: `${prompt}`,
+        userid: displayUserProfile._id,
         firstName: displayUserProfile.firstName,
       });
 
       if (res.status === 200) {
-        const { text } = res.data.text;
-        setSummary(text);
-        // pasteResponse(res.data.text);
+        const markdownRemoved = res.data.text
+          .replace(/\n+/g, ' ') // Remove line breaks
+          .replace(/\*/g, ''); // Remove asterisks
+
+        setSummary(markdownRemoved);
       } else {
         throw new Error(`HTTP error: ${res.status}`);
       }
@@ -40,15 +91,34 @@ function WriteItForMeModal(props) {
     }
   };
 
+  const handleFetchSummary = async () => {
+    try {
+      const promptBody = await getTimeEntriesForWeek(displayUserProfile._id, 0);
+      toggle();
+      fetchSummary(
+        `Please edit the following summary of my week's work. Make sure it is professionally written in 3rd person format. 
+        Write it as only one paragraph. It must be only one paragraph. 
+        Keep it less than 500 words. Start the paragraph with 'This week ${displayUserProfile.firstName}'. 
+        Make sure the paragraph contains no links or URLs and write it in a tone that is matter-of-fact and without embellishment. 
+        Do not add flowery language, keep it simple and factual. Do not add a final summary sentence. 
+        Apply all this to the following:\n${promptBody}`,
+      );
+    } catch (error) {
+      throw new Error(`Error in either fetching time entries or summary ${error}`);
+    }
+  };
+
+  // Assuming you have an endpoints file
+
   return (
     <div>
-      <Button color="info" onClick={fetchSummary} style={{ ...boxStyle, width: '100%' }}>
+      <Button color="info" onClick={handleFetchSummary} style={{ ...boxStyle, width: '100%' }}>
         Write It For Me
       </Button>
 
       <Modal isOpen={modal} toggle={toggle}>
         <ModalHeader toggle={toggle}>Generated Summary</ModalHeader>
-        <ModalBody>{JSON.stringify(summary) || 'Loading summary...'}</ModalBody>
+        <ModalBody>{JSON.stringify(summary) || 'Takeing a few minutes to load summary'}</ModalBody>
         <ModalFooter>
           <Button color="primary" onClick={toggle} style={boxStyle}>
             Close
@@ -63,10 +133,5 @@ const mapStateToProps = state => ({
   displayUserProfile: state.userProfile,
 });
 
-// export default WriteItForMeModal;
+// export default WriteItForMeModal and connect to redux
 export default connect(mapStateToProps, { getUserInfo })(WriteItForMeModal);
-
-export const setCurrentUser = decoded => ({
-  type: SET_CURRENT_USER,
-  payload: decoded,
-});
