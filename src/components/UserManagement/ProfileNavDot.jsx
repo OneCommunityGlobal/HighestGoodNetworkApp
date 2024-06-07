fbc60cac9fed807c5e3004b661967c8f3e358222
Youyou Zhang <jennzhangyy@gmail.com>
2024-06-06 21:39:11 -0500

    feat: click on profile icon on dashboard will open in new tab instead of current window

--- src/components/UserManagement/ProfileNavDot.jsx
+++ src/components/UserManagement/ProfileNavDot.jsx
@@ -1,14 +1,14 @@
 import { React } from 'react';
 import { useHistory } from 'react-router';
 
-// pass userId of an account to navigate to user profile onClick of icon
+// pass userId of an account to navigate to user profile onClick of icon and open in new tab
 export const ProfileNavDot = ({ userId }) => {
   const history = useHistory();
   return (
     <span
       style={{ fontSize: '1.5rem' }}
       onClick={() => {
-        history.push(`/userprofile/${userId}`);
+        window.open(`/userprofile/${userId}`, '_blank', 'noopener,noreferrer');
       }}
       title="Click here to go to the user's profile."
     >
