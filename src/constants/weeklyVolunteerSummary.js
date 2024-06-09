5f2866cac2d96feaeb263f005581bb8872f58e50
Harsh Bodgal <harshbodgal22@gmail.com>
2024-06-08 17:20:30 -0700

    adding_component_initial_files

---
+++ src/constants/weeklyVolunteerSummary.js
@@ -0,0 +1,13 @@
+// export const comparisonPeriod = [
+//   'Compared to Last Week',
+//   'Compared to same Week last month',
+//   'Compared to same week last Year',
+//   'Do not show comparison data',
+// ];
+
+// export const dateFilterOptions = [
+//     'CurrentWeek',
+//     'LastWeek',
+//     'Week Before Last',
+//     'Select Date Range'
+// ];
