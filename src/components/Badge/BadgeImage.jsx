96c648d488e43a1cf0c344cb9c2872a6630463b6
jayyy-s <notactuallyjay@gmail.com>
2024-06-14 18:51:34 -0400

    badge image

--- src/components/Badge/BadgeImage.jsx
+++ src/components/Badge/BadgeImage.jsx
@@ -1,30 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Card, CardTitle, CardBody, CardImg, CardText, Popover } from 'reactstrap';
 
 function BadgeImage({ badgeData, time, index, personalBestMaxHrs, count, cssSuffix }) {
   const [isOpen, setOpen] = useState(false);
   cssSuffix = cssSuffix ? cssSuffix : '';
   const toggle = () => setOpen(prevIsOpen => !prevIsOpen);
-
-  const [badgeSpan, setBadgeSpan] = useState(null);
-
-  useEffect(() => {
-    if (badgeData.type === 'Personal Max') {
-      setBadgeSpan(
-        <span className={'badge_count_personalmax' + cssSuffix}>
-          {Math.floor(personalBestMaxHrs)}
-        </span>,
-      );
-    } else {
-      if (count < 100) {
-        setBadgeSpan(<span className={'badge_count' + cssSuffix}>{Math.round(count)}</span>);
-      } else {
-        setBadgeSpan(
-          <span className={'badge_count_3_digit' + cssSuffix}>{Math.round(count)}</span>,
-        );
-      }
-    }
-  }, []);
 
   return (
     <>
@@ -37,7 +17,12 @@ function BadgeImage({ badgeData, time, index, personalBestMaxHrs, count, cssSuff
             loading="lazy"
           />
         </div>
-        {badgeSpan}
+        <BadgeSpan
+          badgeType={badgeData.type}
+          personalBestMaxHrs={personalBestMaxHrs}
+          count={count}
+          cssSuffix={cssSuffix}
+        />
       </div>
       <Popover
         trigger="hover"
@@ -64,6 +49,22 @@ function BadgeImage({ badgeData, time, index, personalBestMaxHrs, count, cssSuff
       </Popover>
     </>
   );
+}
+
+function BadgeSpan({ badgeType, personalBestMaxHrs, count, cssSuffix }) {
+  if (badgeType === 'Personal Max') {
+    return (
+      <span className={'badge_count_personalmax' + cssSuffix}>
+        {Math.floor(personalBestMaxHrs)}
+      </span>
+    );
+  } else {
+    if (count < 100) {
+      return <span className={'badge_count' + cssSuffix}>{Math.round(count)}</span>;
+    } else {
+      return <span className={'badge_count_3_digit' + cssSuffix}>{Math.round(count)}</span>;
+    }
+  }
 }
 
 export default BadgeImage;
