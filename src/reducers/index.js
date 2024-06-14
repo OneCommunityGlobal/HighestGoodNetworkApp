2b3c53ff33e711bb78b550afd30e1dd330bf33f8
abdelmounaim lallouache <abdelmounaim.lallouache@gmail.com>
2024-06-14 11:21:26 -0500

    added BluequareEmailAssignmentPopUp to user profile. added BluequareEmailAssignment constants, actions and reducer. added new URLs and updated userProfile style

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -32,6 +32,7 @@ import { mouseoverTextReducer } from './mouseoverTextReducer';
 import notificationReducer from './notificationReducer';
 import { weeklySummaryRecipientsReducer } from "./weeklySummaryRecipientsReducer";
 import { followUpReducer } from "./followUpReducer";
+import { BlueSquareEmailAssignment } from './blueSquareEmailBcc';
 
 // bm dashboard
 import { materialsReducer } from './bmdashboard/materialsReducer';
@@ -73,6 +74,7 @@ const localReducers = {
   weeklySummaryRecipients:weeklySummaryRecipientsReducer,
   notification: notificationReducer,
   userFollowUp : followUpReducer,
+  blueSquareEmailAssignment : BlueSquareEmailAssignment,
 
   // bmdashboard
   materials: materialsReducer,
