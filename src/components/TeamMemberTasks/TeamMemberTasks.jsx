e87d43845e8a45cf108f693f4719b34b54fd1829
Jatin Agrawal <jatinagrawal0801@gmail.com>
2024-06-15 16:49:25 -0700

    some changes

--- src/components/TeamMemberTasks/TeamMemberTasks.jsx
+++ src/components/TeamMemberTasks/TeamMemberTasks.jsx
@@ -211,7 +211,7 @@ const TeamMemberTasks = React.memo(props => {
     if(usersWithTasks.length > 0) {
       usersWithTasks.forEach(user => {
         // temporary changes to work on other issues. please fix this.
-        const teamNames = user.teams!==undefined?user.teams.map(team => team.teamName):["tasks"];
+        const teamNames = user.teams.map(team => team.teamName)
         const code = user.teamCode || 'noCodeLabel';
         const color = user.weeklySummaryOption || 'noColorLabel';
 
