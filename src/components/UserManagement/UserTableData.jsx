34838dfa47a4c9c76fd5851d47a34b7d622e21b7
KaushikMreddy <malikireddy.k@northeastern.edu>
2024-06-18 10:07:10 +0530

    fix final day bug

--- src/components/UserManagement/UserTableData.jsx
+++ src/components/UserManagement/UserTableData.jsx
@@ -43,7 +43,7 @@ const UserTableData = React.memo(props => {
   };
 
   return (
-    <tr className={`usermanagement__tr ${darkMode ? 'bg-yinmn-blue' : '' }`} id={`tr_user_${props.index}`}>
+    <tr className={`usermanagement__tr ${darkMode ? 'bg-yinmn-blue' : ''}`} id={`tr_user_${props.index}`}>
       <td className="usermanagement__active--input">
         <ActiveCell
           isActive={props.isActive}
@@ -54,7 +54,7 @@ const UserTableData = React.memo(props => {
         />
       </td>
       <td className="email_cell">
-      <a href={`/userprofile/${props.user._id}`} className={darkMode ? 'text-azure' : ''}>{props.user.firstName} </a>
+        <a href={`/userprofile/${props.user._id}`} className={darkMode ? 'text-azure' : ''}>{props.user.firstName} </a>
         <FontAwesomeIcon
           className="copy_icon"
           icon={faCopy}
@@ -64,8 +64,8 @@ const UserTableData = React.memo(props => {
           }}
         />
       </td>
-       <td className="email_cell">
-       <a href={`/userprofile/${props.user._id}`} className={darkMode ? 'text-azure' : ''}>{props.user.lastName}</a>
+      <td className="email_cell">
+        <a href={`/userprofile/${props.user._id}`} className={darkMode ? 'text-azure' : ''}>{props.user.lastName}</a>
         <FontAwesomeIcon
           className="copy_icon"
           icon={faCopy}
@@ -93,7 +93,7 @@ const UserTableData = React.memo(props => {
           type="button"
           className={`btn btn-outline-${props.isActive ? 'warning' : 'success'} btn-sm`}
           onClick={e => {
-            if(cantUpdateDevAdminDetails(props.user.email , props.authEmail)){
+            if (cantUpdateDevAdminDetails(props.user.email, props.authEmail)) {
               alert('STOP! YOU SHOULDN’T BE TRYING TO CHANGE THIS. Please reconsider your choices.');
               return;
             }
@@ -103,19 +103,18 @@ const UserTableData = React.memo(props => {
               props.isActive ? UserStatus.InActive : UserStatus.Active,
             );
           }}
-          style={darkMode ? {boxShadow: "0 0 0 0", fontWeight: "bold"}  : boxStyle}
+          style={darkMode ? { boxShadow: "0 0 0 0", fontWeight: "bold" } : boxStyle}
         >
           {isChanging ? '...' : props.isActive ? PAUSE : RESUME}
         </button>
       </td>
       <td className="centered-td">
         <button
-          className={`btn btn-outline-primary btn-sm${
-            props.timeOffRequests?.length > 0 ? ` time-off-request-btn-moved` : ''
-          }`}
+          className={`btn btn-outline-primary btn-sm${props.timeOffRequests?.length > 0 ? ` time-off-request-btn-moved` : ''
+            }`}
           onClick={e => props.onLogTimeOffClick(props.user)}
           id="requested-time-off-btn"
-          style={darkMode ? {boxShadow: "0 0 0 0", fontWeight: "bold"}  : boxStyle}
+          style={darkMode ? { boxShadow: "0 0 0 0", fontWeight: "bold" } : boxStyle}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -144,20 +143,21 @@ const UserTableData = React.memo(props => {
       <td>
         <button
           type="button"
-          className={`btn btn-outline-${props.isSet ? 'warning' : 'success'} btn-sm`}
+          className={`btn btn-outline-${props.user.endDate ? 'warning' : 'success'} btn-sm`}
           onClick={e => {
-            if(cantUpdateDevAdminDetails(props.user.email , props.authEmail)){
+            if (cantUpdateDevAdminDetails(props.user.email, props.authEmail)) {
               alert('STOP! YOU SHOULDN’T BE TRYING TO CHANGE THIS. Please reconsider your choices.');
               return;
             }
+
             props.onFinalDayClick(
               props.user,
-              props.isSet ? FinalDay.NotSetFinalDay : FinalDay.FinalDay,
+              props.user.endDate ? FinalDay.NotSetFinalDay : FinalDay.FinalDay,
             );
           }}
-          style={darkMode ? {boxShadow: "0 0 0 0", fontWeight: "bold"}  : boxStyle}
+          style={darkMode ? { boxShadow: "0 0 0 0", fontWeight: "bold" } : boxStyle}
         >
-          {props.isSet ? CANCEL : SET_FINAL_DAY}
+          {props.user.endDate ? CANCEL : SET_FINAL_DAY}
         </button>
       </td>
       <td>
@@ -166,8 +166,8 @@ const UserTableData = React.memo(props => {
           : ''}
       </td>
       <td>{props.user.startDate ? formatDate(props.user.startDate) : 'N/A'}</td>
-       <td className="email_cell">
-      {props.user.endDate ? formatDate(props.user.endDate) : 'N/A'}
+      <td className="email_cell">
+        {props.user.endDate ? formatDate(props.user.endDate) : 'N/A'}
         <FontAwesomeIcon
           className="copy_icon"
           icon={faCopy}
@@ -186,7 +186,7 @@ const UserTableData = React.memo(props => {
               onClick={e => {
                 props.onDeleteClick(props.user, 'archive');
               }}
-              style={darkMode ? {boxShadow: "0 0 0 0", fontWeight: "bold"} : boxStyle}
+              style={darkMode ? { boxShadow: "0 0 0 0", fontWeight: "bold" } : boxStyle}
               disabled={props.auth?.user.userid === props.user._id}
             >
               {DELETE}
