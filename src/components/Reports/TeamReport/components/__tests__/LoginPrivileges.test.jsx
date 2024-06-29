c7e9495a6fec2061c52354083480649d63687b62
Aaryaneil Nimbalkar <aaryaneil.nimbalkar@gmail.com>
2024-06-29 02:00:45 -0500

    added tests for LoginPrivilegest.

--- src/components/Reports/TeamReport/components/__tests__/LoginPrivileges.test.jsx
+++ src/components/Reports/TeamReport/components/__tests__/LoginPrivileges.test.jsx
@@ -33,4 +33,57 @@ describe('LoginPrivileges Component', () => {
 
     expect(handleInputChange).toHaveBeenCalled();
   });
+
+  test('test_default_selected_input', () => {
+    render(<LoginPrivileges selectedInput="" handleInputChange={() => { }} />);
+
+    const managerRadio = screen.getByLabelText('Manager');
+    const adminRadio = screen.getByLabelText('Admin');
+
+    expect(managerRadio).not.toBeChecked();
+    expect(adminRadio).not.toBeChecked();
+  });
+
+  test('test_radio_buttons_interact', () => {
+    const handleInputChange = jest.fn();
+    render(<LoginPrivileges selectedInput="isManager" handleInputChange={handleInputChange} />);
+
+    const managerRadio = screen.getByLabelText('Manager');
+    const adminRadio = screen.getByLabelText('Admin');
+
+    fireEvent.click(adminRadio);
+    expect(handleInputChange).toHaveBeenCalledWith(expect.any(Object));
+
+    fireEvent.click(managerRadio);
+    expect(handleInputChange).toHaveBeenCalledWith(expect.any(Object));
+  });
+
+  test('test_no_role_selected', () => {
+    const handleInputChange = jest.fn();
+    render(<LoginPrivileges selectedInput="" handleInputChange={handleInputChange} />);
+
+    const managerRadio = screen.getByLabelText('Manager');
+    const adminRadio = screen.getByLabelText('Admin');
+
+    expect(managerRadio).not.toBeChecked();
+    expect(adminRadio).not.toBeChecked();
+  });
+
+  test('test_multiple_renders', () => {
+    const { rerender } = render(<LoginPrivileges selectedInput="isManager" handleInputChange={() => { }} />);
+    
+    let managerRadio = screen.getByLabelText('Manager');
+    let adminRadio = screen.getByLabelText('Admin');
+
+    expect(managerRadio).toBeChecked();
+    expect(adminRadio).not.toBeChecked();
+
+    rerender(<LoginPrivileges selectedInput="isAdmin" handleInputChange={() => { }} />);
+
+    managerRadio = screen.getByLabelText('Manager');
+    adminRadio = screen.getByLabelText('Admin');
+
+    expect(managerRadio).not.toBeChecked();
+    expect(adminRadio).toBeChecked();
+  });
 });
