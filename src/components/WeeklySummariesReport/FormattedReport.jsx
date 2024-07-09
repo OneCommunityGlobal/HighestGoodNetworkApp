5b0851872e688df1cd6735cc63e9c2b6078d7850
Jiayu Huang <jiayu.huang@ufl.edu>
2024-07-08 22:20:34 -0500

    delete duplicate

--- src/components/WeeklySummariesReport/FormattedReport.jsx
+++ src/components/WeeklySummariesReport/FormattedReport.jsx
@@ -14,7 +14,7 @@ import { faCopy } from '@fortawesome/free-solid-svg-icons';
 
 import { assignStarDotColors, showStar } from 'utils/leaderboardPermissions';
 import { updateOneSummaryReport } from 'actions/weeklySummariesReport';
-import RoleInfoModal from 'components/UserProfile/EditableModal/RoleInfoModal';
+import RoleInfoModal from 'components/UserProfile/EditableModal/RoleInfoModalnn';
 import {
   Input,
   ListGroup,
