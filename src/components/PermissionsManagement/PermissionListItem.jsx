b8f21e1c540a8a6aa91b88129a091c800cec7017
One Community <one-community@users.noreply.github.com>
2024-07-22 13:53:21 -0700

    Revert "weiyao_permissionManagement_addRoles"

--- src/components/PermissionsManagement/PermissionListItem.jsx
+++ src/components/PermissionsManagement/PermissionListItem.jsx
@@ -148,14 +148,13 @@ function PermissionListItem(props) {
               className="icon-button"
               color={getColor()}
               onClick={() => {
-                // eslint-disable-next-line no-debugger
                 // const state = howManySubpermsInRole !== 'None';
                 setSubpermissions(subperms, howManySubpermsInRole !== 'All');
                 // eslint-disable-next-line react/destructuring-assignment
                 props.onChange();
               }}
               // eslint-disable-next-line react/destructuring-assignment
-              disabled={immutablePermissions.includes(permission)}
+              disabled={!props.hasPermission('putRole')}
               style={darkMode ? boxStyleDark : boxStyle}
             >
               {howManySubpermsInRole === 'All' ? 'Delete' : 'Add'}
@@ -167,7 +166,10 @@ function PermissionListItem(props) {
               onClick={() => {
                 togglePermission(permission);
               }}
-              disabled={immutablePermissions.includes(permission)}
+              disabled={
+                // eslint-disable-next-line react/destructuring-assignment
+                !props.hasPermission('putRole') || immutablePermissions.includes(permission)
+              }
               style={darkMode ? boxStyleDark : boxStyle}
             >
               {hasThisPermission ? 'Delete' : 'Add'}
