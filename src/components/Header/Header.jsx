1a2132e0146cf41168d15756c0587fb267d42ee5
Carlos Gomez <cgomezlugo@hotmail.com>
2024-07-30 13:13:51 -0400

    detailing Dark Mode, tag menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -354,7 +354,7 @@ export function Header(props) {
                         </DropdownItem>
                       )}
                       {canGetWeeklyVolunteerSummary && (
-                        <DropdownItem tag={Link} to="/totalorgsummary">
+                        <DropdownItem tag={Link} to="/totalorgsummary" className={fontColor}>
                           {TOTAL_ORG_SUMMARY}
                         </DropdownItem>
                       )}
