02a91cb41e1f87ae3aa214903f4530679bf1d681
HowieMiao <howie.miao@gmail.com>
2024-07-30 21:27:35 -0600

    comment update

--- src/actions/timeEntries.js
+++ src/actions/timeEntries.js
@@ -71,6 +71,8 @@ export const getTimeEntriesForPeriod = (userId, fromDate, toDate) => {
   };
 };
 
+export const getTimeEndDateEntriesByPeriod = (userId, fromDate, toDate) => { //Find last week of work in date
+  toDate = moment(toDate)
     .endOf('day')
     .format('YYYY-MM-DD');
   const url = ENDPOINTS.TIME_ENTRIES_PERIOD(userId, fromDate,toDate);
