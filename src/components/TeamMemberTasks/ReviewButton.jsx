8510033f3786af3765b7c9c3ccd4650bb4ce1436
Nahiyan16 <nahiyanahmed11@gmail.com>
2024-08-17 11:36:35 -0400

    Fixed hover effect issue

--- src/components/TeamMemberTasks/ReviewButton.jsx
+++ src/components/TeamMemberTasks/ReviewButton.jsx
@@ -106,17 +106,17 @@ const ReviewButton = ({
             </DropdownToggle>
             <DropdownMenu className={darkMode ? 'bg-space-cadet' : ''}>
             {task.relatedWorkLinks && task.relatedWorkLinks.map((link, index) => (
-              <DropdownItem key={index} href={link} target="_blank" className={darkMode ? 'text-light' : ''}>
+              <DropdownItem key={index} href={link} target="_blank" className={darkMode ? 'text-light dark-mode-btn' : ''}>
                 View Link
               </DropdownItem>
             ))}
-            <DropdownItem onClick={() => { setSelectedAction('Complete and Remove'); toggleVerify(); }} className={darkMode ? 'text-light' : ''}>
+            <DropdownItem onClick={() => { setSelectedAction('Complete and Remove'); toggleVerify(); }} className={darkMode ? 'text-light dark-mode-btn' : ''}>
               <FontAwesomeIcon
                 className="team-member-tasks-done"
                 icon={faCheck}
               /> as complete and remove task
             </DropdownItem>
-            <DropdownItem onClick={() => { setSelectedAction('More Work Needed'); toggleVerify()}} className={darkMode ? 'text-light' : ''}>
+            <DropdownItem onClick={() => { setSelectedAction('More Work Needed'); toggleVerify()}} className={darkMode ? 'text-light dark-mode-btn' : ''}>
               More work needed, reset this button
             </DropdownItem>
             </DropdownMenu>
