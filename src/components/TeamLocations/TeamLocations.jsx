41c8434b5e0584594f504929641df2c412d8f2a3
Logeshwari-Renu <logeshwari04@gmail.com>
2024-08-19 15:55:46 -0500

    Resolved Merge conflicts

--- src/components/TeamLocations/TeamLocations.jsx
+++ src/components/TeamLocations/TeamLocations.jsx
@@ -25,45 +25,41 @@ function TeamLocations() {
   const [editingUser, setEditingUser] = useState(null);
   const [searchText, setSearchText] = useState('');
   const [popupsOpen, setPopupsOpen] = useState(false);
-  const [mapMarkers, setMapMarkers] = useState([]);
+  const [mapMarkers,setMapMarkers] = useState([])
   const [tableVisible, setTableVisible] = useState(false);
   const [markerPopupVisible, setMarkerPopupVisible] = useState(false);
-
   const role = useSelector(state => state.auth.user.role);
   const darkMode = useSelector(state => state.theme.darkMode);
   const [loading, setLoading] = useState(true);  // State variable for loading spinner
 
 
   const isAbleToEdit = role === 'Owner';
-  const mapRef = useRef(null);
-  const [currentUser, setCurrentUser] = useState(null);
+  const mapRef = useRef(null); 
+  const [currentUser, setCurrentUser] = useState(null)
 
   useEffect(() => {
     async function getUserProfiles() {
       try {
         const locations = (await axios.get(ENDPOINTS.ALL_MAP_LOCATIONS())).data;
-        const users = locations.users.map((item) => ({ ...item, type: 'user' })) || [];
-        const mUsers = locations.mUsers.map((item) => ({ ...item, type: 'm_user' })) || [];
+        const users = locations.users.map(item => ({ ...item, type: 'user' })) || [];
+        const mUsers = locations.mUsers.map(item => ({ ...item, type: 'm_user' })) || [];
+
         setUserProfiles(users);
         setManuallyAddedProfiles(mUsers);
-
         const allMapMarkers = [...users, ...mUsers];
-        const allMapMarkersOffset = allMapMarkers.map(ele => ({
-
+        const allMapMarkersOffset = allMapMarkers.map(ele =>({
           ...ele,
           location: {
-            ...ele.location,
-            coords: {
-              ...ele.location.coords,
-              lat: randomLocationOffset(ele.location.coords.lat),
-              lng: randomLocationOffset(ele.location.coords.lng),
-            },
+              ...ele.location,
+              coords: {
+                  ...ele.location.coords,
+                  lat: randomLocationOffset(ele.location.coords.lat), 
+                  lng: randomLocationOffset(ele.location.coords.lng),
+              },
           },
-        }));
-
+      }))
         setMapMarkers(allMapMarkersOffset);
         setLoading(false);  // Set loading to false after data is loaded
-
       } catch (error) {
         toast.error(error.message);
         setLoading(false);  // Set loading to false if there's an error
@@ -77,8 +73,9 @@ function TeamLocations() {
     if (coords) {
       handleFlyTo(coords.lat, coords.lng);
     }
-  }, [currentUser]);
+  }, [currentUser])
 
+  // We don't need the back to top button on this page
   useEffect(() => {
     const backToTopButton = document.querySelector('.top');
     backToTopButton.style.display = 'none';
@@ -87,15 +84,15 @@ function TeamLocations() {
     };
   }, []);
 
-  const searchHandler = (e) => {
+  const searchHandler = e => {
     setSearchText(e.target.value);
   };
 
-  const removeLocation = async (id) => {
+  const removeLocation = async id => {
     try {
       const res = await axios.delete(`${ApiEndpoint}/mapLocations/${id}`);
       if (res.status === 200) {
-        setManuallyAddedProfiles((prev) => prev.filter((item) => item._id !== id));
+        setManuallyAddedProfiles(prev => prev.filter(item => item._id !== id));
         toast.success(res.data.message);
       } else {
         throw new Error('Something went wrong. Try again later.');
@@ -105,13 +102,13 @@ function TeamLocations() {
     }
   };
 
-  const editHandler = (profile) => {
+  const editHandler = profile => {
     setEditingUser(profile);
     setEditIsOpen(true);
   };
 
   const toggleListPopUp = () => {
-    setListIsOpen((prev) => !prev);
+    setListIsOpen(prev => !prev);
   };
 
   const addOrEditClose = () => {
@@ -123,13 +120,12 @@ function TeamLocations() {
     }
   };
 
-  const randomLocationOffset = (c) => {
+  const randomLocationOffset = c => {
     const randomOffset = (Math.random() - 0.5) * 2 * 0.05;
-
     const newLongitude = Number(c) + randomOffset;
+
     const modifiedLongitude = Number(newLongitude.toFixed(7));
     return modifiedLongitude;
-
   };
 
   const toggleTableVisibility = () => {
@@ -141,37 +137,44 @@ function TeamLocations() {
       if (mapRef.current.getZoom() >= 13) {
         setPopupsOpen(true);
       }
-    } else {
+    } 
+    else {
       setTableVisible(true);
       setPopupsOpen(false);
     }
-  };
+  }
 
+  // Get an array of all users' non-null countries (some locations may not be associated with a country)
+  // Get the number of unique countries
 
+  
   const countries = mapMarkers.map(user => user.location.country);
   const totalUniqueCountries = [...new Set(countries)].length;
   let filteredMapMarkers = mapMarkers;
   if (searchText) {
     filteredMapMarkers = filteredMapMarkers.filter(
       item =>
-
         item.location.city?.toLowerCase().includes(searchText.toLowerCase()) ||
         item.location.country?.toLowerCase().includes(searchText.toLowerCase()) ||
         item.firstName?.toLowerCase().includes(searchText.toLowerCase()) ||
-        item.lastName?.toLowerCase().includes(searchText.toLowerCase())
+        item.lastName?.toLowerCase().includes(searchText.toLowerCase()),
     );
   }
-
+  let dropdown = false;
+  const noUsersFound = 'No users found.';
+  const isEditing = editIsOpen && editingUser;
+  if (searchText) {
+    dropdown = true;
+  }
 
   const handleFlyTo = (latitude, longitude) => {
     mapRef?.current.flyTo([latitude, longitude], 13, {
-      animate: true,
-      duration: 3.0,
+      animate: true, 
+      duration: 3.0
     });
-  };
+  } 
 
   const markerPopups = filteredMapMarkers.map(profile => {
-
     let userName = getUserName(profile);
 
     return (
@@ -182,17 +185,14 @@ function TeamLocations() {
         isAbleToEdit={isAbleToEdit}
         editHandler={editHandler}
         removeLocation={removeLocation}
-        isOpen={popupsOpen}
-        darkMode={darkMode}
-      />
+        isOpen={popupsOpen} 
+        darkMode={darkMode} />
     );
   });
 
   return (
-
-    <Container fluid className={`${darkMode ? 'bg-oxford-blue text-light dark-mode' : ''}`} style={{ minHeight: "100%", paddingBottom: "73px" }}>
-      {isAbleToEdit ? (
-
+    <Container fluid className={`${darkMode ? 'bg-oxford-blue text-light dark-mode' : ''}`} style={{minHeight: "100%", paddingBottom: "73px"}}>
+      {isAbleToEdit && (
         <>
           <AddOrEditPopup
             open={editIsOpen || addNewIsOpen}
@@ -202,8 +202,8 @@ function TeamLocations() {
             isEdit={editIsOpen && editingUser}
             editProfile={editingUser}
             isAdd={!editIsOpen && addNewIsOpen}
-            title={editIsOpen && editingUser ? 'Edit User Profile' : 'Adding New User'}
-            submitText={editIsOpen && editingUser ? 'Save Changes' : 'Save To Map'}
+            title={isEditing ? 'Edit User Profile' : 'Adding New User'}
+            submitText={isEditing ? 'Save Changes' : 'Save To Map'}
           />
           <ListUsersPopUp
             open={listIsOpen}
@@ -213,43 +213,39 @@ function TeamLocations() {
             setEdit={editHandler}
           />
         </>
-      )}
+      ) }
       <div className="py-2 d-flex justify-content-between flex-column flex-md-row">
-        <div className="text-and-table-icon-container">
+        <div className='text-and-table-icon-container'>
           <h5>Total Countries: {totalUniqueCountries}</h5>
-
           <button id='toggle-table-button' disabled={filteredMapMarkers.length == 0} onClick={toggleTableVisibility}>
-
-            <i className={`fa fa-table ${darkMode ? 'text-light' : 'text-dark'}`} aria-hidden="true" />
+            <i className={`fa fa-table ${darkMode ? 'text-light' : 'text-dark'}`} aria-hidden="true"
+/>
           </button>
         </div>
-        {isAbleToEdit && (
+        {isAbleToEdit ? (
           <div className="d-flex align-center">
-            <div className="d-flex align-center pr-5 flex-column flex-md-row position-relative">
+            <div className="d-flex align-center pr-5 flex-column flex-md-row  position-relative">
               <div className="input-group-prepend">
-
-                <span className={`input-group-text ${darkMode ? 'dark-mode-input' : ''}`}>{SEARCH}</span>
-
+                <span className="input-group-text">{SEARCH}</span>
               </div>
               <div>
                 <input
                   type="text"
-                  className={`form-control ${darkMode ? 'dark-mode-input' : ''}`}
+                  className="form-control"
                   aria-label="Search"
-                  placeholder="Search city/country/name"
+                  placeholder="Search by Location"
                   value={searchText}
                   onChange={searchHandler}
                 />
               </div>
-
-              {dropdown ? (
-                <div className={`position-absolute map-dropdown-table w-100 ${darkMode ? 'dark-mode-dropdown' : ''}`}>
+              {dropdown && (
+                <div className="position-absolute map-dropdown-table w-100">
                   <div
                     className="overflow-auto pr-3"
                     style={{ height: '300px' }}
                   >
                     {filteredMapMarkers.length > 0 ? (
-                      <table className={`table table-bordered table-responsive-md ${darkMode ? 'text-light bg-yinmn-blue' : ''}`}>
+                      <table className="table table-bordered table-responsive-md">
                         <tbody>
                           {filteredMapMarkers.map(profile => {
                             let userName = '';
@@ -259,7 +255,7 @@ function TeamLocations() {
                               userName = profile.firstName || profile.lastName || '-';
                             }
                             return (
-                              <tr className={darkMode ? 'bg-space-cadet' : ''} key={profile._id}>
+                              <tr key={profile._id}>
                                 <td>{userName}</td>
                                 <td>{`${profile.location.city ? `${profile.location.city},` : ''} ${
                                   profile.location.country
@@ -276,7 +272,7 @@ function TeamLocations() {
                                     {profile.type === 'm_user' ? (
                                       <Button
                                         color="danger"
-                                        style={darkMode ? boxStyleDark : boxStyle}
+                                        style={boxStyle}
                                         className="btn mr-1 btn-sm"
                                         onClick={() => removeLocation(profile._id)}
                                       >
@@ -287,7 +283,7 @@ function TeamLocations() {
                                       color="Primary"
                                       className="btn btn-outline-success mr-1 btn-sm"
                                       onClick={() => editHandler(profile)}
-                                      style={darkMode ? boxStyleDark : boxStyle}
+                                      style={boxStyle}
                                     >
                                       Edit
                                     </Button>
@@ -303,7 +299,7 @@ function TeamLocations() {
                     )}
                   </div>
                 </div>
-              ) : null}
+              ) }
             </div>
             <div className="d-flex align-center">
               <Button
@@ -328,63 +324,71 @@ function TeamLocations() {
           </div>
         ) : null}
       </div>
-      <div style={{ position: 'relative' }}>
-        <div>{tableVisible && <TeamLocationsTable visible={tableVisible} filteredMapMarkers={filteredMapMarkers} setCurrentUser={setCurrentUser} darkMode={darkMode} />}</div>
-        {loading ? (
-          <div animation="border" size="md" className="d-flex justify-content-center align-items-center" style={{ minHeight: '50vh' }}>
-            <Spinner animation="border" size="lg" />
-          </div>
-        ) : (
-          <MapContainer
-            id='map-container'
-            center={[51.505, -0.09]}
-            maxBounds={[
-              [-90, -225],
-              [90, 225],
-            ]}
-            maxBoundsViscosity={1.0}
-            zoom={3}
-            scrollWheelZoom
-            style={{ border: '1px solid grey' }}
-            ref={mapRef}
-          >
-            <EventComponent setPopupsOpen={setPopupsOpen} currentUser={currentUser} setMarkerPopupVisible={setMarkerPopupVisible} />
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              minZoom={2}
-              maxZoom={15}
-            />
-            <MarkerClusterGroup disableClusteringAtZoom={13} spiderfyOnMaxZoom={true} chunkedLoading>
-              {tableVisible && currentUser ?
-                <MarkerPopup
-                  key={currentUser._id}
-                  profile={currentUser}
-                  userName={getUserName(currentUser)}
-                  isAbleToEdit={isAbleToEdit}
-                  editHandler={editHandler}
-                  removeLocation={removeLocation}
-                  isOpen={markerPopupVisible}
-                  darkMode={darkMode}
-                />
-                : markerPopups}
-            </MarkerClusterGroup>
-          </MapContainer>
-        )}
+      <div style={{position: 'relative'}}>
+      
+      <div>{tableVisible && <TeamLocationsTable visible={tableVisible} filteredMapMarkers={filteredMapMarkers} setCurrentUser={setCurrentUser} darkMode={darkMode} />}</div>
+      {loading? (
+           <div animation="border" size="md" className="d-flex justify-content-center align-items-center" style={{ minHeight: '50vh' }}>
+           <Spinner animation="border" size="lg" />
+         </div>
+        ):
+      (
+      <MapContainer 
+        id='map-container'
+        center={[51.505, -0.09]}
+        maxBounds={[
+          [-90, -225],
+          [90, 225],
+        ]}
+        maxBoundsViscosity={1.0}
+        zoom={3}
+        scrollWheelZoom
+        style={{ border: '1px solid grey' }}
+        ref={mapRef}
+      >
+        <EventComponent setPopupsOpen={setPopupsOpen} currentUser={currentUser} setMarkerPopupVisible={setMarkerPopupVisible}  />
+        
+          
+           
+           
+          
+          <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          minZoom={2}
+          maxZoom={15} />
+         
+        <MarkerClusterGroup disableClusteringAtZoom={13} spiderfyOnMaxZoom={true} chunkedLoading>
+          {tableVisible && currentUser ?  
+          
+          <MarkerPopup
+            key={currentUser._id}
+            profile={currentUser}
+            userName={getUserName(currentUser)}
+            isAbleToEdit={isAbleToEdit}
+            editHandler={editHandler}
+            removeLocation={removeLocation}
+            isOpen={markerPopupVisible}
+            darkMode={darkMode} /> 
+            
+            : markerPopups }
+        </MarkerClusterGroup>
+      </MapContainer>
+      )}
       </div>
     </Container>
   );
-
 }
 
 function getUserName(profile) {
-let userName = '';
-if (profile.firstName && profile.lastName) {
-userName = `${profile.firstName} ${profile.lastName[0]}.`;
-} else {
-userName = profile.firstName || `${profile.lastName ? `${profile.lastName[0]}.` : ''}`;
-}
-return userName;
+  let userName = '';
+  if (profile.firstName && profile.lastName) {
+    userName = `${profile.firstName} ${`${profile.lastName[0]}.`}`;
+  } else {
+    userName =
+      profile.firstName || `${profile.lastName ? `${profile.lastName[0]}.` : ''}`;
+  }
+  return userName;
 }
 
 function EventComponent({ setPopupsOpen, currentUser, setMarkerPopupVisible }) {
@@ -400,14 +404,13 @@ function EventComponent({ setPopupsOpen, currentUser, setMarkerPopupVisible }) {
       } else {
         setPopupsOpen(false);
       }
-    },
-
+    }, 
+    
     zoomstart() {
       setMarkerPopupVisible(false);
     }
-  });
+  })
   return null;
-
 }
 
 export default TeamLocations;
