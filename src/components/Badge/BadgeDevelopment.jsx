68c829df8e83268fbeba68c1785f798d22a93f96
Reina128 <114252439+Reina128@users.noreply.github.com>
2024-09-05 15:29:44 -1000

    added a drop down menu for types and a text input for rankings

--- src/components/Badge/BadgeDevelopment.jsx
+++ src/components/Badge/BadgeDevelopment.jsx
@@ -2,8 +2,10 @@ import { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import { boxStyle, boxStyleDark } from 'styles';
 import BadgeDevelopmentTable from './BadgeDevelopmentTable';
+import BadgeTypes from './BadgeTypes';
 import CreateNewBadgePopup from './CreateNewBadgePopup';
 import '../Header/DarkMode.css';
+import { matches } from 'lodash';
 
 function BadgeDevelopment(props) {
   const { darkMode, allBadgeData = [] } = props; 
@@ -12,18 +14,24 @@ function BadgeDevelopment(props) {
   const [isAddFiltersOpen, setAddFiltersOpen] = useState(false);
   const [searchType, setSearchType] = useState('');
   const [rankFilter, setRankFilter] = useState(300);
+  const [chooseRankFilter, setChooseRankFilter] = useState('');
   const [searchName, setSearchName] = useState('');
 
   const toggle = () => setCreateNewBadgePopupOpen(prevIsOpen => !prevIsOpen);
   const toggleFilters = () => setAddFiltersOpen(prevState => !prevState);
 
+  // convert rank number into integer
+  const chooseRankFilterNumber = chooseRankFilter ? Number(chooseRankFilter) : null;
+
   // filter badge data based on type, report, and rank
   const filteredBadgeData = allBadgeData.filter(badge => {
     const matchesType = searchType === '' || badge.type.replace(/\s+/g, '').toLowerCase().includes(searchType.replace(/\s+/g, '').toLowerCase());
     const matchesRank = badge.ranking <= rankFilter;
+    const matchesChoosenRank = chooseRankFilterNumber === null || badge.ranking === chooseRankFilterNumber;
+
     const matchesName = searchName === '' || badge.badgeName.replace(/\s+/g, '').toLowerCase().includes(searchName.replace(/\s+/g, '').toLowerCase());
 
-    return matchesType && matchesRank && matchesName;
+    return matchesType && matchesRank && matchesName && matchesChoosenRank;
   });
 
   return (
@@ -54,13 +62,16 @@ function BadgeDevelopment(props) {
             />
           </div>
           <div style={{ marginBottom: '10px' }}>
-            <p style={{ display: 'inline', marginRight: '8px' }}>Filter by Type:</p>
-            <input
-              type="text"
-              placeholder="Enter type here"
+            <p style={{ display: 'inline', marginRight: '8px' }}>Filter by type:</p>
+            <select
               value={searchType}
               onChange={(e) => setSearchType(e.target.value)}
-            />
+            >
+              <option value="">Select a type</option>
+              {BadgeTypes.map((type, index) => (
+                <option key={index} value={type}>{type}</option>
+              ))}
+            </select>
           </div>
           <div>
             <p style={{ display: 'inline', marginRight: '8px' }}>Filter by Rank (0 - {rankFilter}):</p>
@@ -72,6 +83,16 @@ function BadgeDevelopment(props) {
               value={rankFilter}
               onChange={(e) => setRankFilter(e.target.value)}
             />
+            <div style={{ display: 'inline-block', marginLeft: '8px', verticalAlign: 'middle' }}>
+              <p style={{ display: 'inline', marginRight: '8px' }}>Or choose a rank:</p>
+              <input
+                type="text"
+                placeholder="Rank Number"
+                value={chooseRankFilter}
+                onChange={(e) => setChooseRankFilter(e.target.value)}
+                style={{ width: '80px', textAlign: 'center' }}
+              />
+            </div>
           </div>
         </div>
       )}
@@ -88,8 +109,10 @@ function BadgeDevelopment(props) {
           <CreateNewBadgePopup toggle={toggle} />
         </ModalBody>
       </Modal>
+      <br/>
       {filteredBadgeData.length === 0 ? (
-        <p>No badges match the current filters.</p>
+      
+        <p> No badges match the current filters.</p>
       ) : (
         <BadgeDevelopmentTable allBadgeData={filteredBadgeData} />
       )}
