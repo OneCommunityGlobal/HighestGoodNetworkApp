9829161a2c8ca1d04f7560790e1d0fc0a0b67744
luisarevalo21 <arevaloluis89@yahoo.com>
2024-09-10 22:08:52 -0700

    restored tracker modal

--- src/components/Warnings/Warnings.jsx
+++ src/components/Warnings/Warnings.jsx
@@ -93,7 +93,6 @@ export default function Warning({ personId, username, userRole }) {
   return (
     (userRole === 'Administrator' || userRole === 'Owner') && (
       <div className="warnings-container">
-<<<<<<< HEAD
         <div className="button__container">
           <Button
             className="btn btn-warning warning-btn tracking__btn"
@@ -129,11 +128,6 @@ export default function Warning({ personId, username, userRole }) {
             getUsersWarnings={fetchUsersWarningsById}
           />
         )}
-=======
-        <Button className="btn btn-warning warning-btn ml-auto" size="sm" onClick={handleToggle}>
-          {toggle ? 'Hide' : 'Tracking'}
-        </Button>
->>>>>>> development
 
         <div className="warning-wrapper"> {warnings}</div>
         <div className="error-container">
