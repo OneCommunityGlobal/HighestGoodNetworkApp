696193714a85009d0f52449cabebb155dc0b4af0
huijieliu8 <91745551+metaphor987@users.noreply.github.com>
2024-09-21 21:41:14 -0400

    fix: clean up

--- src/components/Reports/LostTime/EditHistoryModal.jsx
+++ src/components/Reports/LostTime/EditHistoryModal.jsx
@@ -177,7 +177,6 @@ const EditHistoryModal = props => {
       result.personId = 'Person is required';
     }
     if (props.entryType == 'team' && inputs.teamId == undefined) {
-      console.log("EditHistory");
       result.teamId = 'Team is required';
     }
 
