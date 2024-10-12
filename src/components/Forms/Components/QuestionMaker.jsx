165104d77f19f28eadba7d9a3539a48536468d21
ArtemisNyx3 <artemisnyx3@yahoo.com>
2024-10-12 03:24:24 -0400

    changed name to be more meaningful

---
+++ src/components/Forms/Components/QuestionMaker.jsx
@@ -0,0 +1,38 @@
+// This component is part of the form builder
+
+import { QuestionType } from 'utils/enums';
+import OptionMaker from './OptionMaker';
+
+export default function QuestionMaker({ data, updateQuestion }) {
+  const handleTypeChange = e => {
+    updateQuestion(data.id, { type: e.target.value });
+  };
+
+  return (
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>
+      <div>
+        <div>
+          <label>{data.label}</label>
+          <select value={data.type} onChange={handleTypeChange}>
+            <option value="select">Select</option>
+            <option value="short_answer">Short Answer</option>
+            <option value="paragraph">Paragraph</option>
+            <option value="multi_select">Multi-Select</option>
+            <option value="radio">Radio</option>
+          </select>
+        </div>
+        <div>
+          {data.type === QuestionType.Paragraph && <textarea id={data.id} disabled />}
+          {data.type === QuestionType.ShortAnswer && <input type="text" id={data.id} disabled />}
+          {data.type === QuestionType.MultiSelect && (
+            <OptionMaker data={data} updateQuestion={updateQuestion} />
+          )}
+          {data.type === QuestionType.Radio && (
+            <OptionMaker data={data} updateQuestion={updateQuestion} />
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
