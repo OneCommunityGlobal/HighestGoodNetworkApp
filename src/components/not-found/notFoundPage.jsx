76f192876f0fdf4021e2384dabfa6e5d3903b059
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2024-10-15 21:05:59 -0300

    Peterson_implement_Default_Error_Page_to_be_created_and_redirected_to

--- src/components/not-found/notFoundPage.jsx
+++ src/components/not-found/notFoundPage.jsx
@@ -7,38 +7,43 @@ function NotFoundPage() {
 
   return (
     <section
-      className={`${
-        darkMode ? 'bg-oxford-blue' : 'bg-white'
-      } h-100 d-flex   align-items-center  flex-column
-        justify-content-center `}
+      className={`h-100 d-flex   align-items-center  flex-column justify-content-center`}
+      style={{ backgroundColor: darkMode ? '#121212' : 'white' }}
     >
-      <div>
-        <h2 style={{ color: darkMode ? 'white' : 'black', textAlign: 'center' }}>
-          {' '}
-          404 | Page not found
-        </h2>
-        <div
-          className={`${darkMode ? 'text-light' : 'text-dark'}`}
-          style={{ marginBottom: '20rem' }}
-        >
-          {validateUserLogin ? (
-            <div className="col-12">
-              <p>
-                It seems like you&apos;ve reached a page that doesn&apos;t exist. Maybe this will
-                help &nbsp;
-                <Link to="/dashboard">home</Link> or you can report this page by clicking &nbsp;
-                <Link to="/dashboard?openModalReport">here</Link>
-              </p>
-            </div>
-          ) : (
-            // eslint-disable-next-line react/jsx-curly-brace-presence
-            <p className={`col-12`}>
-              It seems like you&apos;ve reached a page that doesn&apos;t exist. In addition
-              You&apos;re not currently logged in. Please go back to the &nbsp;
-              <Link to="/login">login page</Link>
-            </p>
-          )}
-        </div>
+      <div
+        className=" d-flex   align-items-center justify-content-center "
+        style={{
+          width: '50%',
+          height: '50%',
+          backgroundImage: !darkMode
+            ? `url("/imagePage404.png")`
+            : `url("/imagePage404DarkMode.png")`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          marginBottom: '20rem',
+        }}
+      >
+        {validateUserLogin ? (
+          <p
+            style={{ marginTop: !darkMode ? '25rem' : '20rem', fontSize: '25px' }}
+            className={`${darkMode ? 'text-light' : 'text-dark'}`}
+          >
+            The rabbits have been nibbling the cables again... Maybe this will help &nbsp;
+            <Link to="/dashboard">home</Link> or you can report this page by clicking &nbsp;
+            <Link to="/dashboard?openModalReport">here</Link>
+          </p>
+        ) : (
+          // eslint-disable-next-line react/jsx-curly-brace-presence
+          <p
+            style={{ marginTop: !darkMode ? '25rem' : '20rem', fontSize: '25px' }}
+            className={`${darkMode ? 'text-light' : 'text-dark'}`}
+          >
+            It seems like you&apos;ve reached a page that doesn&apos;t exist. In addition
+            You&apos;re not currently logged in. Please go back to the &nbsp;
+            <Link to="/login">login page</Link>
+          </p>
+        )}
       </div>
     </section>
   );
