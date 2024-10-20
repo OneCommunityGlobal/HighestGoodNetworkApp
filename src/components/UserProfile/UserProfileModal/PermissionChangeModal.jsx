f95bb2493adfb9f5fd390d09fc6b49c32cee4b5d
lcohen730 <lcohen730@gmail.com>
2024-10-20 01:49:26 -0400

    Pass oldUserProfile to PermissionChangeModal, Add State for newRolePermissions and oldRolePermissions, Add useEffect to Fetch Role Permissions with React-Redux Dispatch Hook, Update Constants to Refer to Correct Properties, and Uncomment Out Code

--- src/components/UserProfile/UserProfileModal/PermissionChangeModal.jsx
+++ src/components/UserProfile/UserProfileModal/PermissionChangeModal.jsx
@@ -1,33 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 // import whatever file I need to pull the information of a given role's permissions
-// import whatever file I need to pull the information of a specific user's permissions
+import { getPresetsByRole } from '../../../actions/rolePermissionPresets';
 
-function PermissionChangeModal({ userProfile }) {
+function PermissionChangeModal({ userProfile, oldUserProfile }) {
   // Creating a modal that pops up when someone changes a user's role
   // and the user has custom permissions that differ from the permissions
   // of their old role. It should show the difference between the current permissions of the user
   // and the permissions of the new role, and ask the user to confirm which permissions they want
   // to keep.
 
+  const dispatch = useDispatch();
+  const [oldRolePermissions, setOldRolePermissions] = useState([]);
+  const [newRolePermissions, setNewRolePermissions] = useState([]);
+
   // create variable for user
   const user = userProfile;
   // create variable for old role
+  const oldRole = oldUserProfile?.role;
   // create variable for new role
-  // const currentUserPermissions = user.permissions; // import user permissions from wherever
-  // const oldRolePermissions = oldRole.permissions; // import old role permissions from wherever
+  const newRole = user.role;
+  const currentUserPermissions =  user.permissions.frontPermissions;
+
+  useEffect(() => {
+    const fetchOldRolePermissions = async () => {
+      if (oldRole) {
+        const oldRolePresets = await dispatch(getPresetsByRole(oldRole));
+        if (oldRolePresets && oldRolePresets.presets && oldRolePresets.presets[2]) {
+          setOldRolePermissions(oldRolePresets.presets[2].permissions);
+        }
+      }
+    };
+
+    const fetchNewRolePermissions = async () => {
+      if (newRole) {
+        const newRolePresets = await dispatch(getPresetsByRole(newRole));
+        if (newRolePresets && newRolePresets.presets && newRolePresets.presets[2]) {
+          setNewRolePermissions(newRolePresets.presets[2].permissions);
+        }
+      }
+    };
+
+    fetchOldRolePermissions();
+    fetchNewRolePermissions();
+  }, [dispatch, oldRole, newRole]);
+
   // difference between old role permissions and user permissions
   // permissions that were added to user (user permissions - old role permissions)
-  // const customAddedPermissions = currentUserPermissions.filter(permission => !oldRolePermissions.includes(permission));
+  const customAddedPermissions = currentUserPermissions.filter(permission => !oldRolePermissions.includes(permission));
   // permissions that were removed from user (old role permissions - user permissions)
-  // const customRemovedPermissions = oldRolePermissions.filter(permission => !currentUserPermissions.includes(permission));
-  // const newRolePermissions = newRole.permissions; // import new role permissions from wherever
+  const customRemovedPermissions = oldRolePermissions.filter(permission => !currentUserPermissions.includes(permission));
   // permissions that were removed from user but are in new role (newRolePermissions - customRemovedPermissions)
-  // const newRolePermissionsToAdd = newRolePermissions.filter(permission => customRemovedPermissions.includes(permission));
+  const newRolePermissionsToAdd = newRolePermissions.filter(permission => customRemovedPermissions.includes(permission));
   // permissions that were added to user but are not in new role (newRolePermissions + customAddedPermissions)
-  // const newRolePermissionsToRemove = customAddedPermissions.filter(permission => !newRolePermissions.includes(permission));
+  const newRolePermissionsToRemove = customAddedPermissions.filter(permission => !newRolePermissions.includes(permission));
 
-  function testUser() {
-    console.log(user);
+  async function testUser() {
+    console.log('user: ', user);
+    console.log('user permissions: ', user.permissions);
+    const userRolePresets = await dispatch(getPresetsByRole(user.role));
+    console.log('user role presets: ', userRolePresets);
   }
+
+  useEffect(() => {
+    testUser();
+  }, [user]);
 
   return (
     <div className="modal-content">
@@ -43,7 +80,7 @@ function PermissionChangeModal({ userProfile }) {
           the following permissions that are different from the {/* {rolename} */} role you are
           changing them to. Please confirm which of these you&apos;d like to keep:
         </p>
-        {/* <ul>
+        <ul>
           {newRolePermissionsToAdd.map(permission => (
             <li key={permission}>
               <input type="checkbox" id={permission} name={permission} value={permission} />
@@ -56,11 +93,11 @@ function PermissionChangeModal({ userProfile }) {
               <label htmlFor={permission}>{permission}</label>
             </li>
           ))}
-        </ul> */}
+        </ul>
         <div className="modal-footer">
           <button className="cancel-button" 
           /* onClick={closeModal} */ 
-          onClick={testUser} 
+          /* onClick={testUser}  */
           type="button"
           >
             Cancel
