b14aaad9ba7147c994d5a02bc849eaf44271d985
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2024-10-29 22:35:13 -0300

    Peterson_implement_Default_Error_Page_to_be_created_and_redirected_to

--- src/components/not-found/notFoundPage.jsx
+++ src/components/not-found/notFoundPage.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import './notFoundPage.css';
 
 function NotFoundPage() {
   const darkMode = useSelector(state => state.theme.darkMode);
@@ -11,40 +12,45 @@ function NotFoundPage() {
       className={`h-100 d-flex   align-items-center  flex-column justify-content-center`}
       style={{ backgroundColor: darkMode ? '#121212' : 'white' }}
     >
-      <div
-        className=" d-flex   align-items-center justify-content-center "
-        style={{
-          width: '50%',
-          height: '50%',
-          backgroundImage: !darkMode
-            ? `url("/imagePage404.png")`
-            : `url("/imagePage404DarkMode.png")`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          marginBottom: '20rem',
-        }}
-      >
-        {validateUserLogin ? (
-          <p
-            style={{ marginTop: !darkMode ? '25rem' : '20rem', fontSize: '25px' }}
-            className={`${darkMode ? 'text-light' : 'text-dark'}`}
-          >
-            The rabbits have been nibbling the cables again... Maybe this will help &nbsp;
-            <Link to="/dashboard">home</Link> or you can report this page by clicking &nbsp;
-            <Link to="/dashboard?openModalReport">here</Link>
-          </p>
-        ) : (
-          // eslint-disable-next-line react/jsx-curly-brace-presence
-          <p
-            style={{ marginTop: !darkMode ? '25rem' : '20rem', fontSize: '25px' }}
-            className={`${darkMode ? 'text-light' : 'text-dark'}`}
-          >
-            It seems like you&apos;ve reached a page that doesn&apos;t exist. In addition
-            You&apos;re not currently logged in. Please go back to the &nbsp;
-            <Link to="/login">login page</Link>
-          </p>
-        )}
+      <div className={`${darkMode ? 'containerImageDarkMode' : 'containerImage'}`}>
+        <div className={`${!darkMode ? 'image-404' : 'image-404DarkMode'}`}>
+          <img
+            src={`${darkMode ? '/imagePage404DarkMode.png' : 'imagePage404.png'}`}
+            className={`${darkMode ? 'imageDarkMode' : 'image'}`}
+            alt="404 error page illustration"
+          />
+          {validateUserLogin ? (
+            <p
+              //  28rem
+              className={`${
+                darkMode ? 'text-light message-error-404DarkMode ' : 'text-dark  message-error-404'
+              }`}
+            >
+              The rabbits have been nibbling the cables again... Maybe this will help
+              <Link style={{ marginLeft: '6px' }} to="/dashboard">
+                home
+              </Link>{' '}
+              or you can report this page by clicking
+              <Link style={{ marginLeft: '6px' }} to="/dashboard?openModalReport">
+                here
+              </Link>
+            </p>
+          ) : (
+            // eslint-disable-next-line react/jsx-curly-brace-presence
+            <p
+              style={{ fontSize: '25px' }}
+              className={`${
+                darkMode ? 'text-light message-error-404DarkMode ' : 'text-dark  message-error-404'
+              }`}
+            >
+              It seems like you&apos;ve reached a page that doesn&apos;t exist. In addition
+              You&apos;re not currently logged in. Please go back to the
+              <Link style={{ marginLeft: '8px' }} to="/login">
+                login page
+              </Link>
+            </p>
+          )}
+        </div>
       </div>
     </section>
   );
