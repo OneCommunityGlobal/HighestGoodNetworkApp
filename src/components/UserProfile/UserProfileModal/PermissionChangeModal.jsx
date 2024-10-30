4f6030ee6a7a125df2edd7fcfb56e87220fa8b77
lcohen730 <lcohen730@gmail.com>
2024-10-30 15:40:59 -0400

    Add state and useEffect for checked permissions check or x state and a function to toggle the check or x, and import and implement useMemo to avoid useEffect loops

--- src/components/UserProfile/UserProfileModal/PermissionChangeModal.jsx
+++ src/components/UserProfile/UserProfileModal/PermissionChangeModal.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { useDispatch } from 'react-redux';
 // import whatever file I need to pull the information of a given role's permissions
 import { getPresetsByRole } from '../../../actions/rolePermissionPresets';
+import './PermissionChangeModal.css';
 
 function PermissionChangeModal({ userProfile, oldUserProfile }) {
   // Creating a modal that pops up when someone changes a user's role
@@ -13,6 +14,8 @@ function PermissionChangeModal({ userProfile, oldUserProfile }) {
   const dispatch = useDispatch();
   const [oldRolePermissions, setOldRolePermissions] = useState([]);
   const [newRolePermissions, setNewRolePermissions] = useState([]);
+  const [checkedAddedPermissions, setCheckedAddedPermissions] = useState({});
+  const [checkedRemovedPermissions, setCheckedRemovedPermissions] = useState({});
 
   // create variable for user
   const user = userProfile;
@@ -32,6 +35,10 @@ function PermissionChangeModal({ userProfile, oldUserProfile }) {
       }
     };
 
+    fetchOldRolePermissions();
+  }, [dispatch, oldRole]);
+
+  useEffect(() => {
     const fetchNewRolePermissions = async () => {
       if (newRole) {
         const newRolePresets = await dispatch(getPresetsByRole(newRole));
@@ -41,19 +48,30 @@ function PermissionChangeModal({ userProfile, oldUserProfile }) {
       }
     };
 
-    fetchOldRolePermissions();
     fetchNewRolePermissions();
-  }, [dispatch, oldRole, newRole]);
+  }, [dispatch, newRole]);
 
   // difference between old role permissions and user permissions
   // permissions that were added to user (user permissions - old role permissions)
-  const customAddedPermissions = currentUserPermissions.filter(permission => !oldRolePermissions.includes(permission));
+  // const customAddedPermissions = currentUserPermissions.filter(permission => !oldRolePermissions.includes(permission));
+  const customAddedPermissions = useMemo(() => {
+    return currentUserPermissions.filter(permission => !oldRolePermissions.includes(permission));
+  }, [currentUserPermissions, oldRolePermissions]);
   // permissions that were removed from user (old role permissions - user permissions)
-  const customRemovedPermissions = oldRolePermissions.filter(permission => !currentUserPermissions.includes(permission));
+  // const customRemovedPermissions = oldRolePermissions.filter(permission => !currentUserPermissions.includes(permission));
+  const customRemovedPermissions = useMemo(() => {
+    return oldRolePermissions.filter(permission => !currentUserPermissions.includes(permission));
+  }, [oldRolePermissions, currentUserPermissions]);
   // permissions that were removed from user but are in new role (newRolePermissions - customRemovedPermissions)
-  const newRolePermissionsToAdd = newRolePermissions.filter(permission => customRemovedPermissions.includes(permission));
+  // const newRolePermissionsToAdd = newRolePermissions.filter(permission => customRemovedPermissions.includes(permission));
+  const newRolePermissionsToAdd = useMemo(() => {
+    return newRolePermissions.filter(permission => customRemovedPermissions.includes(permission));
+  }, [newRolePermissions, customRemovedPermissions]);
   // permissions that were added to user but are not in new role (newRolePermissions + customAddedPermissions)
-  const newRolePermissionsToRemove = customAddedPermissions.filter(permission => !newRolePermissions.includes(permission));
+  // const newRolePermissionsToRemove = customAddedPermissions.filter(permission => !newRolePermissions.includes(permission));
+  const newRolePermissionsToRemove = useMemo(() => {
+    return customAddedPermissions.filter(permission => !newRolePermissions.includes(permission));
+  }, [customAddedPermissions, newRolePermissions]);
 
   const formatPermission = permission => {
     let formattedPermission = '';
@@ -65,8 +83,35 @@ function PermissionChangeModal({ userProfile, oldUserProfile }) {
     }
     return formattedPermission.charAt(0).toUpperCase() + formattedPermission.slice(1);
   };
+
+  useEffect(() => {
+    const initialCheckedAddedPermissions = {};
+    const initialCheckedRemovedPermissions = {};
+    newRolePermissionsToAdd.forEach(permission => {
+      initialCheckedAddedPermissions[permission] = true;
+    });
+    newRolePermissionsToRemove.forEach(permission => {
+      initialCheckedRemovedPermissions[permission] = true;
+    });
+    setCheckedAddedPermissions(initialCheckedAddedPermissions);
+    setCheckedRemovedPermissions(initialCheckedRemovedPermissions);
+  }, [newRolePermissionsToAdd, newRolePermissionsToRemove]);
+
+  const togglePermission = (permission, type) => {
+    if (type === 'added') {
+      setCheckedAddedPermissions(prevState => ({
+        ...prevState,
+        [permission]: !prevState[permission],
+      }));
+    } else {
+      setCheckedRemovedPermissions(prevState => ({
+        ...prevState,
+        [permission]: !prevState[permission],
+      }));
+    }
+  };
   
-  async function testUser() {
+  /* async function testUser() {
     console.log('user: ', user);
     console.log('user permissions: ', user.permissions);
     const userRolePresets = await dispatch(getPresetsByRole(user.role));
@@ -75,7 +120,7 @@ function PermissionChangeModal({ userProfile, oldUserProfile }) {
 
   useEffect(() => {
     testUser();
-  }, [user]);
+  }, [user]); */
 
   return (
     <div className="modal-content">
@@ -88,32 +133,48 @@ function PermissionChangeModal({ userProfile, oldUserProfile }) {
       <div className="modal-body">
         <p>
           You are changing the role of a Special Person with special permissions. This person has
-          the following permissions that are different from the {/* {rolename} */} role you are
+          the following permissions that are different from the {newRole} role you are
           changing them to. Please confirm which of these you&apos;d like to keep:
         </p>
-        <ul>
+        <ul className="list">
           {newRolePermissionsToAdd.map(permission => (
             <li key={permission}>
-              <input type="checkbox" id={permission} name={permission} value={permission} />
-              <label htmlFor={permission}>{formatPermission(permission)}</label> (Added)
+              <input 
+                type="checkbox" 
+                id={permission} 
+                name={permission} 
+                value={permission}
+                checked={!!checkedAddedPermissions[permission]}
+                onChange={() => togglePermission(permission, 'added')}
+                className="custom-checkbox" 
+              />
+              <label className="permission-text" htmlFor={permission}>{formatPermission(permission)}</label> (Added)
             </li>
           ))}
           {newRolePermissionsToRemove.map(permission => (
             <li key={permission}>
-              <input type="checkbox" id={permission} name={permission} value={permission} />
-              <label htmlFor={permission}>{formatPermission(permission)}</label> (Removed)
+              <input 
+                type="checkbox" 
+                id={permission} 
+                name={permission} 
+                value={permission} 
+                checked={!!checkedRemovedPermissions[permission]}
+                onChange={() => togglePermission(permission, 'removed')}
+                className="custom-checkbox"
+              />
+              <label className="permission-text" htmlFor={permission}>{formatPermission(permission)}</label> (Removed)
             </li>
           ))}
         </ul>
         <div className="modal-footer">
-          <button className="cancel-button" 
+          <button className="modal-cancel-button" 
           /* onClick={closeModal} */ 
           /* onClick={testUser}  */
           type="button"
           >
             Cancel
           </button>
-          <button className="confirm-button" /* onClick={confirmModal} */ type="submit">
+          <button className="modal-confirm-button" /* onClick={confirmModal} */ type="submit">
             Confirm
           </button>
         </div>
