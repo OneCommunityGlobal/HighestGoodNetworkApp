f90b756c645cd1ce130daf0a59afceed5abf6a3a
gmon <k.g@northeastern.edu>
2024-11-08 19:23:04 -0800

    Add active/inactive numbers by teams on team page

--- src/components/Teams/TeamsOverview.jsx
+++ src/components/Teams/TeamsOverview.jsx
@@ -3,8 +3,11 @@ import { TOTAL_TEAMS, ACTIVE_TEAMS } from '../../languages/en/ui';
 import "./TeamsOverview.css"
 
 const TeamsOverview = ({ numberOfTeams, numberOfActiveTeams }) => {
+  const numberOfNonActiveTeams = numberOfTeams - numberOfActiveTeams;
+
   return (
     <div className="teams__overview--top">
+      {/* Total Teams Card */}
       <div className="card" id="card_team" data-testid="card_team">
         <div className="card-body">
           <h6 className="card-text">
@@ -13,10 +16,20 @@ const TeamsOverview = ({ numberOfTeams, numberOfActiveTeams }) => {
         </div>
       </div>
 
+      {/* Active Teams Card */}
       <div className="card" id="card_active" data-testid="card_active">
         <div className="card-body">
           <h6 className="card-text">
             <i className="fa fa-circle fa-circle-isActive" aria-hidden="true"></i> {ACTIVE_TEAMS}: {numberOfActiveTeams}
+          </h6>
+        </div>
+      </div>
+
+      {/* Non-Active Teams Card */}
+      <div className="card" id="card_active" data-testid="card_active">
+        <div className="card-body">
+          <h6 className="card-text">
+            <i className="fa fa-circle fa-circle-Active" aria-hidden="true"></i> Non-Active Teams: {numberOfNonActiveTeams}
           </h6>
         </div>
       </div>
