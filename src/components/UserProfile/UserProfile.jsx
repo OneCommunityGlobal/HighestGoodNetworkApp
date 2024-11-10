5debe3df24723c39716d44f92249028aa1918b5e
suaniii <yili.sun3688@gmail.com>
2024-11-10 02:18:42 -0800

    fix reset pass

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -125,6 +125,7 @@ function UserProfile(props) {
   const [showToggleVisibilityModal, setShowToggleVisibilityModal] = useState(false);
   const [pendingRehireableStatus, setPendingRehireableStatus] = useState(null);
   const [isRehireable, setIsRehireable] = useState(null);
+  const [canResetPassword, setCanResetPassword] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [didLinkUpdate, setDidLinkUpdate] = useState(false);
   // Function to toggle the modal
@@ -336,6 +337,7 @@ function UserProfile(props) {
       const response = await axios.get(ENDPOINTS.USER_PROFILE(userId));
       const currentUserEmail = response.data.email;
       dispatch(setCurrentUser({ ...props.auth.user, email: currentUserEmail }));
+      setCanResetPassword(response.data.permissions.frontPermissions.includes('resetPassword'));
     } catch (err) {
       toast.error('Error while getting current logged in user email');
     }
@@ -1403,7 +1405,7 @@ function UserProfile(props) {
               </TabPane>
             </TabContent>
             <div className="profileEditButtonContainer">
-              {canUpdatePassword && canEdit && !isUserSelf && (
+              {(canUpdatePassword && canEdit && !isUserSelf || canResetPassword) && (
                 <ResetPasswordButton
                   className="mr-1 btn-bottom"
                   user={userProfile}
