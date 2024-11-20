e0584f0e6eb4e3c99db7b6c4f9d772a84d1c488b
CodeWithKoushica <koushica.bu01@gmail.com>
2024-11-19 19:05:52 -0600

    UI level changes

--- src/components/TeamMemberTasks/TeamMemberTask.jsx
+++ src/components/TeamMemberTasks/TeamMemberTask.jsx
@@ -180,50 +180,56 @@ const TeamMemberTask = React.memo(
                 {/* green if member has met committed hours for the week, red if not */}
                 <td colSpan={1} className={`${darkMode ? "bg-yinmn-blue" : ""}`}>
                   <div style={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap' }}>
-                    <div className="committed-hours-circle">
-                      <FontAwesomeIcon
-                        style={{
-                          color:
-                            user.totaltangibletime_hrs >= user.weeklycommittedHours ? 'green' : 'red',
-                        }}
-                        icon={faCircle}
-                        data-testid="icon"
-                      />
+                    <div className='member-links-wrapper'>
+                      <div className="committed-hours-circle">
+                        <FontAwesomeIcon
+                          style={{
+                            color:
+                              user.totaltangibletime_hrs >= user.weeklycommittedHours ? 'green' : 'red',
+                          }}
+                          icon={faCircle}
+                          data-testid="icon"
+                        />
+                      </div>
+                      <Link to={`/timelog/${user.personId}`}>
+                        <i
+                          className="fa fa-clock-o"
+                          aria-hidden="true"
+                          style={{
+                            fontSize: 24,
+                            cursor: 'pointer',
+                            color: darkMode ? 'lightgray' : 'black',
+                          }}
+                          title="Click to see user's timelog"
+                        />
+                      </Link>
+                      {canGetWeeklySummaries && <GoogleDocIcon link={userGoogleDocLink} />}
                     </div>
-                    <Link to={`/timelog/${user.personId}`}>
-                      <i
-                        className="fa fa-clock-o"
-                        aria-hidden="true"
-                        style={{
-                          fontSize: 24,
-                          cursor: 'pointer',
-                          color: darkMode ? 'lightgray' : 'black',
-                        }}
-                        title="Click to see user's timelog"
-                      />
-                    </Link>
-                    {canGetWeeklySummaries && teamRoles && ['Manager', 'Assistant Manager', 'Mentor'].map((role, roleIndex) => {
-                      return teamRoles[role]?.map((elm, i) => {
-                        const name = elm.name; // Getting initials and formatting them here
-                        const initials = getInitials(name);
-                        // Getting background color dynamically based on the role
-                        const bg = colors_objs[role];
-                        return (
-                          <a
-                            key={`res_${roleIndex}_${i}`}
-                            title={`${role} : ${name}`}
-                            className="name"
-                            href={`/userprofile/${elm.id}`}
-                            target="_blank"
-                            rel="noreferrer"
-                          >
-                            <span className="name-initial" style={{ backgroundColor: bg }}>
-                              {initials}{' '}
-                            </span>
-                          </a>
-                        );
-                      });
-                    })}
+                    {canGetWeeklySummaries && teamRoles && <div className="name-wrapper">
+                      {['Manager', 'Assistant Manager', 'Mentor'].map((role, roleIndex) => {
+                          return teamRoles[role]?.map((elm, i) => {
+                            const name = elm.name; // Getting initials and formatting them here
+                            const initials = getInitials(name);
+                            // Getting background color dynamically based on the role
+                            const bg = colors_objs[role];
+                            return (
+                              <a
+                                key={`res_${roleIndex}_${i}`}
+                                title={`${role} : ${name}`}
+                                className="name"
+                                href={`/userprofile/${elm.id}`}
+                                target="_blank"
+                                rel="noreferrer"
+                              >
+                                <span className="name-initial" style={{ backgroundColor: bg }}>
+                                  {initials}{' '}
+                                </span>
+                              </a>
+                            );
+                          });
+                        })}
+                      </div>
+                      }
                   </div>
                 </td>
                 <td colSpan={2} className={`${darkMode ? "bg-yinmn-blue" : ""}`}>
@@ -245,7 +251,6 @@ const TeamMemberTask = React.memo(
                               fontSize: '20px'
                             }}
                           >{`${user.name}`}</Link>
-                          {canGetWeeklySummaries && <GoogleDocIcon link={userGoogleDocLink} />}
 
                           <Warning
                             username={user.name}
