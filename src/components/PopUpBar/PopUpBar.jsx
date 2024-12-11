cd6d00be030645d4e3219c1b28dc443c379591ef
Saniya98 <saniyafarheen.98@gmail.com>
2024-12-10 21:46:59 -0600

    pop up has been updated

--- src/components/PopUpBar/PopUpBar.jsx
+++ src/components/PopUpBar/PopUpBar.jsx
@@ -6,7 +6,7 @@ function PopUpBar(props) {
   const { firstName, lastName } = viewingUser;
   return (
     <div className="popup_container" data-testid="test-popup">
-      {`You are currently viewing the header for ${firstName} ${lastName}`}
+      {`You are currently functioning as ${firstName} ${lastName}, you only have the permissions of ${firstName}`}
       <button type="button" className="close_button" onClick={onClickClose}>
         X
       </button>
