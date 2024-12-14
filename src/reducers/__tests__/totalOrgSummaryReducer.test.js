f109c429f5afeefb1b3072b10124ab0527d7c0c0
Vijay Anand <vijay10pk@gmail.com>
2024-12-13 20:47:58 -0500

    add unit test for totalOrgSummaryReducer

--- src/reducers/__tests__/totalOrgSummaryReducer.test.js
+++ src/reducers/__tests__/totalOrgSummaryReducer.test.js
@@ -50,4 +50,30 @@ describe('totalOrgSummaryReducer', () => {
       error: 'Unable to fetch data',
     });
   });
+
+  it('should handle FETCH_TOTAL_ORG_SUMMARY_DATA_SUCCESS', () => {
+    const action = {
+      type: actions.FETCH_TOTAL_ORG_SUMMARY_DATA_SUCCESS,
+      payload: { volunteerOverview: [{ id: 2, name: 'Jane Doe' }] },
+    };
+    const newState = totalOrgSummaryReducer(initialState, action);
+    expect(newState).toEqual({
+      ...initialState,
+      loading: false,
+      volunteerOverview: [{ id: 2, name: 'Jane Doe' }],
+    });
+  });
+
+  it('should handle FETCH_TOTAL_ORG_SUMMARY_DATA_ERROR', () => {
+    const action = {
+      type: actions.FETCH_TOTAL_ORG_SUMMARY_DATA_ERROR,
+      payload: { fetchingError: 'Unable to fetch overview data' },
+    };
+    const newState = totalOrgSummaryReducer(initialState, action);
+    expect(newState).toEqual({
+      ...initialState,
+      loading: false,
+      fetchingError: 'Unable to fetch overview data',
+    });
+  });
 });
