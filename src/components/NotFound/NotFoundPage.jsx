304018d22d4613f0d166c7354e80e2a20c262bf0
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2024-12-18 21:25:47 -0300

    Peterson_implement_improvements_in_404_page

--- src/components/NotFound/NotFoundPage.jsx
+++ src/components/NotFound/NotFoundPage.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import './notFoundPage.css';
+import '../NotFound/notFoundPage.css';
 
 function NotFoundPage() {
   const darkMode = useSelector(state => state.theme.darkMode);
