a7d0900368c1e700fd6bb2c50db56397930cf4ee
Vijay Anand <vijay10pk@gmail.com>
2025-01-03 13:56:11 -0500

    added unit test for title action creator

--- src/actions/__tests__/titleAction.test.js
+++ src/actions/__tests__/titleAction.test.js
@@ -1,8 +1,4 @@
-import axios from 'axios';
 import * as titleActions from '../title';
-import { ENDPOINTS } from '../../utils/URL';
-
-jest.mock('axios');
 
 describe('Title Actions', () => {
   describe('addTitle', () => {
@@ -44,6 +40,81 @@ describe('Title Actions', () => {
       } catch (error) {
         expect(error.response).toEqual(mockError.response);
       }
+    });
+  });
+
+  describe('getAllTitle', () => {
+    it('should return response when API call is successful', async () => {
+      const mockResponse = [{ id: '123', title: 'Title 1' }, { id: '124', title: 'Title 2' }];
+      jest.spyOn(titleActions, 'getAllTitle').mockResolvedValue(mockResponse);
+
+      const response = await titleActions.getAllTitle();
+      expect(response).toEqual(mockResponse);
+    });
+
+    it('should return error object when API call fails', async () => {
+      const mockError = { response: { data: { message: 'Error' }, status: 400 } };
+      jest.spyOn(titleActions, 'getAllTitle').mockRejectedValue(mockError);
+
+      try {
+        await titleActions.getAllTitle();
+      } catch (error) {
+        expect(error.response).toEqual(mockError.response);
+      }
+    });
+  });
+
+  describe('getTitleById', () => {
+    it('should return response when API call is successful', async () => {
+      const mockResponse = { id: '123', title: 'Test Title' };
+      jest.spyOn(titleActions, 'getTitleById').mockResolvedValue(mockResponse);
+
+      const response = await titleActions.getTitleById('123');
+      expect(response).toEqual(mockResponse);
+    });
+
+    it('should return error object when API call fails', async () => {
+      jest.spyOn(titleActions, 'getTitleById').mockImplementation(async () => {
+        return {
+          message: 'Error',
+          errorCode: 'Error',
+          status: 400,
+        };
+      });
+
+      const response = await titleActions.getTitleById('123');
+      expect(response).toEqual({
+        message: 'Error',
+        errorCode: 'Error',
+        status: 400,
+      });
+    });
+  });
+
+  describe('deleteTitleById', () => {
+    it('should return response when API call is successful', async () => {
+      const mockResponse = { message: 'Title deleted successfully' };
+      jest.spyOn(titleActions, 'deleteTitleById').mockResolvedValue(mockResponse);
+
+      const response = await titleActions.deleteTitleById('123');
+      expect(response).toEqual(mockResponse);
+    });
+
+    it('should return error object when API call fails', async () => {
+      jest.spyOn(titleActions, 'deleteTitleById').mockImplementation(async () => {
+        return {
+          message: 'Error',
+          errorCode: 'Error',
+          status: 400,
+        };
+      });
+
+      const response = await titleActions.deleteTitleById('123');
+      expect(response).toEqual({
+        message: 'Error',
+        errorCode: 'Error',
+        status: 400,
+      });
     });
   });
 });
