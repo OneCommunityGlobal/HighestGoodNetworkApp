2f81df03533410f71e67e73acc30fda6e6a64c83
Jatin Agrawal <jatinagrawal0801@gmail.com>
2025-01-24 14:58:56 -0800

    made changes to suggestedprofilemodal.js

--- src/components/UserProfile/UserProfileModal/suggestedProfileModal.js
+++ src/components/UserProfile/UserProfileModal/suggestedProfileModal.js
@@ -4,63 +4,20 @@ import './UserProfileModal.css';  // For custom styling
 import axios from "axios";
 import { ENDPOINTS } from "utils/URL";
 import { toast } from "react-toastify";
-import { useEffect } from "react";
 
 const ProfileImageModal = ({ isOpen, toggleModal, userProfile }) => {
 
   const [selectedImage, setSelectedImage] = useState(null);
   const suggestedProfilePics=userProfile.suggestedProfilePics;
-  const [allImages,setAllImages]=useState(suggestedProfilePics);
-  const [flag,setFlag]=useState(false)
-  // Function to handle image selection
-  
   const handleImageSelect = (image) => {
     setSelectedImage(image);  // Store the selected image info
   };
-  
-  
-  async function imageUrlToPngBase64(url) {
-    try {
-      // Fetch the image as a buffer
-      const response = await axios.get(url, { responseType: "arraybuffer" });
-      
-      if (response.status !== 200) {
-        throw new Error(`Failed to fetch the image: ${response.statusText}`);
-      }
-      
-      const imageBuffer = Buffer.from(response.data);
-      
-      // Convert the image to PNG format using sharp
-      const pngBuffer = await sharp(imageBuffer).png().toBuffer();
-      
-      // Convert the PNG buffer to a base64 string
-      const base64Png = pngBuffer.toString("base64");
-      
-      return `data:image/png;base64,${base64Png}`;;
-    } catch (error) {
-      console.error(`An error occurred: ${error.message}`);
-      return null;
-    }
-  }
-
-  useEffect(()=>{
-    if(suggestedProfilePics.length!==0 && allImages[0].nitro_src===undefined){
-      console.log("Inside");
-      var values=allImages;
-      for(let i=0;i<values.length;i++){
-        imageUrlToPngBase64(values[i].src).then((resolve)=>{
-          values[i].nitro_src=resolve;
-        })
-      }
-      setAllImages(values)
-      setFlag(true)
-    }
-  },[allImages])
 
   const updateProfileImage= async ()=>{
     try {
-        await axios.put(ENDPOINTS.USERS_UPDATE_PROFILE_FROM_WEBSITE,{'selectedImage':selectedImage,'user_id':userProfile._id})
-        toast.success("Profile Image Updated")
+      let image=selectedImage.nitro_src!==undefined && selectedImage.nitro_src!==null?selectedImage.nitro_src:selectedImage.src;
+      await axios.put(ENDPOINTS.USERS_UPDATE_PROFILE_FROM_WEBSITE,{'selectedImage':image,'user_id':userProfile._id})
+      toast.success("Profile Image Updated")
     }    
      catch (error) {
         console.log(error)
@@ -79,8 +36,7 @@ const ProfileImageModal = ({ isOpen, toggleModal, userProfile }) => {
               className={`suggestedProfileTile ${selectedImage === image ? 'selected' : ''}`}
               onClick={() => handleImageSelect(image)}
             >
-              <img src={flag?image.nitro_src:image.nitro_src} alt={image.alt}  />
-              {/* <p>{image.title!==undefined && image.title.trim()!==""?image.title:image.alt.split(" ").slice(0, 3).join(" ")}</p> */}
+              <img src={image.nitro_src!==undefined && image.nitro_src!==null?image.nitro_src:image.src} alt={image.alt} />
             </div>
           ))}
         </div>
