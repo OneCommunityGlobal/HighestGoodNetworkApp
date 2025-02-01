69f3f954c67a88d8ddb5e1c6002d3dbcba3aae49
honglinchen0524 <chen.hongli@northeastern.edu>
2025-01-31 20:54:51 -0500

    partially fixed author display issue

--- src/components/BMDashboard/LessonList/LessonCard.jsx
+++ src/components/BMDashboard/LessonList/LessonCard.jsx
@@ -144,13 +144,15 @@ function LessonCard({ filteredLessons, onEditLessonSummary, onDeliteLessonCard, 
             </Card.Body>
             <Card.Footer className=" lesson-card-footer text-muted">
               <div>
-                <span className="footer-items-author-and-from">Author: {lesson.author.name}</span>
                 <span className="footer-items-author-and-from">
-                  From: {lesson.relatedProject.name}
+                  Author: {lesson.author || 'Unknown'}
+                </span>
+                <span className="footer-items-author-and-from">
+                  From: {lesson.relatedProject || 'Unknown Project'}
                 </span>
               </div>
               <div className="lesson-card-footer-items">
-                {currentUserId === lesson.author.id && (
+                {currentUserId === lesson.author && (
                   <div>
                     <button
                       className="text-muted"
