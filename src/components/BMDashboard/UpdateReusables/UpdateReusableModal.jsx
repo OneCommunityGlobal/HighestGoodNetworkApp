60439d84d895cf052e292d4614759c13c87356b8
ddshah878 <dhrumil878@gmail.com>
2025-02-10 14:19:01 -0600

    Dhrumil-Update-Reusable-Form

--- src/components/BMDashboard/UpdateReusables/UpdateReusableModal.jsx
+++ src/components/BMDashboard/UpdateReusables/UpdateReusableModal.jsx
@@ -1,7 +1,7 @@
+import { useState } from 'react';
 import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
 import UpdateReusable from './UpdateReusable';
 import './UpdateReusable.css';
-import { useState } from 'react';
 
 function UpdateReusableModal({ modal, setModal, record }) {
   const [cancelSignal, setCancelSignal] = useState(0);
@@ -16,12 +16,7 @@ function UpdateReusableModal({ modal, setModal, record }) {
       <Modal isOpen={modal} size="md" toggle={toggle}>
         <ModalHeader toggle={toggle}>Update Reusable Form</ModalHeader>
         <ModalBody>
-          <UpdateReusable
-            record={record}
-            setModal={setModal}
-            bulk={false}
-            cancel={cancelSignal}
-          />
+          <UpdateReusable record={record} setModal={setModal} bulk={false} cancel={cancelSignal} />
         </ModalBody>
         <ModalFooter>
           <Button color="secondary" onClick={toggle}>
