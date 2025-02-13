784b69857083358dc3d8f5b59d0ca4070114c5aa
Anjali Maddila <anjalimaddila02@gmail.com>
2025-02-12 18:51:23 -0600

    updated the materials table to reflect changes

--- src/actions/bmdashboard/materialsActions.js
+++ src/actions/bmdashboard/materialsActions.js
@@ -75,13 +75,24 @@ export const fetchAllMaterials = () => {
   return async dispatch => {
     axios.get(ENDPOINTS.BM_MATERIALS)
       .then(res => {
-        dispatch(setMaterials(res.data))
+        // Calculate stockAvailable for all materials
+        const updatedMaterials = res.data.map(material => ({
+          ...material,
+          stockAvailable: material.stockBought - material.stockUsed - material.stockWasted
+        }));
+
+        // Log each material to see where the issue is
+        console.log("Updated Materials Data:", updatedMaterials);
+
+        dispatch(setMaterials(updatedMaterials));
       })
       .catch(err => {
-        dispatch(setErrors(err))
-      })
+        dispatch(setErrors(err));
+      });
   }
-}
+};
+
+
 
 export const postMaterialUpdate = (payload) => {
   return async dispatch => {
