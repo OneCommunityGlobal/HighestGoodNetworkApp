59054fee59efaaffbeefd5fca139f96cc0fd4ae4
fangedShadow <bhavpreet666singh.com>
2025-02-24 19:24:18 -0500

    changing structure of page

--- src/components/LBDashboard/ListOverview/ListOverview.jsx
+++ src/components/LBDashboard/ListOverview/ListOverview.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import './Listoverview.css';
 import logo from '../../../assets/images/logo2.png';
+import mapIcon from '../../../assets/images/mapIcon.png';
 
 function ListOverview() {
   const [listing, setListing] = React.useState({});
@@ -11,7 +12,8 @@ function ListOverview() {
       'https://nikonrumors.com/wp-content/uploads/2014/03/Nikon-1-V3-sample-photo.jpg',
       'https://photographylife.com/wp-content/uploads/2023/05/Nikon-Z8-Official-Samples-00002.jpg',
     ],
-    description: 'Description',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam vitae, ex officiis iusto, quos eveniet consequatur minima nesciunt, doloremque cupiditate totam at quia asperiores rem reprehenderit explicabo perferendis aliquam tempora!',
     unitAmenities: ['Amenity x', 'Amenity y'],
     villageAmenities: ['Amenity c', 'Amenity v'],
     location: 'Location',
@@ -38,7 +40,7 @@ function ListOverview() {
 
             <div className="amenities">
               <div>
-                <h2>Unit Amenities</h2>
+                <h2>Available amenities in this unit:</h2>
                 <ol>
                   {listing.unitAmenities?.map(amenity => (
                     <li key={amenity}>{amenity}</li>
@@ -46,7 +48,7 @@ function ListOverview() {
                 </ol>
               </div>
               <div>
-                <h2>Village Amenities</h2>
+                <h2>Village level amenities:</h2>
                 <ol>
                   {listing.villageAmenities?.map(amenity => (
                     <li key={amenity}>{amenity}</li>
@@ -55,7 +57,8 @@ function ListOverview() {
               </div>
             </div>
             <div className="location">
-              <p>{listing.location}</p>
+              <img src={mapIcon} alt="Map Icon" />
+              <a href="/">{listing.location}</a>
             </div>
           </div>
           <div className="details-right">
