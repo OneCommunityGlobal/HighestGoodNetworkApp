2d29d4c882f4b8cb8ca0a09b9f5db5a71d331c98
fangedShadow <bhavpreet666singh.com>
2025-03-19 20:26:59 -0400

    map feature is complete

--- src/components/LBDashboard/Map/MasterPlan/MasterPlan.jsx
+++ src/components/LBDashboard/Map/MasterPlan/MasterPlan.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useHistory } from 'react-router';
 import logo from '../../../../assets/images/logo2.png';
 import mastermap from '../../../../assets/images/masterMap.png';
 import mapRouter from '../../../../assets/images/routeMarker.png';
@@ -10,6 +11,8 @@ const villages = [
     id: 0,
     name: 'Duplicable City Center',
     short: 'CC',
+    description:
+      'The Duplicable City Center will be the largest open source/DIY structure in the world. As part of One Community it will be a diversely functional, ultra-eco-friendly (LEED Platinum Certifiable), space and resource saving community center designed to be replicated.',
     url:
       'https://onecommunityglobal.org/wp-content/uploads/2018/02/Duplicable-City-Center-PlanRender_640x335.jpg',
     position: { top: '48%', left: '49.75%' },
@@ -18,6 +21,8 @@ const villages = [
     id: 1,
     name: 'Earthbag Village',
     short: 'Earthbag',
+    description:
+      'The Earthbag Village consists of seventy-eight 150-200 square foot (14-18.6 sq meter) earthbag hotel room styled cabanas plus four communal eco-shower structures, 2 vermiculture waste processing toilet structures, two net-zero water use toilet structures, and the central Tropical Atrium.',
     url:
       'https://onecommunityglobal.org/wp-content/uploads/2018/10/Earthbag-Village-640x335-render.jpg',
     position: { top: '45%', left: '41.1%' },
@@ -26,6 +31,8 @@ const villages = [
     id: 2,
     name: 'Straw Bale Village',
     short: 'Straw',
+    description:
+      'The Straw Bale Village consists of fifty-two 250-300 square foot (23-28 sq meters) studio-style rooms, each with an attached bathroom. They are arranged in groups of 4 that can easily be connected and or converted to create multi-room units.',
     url:
       'https://onecommunityglobal.org/wp-content/uploads/2011/09/Straw-Bale-Village-PlanRender_640x335-1.png',
     position: { top: '75.75%', left: '68%' },
@@ -33,6 +40,8 @@ const villages = [
   {
     id: 3,
     name: 'Cob Village',
+    description:
+      'Cob is an ancient building material composed of dirt, straw, and water that may have been used for construction since prehistoric times. Some of the oldest man-made structures in Afghanistan are composed of rammed earth and cob and still standing! ',
     short: 'Cob',
     url:
       'https://onecommunityglobal.org/wp-content/uploads/2011/09/Cob-Village-PlanRender_640x335.png',
@@ -42,6 +51,8 @@ const villages = [
     id: 4,
     name: 'Earth Block Village',
     short: 'Block',
+    description:
+      'Compressed earth blocks (CEBs) or pressed earth blocks are damp soil compressed at high pressure to form blocks. If the blocks are stabilized with a chemical binder such as Portland Cement they are called compressed stabilized earth blocks (CSEBs) or stabilized earth blocks (SEBs).',
     url: 'https://onecommunityglobal.org/wp-content/uploads/2015/02/P4-Plan-Render_640x335.jpg',
     position: { top: '112.5%', left: '75%' },
   },
@@ -49,6 +60,8 @@ const villages = [
     id: 5,
     name: 'Shipping Container Village',
     short: 'Container',
+    description:
+      'The Shipping Container Village is planned as a semi-subterranean 3-level village constructed using shipping containers. It will provide 36 living units and 18 additional common spaces.',
     url:
       'https://onecommunityglobal.org/wp-content/uploads/2011/09/Shipping-Container-Village-PlanRender_640x335.jpg',
     position: { top: '107.25%', left: '53.75%' },
@@ -57,6 +70,8 @@ const villages = [
     id: 6,
     name: 'Recycled Materials Village',
     short: 'Recycle',
+    description:
+      'The Recycled Materials Village (Pod 6) will be open source shared to demonstrate how to build safely, affordably, and efficiently with maximal use of reclaimed/recycled materials. The design of the Recycled Materials Village is an earthship-inspired semi-subterranean design that will provide 47 living units and 14 common areas.',
     url:
       'https://onecommunityglobal.org/wp-content/uploads/2018/06/Recycled-Materials-Village-PlanRender_640x335-updated.jpg',
     position: { top: '50.5%', left: '66.75%' },
@@ -65,6 +80,8 @@ const villages = [
     id: 7,
     name: 'Tree House Village',
     short: 'Treehouse',
+    description:
+      'The Tree House Village will be a community living model in the trees. It plans to show how a Tree House Village, or off-ground and low-footprint/low-impact housing, can be a viable approach to sustainable living.',
     url:
       'https://onecommunityglobal.org/wp-content/uploads/2014/01/Tree-House-Village-PlanRender_640x335.jpg',
     position: { top: '68.25%', left: '93%' },
@@ -72,13 +89,21 @@ const villages = [
 ];
 function MasterPlan() {
   const [selectedVillage, setSelectedVillage] = useState(null);
+  const router = useHistory();
 
   const handleVillageClick = village => {
     setSelectedVillage(village);
+    if (selectedVillage === village) {
+      router.push(`/master-plan/${village.id}`);
+    }
+  };
+
+  const handleOutsideClick = () => {
+    setSelectedVillage(null);
   };
 
   return (
-    <div className="main-container">
+    <div className="main-container" onClick={handleOutsideClick}>
       <div className="logo-container">
         <img src={logo} alt="One Community Logo" />
       </div>
@@ -100,7 +125,10 @@ function MasterPlan() {
                       className="village-marker"
                       type="button"
                       aria-label={`Marker for ${v.name}`}
-                      onClick={() => handleVillageClick(v)}
+                      onClick={e => {
+                        e.stopPropagation();
+                        handleVillageClick(v);
+                      }}
                     />
                   ))}
                   <img
@@ -117,20 +145,36 @@ function MasterPlan() {
               </div>
               <div className="route">
                 <img src={mapRouter} alt="Route Marker" />
+                <p>
+                  Click on the village marker or on the village to select a village and view more
+                  details.
+                </p>
+                <p>Double Click to view the village Page.</p>
               </div>
             </div>
             <div className="villages">
               {villages.map(v => (
                 <div
                   key={v.id}
-                  className={`${selectedVillage === v ? 'selected ' : ''} 'village`}
+                  className={`${selectedVillage === v ? 'selected ' : ''} village`}
                   style={{ cursor: 'pointer', padding: '0 10px', textAlign: 'center' }}
-                  onClick={() => handleVillageClick(v)}
+                  onClick={e => {
+                    e.stopPropagation();
+                    handleVillageClick(v);
+                  }}
                 >
                   <img src={v.url} alt={v.name} />
                 </div>
               ))}
             </div>
+          </div>
+          <div className="village-details">
+            {selectedVillage && (
+              <div className="village-details-content">
+                <h3>{selectedVillage.name}</h3>
+                <p>{selectedVillage.description}</p>
+              </div>
+            )}
           </div>
         </div>
       </div>
