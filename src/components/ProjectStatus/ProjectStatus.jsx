208605c13c637000d148276ab699784098e4f62a
barnaboss <barnaboss135@gmail.com>
2025-03-25 21:49:52 -0700

    Reapplied changes for donut chart visualization and project status.

--- src/components/ProjectStatus/ProjectStatus.jsx
+++ src/components/ProjectStatus/ProjectStatus.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useState, useEffect } from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
