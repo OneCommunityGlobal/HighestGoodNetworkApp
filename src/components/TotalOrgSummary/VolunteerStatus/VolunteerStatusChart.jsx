81a3cab87f79fe53f40c6d711fbf77e3b4bd90ed
sidhu47 <sidhuemailid@gmail.com>
2025-04-02 16:41:21 -0400

    Adding dynamic comparision text field to Volunteer Status Chart

--- src/components/TotalOrgSummary/VolunteerStatus/VolunteerStatusChart.jsx
+++ src/components/TotalOrgSummary/VolunteerStatus/VolunteerStatusChart.jsx
@@ -2,7 +2,7 @@ import { useMemo } from 'react';
 import Loading from 'components/common/Loading';
 import VolunteerStatusPieChart from './VolunteerStatusPieChart';
 
-function VolunteerStatusChart({ isLoading, volunteerNumberStats }) {
+function VolunteerStatusChart({ isLoading, volunteerNumberStats, comparisonType }) {
   const chartData = useMemo(() => {
     if (!volunteerNumberStats) {
       return null;
@@ -35,7 +35,7 @@ function VolunteerStatusChart({ isLoading, volunteerNumberStats }) {
           </div>
         </div>
       ) : (
-        <VolunteerStatusPieChart data={chartData} />
+        <VolunteerStatusPieChart data={chartData} comparisonType={comparisonType} />
       )}
     </section>
   );
