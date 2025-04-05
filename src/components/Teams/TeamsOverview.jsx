970711725cd43f3d38d20e072c787ea768164377
Angad Anil Gosain <angadgosain@gmail.com>
2025-04-04 20:58:25 -0700

    Made changes for unit tests.

--- src/components/Teams/TeamsOverview.jsx
+++ src/components/Teams/TeamsOverview.jsx
@@ -9,7 +9,7 @@ function TeamsOverview({ numberOfTeams, numberOfActiveTeams }) {
       {/* Total Teams Card */}
       <div className="card" id="card_team" data-testid="card_team">
         <div className="card-body">
-          <h6 className="card-text">
+          <h6 className="card-text" id="total_teams" data-testid="total_teams">
             <i className="fa fa-users" aria-hidden="true" /> {TOTAL_TEAMS}: {numberOfTeams}
           </h6>
         </div>
@@ -18,7 +18,7 @@ function TeamsOverview({ numberOfTeams, numberOfActiveTeams }) {
       {/* Active Teams Card */}
       <div className="card" id="card_active" data-testid="card_active">
         <div className="card-body">
-          <h6 className="card-text">
+          <h6 className="card-text" id="active_teams" data-testid="active_teams">
             <i className="fa fa-circle fa-circle-isActive" aria-hidden="true" /> {ACTIVE_TEAMS}:{' '}
             {numberOfActiveTeams}
           </h6>
