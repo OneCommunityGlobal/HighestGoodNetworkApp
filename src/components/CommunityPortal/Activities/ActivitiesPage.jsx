2d23591d5d022b20ce0623f1f9b570e2e719c903
SharadhaS24 <sharadhashivakumar04@gmail.com>
2025-04-16 23:56:30 -0700

    Update ActivitiesPage.jsx

--- src/components/CommunityPortal/Activities/ActivitiesPage.jsx
+++ src/components/CommunityPortal/Activities/ActivitiesPage.jsx
@@ -1,29 +1,28 @@
-import React from "react";
-import RegistrationForm from "./RegistrationForm";
-import ResourceMonitoring from "./ResourceMonitoring";
-import LatestRegistration from "./LatestRegistration";
-import MyEvent from "./MyEvent";
-import "./styles.css";
+import RegistrationForm from './RegistrationForm';
+import ResourceMonitoring from './ResourceMonitoring';
+import LatestRegistration from './LatestRegistration';
+import MyEvent from './MyEvent';
+import './styles.css';
 
-const ActivitiesPage = () => {
-    return (
-        <div className="activities-page">
-            <header className="header">
-                <h1>Event Registrations</h1>
-            </header>
+function ActivitiesPage() {
+  return (
+    <div className="activities-page">
+      <header className="header">
+        <h1>Event Registrations</h1>
+      </header>
 
-            <ResourceMonitoring />
+      <ResourceMonitoring />
 
-            <div className="middle-section">
-                <RegistrationForm />
-            </div>
+      <div className="middle-section">
+        <RegistrationForm />
+      </div>
 
-            <div className="main-content">
-                <LatestRegistration />
-                <MyEvent />
-            </div>
-        </div>
-    );
-};
+      <div className="main-content">
+        <LatestRegistration />
+        <MyEvent />
+      </div>
+    </div>
+  );
+}
 
 export default ActivitiesPage;
