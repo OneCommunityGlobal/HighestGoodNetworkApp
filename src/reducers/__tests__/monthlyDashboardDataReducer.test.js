4a4ee0b50f39b204272580343e52c7bbfbcaf12d
Ankuriboh <183397864+Ankuriboh@users.noreply.github.com>
2025-05-13 09:55:34 -0700

    fix(lint): fix lint issues
    can't you guys lint before you commit???

--- src/reducers/__tests__/monthlyDashboardDataReducer.test.js
+++ src/reducers/__tests__/monthlyDashboardDataReducer.test.js
@@ -1,48 +1,46 @@
-import { monthlyDashboardDataReducer } from '../monthlyDashboardDataReducer';
+import { monthlyDashboardDataReducer } from "../monthlyDashboardDataReducer";
 
 const monthlyDashboardData = {
-  projectName: '',
+  projectName: "",
   timeSpent_hrs: 0,
 };
 
-describe('Monthly Dashboard Data Reducer', () => {
-
-  it('get monthly dashboard data', () => {
-
-    const newPayload = { projectName: 'Project Name', timeSpent_hrs: '0' };
+describe("Monthly Dashboard Data Reducer", () => {
+  it("get monthly dashboard data", () => {
+    const newPayload = {
+      projectName: "Project Name",
+      timeSpent_hrs: "0",
+    };
 
     const action = {
-      type: 'GET_MONTHLY_DASHBOARD_DATA',
+      type: "GET_MONTHLY_DASHBOARD_DATA",
       payload: newPayload,
     };
 
     const result = monthlyDashboardDataReducer(monthlyDashboardData, action);
     expect(result).toEqual(newPayload);
-
   });
 
-  it('should return the initial state when an action type is not passed', () => {
-
+  it("should return the initial state when an action type is not passed", () => {
     const result = monthlyDashboardDataReducer(monthlyDashboardData, {});
     expect(result).toEqual(monthlyDashboardData);
-
   });
 
-  it('should return null as the initial state', () => {
-
+  it("should return null as the initial state", () => {
     const result = monthlyDashboardDataReducer(undefined, {});
     expect(result).toBeNull();
-
   });
 
-  it('should return previous state if action type is unknown', () => {
+  it("should return previous state if action type is unknown", () => {
     const action = {
-      type: 'UNKNOWN_ACTION',
-      payload: { projectName: 'Project Name', timeSpent_hrs: '0' }
+      type: "UNKNOWN_ACTION",
+      payload: {
+        projectName: "Project Name",
+        timeSpent_hrs: "0",
+      },
     };
 
     const result = monthlyDashboardDataReducer(monthlyDashboardData, action);
     expect(result).toEqual(monthlyDashboardData);
   });
-
-})
+});
