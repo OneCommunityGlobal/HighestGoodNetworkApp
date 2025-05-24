e72de2a6f7f1da4d626639a7ffcb3921b6147768
Rahul Prasad <rahulprasad2823@gmail.com>
2025-05-23 21:14:48 -0400

    SetUpFinalDayPopUp.test.js

--- SetUpFinalDayPopUp.test.js
+++ SetUpFinalDayPopUp.test.js
@@ -1,27 +1,27 @@
-
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import moment from 'moment';
-import SetUpFinalDayPopUp from '../../components/UserManagement/SetUpFinalDayPopUp';
+import SetUpFinalDayPopUp from '../../components/UserManagement/SetUpFinalDayPopUp.jsx';
 
 describe('SetUpFinalDayPopUp', () => {
   it('renders without crashing', () => {
-    render(<SetUpFinalDayPopUp open onClose={() => { }} onSave={() => { }} />);
+    render(<SetUpFinalDayPopUp open onClose={() => {}} onSave={() => {}} />);
   });
 
   it('calls onClose when close button is clicked', () => {
     const onCloseMock = jest.fn();
-    const { getByText } = render(<SetUpFinalDayPopUp open onClose={onCloseMock} onSave={() => { }} />);
+    const { getByText } = render(
+      <SetUpFinalDayPopUp open onClose={onCloseMock} onSave={() => {}} />
+    );
 
     fireEvent.click(getByText('Close'));
-
     expect(onCloseMock).toHaveBeenCalled();
   });
 
   it('calls onSave with correct date when Save button is clicked with a future date', () => {
     const onSaveMock = jest.fn();
     const { getByText, getByTestId } = render(
-      <SetUpFinalDayPopUp open onClose={() => { }} onSave={onSaveMock} />
+      <SetUpFinalDayPopUp open onClose={() => {}} onSave={onSaveMock} />
     );
 
     const futureDate = moment().add(1, 'days').format('YYYY-MM-DD');
@@ -34,7 +34,7 @@ describe('SetUpFinalDayPopUp', () => {
   it('shows date error when Save button is clicked with a past date', () => {
     const onSaveMock = jest.fn();
     const { getByText, getByTestId, getByRole } = render(
-      <SetUpFinalDayPopUp open onClose={() => { }} onSave={onSaveMock} />
+      <SetUpFinalDayPopUp open onClose={() => {}} onSave={onSaveMock} />
     );
 
     const pastDate = moment().subtract(1, 'days').format('YYYY-MM-DD');
