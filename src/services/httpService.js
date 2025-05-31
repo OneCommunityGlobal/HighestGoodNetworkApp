518f0065f84a22473fd302f312d216d6e4ea9390
zhifanjia <allenjia98@gmail.com>
2025-05-31 17:57:21 -0400

    fixed unit test error Cannot read property 'response' of undefined

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -6,13 +6,15 @@ if (axios.defaults && axios.defaults.headers && axios.defaults.headers.post) {
   axios.defaults.headers.post['Content-Type'] = 'application/json';
 }
 
-axios.interceptors.response.use(null, error => {
-  if (!(error.response && error.response.status >= 400 && error.response.status <= 500)) {
-    logService.logError(error);
-    toast.error('Unexpected error');
-  }
-  return Promise.reject(error);
-});
+if (axios.interceptors && axios.interceptors.response && axios.interceptors.response.use) {
+  axios.interceptors.response.use(null, error => {
+    if (!(error.response && error.response.status >= 400 && error.response.status <= 500)) {
+      logService.log(error);
+      toast.error('An unexpected error occurred.');
+    }
+    return Promise.reject(error);
+  });
+}
 
 function setjwt(jwt) {
   if (jwt) {
