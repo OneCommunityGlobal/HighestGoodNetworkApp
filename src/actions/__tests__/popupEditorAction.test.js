010dfc0a871cea2c91162573c39bb8b0cd939aa6
Nikhil Pittala <nikhilpittala7@gmail.com>
2025-06-07 19:13:12 -0400

    solved the errors

--- src/actions/__tests__/popupEditorAction.test.js
+++ src/actions/__tests__/popupEditorAction.test.js
@@ -3,7 +3,6 @@ import thunk from 'redux-thunk';
 import axios from 'axios';
 import * as actions from '../popupEditorAction';
 import * as types from '../../constants/popupEditorConstants';
-import { ENDPOINTS } from '../../utils/URL';
 
 jest.mock('axios');
 
@@ -15,6 +14,7 @@ describe('Popup Editor Actions', () => {
 
   beforeEach(() => {
     store = mockStore({});
+    jest.clearAllMocks();
   });
 
   it('should dispatch RECEIVE_POPUP on successful fetchAllPopupEditor', async () => {
@@ -28,7 +28,18 @@ describe('Popup Editor Actions', () => {
     ]);
   });
 
- 
+  it('should dispatch FETCH_POPUP_ERROR on failed fetchAllPopupEditor', async () => {
+    axios.get.mockRejectedValue(new Error('Fetch failed'));
+  
+    await store.dispatch(actions.fetchAllPopupEditor());
+  
+    // Wait a tick to allow the .catch to run
+    await new Promise(resolve => setTimeout(resolve, 0));
+  
+    expect(store.getActions()).toEqual([
+      { type: types.FETCH_POPUP_ERROR, err: undefined },
+    ]);
+  });
 
   it('should dispatch RECEIVE_POPUP on successful updatePopupEditor', async () => {
     const popupId = 1;
@@ -46,6 +57,16 @@ describe('Popup Editor Actions', () => {
     ]);
   });
 
+  it('should dispatch FETCH_POPUP_ERROR on failed updatePopupEditor', async () => {
+    axios.post.mockRejectedValue(new Error('Update failed'));
+
+    await store.dispatch(actions.updatePopupEditor(1, 'Content', 'Name'));
+
+    expect(store.getActions()).toEqual([
+      { type: types.FETCH_POPUP_ERROR, err: undefined },
+    ]);
+  });
+
   it('should dispatch CURRENT_POPUP on successful getPopupById', async () => {
     const popupId = 1;
     const mockData = { id: popupId, popupContent: 'Test', popupName: 'Popup' };
@@ -60,7 +81,35 @@ describe('Popup Editor Actions', () => {
 
   it('should dispatch FETCH_POPUP_ERROR on failed getPopupById', async () => {
     axios.get.mockRejectedValue(new Error('Fetch error'));
+
     await store.dispatch(actions.getPopupById(1));
+
+    expect(store.getActions()).toEqual([
+      { type: types.FETCH_POPUP_ERROR, err: undefined },
+    ]);
+  });
+
+  it('should dispatch CURRENT_POPUP on successful backupPopupEditor', async () => {
+    const popupId = 1;
+    const popupContent = 'Backup Content';
+    const popupName = 'Backup Name';
+    const mockData = [{ id: popupId, popupContent, popupName }];
+
+    axios.post.mockResolvedValue({});
+    axios.get.mockResolvedValue({ data: mockData });
+
+    await store.dispatch(actions.backupPopupEditor(popupId, popupContent, popupName));
+
+    expect(store.getActions()).toEqual([
+      { type: types.CURRENT_POPUP, currPopup: mockData },
+    ]);
+  });
+
+  it('should dispatch FETCH_POPUP_ERROR on failed backupPopupEditor', async () => {
+    axios.post.mockRejectedValue(new Error('Backup failed'));
+
+    await store.dispatch(actions.backupPopupEditor(1, 'Content', 'Name'));
+
     expect(store.getActions()).toEqual([
       { type: types.FETCH_POPUP_ERROR, err: undefined },
     ]);
