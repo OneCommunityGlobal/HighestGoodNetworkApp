e0f1208353916d3fecea57fc1fbd579d56a46a64
Casstiel <cpss5433@gmail.com>
2025-06-13 20:54:48 -0700

    fix prettier and ESLint issues

--- src/components/common/GoogleDocIcon/GoogleDocIcon.jsx
+++ src/components/common/GoogleDocIcon/GoogleDocIcon.jsx
@@ -3,10 +3,10 @@ import googleDocIconPng from '../../../assets/images/google_doc_icon.png';
 import './style.css';
 
 export default function GoogleDocIcon({ link }) {
-  const handleGoogleDocClick = (e) => {
+  const handleGoogleDocClick = e => {
     const toastGoogleLinkDoesNotExist = 'toast-on-click';
     if (!link) {
-      e.preventDefault(); //prevent browser default behavior
+      e.preventDefault(); // prevent browser default behavior
       toast.error(
         'Uh oh, no Google Doc is present for this user! Please contact an Admin to find out why.',
         {
@@ -22,10 +22,10 @@ export default function GoogleDocIcon({ link }) {
     <a
       href={link || '#'}
       onClick={handleGoogleDocClick}
-      onAuxClick={handleGoogleDocClick} //add onAuxClick to handle middle click(mouse wheel)
-      target='_blank'
-      rel='noopener noreferrer'
-      className='team-member-tasks-user-report-link'
+      onAuxClick={handleGoogleDocClick} // add onAuxClick to handle middle click(mouse wheel)
+      target="_blank"
+      rel="noopener noreferrer"
+      className="team-member-tasks-user-report-link"
     >
       <img
         className={`google-doc-icon ${link ? '' : 'inactive'}`}
