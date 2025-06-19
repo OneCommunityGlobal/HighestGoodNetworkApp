bbdfe0d8e19f5af71827eb591b4910647a67bbef
CodeWithKoushica <koushica.bu01@gmail.com>
2025-06-19 14:41:40 -0500

    lint issue fixes

--- src/components/TeamMemberTasks/TeamMemberTasks.jsx
+++ src/components/TeamMemberTasks/TeamMemberTasks.jsx
@@ -551,14 +551,16 @@ const TeamMemberTasks = React.memo(props => {
                   }`}
                   title={`Timelogs submitted in the past ${days} days`}
                   style={{
-                    color: 
-                    selectedPeriod === days && isTimeFilterActive 
-                    ? `${darkMode ? color : 'white'}` 
-                    : `${darkMode ? 'white' : color}`,
-                    backgroundColor:
-                    selectedPeriod === days && isTimeFilterActive 
-                    ? `${darkMode ? 'white' : color}` 
-                    : `${darkMode ? color : 'white'}`,
+                    color: (
+                      selectedPeriod === days && isTimeFilterActive 
+                      ? `${darkMode ? color : 'white'}` 
+                      : `${darkMode ? 'white' : color}`
+                    ),
+                    backgroundColor: (
+                      selectedPeriod === days && isTimeFilterActive 
+                      ? `${darkMode ? 'white' : color}` 
+                      : `${darkMode ? color : 'white'}`
+                    ),
                     border: `1px solid ${color}`,
                   }}
                   onClick={() => selectPeriod(days)}
@@ -576,12 +578,16 @@ const TeamMemberTasks = React.memo(props => {
                 value={selectedPeriod || ''}
                 title={`Timelogs submitted in the past ${selectedPeriod} days`}
                 style={{
-                  color: isTimeFilterActive 
-                  ? `${darkMode ? hrsFilterBtnColorMap[selectedPeriod] : 'white'}` 
-                  : `${darkMode ? 'white' : hrsFilterBtnColorMap[selectedPeriod]}`,
-                  backgroundColor: isTimeFilterActive
+                  color: (
+                    isTimeFilterActive 
+                    ? `${darkMode ? hrsFilterBtnColorMap[selectedPeriod] : 'white'}` 
+                    : `${darkMode ? 'white' : hrsFilterBtnColorMap[selectedPeriod]}`
+                  ),
+                  backgroundColor: (
+                    isTimeFilterActive
                     ? `${darkMode ? 'white' : hrsFilterBtnColorMap[selectedPeriod]}` 
-                    : `${darkMode ? hrsFilterBtnColorMap[selectedPeriod] : '#007BFF'}`,
+                    : `${darkMode ? hrsFilterBtnColorMap[selectedPeriod] : '#007BFF'}`
+                  ),
                   border: `1px solid ${hrsFilterBtnColorMap[selectedPeriod]}`
                 }}
               >
@@ -750,10 +756,8 @@ const TeamMemberTasks = React.memo(props => {
                         Progress
                       </th>
                       {displayUser.role === 'Administrator' ? ( 
-                       <th className={darkMode ? 'bg-space-cadet' : ''}>
-                        Status
-                        </th>
-                      ): null}
+                       <th className={darkMode ? 'bg-space-cadet' : ''}>Status</th>
+                      ) : null}
                     </tr>
                   </thead>
                 </Table>
