9e7cf5409dd2e7082391fbb776349f098f62c184
Vamsi Krishna Rolla <63103226+vamsikrishna1704@users.noreply.github.com>
2025-06-25 18:20:53 -0400

    Fixed the permission logic to view task extension count

--- src/components/TeamMemberTasks/TeamMemberTask.jsx
+++ src/components/TeamMemberTasks/TeamMemberTask.jsx
@@ -90,9 +90,8 @@ const TeamMemberTask = React.memo(
     const isAllowedToResolveTasks =
       rolesAllowedToResolveTasks.includes(userRole) || dispatch(hasPermission('resolveTask'));
 
-    // this variable is used to check the permission to view task extension count for a role or user
-    const isAllowedToSeeDeadlineCount = 
-      rolesAllowedToSeeDeadlineCount.includes(userRole) || dispatch(hasPermission('viewTaskDeadlineCount'));
+    // this variable is used to check the permission to view task extension count for a user
+    const isAllowedToSeeDeadlineCount = dispatch(hasPermission('viewTaskDeadlineCount'));
 
     const canGetWeeklySummaries = dispatch(hasPermission('getWeeklySummaries'));
     const canSeeReports = rolesAllowedToResolveTasks.includes(userRole)||dispatch(hasPermission('getReports'));
