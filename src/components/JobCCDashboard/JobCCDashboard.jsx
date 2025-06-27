475a3bce8b28053c1d567414198f82785f610a69
Nikhil Routh <nrouth@vt.edu>
2025-06-26 21:25:51 -0400

    fixed lint issue

--- src/components/JobCCDashboard/JobCCDashboard.jsx
+++ src/components/JobCCDashboard/JobCCDashboard.jsx
@@ -89,7 +89,7 @@ function JobCCDashboard() {
 
   return (
     <div
-    className={`${styles.jobCcDashboard} ${darkMode ? styles.darkModeJobCcDashboard : ''}`}
+      className={`${styles.jobCcDashboard} ${darkMode ? styles.darkModeJobCcDashboard : ''}`}
       style={{ height: '100%' }}
     >
       <h1 className={`${styles.dashboardTitle}`}>Job CC Dashboard</h1>
