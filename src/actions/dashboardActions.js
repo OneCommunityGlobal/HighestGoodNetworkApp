53c63c44c853c9665773d6fa8b12adeaa762d347
zhifanjia <allenjia98@gmail.com>
2025-07-12 16:09:49 -0400

    resolved removal of task triggered both 'mark as done' and 'successfully removed user from task' message

--- src/actions/dashboardActions.js
+++ src/actions/dashboardActions.js
@@ -4,6 +4,7 @@ export const INCREMENT_DASHBOARD_TASK_COUNT = 'INCREMENT_DASHBOARD_TASK_COUNT';
 export const UPDATE_SUMMARY_BAR_DATA = 'UPDATE_SUMMARY_BAR_DATA'
 
 export const incrementDashboardTaskCount = taskId => {
+  toast.info(`Dispatching incrementDashboardTaskCount for task ID: ${taskId}`);
   return {
     type: INCREMENT_DASHBOARD_TASK_COUNT,
     payload: { taskId },
--- src/components/TeamMemberTasks/components/TaskCompletedModal.jsx
+++ src/components/TeamMemberTasks/components/TaskCompletedModal.jsx
@@ -29,6 +29,7 @@ const TaskCompletedModal = React.memo(props => {
     const updatedTask = { ...task, resources: newResources };
 
     props.updateTask(task._id, updatedTask);
+    toast.success('Task is successfully marked as done.');
 
     if (props.setUpdatedTasks) {
       props.setUpdatedTasks(prevTasks =>
