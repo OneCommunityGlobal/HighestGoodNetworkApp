4e0a5c8f9b05b2bd1458bcefe98165327457b188
Gopikalakshmi <gopikalakshmia@gmail.com>
2025-08-01 22:42:55 -0400

    Header Updated

--- src/actions/notificationAction.js
+++ src/actions/notificationAction.js
@@ -3,7 +3,6 @@ import httpService from '../services/httpService';
 import { ApiEndpoint, ENDPOINTS } from '../utils/URL';
 
 import axios from 'axios';
-import { ApiEndpoint } from '~/utils/URL';
 import * as actionTypes from '../constants/notification';
 
 const APIEndpoint = ApiEndpoint;
