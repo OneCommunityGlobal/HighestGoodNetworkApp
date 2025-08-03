06b20320e914eb3809647a9ee3817fde6efdd535
favils <fatimavillena@gmail.com>
2025-08-02 22:42:36 -0400

    Add DistributionLaborCost component

---
+++ src/components/BMDashboard/WeeklyProjectSummary/DistributionLaborCost/DistributionLaborCost.jsx
@@ -0,0 +1,11 @@
+import { PieChart, Pie, Cell, Tooltip, Legend } from 'recharts';
+
+const COLORS = ['#003f5c', '#2f4b7c', '#ffa600', '#ff6361', '#f3e5ab'];
+
+const data = [
+  { name: 'Stud Wall Construction', value: 25.9 },
+  { name: 'Foundation Concreting', value: 18.5 },
+  { name: 'Task 1', value: 22.2 },
+  { name: 'Task 2', value: 18.5 },
+  { name: 'Task 3', value: 14.8 },
+];
