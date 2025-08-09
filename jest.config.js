ad2a39ec0b89eefdafc0a33a0311bff98e511a70
karnati007 <karng01@pfw.edu>
2025-08-10 01:43:44 +0530

    Fixed the styles for the dark mode theme

--- jest.config.js
+++ jest.config.js
@@ -37,6 +37,11 @@ module.exports = {
   // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
   testURL: 'http://localhost',
 
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.js$': 'jest-esm-transformer',
+  },
+
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: [
     '/node_modules/(?!d3|d3-array|internmap|delaunator|robust-predicates)',
