bf79db20e17b1785b611b07bd6f86b5d055e7a1d
favils <fatimavillena@gmail.com>
2025-08-09 23:01:00 -0400

    fix promotion actions import

--- src/components/HGNPRDashboard/PromotionEligibility.jsx
+++ src/components/HGNPRDashboard/PromotionEligibility.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import { FaCheck } from 'react-icons/fa';
-import { getPromotionEligibility, postPromotionEligibility } from 'actions/promotionActions';
+import { getPromotionEligibility, postPromotionEligibility } from '../../actions/promotionActions';
 import './PromotionEligibility.css';
 import { useSelector } from 'react-redux';
 
