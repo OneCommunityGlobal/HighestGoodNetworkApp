393f165d468eae0f17c1fe5f8d29f934a78ec385
Neeraj-Kondaveeti <neerajkondaveeti1901@gmail.com>
2025-08-13 20:22:39 -0400

    refactor: Made changes requested by the reviewers in the old PR

--- src/routes.jsx
+++ src/routes.jsx
@@ -211,6 +211,10 @@ const UserRoleTab = lazy(() => import('./components/PermissionsManagement/UserRo
 const Teams = lazy(() => import('./components/Teams/Teams'));
 const JobFormBuilder = lazy(() => import('./components/Collaboration/JobFormbuilder'));
 
+const JobAnalyticsPage = lazy(() =>
+  import('./components/Reports/HitsAndApplicationRatio/JobAnalyticsPage'),
+);
+
 export default (
   <Switch>
     {/* ----- LB Dashboard Routing ----- */}
@@ -334,6 +338,7 @@ export default (
           routePermissions={RoutePermissions.reports}
         />
         <ProtectedRoute path="/teamlocations" exact component={TeamLocations} />
+        <ProtectedRoute path="/job-analytics" component={JobAnalyticsPage} fallback />
         <ProtectedRoute
           path="/projects"
           exact
