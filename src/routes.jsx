9f19d235c284d62e40c165d67189285991f539ae
Meenashi <jeyanthi_sm@yahoo.com>
2025-08-14 11:03:23 +0100

    Revert "Merge branch 'development' of https://github.com/OneCommunityGlobal/HighestGoodNetworkApp into anjali-email-fix-separate"
    This reverts commit 7837e91d88603692feb9411e22d7bde7a9e7dabe, reversing
    changes made to ba2d4c285b482a054d161ef278529ee58fd6f52b.

--- src/routes.jsx
+++ src/routes.jsx
@@ -136,7 +136,6 @@ import MostSusceptibleTools from './components/MostSusceptible/toolBreakdownChar
 
 import HelpPage from './components/LandingPage/HelpPage';
 
-import TeamCard from './components/HGNHelpSkillsDashboard/TeamCard/TeamCard';
 import LandingPage from './components/HGNHelpSkillsDashboard/LandingPage';
 import SkillsOverviewPage from './components/HGNHelpSkillsDashboard/SkillsOverviewPage';
 import CommunityMembersPage from './components/HGNHelpSkillsDashboard/CommunityMembersPage';
@@ -664,8 +663,6 @@ export default (
         <Route path="/forcePasswordUpdate/:userId" component={ForcePasswordUpdate} />
         {/* ----- HGN Help Community Skills Dashboard Routes ----- */}
         <ProtectedRoute path="/hgnhelp" exact component={LandingPage} />
-        <ProtectedRoute path="/hgnteam" exact component={TeamCard} />
-
         <ProtectedRoute path="/hgnhelp/skills-overview" exact component={SkillsOverviewPage} />
         <ProtectedRoute path="/hgnhelp/community" exact component={CommunityMembersPage} />
         <ProtectedRoute path="/hgnhelp/profile/:userId" exact component={UserProfilePage} />
