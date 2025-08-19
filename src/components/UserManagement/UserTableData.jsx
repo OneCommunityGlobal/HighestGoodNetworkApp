3aa5ddf64f14fe7ee582adfecd1a56c081bb302f
Venkataramanan Venkateswaran <venk@Venkataramanans-MacBook-Air.local>
2025-08-19 14:24:09 -0700

    Fix: timelog redirection when opening on new tab in user management page

--- src/components/UserManagement/UserTableData.jsx
+++ src/components/UserManagement/UserTableData.jsx
@@ -230,12 +230,13 @@ const UserTableData = React.memo(props => {
                 return;
               }
 
+              const url = `${window.location.origin}/timelog/${props.user._id}#currentWeek`;
+
               if (e.metaKey || e.ctrlKey || e.button === 1) {
-                window.open(`/timelog/${props.user._id}#currentWeek`);
+                window.open(url, '_blank', 'noopener');
                 return;
               }
-
-              e.preventDefault(); // prevent full reload
+              e.preventDefault();
               history.push(`/timelog/${props.user._id}#currentWeek`);
             }}
           />
