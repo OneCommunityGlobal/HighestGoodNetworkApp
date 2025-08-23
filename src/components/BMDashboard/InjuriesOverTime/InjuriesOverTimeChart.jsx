69decfd804faefeb3d47f8cefcf89fea2927b980
Aayush <aayush.shetty12@gmail.com>
2025-08-23 17:43:32 -0400

    feat(ui): created actions, local states and api constants

--- src/components/BMDashboard/InjuriesOverTime/InjuriesOverTimeChart.jsx
+++ src/components/BMDashboard/InjuriesOverTime/InjuriesOverTimeChart.jsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect, useDispatch, useSelector } from 'react-redux';
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  LabelList,
+  ResponsiveContainer,
+} from 'recharts';
+import { Select, DatePicker, Spin } from 'antd';
 
-function InjuriesOverTimeChart() {
-  return <>InjuriesOverTimeChart</>;
+import axios from 'axios';
+import { fetchInjuriesOverTime } from 'actions/bmdashboard/injuryActions';
+
+function InjuriesOverTimeChart(props) {
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+
+  const [selProjects, setSelProjects] = useState([]);
+  const [dateRange, setDateRange] = useState([null, null]);
+  const [selInjTypes, setSelInjTypes] = useState([]);
+  const [selDepts, setSelDepts] = useState([]);
+  const [selSeverties, setSelSeverities] = useState([]);
+
+  useEffect(() => {
+    dispatch(
+      fetchInjuriesOverTime({
+        projectId: selProjects,
+        date: dateRange,
+        injuryType: selInjTypes,
+        department: selDepts,
+        severity: selSeverties,
+      }),
+    );
+  }, []);
+
+  return (
+    <>
+      <>InjuriesOverTimeChart</>
+    </>
+  );
 }
 
 export default InjuriesOverTimeChart;
