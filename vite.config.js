3116b9f94bb81cd13f63d940ffa3d4ae61489077
Sundar Machani <sundarmachani@Sundars-MacBook-Pro.local>
2025-08-23 15:10:25 -0500

    misc: changed application port from 5173 to 3000

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
+    server: {
+      port: 3000,
+    },
     resolve: {
       alias: {
         '~': resolve('src/'),
