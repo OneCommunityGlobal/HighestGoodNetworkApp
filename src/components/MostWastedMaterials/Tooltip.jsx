74f45b77a6236bcf21bed899025995fd21d74a3b
ShravyaKudlu <shravyakshanbhogue@gmail.com>
2025-09-10 15:36:06 -0400

    Final changes to correct the styles

--- src/components/MostWastedMaterials/Tooltip.jsx
+++ src/components/MostWastedMaterials/Tooltip.jsx
@@ -1,20 +1,12 @@
 // Custom Tooltip Component
+import styles from './Tooltip.module.css';
+
 export default function CustomTooltip({ active, payload, label }) {
   if (active && payload && payload.length) {
     return (
-      <div
-        style={{
-          backgroundColor: '#ffffff',
-          border: '1px solid #e5e7eb',
-          borderRadius: '8px',
-          boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-          padding: '12px',
-        }}
-      >
-        <p style={{ fontWeight: '500', color: '#111827', margin: '0 0 4px 0' }}>{label}</p>
-        <p style={{ fontSize: '14px', color: '#6b7280', margin: '0' }}>
-          Waste: {payload[0].value}%
-        </p>
+      <div className={`${styles.tooltipWrapper}`}>
+        <p className={`${styles.tooltipLabel}`}>{label}</p>
+        <p className={`${styles.tooltipValue}`}>Waste: {payload[0].value}%</p>
       </div>
     );
   }
