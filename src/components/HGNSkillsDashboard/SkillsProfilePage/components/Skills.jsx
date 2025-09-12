8699f0e2ec0636af111f27b2f71df244752cd794
sircarmart <carmartu7@gmail.com>
2025-09-12 14:19:05 -0700

    add dark mode styling to Skills component

--- src/components/HGNSkillsDashboard/SkillsProfilePage/components/Skills.jsx
+++ src/components/HGNSkillsDashboard/SkillsProfilePage/components/Skills.jsx
@@ -1,18 +1,30 @@
 import { useSelector } from 'react-redux';
 import styles from '../styles/Skills.module.css';
+import { useSelector } from 'react-redux';
+import { getFontColor } from '../../../../styles';
 
 function Skills({ selectedSkill, onSkillClick }) {
+  const darkMode = useSelector(state => state.theme.darkMode);
   const skills = ['Dashboard', 'Frontend', 'Backend', 'Deployment & DevOps', 'Software Practices'];
   const darkMode = useSelector(state => state?.theme?.darkMode);
 
   return (
+<<<<<<< HEAD
     <div className={`${styles.skills} ${darkMode ? styles.darkMode : ''}`}>
       <h3>Skills</h3>
+=======
+    <div className={`${styles.skills}`}>
+      <h3 className={getFontColor(darkMode)}>Skills</h3>
+>>>>>>> b850b0ee5 (add dark mode styling to Skills component)
       {skills.map(skill => (
         <button
           key={skill}
           type="button"
-          className={`${styles.skillsButton} ${selectedSkill === skill ? styles.selected : ''}`}
+          className={`
+            ${styles.skillsButton}
+            ${selectedSkill === skill ? styles.selected : getFontColor(darkMode)}
+            ${darkMode ? 'bg-yinmn-blue' : ''}
+            `}
           onClick={() => onSkillClick(skill)}
         >
           {skill}
