cbdd7afbd4ff10ead386fcb96915ffb3672d0ded
Ramaruva <ramaruva54@gmail.com>
2025-09-27 18:57:48 -0500

    bmdashboard lint fixes

--- eslint.config.js
+++ eslint.config.js
@@ -29,7 +29,7 @@ module.exports = [
       'public/**',
       'build/**',
       // Ignore test files inside /src/components
-      'src/components/BMDashboard/_tests_/BMDashboard.test.jsx',
+      // 'src/components/BMDashboard/_tests_/BMDashboard.test.jsx',
       'src/components/Reports/PeopleReport/components/PeopleTasksPieChart.test.jsx',
       // Ignore entire component folders
       'src/components/Badge/**',
