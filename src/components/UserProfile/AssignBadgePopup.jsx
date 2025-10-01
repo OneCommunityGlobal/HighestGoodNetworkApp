19332c70014dbf248d5662b882bf7474f0fd1c71
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2025-09-30 21:24:57 -0300

    peterson-fix-trim-input-prevent-empty-table

--- src/components/UserProfile/AssignBadgePopup.jsx
+++ src/components/UserProfile/AssignBadgePopup.jsx
@@ -3,14 +3,18 @@ import { Table, Button, UncontrolledTooltip } from 'reactstrap';
 import { connect } from 'react-redux';
 import axios from 'axios';
 import AssignTableRow from '../Badge/AssignTableRow';
-import { assignBadgesByUserID, clearNameAndSelected, addSelectBadge } from '../../actions/badgeManagement';
+import {
+  assignBadgesByUserID,
+  clearNameAndSelected,
+  addSelectBadge,
+} from '../../actions/badgeManagement';
 import { ENDPOINTS } from '~/utils/URL';
 import { boxStyle, boxStyleDark } from '../../styles';
 import { toast } from 'react-toastify';
 import { PROTECTED_ACCOUNT_MODIFICATION_WARNING_MESSAGE } from '~/utils/constants';
 
 function AssignBadgePopup(props) {
-  const {darkMode} = props;
+  const { darkMode } = props;
   const [searchedName, setSearchedName] = useState('');
   const [badgeList, setBadgeList] = useState([]);
   // Added state to disable confirm button while updating.
@@ -22,7 +26,7 @@ function AssignBadgePopup(props) {
 
   // Update: Added toast message effect for success and error. Added restriction: Jae's badges only editable by Jae or Owner
   const assignBadges = async () => {
-    if(props.isRecordBelongsToJaeAndUneditable){
+    if (props.isRecordBelongsToJaeAndUneditable) {
       // eslint-disable-next-line no-alert
       alert(PROTECTED_ACCOUNT_MODIFICATION_WARNING_MESSAGE);
       return;
@@ -56,13 +60,20 @@ function AssignBadgePopup(props) {
     } catch (error) {}
   };
 
- const filterBadges = (allBadges = []) => {
-   // guard against non-array inputs
-   if (!Array.isArray(allBadges)) return [];
-   return allBadges.filter(({ badgeName }) =>
-     badgeName.toLowerCase().includes(searchedName.toLowerCase())
-   );
- };
+  const formatSearchInput = text => {
+    return text
+      .toLowerCase()
+      .replace(/\s+/g, '')
+      .trim();
+  };
+
+  const filterBadges = (allBadges = []) => {
+    // guard against non-array inputs
+    if (!Array.isArray(allBadges)) return [];
+    return allBadges.filter(({ badgeName }) =>
+      formatSearchInput(badgeName).includes(formatSearchInput(searchedName)),
+    );
+  };
 
   let filteredBadges = filterBadges(badgeList);
 
@@ -77,8 +88,6 @@ function AssignBadgePopup(props) {
   };
 
   let existBadges = addExistBadges();
-
-
 
   return (
     <div data-testid="test-assignbadgepopup">
@@ -127,12 +136,12 @@ function AssignBadgePopup(props) {
       </div>
       <Button
         className="btn--dark-sea-green float-right"
-        style={darkMode ? {...boxStyleDark, margin: 5 } : { ...boxStyle, margin: 5 }}
+        style={darkMode ? { ...boxStyleDark, margin: 5 } : { ...boxStyle, margin: 5 }}
         onClick={assignBadges}
         disabled={shouldConfirmButtonDisable}
         data-testid="test-button"
       >
-        {!shouldConfirmButtonDisable ? 'Confirm' : 'Updating...'} 
+        {!shouldConfirmButtonDisable ? 'Confirm' : 'Updating...'}
       </Button>
     </div>
   );
