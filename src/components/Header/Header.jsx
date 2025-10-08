6824e01b3e12322615c6526b9a87d3dca09a94c3
somramnani <som.ramnani@gmail.com>
2025-10-08 18:19:30 -0400

    fix: update PR Team Analytics route to /pr-dashboard/analytics

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -533,7 +533,7 @@ export function Header(props) {
                         </>
                       )}
                       <DropdownItem divider />
-                      <DropdownItem tag={Link} to="/pr-dashboard/overview" className={fontColor}>
+                      <DropdownItem tag={Link} to="/pr-dashboard/analytics" className={fontColor}>
                         PR Team Analytics
                       </DropdownItem>
                       {canAccessBlueSquareEmailManagement && (
