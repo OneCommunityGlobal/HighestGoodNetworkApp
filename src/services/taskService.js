2fe4ca5cb1d90fe7b57701a48f63170431334ab6
Amalesh Arivanan <amalesh.unofficial@gmail.com>
2025-10-09 21:15:35 -0700

    Added taskService.js

---
+++ src/services/taskService.js
@@ -0,0 +1,15 @@
+import axios from './axios';
+
+export async function replicateTask({
+  taskId,
+  resourceUserIds,
+  includeAttachments = true,
+  requestor,
+}) {
+  const { data } = await axios.post(`/api/task/replicate/${taskId}`, {
+    requestor,
+    resourceUserIds,
+    includeAttachments,
+  });
+  return data;
+}
