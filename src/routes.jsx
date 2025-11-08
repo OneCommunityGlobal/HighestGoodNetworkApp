7804f332ae5dd806796fd676002de8a988901a77
Mani Shashank Marneni <93523613+shashankm9@users.noreply.github.com>
2025-11-07 21:02:33 -0600

    Clean up imports in routes.jsx
    Removed duplicate imports for Resources and EventParticipation.

--- src/routes.jsx
+++ src/routes.jsx
@@ -156,9 +156,6 @@ import Register from './components/CommunityPortal/Activities/Register/Register'
 import ActivitiesPage from './components/CommunityPortal/Activities/ActivitiesPage';
 import EventStats from './components/CommunityPortal/EventPersonalization/EventStats';
 
-import Resources from './components/CommunityPortal/Activities/activityId/Resources';
-import EventParticipation from './components/CommunityPortal/Reports/Participation/EventParticipation';
-
 // Community Calendar
 import CommunityCalendar from './components/CommunityPortal/Calendar/CommunityCalendar';
 import EPProtectedRoute from './components/common/EPDashboard/EPProtectedRoute';
