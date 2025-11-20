34794dab578a75231f0b9de9278650a47c014fa5
Deekshith Kumar <deekshithkumar.singirikonda@gmail.com>
2025-11-19 19:55:52 -0500

    Optimize the dark mode

--- src/components/BMDashboard/Tools/EquipmentUpdate.jsx
+++ src/components/BMDashboard/Tools/EquipmentUpdate.jsx
@@ -1,13 +1,22 @@
+import { useSelector } from 'react-redux';
+import { Container } from 'reactstrap';
 import EquipmentUpdateForm from './EquipmentUpdateForm';
 import './EquipmentUpdate.css';
 
 export default function EquipmentUpdate() {
+  const darkMode = useSelector(state => state.theme.darkMode);
+
   return (
-    <main className="equipment-update-container">
-      <header className="equipment-update-header">
-        <h2>Which Tool or Equipment to Update</h2>
-      </header>
-      <EquipmentUpdateForm />
-    </main>
+    <Container fluid className={darkMode ? 'bg-oxford-blue' : ''}>
+      <div className="equipment-update-page-wrapper">
+        <main className="equipment-update-container">
+          <header className="equipment-update-header">
+            <h2>Which Tool or Equipment to Update</h2>
+          </header>
+
+          <EquipmentUpdateForm />
+        </main>
+      </div>
+    </Container>
   );
 }
