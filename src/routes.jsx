c1aa96a500fa411f6d03ceb2c58dd04b4c58c245
ChiragBellara <chiragbellara7@gmail.com>
2025-12-19 10:53:08 -0800

    Fixed header dropdown. Fixed CSS. Added dark mode for usage page

--- src/routes.jsx
+++ src/routes.jsx
@@ -103,7 +103,8 @@ import ActivityAttendance from './components/CommunityPortal/Activities/Activity
 import ActivityAgenda from './components/CommunityPortal/Activities/ActivityAgenda';
 import NoshowViz from './components/CommunityPortal/Attendence/NoshowViz';
 import EventList from './components/CommunityPortal/Event/EventList/EventList';
-import Resources from './components/CommunityPortal/Activities/activityId/Resources';
+import ResourcesUsage from './components/CommunityPortal/Activities/activityId/ResourcesUsage';
+import ResourceManagement from './components/ResourceManagement/ResourceManagement';
 import EventParticipation from './components/CommunityPortal/Reports/Participation/EventParticipation';
 
 import MaterialSummary from './components/MaterialSummary/MaterialSummary';
@@ -181,7 +182,6 @@ import IntermediateTaskList from './components/EductionPortal/IntermediateTasks/
 // Social Architecture
 
 import JobApplicationForm from './components/Collaboration/JobApplicationForm/JobApplicationForm';
-const ResourceManagement = lazy(() => import('./components/ResourceManagement/ResourceManagement'));
 const RequestResources = lazy(() => import('./components/SocialArchitecture/RequestResources'));
 
 const ReusableListView = lazy(() => import('./components/BMDashboard/ReusableList'));
@@ -421,13 +421,6 @@ export default (
           component={SameFolderTasks}
           fallback
           routePermissions={RoutePermissions.workBreakdownStructure}
-        />
-        <ProtectedRoute
-          path="/communityportal/activity/:activityId/resources"
-          exact
-          component={ResourceManagement}
-          fallback
-          routePermissions={RoutePermissions.resourceManagement}
         />
         <ProtectedRoute
           path="/communityportal/resources/add"
@@ -733,6 +726,24 @@ export default (
           exact
           component={Register}
         />
+        <CPProtectedRoute path="/communityportal/ActivityAgenda" exact component={ActivityAgenda} />
+        <CPProtectedRoute
+          path="/communityportal/activity/:activityId/engagement/Comments"
+          exact
+          component={ActivityComments}
+        />
+        <CPProtectedRoute
+          path="/communityportal/activity/:activityId/resourcesusage"
+          exact
+          component={ResourcesUsage}
+        />
+        <CPProtectedRoute
+          path="/communityportal/activity/:activityId/resources"
+          exact
+          component={ResourceManagement}
+          fallback
+          routePermissions={RoutePermissions.resourceManagement}
+        />
         {/* Listing and Bidding Routes */}
         <LBProtectedRoute path="/lbdashboard" exact component={LBDashboard} />
         <LBProtectedRoute path="/lbdashboard/listOverview/:id" exact component={ListOveriew} />
@@ -781,12 +792,6 @@ export default (
           component={ResourceUsage}
         />
         {/* <BMProtectedRoute path="/bmdashboard/tools/add" exact component={AddTool} /> */}
-        <CPProtectedRoute path="/communityportal/ActivityAgenda" exact component={ActivityAgenda} />
-        <CPProtectedRoute
-          path="/communityportal/activity/:activityId/engagement/Comments"
-          exact
-          component={ActivityComments}
-        />
         {/* Temporary route to redirect all subdirectories to login if unauthenticated */}
         {/* <BMProtectedRoute path="/bmdashboard/:path" component={BMDashboard} /> */}
         {/* ----- END BM Dashboard Routing ----- */}
