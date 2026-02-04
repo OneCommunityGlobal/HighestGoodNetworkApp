f1659c8c2c9ba6efd83b209afd992860c7fa86da
Venkataramanan Venkateswaran <venk@Venkataramanans-MacBook-Air.local>
2026-02-04 15:48:26 -0800

    Venkataramanan fix: sonarqube issues

--- src/components/WeeklySummariesReport/components/WeeklySummariesToggleFilter.jsx
+++ src/components/WeeklySummariesReport/components/WeeklySummariesToggleFilter.jsx
@@ -2,9 +2,7 @@ import PropTypes from 'prop-types';
 import { Label } from 'reactstrap';
 import styles from '../WeeklySummariesReport.module.css';
 import ReactTooltip from 'react-tooltip';
-import { toggleField, setChildField } from '~/utils/stateHelper';
-// import SlideToggle from './SlideToggle';
-// import cn from 'classnames';
+import { toggleField } from '~/utils/stateHelper';
 
 export default function WeeklySummariesToggleFilter({
   state,
@@ -15,8 +13,6 @@ export default function WeeklySummariesToggleFilter({
   hasPermission,
   canSeeBioHighlight,
 }) {
-  // const safeColors = state.selectedSpecialColors || { purple: false, green: false, navy: false };
-
   const handleTrophyToggleChange = () => {
     toggleField(setState, 'selectedTrophies');
   };
@@ -29,148 +25,8 @@ export default function WeeklySummariesToggleFilter({
     toggleField(setState, 'selectedOverTime');
   };
 
-  // const handleSpecialColorToggleChange = (color, isEnabled) => {
-  //   setState(prevState => ({
-  //     ...prevState,
-  //     selectedSpecialColors: {
-  //       ...prevState.selectedSpecialColors,
-  //       [color]: isEnabled,
-  //     },
-  //   }));
-  // };
-
   return (
     <div className={`${styles.filterContainer}`}>
-      {/* {hasPermissionToFilter && (
-        <div className={`${styles.filterStyle}`}>
-          <span>Filter by Special Colors</span>
-          <div style={{ display: 'flex', alignItems: 'center', gap: '8px', marginTop: '2px' }}>
-            {['purple', 'green', 'navy'].map(color => (
-              <div key={`${color}-toggle`} style={{ display: 'flex', alignItems: 'center' }}>
-                <div className={`${styles.switchToggleControl}`}>
-                  <input
-                    type="checkbox"
-                    className={`${styles.switchToggle}`}
-                    id={`${formId}-${color}-toggle`}
-                    checked={state.selectedSpecialColors[color]}
-                    disabled={!editable}
-                    onChange={e =>
-                      setChildField(setState, 'selectedSpecialColors', color, e.target.checked)
-                    }
-                  />
-                  <Label
-                    className={`${styles.switchToggleLabel}`}
-                    for={`${formId}-${color}-toggle`}
-                  >
-                    <span className={`${styles.switchToggleInner}`} />
-                    <span className={`${styles.switchToggleSwitch}`} />
-                  </Label>
-                </div>
-                <span
-                  style={{
-                    marginLeft: '3px',
-                    fontSize: 'inherit',
-                    textTransform: 'capitalize',
-                    whiteSpace: 'nowrap',
-                    fontWeight: 'normal',
-                  }}
-                >
-                  {color}
-                </span>
-              </div>
-            ))}
-          </div>
-        </div>
-      )} */}
-      {/* {(hasPermissionToFilter || canSeeBioHighlight) && (
-        <div className={`${styles.filterStyle} ml-3`} style={{ minWidth: 'max-content' }}>
-          <span>Filter by Bio Status</span>
-          <div className={styles.switchToggleControl}>
-            <input
-              type="checkbox"
-              className={styles.switchToggle}
-              id={`${formId}-bio-status-toggle`}
-              checked={state.selectedBioStatus}
-              disabled={!editable}
-              onChange={handleBioStatusToggleChange}
-            />
-            <Label className={`${styles.switchToggleLabel}`} for={`${formId}-bio-status-toggle`}>
-              <span className={styles.switchToggleInner} />
-              <span className={styles.switchToggleSwitch} />
-            </Label>
-          </div>
-        </div>
-      )} */}
-      {/* {hasPermissionToFilter && (
-        <div className={`${styles.filterStyle}`}>
-          <span>Filter by Special Colors</span>
-          <div style={{ display: 'flex', alignItems: 'center', gap: '8px', marginTop: '2px' }}>
-            {['purple', 'green', 'navy'].map(color => (
-              <div key={`${color}-toggle`} style={{ display: 'flex', alignItems: 'center' }}>
-                <div className={`${styles.switchToggleControl}`}>
-                  <input
-                    type="checkbox"
-                    className={`${styles.switchToggle}`}
-                    id={`${formId}-${color}-toggle`}
-                    // 🟢 FIX: Use safeColors here to prevent the crash
-                    checked={safeColors[color] || false}
-                    disabled={!editable}
-                    onChange={e =>
-                      setChildField(setState, 'selectedSpecialColors', color, e.target.checked)
-                    }
-                  />
-                  <Label
-                    className={`${styles.switchToggleLabel}`}
-                    for={`${formId}-${color}-toggle`}
-                  >
-                    <span className={`${styles.switchToggleInner}`} />
-                    <span className={`${styles.switchToggleSwitch}`} />
-                  </Label>
-                </div>
-                <span
-                  style={{
-                    marginLeft: '3px',
-                    fontSize: 'inherit',
-                    textTransform: 'capitalize',
-                    whiteSpace: 'nowrap',
-                    fontWeight: 'normal',
-                  }}
-                >
-                  {color}
-                </span>
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
-      {hasPermissionToFilter && state.selectedCodes.length > 0 && (
-        <div className={cn(styles.filterStyle, styles.filterMarginRight, 'mt-2', 'mb-2', 'ml-7')}>
-          <span className={styles.selectAllLabel}>Select All (Visible Users): </span>
-          <div className={styles.dotSelector}>
-            {['purple', 'green', 'navy'].map(color => (
-              // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-              <span
-                key={color}
-                onClick={e => {
-                  e.preventDefault();
-                  handleBulkDotClick(color);
-                }}
-                className={cn(styles.bulkDot, state.bulkSelectedColors[color] && styles.active)}
-                style={{
-                  display: 'inline-block',
-                  width: '15px',
-                  height: '15px',
-                  margin: '0 5px',
-                  borderRadius: '50%',
-                  backgroundColor: state.bulkSelectedColors[color] ? color : 'transparent',
-                  border: `3px solid ${color}`,
-                  cursor: 'pointer',
-                }}
-              />
-            ))}
-          </div>
-        </div>
-      )} */}
       {(hasPermissionToFilter || props.hasPermission('highlightEligibleBios')) && (
         <div
           className={`${styles.filterStyle} ${styles.marginRight}`}
@@ -191,26 +47,6 @@ export default function WeeklySummariesToggleFilter({
           </div>
         </div>
       )}
-      {/* old */}
-      {/* {hasPermissionToFilter && (
-        <div className={`${styles.filterStyle} ml-3`} style={{ minWidth: 'max-content' }}>
-          <span>Filter by Trophies</span>
-          <div className={`${styles.switchToggleControl}`}>
-            <input
-              type="checkbox"
-              className={`${styles.switchToggle}`}
-              id={`${formId}-trophy-toggle`}
-              checked={state.selectedTrophies}
-              disabled={!editable}
-              onChange={handleTrophyToggleChange}
-            />
-            <Label className={`${styles.switchToggleLabel}`} for={`${formId}-trophy-toggle`}>
-              <span className={`${styles.switchToggleInner}`} />
-              <span className={`${styles.switchToggleSwitch}`} />
-            </Label>
-          </div>
-        </div>
-      )} */}
       {hasPermissionToFilter && (
         <div
           className={`${styles.filterStyle} ${styles.marginRight}`}
@@ -231,30 +67,6 @@ export default function WeeklySummariesToggleFilter({
           </div>
         </div>
       )}
-      {/* {hasPermissionToFilter && (
-        <div className={`${styles.filterStyle} ml-3`} style={{ minWidth: 'max-content' }}>
-          <span>Filter by Over Hours</span>
-          <div className={`${styles.switchToggleControl}`}>
-            <input
-              type="checkbox"
-              className={`${styles.switchToggle}`}
-              id={`${formId}-over-hours-toggle`}
-              checked={state.selectedOverTime}
-              disabled={!editable}
-              onChange={handleOverHoursToggleChange}
-            />
-            <Label className={`${styles.switchToggleLabel}`} for={`${formId}-over-hours-toggle`}>
-              <span className={`${styles.switchToggleInner}`} />
-              <span className={`${styles.switchToggleSwitch}`} />
-            </Label>
-          </div>
-          <ReactTooltip id="filterTooltip" place="top" effect="solid" className="custom-tooltip">
-            <span style={{ whiteSpace: 'normal', wordWrap: 'break-word', maxWidth: '200px' }}>
-              Filter people who contributed more than 25% of their committed hours
-            </span>
-          </ReactTooltip>
-        </div>
-      )} */}
       {hasPermissionToFilter && (
         <div className={`${styles.filterStyle}`} style={{ minWidth: 'max-content' }}>
           <span>Filter by Over Hours</span>
