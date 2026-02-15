74c79c09941005422b67486f92745db89746f897
ShravyaKudlu <shravyakshanbhogue@gmail.com>
2026-02-15 01:05:40 -0500

    fix: darkMode tooltip

--- src/components/MostWastedMaterials/Tooltip.jsx
+++ src/components/MostWastedMaterials/Tooltip.jsx
@@ -1,12 +1,15 @@
 // Custom Tooltip Component
 import styles from './Tooltip.module.css';
-
+import { useSelector } from 'react-redux';
 export default function CustomTooltip({ active, payload, label }) {
+  const darkMode = useSelector(state => state.theme.darkMode);
   if (active && payload && payload.length) {
     return (
-      <div className={`${styles.tooltipWrapper}`}>
-        <p className={`${styles.tooltipLabel}`}>{label}</p>
-        <p className={`${styles.tooltipValue}`}>Waste: {payload[0].value}%</p>
+      <div className={`${darkMode ? styles.darkMode : ''}`}>
+        <div className={`${styles.tooltipWrapper}`}>
+          <p className={`${styles.tooltipLabel} ${styles.labelBlue}`}>{label}</p>
+          <p className={`${styles.tooltipValue}`}>Waste: {payload[0].value}%</p>
+        </div>
       </div>
     );
   }
