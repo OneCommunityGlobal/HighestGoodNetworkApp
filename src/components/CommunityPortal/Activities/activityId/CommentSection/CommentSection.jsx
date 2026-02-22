0f921272fde309b7faf12b110bfd94ea47f3f596
ShravyaKudlu <shravyakshanbhogue@gmail.com>
2026-02-21 22:11:39 -0500

    fix: final fix for comments.jsx

--- src/components/CommunityPortal/Activities/activityId/CommentSection/CommentSection.jsx
+++ src/components/CommunityPortal/Activities/activityId/CommentSection/CommentSection.jsx
@@ -4,11 +4,11 @@ import { useSelector } from 'react-redux';
 function CommentSection({ comments }) {
   const darkMode = useSelector(state => state.theme?.darkMode);
   return (
-    <div className={`${darkMode ? styles.darkMode : ''}`}>
-      <div className={`${styles.activityCommentsSection}`}>
+    <div className={darkMode ? styles.darkMode : ''}>
+      <div className={styles.activityCommentsSection}>
         {comments.map(comment => (
-          <div key={comment.id} className={`${styles.activityComment}`}>
-            <div className={`${styles.activityCommentUser}`}>
+          <div key={comment.id} className={styles.activityComment}>
+            <div className={styles.activityCommentUser}>
               <span
                 className={`${styles.activityIcon} ${
                   Math.random() > 0.5 ? styles.purple : styles.blue
@@ -17,9 +17,9 @@ function CommentSection({ comments }) {
                 {comment.name[0]}
               </span>
             </div>
-            <div className={`${styles.activityCommentText}`}>
+            <div className={styles.activityCommentText}>
               {comment.comment}
-              <div className={`${styles.activityCommentFooter}`}>
+              <div className={styles.activityCommentFooter}>
                 <span>{comment.name} - </span>
                 <span>{comment.time}</span>
               </div>
