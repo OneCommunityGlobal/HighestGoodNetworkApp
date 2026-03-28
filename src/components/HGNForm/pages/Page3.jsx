c3be21bc7b5f12439118a44b931b9204c8e5f9ec
naznin07 <naffect1@gmail.com>
2026-03-28 10:01:25 -0400

    Remove unused imports in Page3.jsx

--- src/components/HGNForm/pages/Page3.jsx
+++ src/components/HGNForm/pages/Page3.jsx
@@ -5,7 +5,7 @@ import FrontendQuestions from '../questionpages/FrontendQuestions';
 import Progress from '../questionpages/Progress';
 import { useSelector } from 'react-redux';
 import styles from '../styles/hgnform.module.css';
-import { getBoxStyling, getFontColor } from '../../../styles';
+
 
 function Page3() {
   const headerRef = useRef(null);
