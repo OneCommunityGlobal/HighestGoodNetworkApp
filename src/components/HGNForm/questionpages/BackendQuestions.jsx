3f15a8c0c28df56ddb6ba31e636327c8e11c6900
naznin07 <naffect1@gmail.com>
2026-03-29 00:19:06 -0400

    Refactor BackendQuestions component structure

--- src/components/HGNForm/questionpages/BackendQuestions.jsx
+++ src/components/HGNForm/questionpages/BackendQuestions.jsx
@@ -1,206 +1,31 @@
-import { useState, useEffect } from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
-import { FaEdit, FaRegSave } from 'react-icons/fa';
-import axios from 'axios';
-import { useSelector, useDispatch } from 'react-redux';
-import { ENDPOINTS } from '~/utils/URL';
-import { toast } from 'react-toastify';
-import { setformData } from '~/actions/hgnFormAction';
-import { Spinner } from 'reactstrap';
-import styles from '../styles/FrontendBackendQuestions.module.css';
-import { getBoxStyling, getFontColor } from '../../../styles';
+import React from 'react';
+import QuestionPage  from './QuestionPage';
+import { endsWith } from 'lodash';
+import { ENDPOINTS } from 'utils/URL';
 
-function BackendQuestions() {
-  const navigate = useHistory();
-  const [questions, setQuestions] = useState([]);
-  const formData = useSelector(state => state.hgnForm);
-  const [newVolunteer, setNewVolunteer] = useState(formData);
-  const dispatch = useDispatch();
-  const [editingIndex, setEditingIndex] = useState(null);
-  const [editedText, setEditedText] = useState('');
-  const [loading, setLoading] = useState(true);
-  const location = useLocation();
-  const { isOwner } = location.state;
-  const darkMode = useSelector(state => state.theme.darkMode);
-  // Fetch questions from database
-  useEffect(() => {
-    const fetchQuestions = async () => {
-      try {
-        const response = await axios.get(`${ENDPOINTS.HGN_FORM_GET_QUESTION}`, {
-          params: {
-            page: 4,
-            title: 'backend',
-          },
-        });
-        setQuestions(response.data);
-      } catch (error) {
-        toast.error('Error fetching questions. Please try again later.');
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchQuestions();
-  }, [ENDPOINTS.HGN_FORM_GET_QUESTION]);
-
-  const handleNext = e => {
-    e.preventDefault();
-    dispatch(setformData(newVolunteer));
-    navigate.push('/hgnform/page5', { isOwner });
-  };
-
-  const handleBack = () => {
-    navigate.goBack('/hgnform/page3');
-  };
-
-  const handleRadioChange = e => {
-    const { name, value } = e.target;
-    setNewVolunteer({ ...newVolunteer, [name]: value });
-  };
-
-  // Handle edit button click to start editing a question in "Owner" mode
-  const handleEditClick = index => {
-    setEditingIndex(index);
-    setEditedText(questions[index].text); // Set the current question text to be edited
-  };
-
-  // Save edited question
-  const handleSaveClick = async index => {
-    try {
-      // Save the updated question to the database
-      await axios.put(ENDPOINTS.HGN_FORM_UPDATE_QUESTION(questions[index]._id), {
-        text: editedText,
-        page: '4',
-        title: 'backend',
-      });
-
-      setQuestions(prevQuestions => {
-        const updatedQuestions = [...prevQuestions];
-        updatedQuestions[index].text = editedText; // Update the question text locally
-        return updatedQuestions;
-      });
-
-      setEditingIndex(null); // Exit editing mode
-      toast.success('Question saved successfully.');
-    } catch (error) {
-      toast.error('Error saving question. Please try again.');
-    }
-  };
-
-  // Mapping question keys to structured field names
-  const fieldNameMap = [
-    'backend_Overall',
-    'backend_Database',
-    'backend_MongoDB',
-    'backend_MongoDB_Advanced',
-    'backend_TestDrivenDev',
-    'backend_Deployment',
-    'backend_VersionControl',
-    'backend_CodeReview',
-    'backend_EnvironmentSetup',
-    'backend_AdvancedCoding',
-    'backend_AgileDevelopment',
-  ];
-
-  const searchQuestion = (page, qno) => {
-    const questiontext = questions.find(question => question.page === page && question.qno === qno);
-    return questiontext.text;
-  };
-
-  if (loading) {
-    return (
-      <div>
-        <Spinner color="primary" className={`${styles.spinnerHgnform}`} />;
-      </div>
-    );
-  }
-
+ const fieldNameMapBackend = [
+   'backend_Overall',
+   'backend_Database',
+   'backend_MongoDB',
+   'backend_MongoDB_Advanced',
+   'backend_TestDrivenDev',
+   'backend_Deployment',
+   'backend_VersionControl',
+   'backend_CodeReview',
+   'backend_EnvironmentSetup',
+   'backend_AdvancedCoding',
+   'backend_AgileDevelopment',
+ ];
+ function BackendQuestions(){
   return (
-    <div
-      className={`${styles.frontendBackendQuestions} ${darkMode ? 'bg-space-cadet' : ''}`}
-      style={getBoxStyling(darkMode)}
-    >
-      <h3 className={`${styles.blueStrip}`}>Backend Questions</h3>
-      <form onSubmit={handleNext}>
-        {questions.map((question, index) => {
-          const fieldName = fieldNameMap[index] || `backend_Question_${index}`;
-
-          return (
-            <div className={`${styles.frontendBackend}`} key={question._id || index}>
-              <div className={`${styles.questionContainer} `}>
-                {editingIndex === index && isOwner ? (
-                  <div
-                    className={`${styles.editQuestionContainer}  ${
-                      darkMode ? 'bg-yinmn-blue' : ''
-                    }`}
-                  >
-                    <p className={`${styles.editTitle} ${getFontColor(darkMode)}`}>Edit Question</p>
-                    <div className={`${styles.editQuestion}`}>
-                      <input
-                        type="text"
-                        value={editedText}
-                        onChange={e => setEditedText(e.target.value)}
-                        className={`${styles.editInput} ${getFontColor(darkMode)} ${
-                          darkMode ? 'bg-space-cadet' : ''
-                        }`}
-                      />
-                      <FaRegSave
-                        title="Save"
-                        className={`${styles.saveIcon}`}
-                        onClick={() => handleSaveClick(index)}
-                      />
-                    </div>
-                  </div>
-                ) : (
-                  <p className={`${styles.question} ${getFontColor(darkMode)}`}>
-                    {searchQuestion(4, index + 1)}
-                    {isOwner && (
-                      <FaEdit
-                        className={`${styles.editIcon}`}
-                        onClick={() => handleEditClick(index)}
-                        title="Edit"
-                      />
-                    )}
-                  </p>
-                )}
-              </div>
-
-              <div className={`${styles.frontendBackendRating}`}>
-                {Array.from({ length: 10 }, (_, i) => (
-                  <div key={i}>
-                    <label
-                      htmlFor={`${fieldName}_${i + 1}`}
-                      className={`${getFontColor(darkMode)}`}
-                    >
-                      {i + 1}
-                    </label>
-                    <input
-                      type="radio"
-                      name={fieldName}
-                      id={`${fieldName}_${i + 1}`}
-                      value={i + 1}
-                      onChange={handleRadioChange}
-                      checked={newVolunteer[fieldName] === (i + 1).toString()}
-                      required
-                    />
-                  </div>
-                ))}
-              </div>
-            </div>
-          );
-        })}
-
-        <div className={`${styles.buttonContainer}`}>
-          <button type="button" className={`${styles.hgnReturnButton}`} onClick={handleBack}>
-            Back
-          </button>
-          <button type="submit" className={`${styles.nextButton}`}>
-            Next
-          </button>
-        </div>
-      </form>
-    </div>
+    <QuestionPage
+      pageNumber={4}
+      title="Backend"
+      fieldNameMap={fieldNameMapBackend}
+      nextPage="/hgnForm/page5"
+      backpage="/hgnForm/page5"
+      ENDPOINTS={ENDPOINTS}
+    />
   );
-}
-
+ }
 export default BackendQuestions;
