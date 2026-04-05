283ae05936f3d9c473fcc446c05382f330da69e7
naznin07 <naffect1@gmail.com>
2026-04-04 23:13:30 -0400

    Update Page2.jsx

--- src/components/HGNForm/pages/Page2.jsx
+++ src/components/HGNForm/pages/Page2.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+
 import { useSelector } from 'react-redux';
 import Banner from '../questionpages/Banner';
 import Progress from '../questionpages/Progress';
