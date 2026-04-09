5b4af97bbf755ed25042bf51947bbc4ca8e3d83f
Sai Teja Kaasoju <saitejakaasoju17@gmail.com>
2026-04-08 23:23:20 -0500

    Resolve PR analytics 404 merge

--- src/components/NotFound/NotFoundPage.jsx
+++ src/components/NotFound/NotFoundPage.jsx
@@ -18,36 +18,43 @@ function NotFoundPage() {
     <div
       className={cn(styles.notFoundContainer, darkMode ? cn(styles.darkMode, styles.bgBlack) : '')}
     >
-      <img
-        className={styles.notFoundImage}
-        src={darkMode ? NotFoundDarkImage : NotFoundImage}
-        alt="Page Not Found"
-      />
-      <div className={styles.notFoundText}>
-        {isPrAnalytics ? (
-          <>
-            <h1>PR ANALYTICS UNAVAILABLE</h1>
-            <p>PR Analytics is temporarily unavailable.</p>
-            <p>Please try again later or contact an administrator.</p>
-
-            <p>
-              <Link to="/reports" className={styles.backHomeLink}>
-                Go to Reports Dashboard
-              </Link>
-            </p>
-          </>
-        ) : (
-          <>
-            <h1>PAGE NOT FOUND</h1>
-            <p>The rabbits have been nibbling the cables again...</p>
-            <p>
-              Maybe this will help{' '}
-              <Link to="/" className={styles.backHomeLink}>
-                Home
-              </Link>
-            </p>
-          </>
-        )}
+      <div
+        className={styles.notFoundContent}
+        //prettier-ignore
+        style={{border: `2px solid ${darkMode ? '#f1f1f1' : '#333'}`}}
+      >
+        <img
+          className={styles.notFoundImage}
+          src={darkMode ? NotFoundDarkImage : NotFoundImage}
+          alt="Page Not Found"
+        />
+        <div className={styles.notFoundText}>
+          {isPrAnalytics ? (
+            <>
+              <h1>PR ANALYTICS UNAVAILABLE</h1>
+              <p>PR Analytics is temporarily unavailable.</p>
+              <p>Please try again later or contact an administrator.</p>
+              <p>
+                <Link to="/reports" className={styles.backHomeLink}>
+                  Go to Reports Dashboard
+                </Link>
+              </p>
+            </>
+          ) : (
+            <>
+              <h1 className={darkMode ? `text-light` : `text-dark`}>PAGE NOT FOUND</h1>
+              <p className={darkMode ? `text-light` : `text-dark`}>
+                The rabbits have been nibbling the cables again...
+              </p>
+              <p className={darkMode ? `text-light` : `text-dark`}>
+                Maybe this will help{' '}
+                <Link to="/" className={styles.backHomeLink}>
+                  Home
+                </Link>
+              </p>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
