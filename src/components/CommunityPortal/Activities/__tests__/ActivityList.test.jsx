cc8784de7676bda3b83b086276d7636adb172366
Aayush <aayush.shetty12@gmail.com>
2026-04-12 21:43:21 -0400

    added mockstore for ActivityList test

--- src/components/CommunityPortal/Activities/__tests__/ActivityList.test.jsx
+++ src/components/CommunityPortal/Activities/__tests__/ActivityList.test.jsx
@@ -1,21 +1,34 @@
 import { render, screen, fireEvent } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
 import ActivityList from '../ActivityList';
+
+const mockStore = configureMockStore([]);
+
+const renderActivityList = (initialState = { theme: { darkMode: false } }) => {
+  const store = mockStore(initialState);
+  return render(
+    <Provider store={store}>
+      <ActivityList />
+    </Provider>,
+  );
+};
 
 describe('ActivityList', () => {
   test('renders Activity List heading', () => {
-    render(<ActivityList />);
+    renderActivityList();
     expect(screen.getByText('Activity List')).toBeInTheDocument();
   });
 
   test('renders activities from mock data', () => {
-    render(<ActivityList />);
+    renderActivityList();
     fireEvent.click(screen.getByLabelText(/Show Past Events:/i));
     expect(screen.getByText('Yoga Class')).toBeInTheDocument();
     expect(screen.getByText('Book Club')).toBeInTheDocument();
   });
 
   test('filters activities by type', () => {
-    render(<ActivityList />);
+    renderActivityList();
     fireEvent.click(screen.getByLabelText(/Show Past Events:/i));
 
     fireEvent.change(screen.getByLabelText(/Type:/i), {
@@ -27,7 +40,7 @@ describe('ActivityList', () => {
   });
 
   test('filters activities by date', () => {
-    render(<ActivityList />);
+    renderActivityList();
     fireEvent.click(screen.getByLabelText(/Show Past Events:/i));
 
     fireEvent.change(screen.getByLabelText(/Date:/i), {
@@ -40,7 +53,7 @@ describe('ActivityList', () => {
   });
 
   test('sorts activities by date (latest to earliest)', () => {
-    render(<ActivityList />);
+    renderActivityList();
 
     fireEvent.change(screen.getByLabelText(/Sort By:/i), {
       target: { value: 'latest' },
