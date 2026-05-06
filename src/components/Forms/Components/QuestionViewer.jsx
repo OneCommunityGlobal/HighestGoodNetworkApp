0be2df5e90b0de3a043f67111ffa6ace3c1a9535
Ram-blip <ramcruze2000@gmail.com>
2026-05-06 11:06:43 +0530

    fix(forms): resolve props validation warning and align form dark-mode styling

--- src/components/Forms/Components/QuestionViewer.jsx
+++ src/components/Forms/Components/QuestionViewer.jsx
@@ -1,5 +1,7 @@
 import dompurify from 'dompurify';
+import PropTypes from 'prop-types';
 import OptionViewer from './OptionViewer';
+import styles from '../formsPage.module.css';
 
 export default function QuestionViewer({ data, SetFormAnswers, formAnwers }) {
   const handleAnswerChange = (e, questionId) => {
@@ -19,12 +21,15 @@ export default function QuestionViewer({ data, SetFormAnswers, formAnwers }) {
   return (
     // eslint-disable-next-line react/jsx-no-useless-fragment
     <>
-      <div className="border border-primary m-2 p-3 bg-light">
-        <div className="mb-2 bg-info pl-1">
-          <label className="text-white mb-0">{data.label}</label>
+      <div className={styles.viewerCard}>
+        <div className={styles.questionLabelBar}>
+          <label className="mb-0">{data.label}</label>
         </div>
         {/* eslint-disable-next-line react/no-danger */}
-        <div className="mb-2" dangerouslySetInnerHTML={{ __html: sanitizer(data.description) }} />
+        <div
+          className={styles.viewerDescription}
+          dangerouslySetInnerHTML={{ __html: sanitizer(data.description) }}
+        />
         <div>
           {data.type === 'short_answer' && (
             <input
@@ -51,3 +56,28 @@ export default function QuestionViewer({ data, SetFormAnswers, formAnwers }) {
     </>
   );
 }
+
+QuestionViewer.propTypes = {
+  data: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string,
+    description: PropTypes.string,
+    type: PropTypes.string.isRequired,
+    required: PropTypes.bool,
+    options: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        value: PropTypes.string,
+      }),
+    ),
+  }).isRequired,
+  SetFormAnswers: PropTypes.func.isRequired,
+  formAnwers: PropTypes.arrayOf(
+    PropTypes.shape({
+      questionId: PropTypes.string.isRequired,
+      type: PropTypes.string,
+      question: PropTypes.string,
+      answer: PropTypes.string,
+    }),
+  ).isRequired,
+};
