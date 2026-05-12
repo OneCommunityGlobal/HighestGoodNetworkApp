db9b6a8735343ef686a223dd1620b0f38f46a5c9
ChiragBellara <chiragbellara7@gmail.com>
2026-05-11 22:14:05 -0700

    fixed merge errors

--- src/components/CommunityPortal/CPDashboard.jsx
+++ src/components/CommunityPortal/CPDashboard.jsx
@@ -1,25 +1,11 @@
 import { useState, useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
-import {
-  Container,
-  Row,
-  Alert,
-  Col,
-  Card,
-  CardBody,
-  Button,
-  Input,
-  FormGroup,
-  Label,
-} from 'reactstrap';
+import { Container, Row, Alert, Col, Card, CardBody, Button, Input } from 'reactstrap';
 import { FaCalendarAlt, FaMapMarkerAlt, FaUserAlt, FaSearch, FaTimes } from 'react-icons/fa';
 import styles from './CPDashboard.module.css';
 import { ENDPOINTS } from '../../utils/URL';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
-import { el } from 'date-fns/locale';
-import { fuzzySearch } from '../../utils/fuzzySearch';
 
 const FixedRatioImage = ({ src, alt, fallback }) => (
   <div
