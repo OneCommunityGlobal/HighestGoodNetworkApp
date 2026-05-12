9d832b1987a0bf575e64ac5e244dabe750308231
unknown <saurabhditpe11@gmail.com>
2026-05-12 15:00:19 -0400

    Resolve merge conflicts with dev branch

--- src/routes.jsx
+++ src/routes.jsx
@@ -87,8 +87,7 @@ import SkillsOverviewPage from './components/HGNHelpSkillsDashboard/SkillsOvervi
 import CommunityMembersPage from './components/HGNHelpSkillsDashboard/CommunityMembersPage';
 import UserProfilePage from './components/HGNHelpSkillsDashboard/UserProfilePage';
 import FeedbackModal from './components/HGNHelpSkillsDashboard/FeedbackModal';
-import Activity from './components/CommunityPortal/Activities/activityId/Activity';
-import ActivityAttendance from './components/CommunityPortal/Activities/ActivityAttendance';
+
 import ActivityAgenda from './components/CommunityPortal/Activities/ActivityAgenda';
 import EventNoShowChart from './components/CommunityPortal/Attendence/NoshowViz';
 import EventList from './components/CommunityPortal/Event/EventList/EventList';
