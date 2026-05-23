91569c61cb0190f6c44217c929852f210847b433
unknown <saurabhditpe11@gmail.com>
2026-05-23 13:22:28 -0400

    fix: restore configureMockStore import in OwnerMessage test

--- src/components/OwnerMessage/__tests__/OwnerMessage.test.jsx
+++ src/components/OwnerMessage/__tests__/OwnerMessage.test.jsx
@@ -2,6 +2,7 @@ import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
 
 import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
 import OwnerMessage from '../OwnerMessage';
 
 // Create mock for the hasPermission utility
@@ -11,7 +12,7 @@ vi.mock('utils/permissions', () => {
 
 describe('OwnerMessage Component', () => {
   const middlewares = [thunk];
-  const mockStore = configureStore(middlewares);
+  const mockStore = configureMockStore(middlewares);
   const baseState = {
     auth: {
       user: {
