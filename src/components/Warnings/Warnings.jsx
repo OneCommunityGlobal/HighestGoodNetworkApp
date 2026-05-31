329d3b37f3bc97121b8b4b47212b6d2cabe6517d
sayali-2308 <sayalisable23@gmail.com>
2026-05-31 00:16:58 -0400

    fix: use codePointAt over charCodeAt per SonarCloud recommendation

--- src/components/Warnings/Warnings.jsx
+++ src/components/Warnings/Warnings.jsx
@@ -70,7 +70,10 @@ export default function Warning({
     if (showTrackers) {
       setToggle(true);
       if (usersWarnings.length === 0) {
-        const index = Array.from(personId ?? '').reduce((acc, c) => acc + c.charCodeAt(0), 0);
+        const index = Array.from(personId ?? '').reduce(
+          (acc, c) => acc + (c.codePointAt(0) ?? 0),
+          0,
+        );
         const delay = index % 5000;
         const timer = setTimeout(() => {
           fetchUsersWarningsById();
