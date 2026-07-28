d6d077ce74d76d826aa0671de9524838745a85db
skarawalsatishrao@hawk.iit.edu <skarawalsatishrao@hawk.iit.edu>
2026-07-28 12:28:29 -0500

    Fix sonarcube pass gate issues

--- src/components/EnhancedPopularityTimelineAnalytics/EnhancedPopularityTimelineChart.jsx
+++ src/components/EnhancedPopularityTimelineAnalytics/EnhancedPopularityTimelineChart.jsx
@@ -10,11 +10,8 @@ import {
   ResponsiveContainer,
 } from 'recharts';
 import Select from 'react-select';
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import styles from './EnhancedPopularityTimelineChart.module.css';
-import { ENDPOINTS } from '../../utils/URL';
 import {
   fetchEnhancedPopularityData,
   fetchEnhancedRoles,
@@ -23,27 +20,6 @@ import {
 /**
  * Enhanced Popularity Timeline Chart Component
  */
-
-// --- Data Fetching Functions ---
-/*const fetchEnhancedPopularityData = async ({ range, roles = [], includeLowVolume }) => {
-  const url = ENDPOINTS.ENHANCED_POPULARITY(
-    range,
-    roles,
-    null, // start (optional)
-    null, // end (optional)
-    includeLowVolume,
-  );
-
-  const { data } = await axios.get(url);
-
-  console.log('data ================= ', data);
-  return data;
-};*/
-
-/*const fetchEnhancedRoles = async () => {
-  const { data } = await axios.get(ENDPOINTS.ENHANCED_POPULARITY_ROLES);
-  return data;
-};*/
 
 // --- Helper Functions ---
 const getRoleColor = (role, index) => {
@@ -105,23 +81,6 @@ const EnhancedPopularityTimelineChart = () => {
   useEffect(() => {
     dispatch(fetchEnhancedRoles(token));
   }, []);
-
-  // Fetch enhanced roles data
-  /*const { data: rolesData, isLoading: rolesLoading, error: rolesError } = useQuery({
-    queryKey: ['enhancedRoles'],
-    queryFn: fetchEnhancedRoles,
-  });
-
-  // Fetch enhanced popularity data
-  const { data: popularityData, isLoading: dataLoading, error: queryError } = useQuery({
-    queryKey: ['enhancedPopularityData', dateRangeOption, selectedRoles.join(','), showLowVolume],
-    queryFn: () =>
-      fetchEnhancedPopularityData({
-        range: dateRangeOption.replace('months', ''),
-        roles: selectedRoles,
-        includeLowVolume: showLowVolume,
-      }),
-  });*/
 
   // Process chart data for Recharts
   const { chartData, roleMetrics } = useMemo(() => {
@@ -192,6 +151,8 @@ const EnhancedPopularityTimelineChart = () => {
         roleMetrics: metrics.sort((a, b) => (b.popularityScore || 0) - (a.popularityScore || 0)),
       };
     } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error('Error processing chart data:', error);
       return { chartData: [], roleMetrics: [] };
     }
   }, [popularityData]);
