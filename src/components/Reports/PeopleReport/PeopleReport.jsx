12ab68d6031a0fbd056eb0c4447877495fbb3d39
handikaharianto <handikaharianto01@gmail.com>
2026-08-03 10:16:47 -0400

    chore: add graph section back

--- src/components/Reports/PeopleReport/PeopleReport.jsx
+++ src/components/Reports/PeopleReport/PeopleReport.jsx
@@ -2,6 +2,7 @@
 import { useCallback, useEffect, useState } from 'react';
 import styles from './PeopleReport.module.css';
 import { Link } from 'react-router-dom';
+import { Spinner, Alert } from 'reactstrap';
 import { connect } from 'react-redux';
 import { FiUser } from 'react-icons/fi';
 import { toast } from 'react-toastify';
@@ -19,6 +20,11 @@ import { getPeopleReportData } from './selectors';
 import { PeopleTasksPieChart } from './components';
 import { Checkbox } from '../../common/Checkbox';
 import { updateRehireableStatus } from '../../../actions/userManagement';
+import InfringementsViz from '../InfringementsViz';
+import TimeEntriesViz from '../TimeEntriesViz';
+import BadgeSummaryViz from '../BadgeSummaryViz';
+import BadgeSummaryPreview from '../BadgeSummaryPreview';
+import PeopleTableDetails from '../PeopleTableDetails';
 import CompletedTasksPieChart from '~/components/Reports/PeopleReport/CompletedTasksPieChart';
 import clsx from 'clsx';
 
@@ -35,10 +41,16 @@ function PeopleReport(props) {
     infringements,
     tangibleHoursReportedThisWeek,
     darkMode,
+    userProjects,
+    auth,
+    userTask
   } = props;
 
   const [isRehireable, setIsRehireableState] = useState(true);
   const [isLoading, setIsLoading] = useState(true);
+  const [fromDate, setFromDate] = useState('2016-01-01')
+  const [toDate, setToDate] = useState('3000-12-31')
+  const [authRole, setAuthRole] = useState('')
 
   useEffect(() => {
     if (!match) return undefined;
@@ -50,7 +62,7 @@ function PeopleReport(props) {
       getUserTasks(userId),
       getUserProjects(userId),
       getWeeklySummaries(userId),
-      getTimeEntriesForPeriod(userId, '2016-01-01', '3000-12-31'),
+      getTimeEntriesForPeriod(userId, fromDate, toDate),
     ])
       .catch(() => {
         // Errors are surfaced by the individual action creators; nothing to do here.
@@ -91,6 +103,107 @@ function PeopleReport(props) {
 
   const { firstName, lastName, weeklycommittedHours } = userProfile;
   const { profilePic, role, jobTitle, endDate, _id, startDate } = userProfile;
+
+  function UserProject(props) {
+    const userProjectList = [];
+    return <div>{userProjectList}</div>;
+  }
+
+  function Infringements(props) {
+    const dict = {};
+
+    // aggregate infringements
+    for (let i = 0; i < props.infringements.length; i += 1) {
+      if (props.infringements[i].date in dict) {
+        dict[props.infringements[i].date].count += 1;
+        dict[props.infringements[i].date].des.push(props.infringements[i].description);
+      } else {
+        dict[props.infringements[i].date] = {
+          count: 1,
+          des: [props.infringements[i].description],
+        };
+      }
+    }
+
+    const [startdate] = Object.keys(dict);
+    if (startdate) {
+      startdate.toString();
+    }
+    if (props.infringements.length > 0) {
+      props.infringements.map((current, index) => (
+        <tr className={styles.teams__tr} key={index}>
+          <td>{index + 1}</td>
+          <td>{current.date}</td>
+          <td>{current.description}</td>
+        </tr>
+      ));
+    }
+    return (
+      <div>
+        <div />
+      </div>
+    );
+  }
+
+    const PeopleDataTable = () => {
+    const peopleData = {
+      alertVisible: false,
+      taskData: [],
+      color: null,
+      message: '',
+    };
+
+    for (let i = 0; i < (userTask || []).length; i += 1) {
+      const task = {
+        taskName: '', priority: '', status: '', resources: [], active: '', assign: '',
+        estimatedHours: '', _id: '', startDate: '', endDate: '', hoursBest: '',
+        hoursMost: '', hoursWorst: '', whyInfo: '', endstateInfo: '', intentInfo: '',
+      };
+      const resourcesName = [];
+
+      task.active = userTask[i].isActive ? 'Yes' : 'No';
+      task.assign = userTask[i].isAssigned ? 'Yes' : 'No';
+      task.taskName = userTask[i].taskName;
+      task.priority = userTask[i].priority;
+      task.status = userTask[i].status;
+      const n = userTask[i].estimatedHours || 0;
+      task.estimatedHours = n.toFixed(2);
+
+      for (let j = 0; j < (userTask[i].resources || []).length; j += 1) {
+        const tempResource = {
+          name: userTask[i].resources[j].name,
+          profilePic: userTask[i].resources[j].profilePic || '/pfp-default.png',
+        };
+        resourcesName.push(tempResource);
+      }
+      task._id = userTask[i]._id;
+      task.resources.push(resourcesName);
+      task.startDate = userTask[i].startedDatetime ? userTask[i].startedDatetime.split('T')[0] : 'null';
+      task.endDate = userTask[i].dueDatetime ? userTask[i].dueDatetime.split('T')[0] : 'null';
+      task.hoursBest = userTask[i].hoursBest;
+      task.hoursMost = userTask[i].hoursMost;
+      task.hoursWorst = userTask[i].hoursWorst;
+      task.whyInfo = userTask[i].whyInfo;
+      task.intentInfo = userTask[i].intentInfo;
+      task.endstateInfo = userTask[i].endstateInfo;
+      peopleData.taskData.push(task);
+    }
+
+    return (
+      <PeopleTableDetails
+        taskData={peopleData.taskData}
+        showFilter={tangibleHoursReportedThisWeek !== 0}
+        darkMode={darkMode}
+      />
+    );
+  };
+
+  const activeTasks = (userTask || []).reduce((accumulator, item) => {
+    const incompleteTasks = (item.resources || []).filter(
+      task => task.completedTask === false && task.userID === userProfile._id,
+    );
+    return accumulator.concat(incompleteTasks);
+  }, []);
 
   if(isLoading) {
     return (
@@ -187,12 +300,64 @@ function PeopleReport(props) {
             </div>
           </div>
 
-          <div
-           className={styles.peopleTasksPieChartWrapper}>
-            <PeopleTasksPieChart darkMode={darkMode} />
+          <PeopleTasksPieChart darkMode={darkMode} />
+          <CompletedTasksPieChart darkMode={darkMode} />
+
+          <div className={`${styles.mobilePeopleTable}`}>
+            <ReportPage.ReportBlock darkMode={darkMode}>
+              {isLoading ? (
+                <p
+                  className={`${darkMode ? styles.textLight : ''}
+                d-flex align-items-center flex-row justify-content-center`}
+                >
+                  Loading tasks: &nbsp; <Spinner color={`${darkMode ? 'light' : 'dark'}`} />
+                </p>
+              ) : activeTasks.length > 0 ? (
+                <>
+                  <div className={`intro_date ${darkMode ? styles.textLight : ''}`}>
+                    <h4>Tasks contributed</h4>
+                  </div>
+                  <PeopleDataTable />
+                </>
+              ) : (
+                <Alert color="danger" style={{ margin: '0 35% ' }}>You have no tasks.</Alert>
+              )}
+              <div className={`${styles.infringementContainer}`}>
+                <div className={`${styles.infringementContainerInner}`}>
+                  <UserProject userProjects={userProjects} />
+                  <Infringements
+                    infringements={infringements}
+                    fromDate={fromDate}
+                    toDate={toDate}
+                    timeEntries={timeEntries}
+                  />
+                  <div className={`${styles.visualizationDiv}`}>
+                    <TimeEntriesViz timeEntries={timeEntries} fromDate={fromDate} toDate={toDate} darkMode={darkMode} />
+                  </div>
+                  <div className={`${styles.visualizationDiv}`}>
+                    <InfringementsViz
+                      infringements={infringements}
+                      fromDate={fromDate}
+                      toDate={toDate}
+                      darkMode={darkMode}
+                    />
+                  </div>
+                  <div className={`${styles.visualizationDivRow}`}>
+                    <div className={`${styles.badgeSummaryDiv}`}>
+                      <BadgeSummaryViz
+                        authId={auth.user.userid}
+                        userId={match.params.userId}
+                        badges={userProfile.badgeCollection}
+                      />
+                    </div>
+                    <div className={`${styles.badgeSummaryPreviewDiv}`}>
+                      <BadgeSummaryPreview badges={userProfile.badgeCollection} darkMode={darkMode} />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </ReportPage.ReportBlock>
           </div>
-          <CompletedTasksPieChart darkMode={darkMode} 
-           />
         </div>
       </div>
     </div>
