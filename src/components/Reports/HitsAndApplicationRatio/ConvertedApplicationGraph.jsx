60972e98f8c359251546bb24b6458e4f9c735f93
Mahati <Mah@Mahs-Mac.local>
2026-09-19 20:50:20 -0400

    Refactor Job Analytics graphs to reduce code duplication

--- src/components/Reports/HitsAndApplicationRatio/ConvertedApplicationGraph.jsx
+++ src/components/Reports/HitsAndApplicationRatio/ConvertedApplicationGraph.jsx
@@ -1,65 +1,35 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import JobAnalyticsBarChart from './JobAnalyticsBarChart';
-import {
-  aggregateJobsByTitle,
-  sortJobsByMetric,
-  getChartMaxValue,
-} from './jobAnalyticsUtils';
+import JobAnalyticsGraph from './JobAnalyticsGraph';
 
 function ConvertedApplicationGraph({
-  data = [],
+  data,
   usePercentage,
   isDark,
   dateRange,
 }) {
-  const metricKey = usePercentage
-    ? 'conversionRate'
-    : 'applications';
-
-  const rows = useMemo(() => {
-    const aggregated = aggregateJobsByTitle(data);
-
-    return sortJobsByMetric(
-      aggregated,
-      metricKey,
-      'descending',
-    ).slice(0, 10);
-  }, [data, metricKey]);
-
-  const xDomain = useMemo(() => {
-    if (usePercentage) {
-      return [0, 100];
-    }
-
-    return [0, getChartMaxValue(rows, metricKey)];
-  }, [rows, metricKey, usePercentage]);
-
   return (
-    <JobAnalyticsBarChart
-      rows={rows}
+    <JobAnalyticsGraph
+      data={data}
       usePercentage={usePercentage}
       isDark={isDark}
       dateRange={dateRange}
+      sortDirection="descending"
       barColor="#4CAF50"
       titlePrefix="Top 10 Job Postings by"
-      xDomain={xDomain}
     />
   );
 }
 
 ConvertedApplicationGraph.propTypes = {
-  data: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string,
-      hits: PropTypes.number,
-      applications: PropTypes.number,
-      conversionRate: PropTypes.number,
-    }),
-  ),
+  data: PropTypes.arrayOf(PropTypes.object),
   usePercentage: PropTypes.bool.isRequired,
   isDark: PropTypes.bool.isRequired,
   dateRange: PropTypes.string.isRequired,
+};
+
+ConvertedApplicationGraph.defaultProps = {
+  data: [],
 };
 
 export default ConvertedApplicationGraph;
--- src/components/Reports/HitsAndApplicationRatio/NonConvertedApplicationsGraph.jsx
+++ src/components/Reports/HitsAndApplicationRatio/NonConvertedApplicationsGraph.jsx
@@ -1,65 +1,35 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import JobAnalyticsBarChart from './JobAnalyticsBarChart';
-import {
-  aggregateJobsByTitle,
-  sortJobsByMetric,
-  getChartMaxValue,
-} from './jobAnalyticsUtils';
+import JobAnalyticsGraph from './JobAnalyticsGraph';
 
 function NonConvertedApplicationsGraph({
-  data = [],
+  data,
   usePercentage,
   isDark,
   dateRange,
 }) {
-  const metricKey = usePercentage
-    ? 'conversionRate'
-    : 'applications';
-
-  const rows = useMemo(() => {
-    const aggregated = aggregateJobsByTitle(data);
-
-    return sortJobsByMetric(
-      aggregated,
-      metricKey,
-      'ascending',
-    ).slice(0, 10);
-  }, [data, metricKey]);
-
-  const xDomain = useMemo(() => {
-    if (usePercentage) {
-      return [0, 100];
-    }
-
-    return [0, getChartMaxValue(rows, metricKey)];
-  }, [rows, metricKey, usePercentage]);
-
   return (
-    <JobAnalyticsBarChart
-      rows={rows}
+    <JobAnalyticsGraph
+      data={data}
       usePercentage={usePercentage}
       isDark={isDark}
       dateRange={dateRange}
+      sortDirection="ascending"
       barColor="#F44336"
       titlePrefix="Top 10 Job Postings with Lowest"
-      xDomain={xDomain}
     />
   );
 }
 
 NonConvertedApplicationsGraph.propTypes = {
-  data: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string,
-      hits: PropTypes.number,
-      applications: PropTypes.number,
-      conversionRate: PropTypes.number,
-    }),
-  ),
+  data: PropTypes.arrayOf(PropTypes.object),
   usePercentage: PropTypes.bool.isRequired,
   isDark: PropTypes.bool.isRequired,
   dateRange: PropTypes.string.isRequired,
+};
+
+NonConvertedApplicationsGraph.defaultProps = {
+  data: [],
 };
 
 export default NonConvertedApplicationsGraph;
